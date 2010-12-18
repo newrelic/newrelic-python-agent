42cba6a9c50079885f6928d0aae64742bfdbfc26
Graham Dumpleton <graham@newrelic.com>
2010-12-18 21:43:08 +1100

    Added WSGI application wrapper/decorator.

--- newrelic/__init__.py
+++ newrelic/__init__.py
@@ -9,15 +9,12 @@ LOG_VERBOSE = _newrelic.LOG_VERBOSE
 LOG_DEBUG = _newrelic.LOG_DEBUG
 LOG_VERBOSEDEBUG = _newrelic.LOG_VERBOSEDEBUG
 
-_global_settings = _newrelic.GlobalSettings()
-
-def GlobalSettings():
-    return _global_settings
+settings = _newrelic.GlobalSettings()
 
 _applications_lock = threading.RLock()
 _applications = {}
 
-def Application(name, framework=None):
+def _Application(name, framework=None):
     # We ensure only single application object for a specific
     # name is created as it is the Python object and not the
     # internal agent client application instance that holds the
@@ -35,3 +32,43 @@ def Application(name, framework=None):
     _applications_lock.release()
 
     return application 
+
+class _CallbackGenerator:
+
+    def __init__(self, generator, callback):
+        self.__generator = generator
+        self.__callback = callback
+
+    def __iter__(self):
+        for item in self.__generator:
+            yield item
+
+    def close(self):
+        if hasattr(self.__generator, 'close'):
+            self.__generator.close()
+        self.__callback()
+
+class _ExecuteOnCompletion:
+
+    def __init__(self, application, callable):
+        self.__application = application
+        self.__callable = callable
+
+    def __call__(self, environ, start_response):
+        transaction = self.__application.web_transaction(environ)
+        transaction.__enter__()
+        try:
+            result = self.__callable(environ, start_response)
+        except:
+            transaction.__exit__()
+            raise
+        return _CallbackGenerator(result, transaction.__exit__)
+
+def application_monitor(name, framework=None):
+    application = _Application(name, framework)
+
+    def decorator(callable):
+        return _ExecuteOnCompletion(application, callable)
+
+    return decorator
+

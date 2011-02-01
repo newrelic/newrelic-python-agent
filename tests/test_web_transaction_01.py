2ae899199facebb9a9d25b8c9976a66acf9ce0e8
Graham Dumpleton <graham@newrelic.com>
2011-02-02 02:41:31 +1100

    Add test for web transactions.

---
+++ tests/test_web_transaction_01.py
@@ -0,0 +1,48 @@
+import unittest
+import time
+
+import _newrelic
+
+settings = _newrelic.settings()
+settings.logfile = "%s.log" % __file__
+settings.loglevel = _newrelic.LOG_VERBOSEDEBUG
+
+application = _newrelic.application("UnitTests")
+
+class WebTransactionTests01(unittest.TestCase):
+
+    def setUp(self):
+        _newrelic.log(_newrelic.LOG_DEBUG, "STARTING - %s" %
+                      self._testMethodName)
+
+    def tearDown(self):
+        _newrelic.log(_newrelic.LOG_DEBUG, "STOPPING - %s" %
+                      self._testMethodName)
+
+    def test_inactive(self):
+        self.assertEqual(_newrelic.transaction(), None)
+
+    def test_transaction(self):
+        environ = { "REQUEST_URI": "/test_transaction" }
+        transaction = _newrelic.WebTransaction(application, environ)
+        with transaction:
+            self.assertEqual(_newrelic.transaction(), transaction)
+            time.sleep(1.0)
+
+    def test_custom_parameters(self):
+        environ = { "REQUEST_URI": "/custom_parameters" }
+        transaction = _newrelic.WebTransaction(application, environ)
+        with transaction:
+            transaction.custom_parameters["1"] = "1" 
+            transaction.custom_parameters["2"] = "2" 
+            transaction.custom_parameters["3"] = 3
+            transaction.custom_parameters["4"] = 4.0
+            transaction.custom_parameters["5"] = ("5", 5)
+            transaction.custom_parameters["6"] = ["6", 6]
+            transaction.custom_parameters["7"] = {"7": 7}
+            transaction.custom_parameters[8] = "8"
+            transaction.custom_parameters[9.0] = "9.0"
+            time.sleep(1.0)
+
+if __name__ == '__main__':
+    unittest.main()

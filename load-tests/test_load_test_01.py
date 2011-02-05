0befd61fb24c8273e084b7d5dcdaba395517b0d8
Graham Dumpleton <graham@newrelic.com>
2011-02-05 18:51:21 +1100

    Added directory for load tests.

---
+++ load-tests/test_load_test_01.py
@@ -0,0 +1,33 @@
+import unittest
+import time
+import sys
+
+import _newrelic
+
+settings = _newrelic.settings()
+settings.logfile = "%s.log" % __file__
+settings.loglevel = _newrelic.LOG_VERBOSEDEBUG
+
+application = _newrelic.application("LoadTests")
+
+class LoadTest01(unittest.TestCase):
+
+    def setUp(self):
+        _newrelic.log(_newrelic.LOG_DEBUG, "STARTING - %s" %
+                      self._testMethodName)
+
+    def tearDown(self):
+        _newrelic.log(_newrelic.LOG_DEBUG, "STOPPING - %s" %
+                      self._testMethodName)
+
+    def test_run(self):
+        environ = { "REQUEST_URI": "/load_test_01" }
+        for i in range(1000):
+            transaction = _newrelic.WebTransaction(application, environ)
+            with transaction:
+                sys.stderr.write(".")
+                time.sleep(0.02)
+        sys.stderr.write("\n")
+
+if __name__ == '__main__':
+    unittest.main()

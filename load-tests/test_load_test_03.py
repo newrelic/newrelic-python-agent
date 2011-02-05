f3ac9052014f159bfdd26dabc43cc7ab47729295
Graham Dumpleton <graham@newrelic.com>
2011-02-05 23:58:22 +1100

    Multithreaded load test.

---
+++ load-tests/test_load_test_03.py
@@ -0,0 +1,47 @@
+import unittest
+import time
+import sys
+import random
+import threading
+
+import _newrelic
+
+settings = _newrelic.settings()
+settings.logfile = "%s.log" % __file__
+settings.loglevel = _newrelic.LOG_VERBOSEDEBUG
+
+application = _newrelic.application("LoadTests")
+
+def run(n):
+    for i in range(2000):
+        now = time.time()
+        ts = int((now-(random.random()*0.04)) * 1000000)
+        environ = { "REQUEST_URI": "/load_test_03/%s" % n,
+                    "HTTP_X_NEWRELIC_QUEUE_START": "t=%d" % ts }
+        transaction = _newrelic.WebTransaction(application, environ)
+        with transaction:
+            sys.stderr.write(".")
+            time.sleep(random.random()*0.16)
+
+class LoadTest03(unittest.TestCase):
+
+    def setUp(self):
+        _newrelic.log(_newrelic.LOG_DEBUG, "STARTING - %s" %
+                      self._testMethodName)
+
+    def tearDown(self):
+        _newrelic.log(_newrelic.LOG_DEBUG, "STOPPING - %s" %
+                      self._testMethodName)
+
+    def test_run(self):
+        threads = []
+        for i in range(10):
+            thread = threading.Thread(target=run, args=(i,))
+            thread.start()
+            threads.append(thread)
+        for thread in threads:
+            thread.join()
+        sys.stderr.write("\n")
+
+if __name__ == '__main__':
+    unittest.main()

7c256f1bebc81115a84b958cf3f772146178713b
Graham Dumpleton <graham@newrelic.com>
2011-04-05 16:29:07 +1000

    Test for generic wrap function.

---
+++ unit-tests/test_wrap_object_01.py
@@ -0,0 +1,43 @@
+import unittest
+import types
+
+import _newrelic
+
+settings = _newrelic.settings()
+settings.logfile = "%s.log" % __file__
+settings.loglevel = _newrelic.LOG_VERBOSEDEBUG
+
+_function_args = None
+
+def _function(*args, **kwargs):
+    return (args, kwargs)
+
+class _wrapper(object):
+   def __init__(self, wrapped):
+       self._wrapped = wrapped
+   def __get__(self, obj, objtype=None):
+       return types.MethodType(self, obj, objtype)
+   def __call__(self, *args, **kwargs):
+       global _function_args
+       _function_args = (args, kwargs)
+       return self._wrapped(*args, **kwargs)
+
+class ApplicationTests01(unittest.TestCase):
+
+    def setUp(self):
+        _newrelic.log(_newrelic.LOG_DEBUG, "STARTING - %s" %
+                      self._testMethodName)
+
+    def tearDown(self):
+        _newrelic.log(_newrelic.LOG_DEBUG, "STOPPING - %s" %
+                      self._testMethodName)
+
+    def test_wrap_object(self):
+        _newrelic.wrap_object(__name__, None, '_function', _wrapper)
+        args = (1, 2, 3)
+        kwargs = { "one": 1, "two": 2, "three": 3 }
+        result = _function(*args, **kwargs)
+        self.assertEqual(result, (args, kwargs))
+
+if __name__ == '__main__':
+    unittest.main()

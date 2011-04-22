9e935bcf5d410b1329ee2e59728f4bf23550cb61
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2011-04-22 13:40:32 +1000

    Fix up vim editor macros.

--- wrapper/py_utilities.c
+++ wrapper/py_utilities.c
@@ -543,5 +543,5 @@ extern PyObject *NRUtilities_ObfuscateTransactionName(const char *name,
 /* ------------------------------------------------------------------------- */
 
 /*
- * vim: et cino=>2,e0,n0,f0,{2,}0,^0,\:2,=2,p2,t2,c1,+2,(2,u2,)20,*30,g2,h2 ts=8
+ * vim: set cino=>2,e0,n0,f0,{2,}0,^0,\:2,=2,p2,t2,c1,+2,(2,u2,)20,*30,g2,h2 ts=8
  */

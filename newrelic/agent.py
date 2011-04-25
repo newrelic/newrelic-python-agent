6421b0c45788c15823f7006fa439e34004df4f5b
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2011-04-25 14:01:24 +1000

    Read and apply configuration during Python agent module import.

--- newrelic/agent.py
+++ newrelic/agent.py
@@ -1,6 +1,43 @@
 import sys
+import os
+import ConfigParser
 
 from _newrelic import *
+
+# Read in and apply agent configuration.
+
+_LOG_LEVELS = {
+    'ERROR' : LOG_ERROR,
+    'INFO' : LOG_INFO,
+    'WARNING': LOG_WARNING,
+    'VERBOSE' : LOG_VERBOSE,
+    'DEBUG' : LOG_DEBUG,
+    'VERBOSEDEBUG': LOG_VERBOSEDEBUG,
+}
+
+_CONFIG_VALUES = {
+    'logfile': str,
+    'loglevel': _LOG_LEVELS.__getitem__,
+}
+
+_settings = settings()
+
+_config_file = os.environ.get('NEWRELIC_CONFIG', None)
+
+if _config_file:
+    _config_object = ConfigParser.SafeConfigParser()
+    _config_object.read([_config_file])
+    for key, format in _CONFIG_VALUES.iteritems():
+        string = _config_object.get('newrelic', key, None)
+        if string is not None:
+            try:
+                value = format(string)
+            except:
+                raise RuntimeError('Invalid configuration entry with name '
+                                   '"%s" and value "%s".' % (key, string))
+            setattr(_settings, key, value)
+
+# Setup instrumentation by triggering off module imports.
 
 sys.meta_path.insert(0, ImportHookFinder())
 

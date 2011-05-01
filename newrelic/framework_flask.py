99f23af970646dee186dc961a4c478b6f491be98
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2011-05-01 13:11:39 +1000

    Convert Flask instrumentation to use name function wrapper.

--- newrelic/framework_flask.py
+++ newrelic/framework_flask.py
@@ -1,12 +1,14 @@
 import sys
 
 from newrelic.agent import (FunctionTraceWrapper, wrap_in_function,
-        wrap_pre_function, wrap_function_trace, transaction)
+        wrap_pre_function, wrap_function_trace, transaction,
+        NameTransactionWrapper)
 
 def wrap_add_url_rule_input(self, rule, endpoint=None, view_func=None,
         **options):
     if view_func is not None:
-        view_func = FunctionTraceWrapper(view_func, override_path=True)
+        view_func = NameTransactionWrapper(view_func)
+        view_func = FunctionTraceWrapper(view_func)
     return ((self, rule, endpoint, view_func), options)
 
 def wrap_handle_exception(self, e):

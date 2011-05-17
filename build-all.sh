76d75e582f5bc46032a706719c4140cde0ea39b3
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2011-05-17 21:36:05 +1000

    Script for building variants for all Python versions and Unicode variants for platform.

---
+++ build-all.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+TOOLSDIR=$1
+
+if test x"$TOOLSDIR" = x""
+then
+    TOOLSDIR=python-tools/parts
+fi
+
+PLATFORM=`./config.guess | sed -e "s/[0-9.]*$//"`
+
+case $PLATFORM in
+    i386-apple-darwin)
+        PYTHON_VERSIONS='2.6 2.7'
+        UNICODE_VARIANTS='ucs2'
+        ;;
+    i686-pc-linux-gnu)
+        PYTHON_VERSIONS='2.4 2.5 2.6 2.7'
+        UNICODE_VARIANTS='ucs2 ucs4'
+        ;;
+    x86_64-unknown-linux-gnu)
+        PYTHON_VERSIONS='2.4 2.5 2.6 2.7'
+        UNICODE_VARIANTS='ucs2 ucs4'
+        ;;
+esac
+
+ROOTDIR=newrelic-$PLATFORM
+
+test -f Makefile && make distclean
+test -d $ROOTDIR && rm -rf $ROOTDIR
+
+for i in $PYTHON_VERSIONS
+do
+    for j in $UNICODE_VARIANTS
+    do
+        echo ./configure --with-python=$TOOLSDIR/python-$i-$j/bin/python$i
+        ./configure --with-python=$TOOLSDIR/python-$i-$j/bin/python$i
+        echo make install-destdir DESTDIR=$ROOTDIR/python-$i-$j
+        make install-destdir DESTDIR=$ROOTDIR/python-$i-$j
+    done
+done

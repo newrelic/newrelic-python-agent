233f5aa7fe376ae968ddbfdcdf2000b233e6a31c
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2011-05-19 16:28:40 +1000

    Eliminate deprecation warnings about popen4. Use config.guess for platform in main distutils definition. Check for directory renaming.

--- package.py
+++ package.py
@@ -15,25 +15,44 @@ if not script_directory:
 release_name = os.path.basename(script_directory)
 
 release_fields = string.splitfields(release_name, '-', 3)
-package_version, package_platform = release_fields[-2:]
+
+try:
+  package_version, package_platform = release_fields[-2:]
+except ValueError:
+  print >> sys.stderr
+  print >> sys.stderr, 'Package corrupted, did you rename the directory?'
+  print >> sys.stderr
+  raise SystemExit(1)
 
 config_guess = 'config.guess | sed -e "s/[0-9.]*$//"'
 config_guess = os.path.join(script_directory, config_guess)
-actual_platform = os.popen4(config_guess)[1].read().strip()
+
+try:
+  import subprocess
+  actual_platform = subprocess.Popen(config_guess,
+      stdout=subprocess.PIPE).stdout.read().strip()
+except:
+  actual_platform = os.popen4(config_guess)[1].read().strip()
 
 if package_platform != actual_platform:
+  print >> sys.stderr
   print >> sys.stderr, 'Sorry, this is the wrong release for this platform.'
   print >> sys.stderr, 'Require release for "%s" platform.' % actual_platform
+  print >> sys.stderr
   raise SystemExit(1)
 
 target_directory = os.path.join(script_directory, package_name)
 
 if not os.path.isdir(target_directory):
+  print >> sys.stderr
   print >> sys.stderr, 'Sorry, no matching package for Python installation.'
   print >> sys.stderr, 'Require package for "%s".' % package_name
+  print >> sys.stderr
   raise SystemExit(1)
 
+print
 print 'Using package %s.' % package_name
+print
 
 os.chdir(target_directory)
 

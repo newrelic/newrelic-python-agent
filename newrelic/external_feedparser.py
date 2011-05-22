057cb723be29a34a03ae9027b865a7ea16e18851
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2011-05-22 11:37:52 +1000

    Only time feedparser calls as external trace if url is http, https or ftp. This is can be file or string as well where external call done separately.

--- newrelic/external_feedparser.py
+++ newrelic/external_feedparser.py
@@ -1,4 +1,30 @@
-from newrelic.agent import wrap_external_trace
+import sys
+import types
+
+from newrelic.agent import (transaction, wrap_object, ExternalTrace)
+
+class capture_external_trace(object):
+    def __init__(self, wrapped):
+        self.__wrapped__ = wrapped
+    def __get__(self, obj, objtype=None):
+        return types.MethodType(self, obj, objtype)
+    def __call__(self, url):
+        if url.split(':')[0].lower() in ['http', 'https', 'ftp']:
+            current_transaction = transaction()
+            if current_transaction:
+                trace = ExternalTrace(current_transaction, 'feedparser', url)
+                try:
+                    context_manager = trace.__enter__()
+                    return self.__wrapped__(url)
+                except:
+                    context_manager.__exit__(*sys.exc_info())
+                    raise
+                finally:
+                    context_manager.__exit__(None, None, None)
+            else:
+                return self.__wrapped__(url)
+        else:
+            return self.__wrapped__(url)
 
 def instrument(module):
-    wrap_external_trace(module, 'parse', 'feedparser', lambda url: url)
+    wrap_object(module, 'parse', capture_external_trace)

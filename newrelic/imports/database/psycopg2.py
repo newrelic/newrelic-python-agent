15e94bd468e9cab9231b2c10ce85b0fd572061fd
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2011-05-26 16:02:27 +1000

    Cursor held in submodule not top level.

--- newrelic/imports/database/psycopg2.py
+++ newrelic/imports/database/psycopg2.py
@@ -3,7 +3,7 @@ import newrelic.agent
 from newrelic.agent import wrap_database_trace
 
 def instrument(module):
-    wrap_database_trace(module, 'cursor.execute',
+    wrap_database_trace('psycopg2._psycopg', 'cursor.execute',
             lambda self, sql, parameters=(): sql)
-    wrap_database_trace(module, 'cursor.executemany',
+    wrap_database_trace('psycopg2._psycopg', 'cursor.executemany',
             lambda self, sql, seq_of_parameters=[]: sql)

fd65128a84d0926c3886eb5c7b7066c620e49289
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2011-05-31 09:35:07 +1000

    Use __wrapped__ convention for wrapped object.

--- newrelic/imports/database/dbapi2.py
+++ newrelic/imports/database/dbapi2.py
@@ -4,35 +4,35 @@ from newrelic.agent import DatabaseTraceWrapper
 
 class CursorWrapper(object):
     def __init__(self, cursor):
-        self.__cursor = cursor
+        self.__wrapped__ = cursor
     def __getattr__(self, name):
         if name == 'execute':
-            return DatabaseTraceWrapper(getattr(self.__cursor, name),
+            return DatabaseTraceWrapper(getattr(self.__wrapped__, name),
                     (lambda sql, parameters=(): sql))
         elif name == 'executemany':
-            return DatabaseTraceWrapper(getattr(self.__cursor, name),
+            return DatabaseTraceWrapper(getattr(self.__wrapped__, name),
                     (lambda sql, seq_of_parameters=[]: sql))
-        return getattr(self.__cursor, name)
+        return getattr(self.__wrapped__, name)
 
 class CursorFactory(object):
     def __init__(self, function):
-        self.__function = function
+        self.__wrapped__ = function
     def __call__(self, *args, **kwargs):
-        return CursorWrapper(self.__function(*args, **kwargs))
+        return CursorWrapper(self.__wrapped__(*args, **kwargs))
 
 class ConnectionWrapper(object):
     def __init__(self, connection):
-        self.__connection = connection
+        self.__wrapped__ = connection
     def __getattr__(self, name):
         if name == 'cursor':
-            return CursorFactory(self.__connection.cursor)
-        return getattr(self.__connection, name)
+            return CursorFactory(self.__wrapped__.cursor)
+        return getattr(self.__wrapped__, name)
 
 class ConnectionFactory(object):
     def __init__(self, function):
-        self.__function = function
+        self.__wrapped__ = function
     def __call__(self, *args, **kwargs):
-        return ConnectionWrapper(self.__function(*args, **kwargs))
+        return ConnectionWrapper(self.__wrapped__(*args, **kwargs))
 
 def instrument(module):
     module.connect = ConnectionFactory(module.connect)

7190a50dd49d9caebb988f1637a24e13058f2fa7
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2011-07-17 16:34:33 +1000

    For backwards compatibility, add back in use of environment variables for initialisation. Will be removed prior to general release.

--- newrelic/agent.py
+++ newrelic/agent.py
@@ -1,4 +1,16 @@
+import os
+
 from _newrelic import *
 
 from newrelic.config import *
 from newrelic.profile import *
+
+# Setting from environment variables is only provided for backward
+# compatibity with early Python agent BETA versions. This will be
+# removed prior to general release.
+
+_config_file = os.environ.get('NEWRELIC_CONFIG_FILE', None)
+_environment = os.environ.get('NEWRELIC_ENVIRONMENT', None)
+
+if _config_file:
+    initialize(_config_file, _environment)

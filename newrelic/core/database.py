e276d3fa2848a6990671880440580dda1a77e294
Saxon <saxon@newrelic.com>
2011-07-27 15:57:05 -0700

    Stubbing out modules

---
+++ newrelic/core/database.py
@@ -0,0 +1,9 @@
+'''
+Created on Jul 27, 2011
+
+@author: sdaubin
+'''
+
+'''
+This is where we'll implement the sql obfuscator, explain plan running, sql tracer, etc.
+'''
---
+++ unit-tests/core/test_database.py
@@ -0,0 +1,18 @@
+'''
+Created on Jul 27, 2011
+
+@author: sdaubin
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()

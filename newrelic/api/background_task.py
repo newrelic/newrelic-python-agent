16978eb6ed3a0dd810f2f969740e7f9f0caae1d6
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2011-07-30 10:47:40 +1000

    Python implementation of decorator and wrap functions for web transactions and background tasks. PYTHON-151

--- newrelic/api/background_task.py
+++ newrelic/api/background_task.py
@@ -1,6 +1,28 @@
+import os
+
 import _newrelic
+
+import newrelic.api.object_wrapper
+
+_agent_mode = os.environ.get('NEWRELIC_AGENT_MODE', '').lower()
 
 BackgroundTask = _newrelic.BackgroundTask
 BackgroundTaskWrapper = _newrelic.BackgroundTaskWrapper
-background_task = _newrelic.background_task
-wrap_background_task = _newrelic.wrap_background_task
+
+# TODO BackgroundTaskWrapper will be like FunctionTraceWrapper
+# with exception that needs to flag an outer web transaction as
+# a background task. Need to consider nesting in general.
+
+def background_task(application=None, name=None, scope=None):
+    def decorator(wrapped):
+        return BackgroundTaskWrapper(wrapped, application, name, scope)
+    return decorator
+
+def wrap_background_task(module, object_path, application=None, name=None,
+                         scope=None):
+    newrelic.api.object_wrapper.wrap_object(module, object_path,
+            BackgroundTaskWrapper, (application, name, scope))
+
+if not _agent_mode in ('ungud', 'julunggul'):
+    background_task = _newrelic.background_task
+    wrap_background_task = _newrelic.wrap_background_task
--- newrelic/api/web_transaction.py
+++ newrelic/api/web_transaction.py
@@ -1,6 +1,23 @@
+import os
+
 import _newrelic
+
+import newrelic.api.object_wrapper
+
+_agent_mode = os.environ.get('NEWRELIC_AGENT_MODE', '').lower()
 
 WebTransaction = _newrelic.WebTransaction
 WSGIApplicationWrapper = _newrelic.WSGIApplicationWrapper
-wsgi_application = _newrelic.wsgi_application
-wrap_wsgi_application = _newrelic.wrap_wsgi_application
+
+def wsgi_application(application=None):
+    def decorator(wrapped):
+        return WSGIApplicationWrapper(wrapped, application)
+    return decorator
+
+def wrap_wsgi_application(module, object_path, application=None):
+    newrelic.api.object_wrapper.wrap_object(module, object_path,
+            WSGIApplicationWrapper, (application, ))
+
+if not _agent_mode in ('ungud', 'julunggul'):
+    wsgi_application = _newrelic.wsgi_application
+    wrap_wsgi_application = _newrelic.wrap_wsgi_application

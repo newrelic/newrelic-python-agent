1bea6bf9ea6d53a6c6bcb1df09efc39995aa4293
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2011-08-01 20:47:05 +1000

    Implement Python stub for application object. PYTHON-236

--- newrelic/api/application.py
+++ newrelic/api/application.py
@@ -1,4 +1,51 @@
+import os
+import threading
+
 import _newrelic
 
-application = _newrelic.application
-Application = _newrelic.Application
+_agent_mode = os.environ.get('NEWRELIC_AGENT_MODE', '').lower()
+
+class Application(object):
+
+    _lock = threading.Lock()
+    _instances = {}
+
+    @staticmethod
+    def _instance(name):
+        Application._lock.acquire()
+        try:
+            instance = Application._instances.get(name, None)
+            if not instance:
+                instance = Application(name)
+                Application._instances[name] = instance
+            return instance
+        finally:
+            Application._lock.release()
+
+    def __init__(self, name):
+        self._name = name
+        self._clusters = {}
+        self.enabled = True
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def clusters(self):
+        return self._clusters.keys()
+
+    @property
+    def running(self):
+        return True
+
+    def add_to_cluster(self, name):
+        self._clusters[name] = True
+
+def application(name):
+    return Application._instance(name)
+
+if _agent_mode not in ('julunggul',):
+    import _newrelic
+    application = _newrelic.application
+    Application = _newrelic.Application

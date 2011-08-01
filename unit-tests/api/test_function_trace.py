8da8f903616af43e4f7ae3f453708996ce908b05
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2011-08-01 14:32:12 +1000

    Update function trace test script to new API. PYTHON-152

--- unit-tests/api/test_function_trace.py
+++ unit-tests/api/test_function_trace.py
@@ -2,32 +2,38 @@ import unittest
 import time
 import sys
 
-import _newrelic
+import newrelic.api.settings
+import newrelic.api.log_file
+import newrelic.api.application
+import newrelic.api.web_transaction
+import newrelic.api.function_trace
 
-settings = _newrelic.settings()
+settings = newrelic.api.settings.settings()
 settings.log_file = "%s.log" % __file__
-settings.log_level = _newrelic.LOG_VERBOSEDEBUG
+settings.log_level = newrelic.api.log_file.LOG_VERBOSEDEBUG
 settings.transaction_tracer.transaction_threshold = 0
 
-application = _newrelic.application("UnitTests")
+application = newrelic.api.application.application("UnitTests")
 
-#@_newrelic.function_trace(name='_test_function_1')
+#@newrelic.api.function_trace.function_trace(name='_test_function_1')
 def _test_function_1():
     time.sleep(1.0)
-_test_function_1 = _newrelic.function_trace(
+_test_function_1 = newrelic.api.function_trace.function_trace(
         name='_test_function_1')(_test_function_1)
 
-#@_newrelic.function_trace()
+#@newrelic.api.function_trace.function_trace()
 def _test_function_nn_1():
     time.sleep(0.1)
-_test_function_nn_1 = _newrelic.function_trace()(_test_function_nn_1)
+_test_function_nn_1 = newrelic.api.function_trace.function_trace()(
+        _test_function_nn_1)
 
 class _test_class_nn_2:
     def _test_function(self):
         time.sleep(0.1)
 
 _test_class_instance_nn_2 = _test_class_nn_2()
-_test_class_instance_nn_2._test_function = _newrelic.function_trace()(
+_test_class_instance_nn_2._test_function = \
+        newrelic.api.function_trace.function_trace()(
         _test_class_instance_nn_2._test_function)
 
 class _test_class_nn_3(object):
@@ -35,56 +41,67 @@ class _test_class_nn_3(object):
         time.sleep(0.1)
 
 _test_class_instance_nn_3 = _test_class_nn_3()
-_test_class_instance_nn_3._test_function = _newrelic.function_trace()(
+_test_class_instance_nn_3._test_function = \
+        newrelic.api.function_trace.function_trace()(
         _test_class_instance_nn_3._test_function)
 
 class _test_class_nn_4:
-    #@_newrelic.function_trace()
+    #@newrelic.api.function_trace.function_trace()
     def _test_function(self):
         time.sleep(0.1)
-    _test_function = _newrelic.function_trace()(_test_function)
+    _test_function = newrelic.api.function_trace.function_trace()(
+            _test_function)
 
 _test_class_instance_nn_4 = _test_class_nn_4()
 
 class _test_class_nn_5(object):
-    #@_newrelic.function_trace()
+    #@newrelic.api.function_trace.function_trace()
     def _test_function(self):
         time.sleep(0.1)
-    _test_function = _newrelic.function_trace()(_test_function)
+    _test_function = newrelic.api.function_trace.function_trace()(
+            _test_function)
 
 _test_class_instance_nn_5 = _test_class_nn_5()
 
 def _test_function_6(name):
     time.sleep(0.1)
-_test_function_6 = _newrelic.function_trace(lambda x: x)(_test_function_6)
+_test_function_6 = newrelic.api.function_trace.function_trace(
+        lambda x: x)(_test_function_6)
 
 class FunctionTraceTests(unittest.TestCase):
 
     def setUp(self):
-        _newrelic.log(_newrelic.LOG_DEBUG, "STARTING - %s" %
-                      self._testMethodName)
+        newrelic.api.log_file.log(newrelic.api.log_file.LOG_DEBUG,
+                "STARTING - %s" % self._testMethodName)
 
     def tearDown(self):
-        _newrelic.log(_newrelic.LOG_DEBUG, "STOPPING - %s" %
-                      self._testMethodName)
+        newrelic.api.log_file.log(newrelic.api.log_file.LOG_DEBUG,
+                "STOPPING - %s" % self._testMethodName)
 
     def test_function_trace(self):
         environ = { "REQUEST_URI": "/function_trace" }
-        transaction = _newrelic.WebTransaction(application, environ)
+        transaction = newrelic.api.web_transaction.WebTransaction(
+              application, environ)
         with transaction:
             time.sleep(0.2)
-            with _newrelic.FunctionTrace(transaction, "function-1"):
+            with newrelic.api.function_trace.FunctionTrace(
+                    transaction, "function-1"):
                 time.sleep(0.1)
-                with _newrelic.FunctionTrace(transaction, "function-1-1"):
+                with newrelic.api.function_trace.FunctionTrace(
+                        transaction, "function-1-1"):
                     time.sleep(0.1)
-                with _newrelic.FunctionTrace(transaction, "function-1-2"):
+                with newrelic.api.function_trace.FunctionTrace(
+                        transaction, "function-1-2"):
                     time.sleep(0.1)
                 time.sleep(0.1)
-            with _newrelic.FunctionTrace(transaction, "function-2"):
+            with newrelic.api.function_trace.FunctionTrace(
+                    transaction, "function-2"):
                 time.sleep(0.1)
-                with _newrelic.FunctionTrace(transaction, "function-2-1"):
+                with newrelic.api.function_trace.FunctionTrace(
+                        transaction, "function-2-1"):
                     time.sleep(0.1)
-                    with _newrelic.FunctionTrace(transaction, "function-2-1-1"):
+                    with newrelic.api.function_trace.FunctionTrace(
+                            transaction, "function-2-1-1"):
                         time.sleep(0.1)
                     time.sleep(0.1)
                 time.sleep(0.1)
@@ -92,16 +109,19 @@ class FunctionTraceTests(unittest.TestCase):
 
     def test_transaction_not_running(self):
         environ = { "REQUEST_URI": "/transaction_not_running" }
-        transaction = _newrelic.WebTransaction(application, environ)
+        transaction = newrelic.api.web_transaction.WebTransaction(
+                application, environ)
         try:
-            with _newrelic.FunctionTrace(transaction, "function"):
+            with newrelic.api.function_trace.FunctionTrace(
+                    transaction, "function"):
                 time.sleep(0.1)
         except RuntimeError:
             pass
 
     def test_function_trace_decorator(self):
         environ = { "REQUEST_URI": "/function_trace_decorator" }
-        transaction = _newrelic.WebTransaction(application, environ)
+        transaction = newrelic.api.web_transaction.WebTransaction(
+                application, environ)
         with transaction:
             time.sleep(0.1)
             _test_function_1()
@@ -109,7 +129,8 @@ class FunctionTraceTests(unittest.TestCase):
 
     def test_function_trace_decorator_error(self):
         environ = { "REQUEST_URI": "/function_trace_decorator_error" }
-        transaction = _newrelic.WebTransaction(application, environ)
+        transaction = newrelic.api.web_transaction.WebTransaction(
+                application, environ)
         with transaction:
             try:
                 _test_function_1()
@@ -118,7 +139,8 @@ class FunctionTraceTests(unittest.TestCase):
 
     def test_function_trace_decorator_no_name(self):
         environ = { "REQUEST_URI": "/function_trace_decorator_no_name" }
-        transaction = _newrelic.WebTransaction(application, environ)
+        transaction = newrelic.api.web_transaction.WebTransaction(
+                application, environ)
         with transaction:
             time.sleep(0.1)
             _test_function_nn_1()

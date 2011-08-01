63d2c354f9c65e4d3ee2bb48e9052b4babb970b6
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2011-08-01 14:13:33 +1000

    Update error trace test script to new API. PYTHON-152

--- unit-tests/api/test_error_trace.py
+++ unit-tests/api/test_error_trace.py
@@ -4,16 +4,20 @@ import unittest
 import time
 import sys
 
-import _newrelic
+import newrelic.api.settings
+import newrelic.api.log_file
+import newrelic.api.application
+import newrelic.api.web_transaction
+import newrelic.api.error_trace
 
-settings = _newrelic.settings()
+settings = newrelic.api.settings.settings()
 settings.log_file = "%s.log" % __file__
-settings.log_level = _newrelic.LOG_VERBOSEDEBUG
+settings.log_level = newrelic.api.log_file.LOG_VERBOSEDEBUG
 settings.transaction_tracer.transaction_threshold = 0
 
 settings.error_collector.ignore_errors = ['exceptions.NotImplementedError']
 
-application = _newrelic.application("UnitTests")
+application = newrelic.api.application.application("UnitTests")
 
 class Error:
     def __init__(self, message):
@@ -21,45 +25,47 @@ class Error:
     def __str__(self):
         return self.message
 
-@_newrelic.error_trace()
+@newrelic.api.error_trace.error_trace()
 def function_1():
     raise RuntimeError("runtime_error 1")
 
 def function_2():
     raise Error("runtime_error 2")
 
-@_newrelic.error_trace()
+@newrelic.api.error_trace.error_trace()
 def function_3():
     raise NotImplementedError("runtime_error 3")
 
-@_newrelic.error_trace(ignore_errors=['__main__.Error'])
+@newrelic.api.error_trace.error_trace(ignore_errors=['__main__.Error'])
 def function_4():
     raise Error("runtime_error 4")
 
 class ErrorTraceTransactionTests(unittest.TestCase):
 
     def setUp(self):
-        _newrelic.log(_newrelic.LOG_DEBUG, "STARTING - %s" %
-                      self._testMethodName)
+        newrelic.api.log_file.log(newrelic.api.log_file.LOG_DEBUG,
+                "STARTING - %s" % self._testMethodName)
 
     def tearDown(self):
-        _newrelic.log(_newrelic.LOG_DEBUG, "STOPPING - %s" %
-                      self._testMethodName)
+        newrelic.api.log_file.log(newrelic.api.log_file.LOG_DEBUG,
+                "STOPPING - %s" % self._testMethodName)
 
     def test_implicit_runtime_error(self):
         environ = { "REQUEST_URI": "/error_trace" }
-        transaction = _newrelic.WebTransaction(application, environ)
+        transaction = newrelic.api.web_transaction.WebTransaction(
+                application, environ)
         with transaction:
             time.sleep(0.5)
             try:
-                with _newrelic.ErrorTrace(transaction):
+                with newrelic.api.error_trace.ErrorTrace(transaction):
                     raise RuntimeError("runtime_error")
             except:
                 pass
 
     def test_implicit_runtime_error_decorator(self):
         environ = { "REQUEST_URI": "/error_trace_decorator" }
-        transaction = _newrelic.WebTransaction(application, environ)
+        transaction = newrelic.api.web_transaction.WebTransaction(
+                application, environ)
         with transaction:
             time.sleep(0.5)
             try:
@@ -69,8 +75,9 @@ class ErrorTraceTransactionTests(unittest.TestCase):
 
     def test_implicit_runtime_error_wrap(self):
         environ = { "REQUEST_URI": "/error_trace_wrap" }
-        transaction = _newrelic.WebTransaction(application, environ)
-        _newrelic.wrap_error_trace(__name__, 'function_2')
+        transaction = newrelic.api.web_transaction.WebTransaction(
+                application, environ)
+        newrelic.api.error_trace.wrap_error_trace(__name__, 'function_2')
         with transaction:
             time.sleep(0.5)
             try:
@@ -80,7 +87,8 @@ class ErrorTraceTransactionTests(unittest.TestCase):
 
     def test_implicit_runtime_error_ignore(self):
         environ = { "REQUEST_URI": "/error_trace_ignore" }
-        transaction = _newrelic.WebTransaction(application, environ)
+        transaction = newrelic.api.web_transaction.WebTransaction(
+              application, environ)
         with transaction:
             time.sleep(0.5)
             try:
@@ -90,7 +98,8 @@ class ErrorTraceTransactionTests(unittest.TestCase):
 
     def test_implicit_runtime_error_ignore_api(self):
         environ = { "REQUEST_URI": "/error_trace_ignore_api" }
-        transaction = _newrelic.WebTransaction(application, environ)
+        transaction = newrelic.api.web_transaction.WebTransaction(
+                application, environ)
         with transaction:
             time.sleep(0.5)
             try:
@@ -100,11 +109,12 @@ class ErrorTraceTransactionTests(unittest.TestCase):
 
     def test_implicit_runtime_unicode(self):
         environ = { "REQUEST_URI": "/error_trace_unicode" }
-        transaction = _newrelic.WebTransaction(application, environ)
+        transaction = newrelic.api.web_transaction.WebTransaction(
+                application, environ)
         with transaction:
             time.sleep(0.5)
             try:
-                with _newrelic.ErrorTrace(transaction):
+                with newrelic.api.error_trace.ErrorTrace(transaction):
                     import sys
                     raise RuntimeError(u"runtime_error %s √√√√" %
                                        sys.getdefaultencoding())

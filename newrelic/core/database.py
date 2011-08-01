d1c53efe6cfcf39eda91346e03d5e935683b2828
Jon Guymon <jon@newrelic.com>
2011-08-01 16:52:51 -0700

    PYT-239 sql obfuscator

--- newrelic/core/database.py
+++ newrelic/core/database.py
@@ -6,13 +6,30 @@ Created on Jul 27, 2011
 
 '''
 This is where we'll implement the sql obfuscator, explain plan running, sql tracer, etc.
-
-TODO: investigate http://code.google.com/p/python-sqlparse/
 '''
 
-def default_sql_obfuscation(sql):
-    return sql
+import re
 
-def obfuscate_sql(sql):
-    # FIXME
-    return sql
+def obfuscator(database_type="postgresql"):
+    if database_type == "mysql":
+        return MysqlObfuscator()
+    elif database_type == "postgresql":
+        return PostgresqlObfuscator()
+
+class SqlObfuscator:
+    def numeric(self, sql):
+        return re.sub(r'\d+', "?", sql)
+
+    def single_quoted_string(self, sql):
+        return re.sub(r"'(.*?[^\\'])??'(?!')", "?", sql)
+
+    def double_quoted_string(self, sql):
+        return re.sub(r'"(.*?[^\\"])??"(?!")', "?", sql)
+
+class PostgresqlObfuscator(SqlObfuscator):
+    def obfuscate(self, sql):
+        return self.single_quoted_string(self.numeric(sql))
+
+class MysqlObfuscator(SqlObfuscator):
+    def obfuscate(self, sql):
+        return self.double_quoted_string(self.single_quoted_string(self.numeric(sql)))
--- unit-tests/core/test_database.py
+++ unit-tests/core/test_database.py
@@ -4,14 +4,44 @@ Created on Jul 27, 2011
 @author: sdaubin
 '''
 import unittest
+from newrelic.core import database
 
+class TestDatabase(unittest.TestCase):
 
-class Test(unittest.TestCase):
+    def setUp(self):
+        self.obfuscator = database.obfuscator("postgresql")
 
+    def test_obfuscator_returns_class_for_given_database_type(self):
+        self.assertEqual("MysqlObfuscator", 
+                         database.obfuscator("mysql").__class__.__name__)
+        self.assertEqual("PostgresqlObfuscator", 
+                         database.obfuscator("postgresql").__class__.__name__)
+        self.assertEqual("PostgresqlObfuscator", 
+                         database.obfuscator().__class__.__name__)
 
-    def testName(self):
-        pass
+    def test_obfuscator_obfuscates_numeric_literals(self):
+        select = "SELECT * FROM table WHERE table.column = 1 AND 2 = 3"
+        self.assertEqual("SELECT * FROM table WHERE table.column = ? AND ? = ?",
+                         self.obfuscator.obfuscate(select))
+        insert = "INSERT INTO table VALUES (1,2, 3 ,  4)"
+        self.assertEqual("INSERT INTO table VALUES (?,?, ? ,  ?)",
+                         self.obfuscator.obfuscate(insert))
 
+    def test_obfuscator_obfuscates_string_literals(self):
+        insert = "INSERT INTO X values('', 'jim''s ssn',0, 1 , 'jim''s son''s son')"
+        self.assertEqual("INSERT INTO X values(?, ?,?, ? , ?)",
+                         self.obfuscator.obfuscate(insert))
+
+    def test_obfuscator_does_not_obfuscate_table_or_column_names(self):
+        select = 'SELECT "table"."column" FROM "table" WHERE "table"."column" = \'value\' LIMIT 1'
+        self.assertEqual('SELECT "table"."column" FROM "table" WHERE "table"."column" = ? LIMIT ?',
+                         self.obfuscator.obfuscate(select))
+
+    def test_mysql_obfuscation(self):
+        obfuscator = database.obfuscator("mysql")
+        select = 'SELECT `table`.`column` FROM `table` WHERE `table`.`column` = \'value\' AND `table`.`other_column` = "other value" LIMIT 1'
+        self.assertEqual('SELECT `table`.`column` FROM `table` WHERE `table`.`column` = ? AND `table`.`other_column` = ? LIMIT ?',
+                         obfuscator.obfuscate(select))
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']

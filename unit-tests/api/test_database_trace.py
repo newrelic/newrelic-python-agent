02166b85f01a47016e981619201dab299360ea0f
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2011-08-01 11:41:56 +1000

    Update database trace test script to new API. PYTHON-152

--- unit-tests/api/test_database_trace.py
+++ unit-tests/api/test_database_trace.py
@@ -2,50 +2,59 @@ import unittest
 import time
 import sys
 
-import _newrelic
+import newrelic.api.settings
+import newrelic.api.log_file
+import newrelic.api.application
+import newrelic.api.web_transaction
+import newrelic.api.database_trace
 
-settings = _newrelic.settings()
+settings = newrelic.api.settings.settings()
 settings.log_file = "%s.log" % __file__
-settings.log_level = _newrelic.LOG_VERBOSEDEBUG
+settings.log_level = newrelic.api.log_file.LOG_VERBOSEDEBUG
 settings.transaction_tracer.transaction_threshold = 0
 
-application = _newrelic.application("UnitTests")
+application = newrelic.api.application.application("UnitTests")
 
-@_newrelic.database_trace(lambda sql: sql)
+@newrelic.api.database_trace.database_trace(lambda sql: sql)
 def _test_function_1(sql):
     time.sleep(1.0)
 
 class DatabaseTraceTests(unittest.TestCase):
 
     def setUp(self):
-        _newrelic.log(_newrelic.LOG_DEBUG, "STARTING - %s" %
-                      self._testMethodName)
+        newrelic.api.log_file.log(newrelic.api.log_file.LOG_DEBUG,
+                "STARTING - %s" % self._testMethodName)
 
     def tearDown(self):
-        _newrelic.log(_newrelic.LOG_DEBUG, "STOPPING - %s" %
-                      self._testMethodName)
+        newrelic.api.log_file.log(newrelic.api.log_file.LOG_DEBUG,
+                "STOPPING - %s" % self._testMethodName)
 
     def test_database_trace(self):
         environ = { "REQUEST_URI": "/database_trace" }
-        transaction = _newrelic.WebTransaction(application, environ)
+        transaction = newrelic.api.web_transaction.WebTransaction(
+                application, environ)
         with transaction:
             time.sleep(0.1)
-            with _newrelic.DatabaseTrace(transaction, "select * from cat"):
+            with newrelic.api.database_trace.DatabaseTrace(
+                    transaction, "select * from cat"):
                 time.sleep(1.0)
             time.sleep(0.1)
 
     def test_transaction_not_running(self):
         environ = { "REQUEST_URI": "/transaction_not_running" }
-        transaction = _newrelic.WebTransaction(application, environ)
+        transaction = newrelic.api.web_transaction.WebTransaction(
+                application, environ)
         try:
-            with _newrelic.DatabaseTrace(transaction, "select * from cat"):
+            with newrelic.api.database_trace.DatabaseTrace(
+                    transaction, "select * from cat"):
                 time.sleep(0.1)
         except RuntimeError:
             pass
 
     def test_database_trace_decorator(self):
         environ = { "REQUEST_URI": "/database_trace_decorator" }
-        transaction = _newrelic.WebTransaction(application, environ)
+        transaction = newrelic.api.web_transaction.WebTransaction(
+                application, environ)
         with transaction:
             time.sleep(0.1)
             _test_function_1("select * from cat")
@@ -53,7 +62,8 @@ class DatabaseTraceTests(unittest.TestCase):
 
     def test_database_trace_decorator_error(self):
         environ = { "REQUEST_URI": "/database_trace_decorator_error" }
-        transaction = _newrelic.WebTransaction(application, environ)
+        transaction = newrelic.api.web_transaction.WebTransaction(
+                application, environ)
         with transaction:
             try:
                 _test_function_1("select * from cat", None)

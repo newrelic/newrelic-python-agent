d5190e68c3aad557da7fb3f2369e5eee967b194c
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2011-08-03 19:36:45 +1000

    Example of how to override global settings for unit tests to direct to staging.

--- unit-tests/core/test_agent.py
+++ unit-tests/core/test_agent.py
@@ -4,8 +4,17 @@ Created on Jul 27, 2011
 @author: sdaubin
 '''
 import unittest
+
 from newrelic.core.agent import Agent, newrelic_agent
 
+import newrelic.core.config
+
+_settings = newrelic.core.config.settings()
+
+_settings.host = 'staging-collector.newrelic.com'
+_settings.port = 80
+_settings.license_key = 'd67afc830dab717fd163bfcb0b8b88423e9a1a3b'
+_settings.app_name = 'Python Unit Test1'
 
 class AgentTest(unittest.TestCase):
 

819b756b0e55b663b6ded6bb98245086286bc516
Jon Guymon <jon@newrelic.com>
2011-08-04 16:42:03 -0700

    early support for !replace_all, multiple rules and each_segment rules

---
+++ newrelic/core/transaction.py
@@ -0,0 +1,37 @@
+import re
+
+class NormalizationRule:
+    def __init__(self, match, replacement, ignore=False, order=0, 
+                 terminate_chain=False, each_segment=False, replace_all=True):
+        self.match = match
+        self.replacement = replacement
+        self.ignore = ignore
+        self.order = order
+        self.terminate_chain = terminate_chain
+        self.each_segment = each_segment
+        self.replace_all = replace_all
+
+class Normalizer:
+    def __init__(self, *rules):
+        self.rules = rules
+
+    def normalize(self, string):
+        result = string
+        sorted_rules = sorted(self.rules, key=lambda rule: rule.order)
+        for rule in sorted_rules:
+            if rule.each_segment:
+                result_list = []
+                for segment in result.split('/'):
+                    if segment == '':
+                        continue
+                    result_list.append(self.apply_rule(rule, segment))
+                result = '/' + ('/').join(result_list)
+            else:
+                result = self.apply_rule(rule, result)        
+        return result
+
+    def apply_rule(self, rule, string):
+        max = 1
+        if rule.replace_all:
+            max = 0
+        return re.sub(rule.match, rule.replacement, string, max)

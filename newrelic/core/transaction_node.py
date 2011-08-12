cda9bc16b1241b4f3a14c95c1154778b8f37d550
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2011-08-12 13:12:19 +1000

    Remove unwanted documentation.

--- newrelic/core/transaction_node.py
+++ newrelic/core/transaction_node.py
@@ -1,63 +1,6 @@
-"""This module provides class types used in the construction of the raw
-transaction trace hierarchy from which metrics are then to be generated.
-
-The higher level instrumentation layer will construct a tree for each web
-transaction or background task. The root node should be an instance of the
-TransactionNode class. This would be passed to the agent core via the
-record_transaction() method of the Agent class instance which in turn gets
-passed to the process_raw_transaction() function of this module. In
-processing to generic metric data, each node and its children would in turn
-be processed as necessary. The base metric data would then be injected
-into the application object.
-
-For a TransactionNode the 'type' attribute will be either 'WebTransaction'
-or 'OtherTransaction' corresponding to a web transaction or background task.
-
-The 'group' attribute of both TransactionNode and FunctionNode can consist
-of multiple path segments representing a primary category and any sub
-categories to which the transaction is being associated. For example:
-
-    Uri
-    Function
-    Script/Import
-    Script/Execute
-    Template/Compile
-    Template/Render
-
-The 'name' attribute is then the actual name being associated with the web
-transaction, background task or function trace node.
-
-The final composite metric path for a transaction may then be as example:
-
-    WebTransaction/Uri/some/url
-    WebTransaction/Function/module:class.function
-
-    OtherTransaction/Script/Import//absolute/path/to/some/module.py
-    OtherTransaction/Script/Import/relative/path/to/some/module.py
-
-The rule for constructing the metric path for a transaction is:
-
-    if group == 'Uri' and name[:1] == '/':
-        path = '%s/%s%s' % (type, group, name)
-    else:
-        path = '%s/%s/%s' % (type, group, name)
-
-The special case for 'Uri' with a leading slash on name is necessary to
-maintain compatibility with how existing agents and core application appear
-to behave.
-
-The 'type' and 'group' attributes are separate to allow easier construction
-of rollup and apdex metrics based on the categories/sub categories.
-
-For a function trace node the final composite metric path may be:
-
-    Function/module:class.function
-    Template/Render//absolute/path/to/template.py
-    Template/Render/relative/path/to/template.py
-
-The rule for construction the metric path for a function trace node is:
-
-    path = '%s/%s' % (group, name)
+"""This module provides the transaction node type used as root in the
+construction of the raw transaction trace hierarchy from which metrics are
+then to be generated.
 
 """
 

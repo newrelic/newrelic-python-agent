04d390df3f8b030ae6b74929416c44b3df1b9abd
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2011-09-27 20:52:51 +1000

    Avoid additional layer of wrappers and calls in dbapi2 instrumentation.

--- newrelic/hooks/database_dbapi2.py
+++ newrelic/hooks/database_dbapi2.py
@@ -1,3 +1,4 @@
+import newrelic.api.transaction
 import newrelic.api.database_trace
 import newrelic.api.function_trace
 import newrelic.api.external_trace
@@ -5,42 +6,64 @@ import newrelic.api.external_trace
 def instrument(module):
 
     class CursorWrapper(object):
+
         def __init__(self, cursor):
             self.__cursor = cursor
-        def __iter__(self):
-            return iter(self.__cursor)
-        def execute(self, *args, **kwargs):
-            return newrelic.api.database_trace.DatabaseTraceWrapper(
-                    self.__cursor.execute,
-                    (lambda sql, parameters=(): sql),
-                    module)(*args, **kwargs)
-        def executemany(self, *args, **kwargs): 
-            return newrelic.api.database_trace.DatabaseTraceWrapper(
-                    self.__cursor.executemany,
-                    (lambda sql, seq_of_parameters=[]: sql),
-                    module)(*args, **kwargs)
+
         def __getattr__(self, name):
             return getattr(self.__cursor, name)
 
+        def __iter__(self):
+            return iter(self.__cursor)
+
+        def execute(self, sql, parameters=()):
+            transaction = newrelic.api.transaction.transaction()
+            if not transaction:
+                return self.__cursor.execute(sql, parameters)
+            with newrelic.api.database_trace.DatabaseTrace(
+                    transaction, sql, module):
+                return self.__cursor.execute(sql, parameters)
+
+        def executemany(self, sql, seq_of_parameters=[]): 
+            transaction = newrelic.api.transaction.transaction()
+            if not transaction:
+                return self.__cursor.executemany(sql, seq_of_parameters)
+            with newrelic.api.database_trace.DatabaseTrace(
+                    transaction, sql, module):
+                return self.__cursor.executemany(sql, seq_of_parameters)
+
     class ConnectionWrapper(object):
+
         def __init__(self, connection):
             self.__connection = connection
-        def cursor(self, *args, **kwargs):
-            return CursorWrapper(self.__connection.cursor(*args, **kwargs))
-        def commit(self, *args, **kwargs):
-            return newrelic.api.database_trace.DatabaseTraceWrapper(
-                self.__connection.commit, 'COMMIT',
-                module)(*args, **kwargs)
-        def rollback(self, *args, **kwargs):
-            return newrelic.api.database_trace.DatabaseTraceWrapper(
-                self.__connection.rollback, 'ROLLBACK',
-                module)(*args, **kwargs)
+
         def __getattr__(self, name):
             return getattr(self.__connection, name)
 
+        def cursor(self, *args, **kwargs):
+            return CursorWrapper(self.__connection.cursor(*args, **kwargs))
+
+        def commit(self):
+            transaction = newrelic.api.transaction.transaction()
+            if not transaction:
+                return self.__connection.commit()
+            with newrelic.api.database_trace.DatabaseTrace(
+                    transaction, 'COMMIT', module):
+                return self.__connection.commit()
+
+        def rollback(self):
+            transaction = newrelic.api.transaction.transaction()
+            if not transaction:
+                return self.__connection.rollback()
+            with newrelic.api.database_trace.DatabaseTrace(
+                    transaction, 'ROLLBACK', module):
+                return self.__connection.rollback()
+
     class ConnectionFactory(object):
+
         def __init__(self, connect):
             self.__connect = connect
+
         def __call__(self, *args, **kwargs):
             return ConnectionWrapper(self.__connect(*args, **kwargs))
 

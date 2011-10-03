766fee36315e9953a159f52b8b034f192c8225df
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2011-10-03 20:07:13 +1100

    Allow configuration file and environment to be specified via environment variables when using run-program and run-python admin commands.

--- newrelic/bootstrap/sitecustomize.py
+++ newrelic/bootstrap/sitecustomize.py
@@ -1,2 +1,8 @@
+import os
+
 import newrelic.agent
-newrelic.agent.initialize()
+
+config_file = os.environ.get('NEW_RELIC_CONFIG_FILE', None)
+environment = os.environ.get('NEW_RELIC_ENVIRONMENT', None)
+
+newrelic.agent.initialize(config_file, environment)

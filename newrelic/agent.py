fad5bb1e0f093ae7ee12e521be1a48492ff137d2
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2011-10-21 13:57:41 +1100

    Remove backward compatibility code for old way of initialising agent.

--- newrelic/agent.py
+++ newrelic/agent.py
@@ -9,13 +9,3 @@ initialize = newrelic.config.initialize
 
 wsgi_application = newrelic.api.web_transaction.wsgi_application
 background_task = newrelic.api.background_task.background_task
-
-# Setting from environment variables is only provided for backward
-# compatibity with early Python agent BETA versions. This will be
-# removed prior to general release.
-
-_config_file = os.environ.get('NEWRELIC_CONFIG_FILE', None)
-_environment = os.environ.get('NEWRELIC_ENVIRONMENT', None)
-
-if _config_file:
-    initialize(_config_file, _environment)

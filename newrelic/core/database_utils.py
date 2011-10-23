7633437f5b201c3dab6a8c5d2254889a1a426b5b
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2011-10-23 17:49:33 +1100

    Precompile regular expressions used when cleaning up SQL table name.

--- newrelic/core/database_utils.py
+++ newrelic/core/database_utils.py
@@ -112,6 +112,7 @@ def obfuscate_sql(name, sql):
     return obfuscated
 
 class SqlParser:
+
     def __init__(self, sql):
         self.stmt = newrelic.lib.sqlparse.parse(sql)[0]
         self.operation = self.get_operation()
@@ -122,6 +123,7 @@ class SqlParser:
                      newrelic.lib.sqlparse.tokens.Keyword.DDL)
         dml = self.stmt.token_next_by_type(0, 
                      newrelic.lib.sqlparse.tokens.Keyword.DML)
+
         if ddl and not dml:
             return ddl.value.lower()
         elif dml and not ddl:
@@ -138,19 +140,27 @@ class SqlParser:
 
     def get_table(self):
         if self.operation == 'select' or self.operation == 'delete':
-            token = self._find_table_token_for(newrelic.lib.sqlparse.tokens.Keyword, 'FROM')
+            token = self._find_table_token_for(
+                    newrelic.lib.sqlparse.tokens.Keyword, 'FROM')
         elif self.operation == 'update':
-            token = self._find_table_token_for(newrelic.lib.sqlparse.tokens.Keyword.DML, 'UPDATE')
+            token = self._find_table_token_for(
+                    newrelic.lib.sqlparse.tokens.Keyword.DML, 'UPDATE')
         elif self.operation == 'set' or self.operation == 'show':
-            token = self._find_table_token_for(newrelic.lib.sqlparse.tokens.Keyword, self.operation.upper())
+            token = self._find_table_token_for(
+                    newrelic.lib.sqlparse.tokens.Keyword,
+                    self.operation.upper())
         elif self.operation == 'insert':
-            token = self._find_table_token_for(newrelic.lib.sqlparse.tokens.Keyword, 'INTO')
+            token = self._find_table_token_for(
+                    newrelic.lib.sqlparse.tokens.Keyword, 'INTO')
         elif self.operation == 'create':
-            idx = self._find_idx_for(newrelic.lib.sqlparse.tokens.Keyword.DDL, 'CREATE') 
-            token = self.stmt.token_next_by_type(idx + 1, newrelic.lib.sqlparse.tokens.Keyword)
+            idx = self._find_idx_for(
+                    newrelic.lib.sqlparse.tokens.Keyword.DDL, 'CREATE') 
+            token = self.stmt.token_next_by_type(
+                    idx+1, newrelic.lib.sqlparse.tokens.Keyword)
         elif self.operation == 'call':
             self.operation = 'ExecuteProcedure'
-            token = self._find_table_token_for(newrelic.lib.sqlparse.tokens.Keyword, 'CALL')
+            token = self._find_table_token_for(
+                    newrelic.lib.sqlparse.tokens.Keyword, 'CALL')
         else:
             token = None
 
@@ -167,10 +177,15 @@ class SqlParser:
             elif token.__class__.__name__ == 'Function':
                 return token
 
+    table_name_re_1 = re.compile('[",`,\[,\]]*')
+    table_name_re_2 = re.compile('\(.*')
+
     def _format_table_token(self, token):
-        table_name = re.sub('[",`,\[,\]]*', '', token.to_unicode()).lower()
+        #table_name = re.sub('[",`,\[,\]]*', '', token.to_unicode()).lower()
+        table_name = self.table_name_re_1.sub('', token.to_unicode()).lower()
         if token.__class__.__name__ == 'Function':
-            return re.sub('\(.*', '', table_name)
+            #return re.sub('\(.*', '', table_name)
+            return self.table_name_re_2.sub('', table_name)
         return table_name
 
     def _find_idx_for(self, ttype, match):
@@ -188,8 +203,9 @@ def parsed_sql(sql):
     if entry.parsed is not None:
         return entry.parsed
 
-    # FIXME The SqlParser class doesn't cope well with badly
-    # formed input data, so need to catch exceptions here.
+    # The SqlParser class doesn't cope well with badly formed
+    # input data, so need to catch exceptions here. We return
+    # (None, None) to indicate could parse out the details.
 
     try:
         parsed_sql = SqlParser(sql)

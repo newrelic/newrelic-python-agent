506c3757caa362167ad2ae4035dc8641c4d42294
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2011-12-30 11:40:51 +1100

    Bottle 0.10.X has changed how URL routing works and broken instrumentation.  PYTHON-386

--- newrelic/hooks/framework_bottle.py
+++ newrelic/hooks/framework_bottle.py
@@ -1,5 +1,6 @@
 import newrelic.api.web_transaction
 import newrelic.api.out_function
+import newrelic.api.post_function
 import newrelic.api.name_transaction
 import newrelic.api.error_trace
 import newrelic.api.function_trace
@@ -16,23 +17,26 @@ def instrument(module):
                 ignore_errors=['bottle.HTTPResponse', 'bottle.RouteReset'])
         return callback, args
 
-    if version >= [0, 9, 0]:
+    def out_Route_make_callback(callback):
+        callback = newrelic.api.name_transaction.NameTransactionWrapper(
+                callback)
+        callback = newrelic.api.error_trace.ErrorTraceWrapper(callback,
+                ignore_errors=['bottle.HTTPResponse', 'bottle.RouteReset'])
+        return callback
+
+    if version >= [0, 10, 0]:
+        newrelic.api.web_transaction.wrap_wsgi_application(
+                module, 'Bottle.wsgi')
+
+        newrelic.api.out_function.wrap_out_function(
+                module, 'Route._make_callback', out_Route_make_callback)
+
+    elif version >= [0, 9, 0]:
         newrelic.api.web_transaction.wrap_wsgi_application(
                 module, 'Bottle.wsgi')
 
         newrelic.api.out_function.wrap_out_function(
                 module, 'Bottle._match', out_Bottle_match)
-
-        newrelic.api.function_trace.wrap_function_trace(
-                module, 'SimpleTemplate.render')
-        newrelic.api.function_trace.wrap_function_trace(
-                module, 'MakoTemplate.render')
-        newrelic.api.function_trace.wrap_function_trace(
-                module, 'CheetahTemplate.render')
-        newrelic.api.function_trace.wrap_function_trace(
-                module, 'Jinja2Template.render')
-        newrelic.api.function_trace.wrap_function_trace(
-                module, 'SimpleTALTemplate.render')
 
     else:
         newrelic.api.web_transaction.wrap_wsgi_application(
@@ -41,11 +45,22 @@ def instrument(module):
         newrelic.api.out_function.wrap_out_function(
                 module, 'Bottle.match_url', out_Bottle_match)
 
+    if hasattr(module, 'SimpleTemplate'):
         newrelic.api.function_trace.wrap_function_trace(
                 module, 'SimpleTemplate.render')
+
+    if hasattr(module, 'MakoTemplate'):
         newrelic.api.function_trace.wrap_function_trace(
                 module, 'MakoTemplate.render')
+
+    if hasattr(module, 'CheetahTemplate'):
         newrelic.api.function_trace.wrap_function_trace(
                 module, 'CheetahTemplate.render')
+
+    if hasattr(module, 'Jinja2Template'):
         newrelic.api.function_trace.wrap_function_trace(
                 module, 'Jinja2Template.render')
+
+    if hasattr(module, 'SimpleTALTemplate'):
+        newrelic.api.function_trace.wrap_function_trace(
+                module, 'SimpleTALTemplate.render')

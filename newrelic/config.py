18b49cb047afa6f75524f27b2a111fdd3ac7eb39
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2012-06-25 15:27:19 +1000

    Drop redundant configuration entries after dropping sqlparse. PYTHON-506

--- newrelic/config.py
+++ newrelic/config.py
@@ -277,8 +277,6 @@ def _process_configuration(section):
                      'getint', None)
     _process_setting(section, 'agent_limits.merge_stats_maximum',
                      'getint', None)
-    _process_setting(section, 'agent_limits.sql_cache_buckets',
-                     'getint', None)
     _process_setting(section, 'console.listener_socket',
                      'get', _map_console_listener_socket)
     _process_setting(section, 'console.allow_interpreter_cmd',

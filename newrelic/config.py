34a31b1ae28b7c4548beb6a1e96672f47adaef2b
Amjith Ramanujam <amjith@newrelic.com>
2012-07-12 13:29:58 -0700

    Adding new aget_limits to the list of params that can be configured via ini file. PYTHON-310, PYTHON-519.

--- newrelic/config.py
+++ newrelic/config.py
@@ -275,6 +275,12 @@ def _process_configuration(section):
                      'getint', None)
     _process_setting(section, 'agent_limits.merge_stats_maximum',
                      'getint', None)
+    _process_setting(section, 'agent_limits.saved_transactions',
+                     'getint', None)
+    _process_setting(section, 'agent_limits.errors_per_transaction',
+                     'getint', None)
+    _process_setting(section, 'agent_limits.errors_per_harvest',
+                     'getint', None)
     _process_setting(section, 'console.listener_socket',
                      'get', _map_console_listener_socket)
     _process_setting(section, 'console.allow_interpreter_cmd',

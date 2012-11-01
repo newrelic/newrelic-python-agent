c709708481830e4f41c9e0e5d0fdf3ee63a96b5a
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2012-11-01 12:35:34 +1100

    Update test of thread profiling session duration. PYTHON-590

--- newrelic/core/tests/test_thread_profiler.py
+++ newrelic/core/tests/test_thread_profiler.py
@@ -8,19 +8,19 @@ class TestThreadProfiler(unittest.TestCase):
     def setUp(self):
         self.profile_id = 42
         self.sample = 0.1
-        self.duration = 0.2
+        self.duration = 2.0
         self.profile_agent_code = True
         self.tp = ThreadProfiler(self.profile_id, self.sample, self.duration,
                 self.profile_agent_code)
         self.tp.start_profiling()
         import time
-        time.sleep(0.3)
+        time.sleep(self.duration+self.sample)
         self.pd = self.tp.profile_data()
 
     def test_profiler(self):
         p = self.pd[0]
         self.assertEqual(p[0], self.profile_id)
-        self.assertAlmostEqual((p[2] - p[1])/1000, self.duration, 1) 
+        self.assertTrue(abs(self.duration-((p[2]-p[1])/1000)) < self.sample)
         self.assertEqual(p[3] , self.duration/self.sample)
 
     #def test_profile_data(self):

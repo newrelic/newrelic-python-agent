a2a9ecde43fb4c5b680daa699ca55d5ca1cbb6af
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2012-11-02 22:39:17 +1100

    Wasn't handling file objects being passed to feedparser module. PYTHON-627

--- newrelic/hooks/external_feedparser.py
+++ newrelic/hooks/external_feedparser.py
@@ -6,21 +6,37 @@ import newrelic.api.object_wrapper
 import newrelic.api.external_trace
 
 class capture_external_trace(object):
+
     def __init__(self, wrapped):
         newrelic.api.object_wrapper.update_wrapper(self, wrapped)
         self._nr_next_object = wrapped
         if not hasattr(self, '_nr_last_object'):
             self._nr_last_object = wrapped
+
     def __call__(self, url, *args, **kwargs):
-        if url.startswith('feed:http'):
-            url = url[5:]
-        elif url.startswith('feed:'):
-            url = 'http:' + url[5:]
-        if url.split(':')[0].lower() in ['http', 'https', 'ftp']:
+
+        # The URL be a string or a file like object. Pass call
+        # through if not a string.
+
+        if not isinstance(url, basestring):
+            return self._nr_next_object(url, *args, **kwargs)
+
+        # Only then wrap the call if it looks like a URL. To
+        # work that out need to first do some conversions of
+        # accepted 'feed' formats to proper URL format.
+
+        parsed_url = url
+
+        if parsed_url.startswith('feed:http'):
+            parsed_url = parsed_url[5:]
+        elif parsed_url.startswith('feed:'):
+            parsed_url = 'http:' + url[5:]
+
+        if parsed_url.split(':')[0].lower() in ['http', 'https', 'ftp']:
             current_transaction = newrelic.api.transaction.current_transaction()
             if current_transaction:
                 trace = newrelic.api.external_trace.ExternalTrace(
-                        current_transaction, 'feedparser', url, 'GET')
+                        current_transaction, 'feedparser', parsed_url, 'GET')
                 context_manager = trace.__enter__()
                 try:
                     result = self._nr_next_object(url, *args, **kwargs)
@@ -33,6 +49,7 @@ class capture_external_trace(object):
                 return self._nr_next_object(url, *args, **kwargs)
         else:
             return self._nr_next_object(url, *args, **kwargs)
+
     def __getattr__(self, name):
        return getattr(self._nr_next_object, name)
 

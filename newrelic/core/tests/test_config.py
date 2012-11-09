364f93df60ff0658a3ed9db89ef4ff4d32ccfbbd
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2012-11-09 20:54:35 +1100

    Fix up tests for changes to apdex handling in settings. PYTHON-630

--- newrelic/core/tests/test_config.py
+++ newrelic/core/tests/test_config.py
@@ -11,26 +11,13 @@ class TestSettings(unittest.TestCase):
         self.assertEqual(2, c.a2.b2)
         self.assertEqual(3, c.a3.b3.c3)
 
-class TestAgentConfig(unittest.TestCase):
-
-    def test_defaults(self):
-        c = newrelic.core.config.create_settings_snapshot()
-        self.assertEqual(0.5, c.apdex_t)
-        self.assertEqual(2.0, c.apdex_f)
-
-    def test_apdex_t(self):
-        d = { "apdex_t": 0.666 }
-        c = newrelic.core.config.create_settings_snapshot(d)
-        self.assertEqual(0.666, c.apdex_t)
-        self.assertEqual(0.666*4, c.apdex_f)
-
 class TestTransactionTracerConfig(unittest.TestCase):
 
     def test_defaults(self):
         c = newrelic.core.config.create_settings_snapshot()
         tt = c.transaction_tracer
         self.assertTrue(tt.enabled)
-        self.assertEqual(2, tt.transaction_threshold)
+        self.assertEqual(None, tt.transaction_threshold)
 
     def test_enabled(self):
         d = { "transaction_tracer.enabled": False }

0b9a35f8b0b139df91af796969acfcaa8e6fefa5
Amjith Ramanujam <amjith@newrelic.com>
2012-12-28 11:16:59 -0800

    Fix tests for build_cross_process_header(). PYTHON-656

--- newrelic/api/tests/test_cross_tier.py
+++ newrelic/api/tests/test_cross_tier.py
@@ -1,9 +1,8 @@
 import unittest
+
 import newrelic.api.settings
 import newrelic.api.application
-#import newrelic.tests.test_cases
-
-import newrelic.agent
+from newrelic.api.web_transaction import WebTransaction, WSGIApplicationWrapper
 
 settings = newrelic.api.settings.settings()
 application = newrelic.api.application.application_instance()
@@ -13,14 +12,10 @@ class TestCrossProcess(unittest.TestCase):
     Test the cross tier feature.
     """
 
-    def setUp(self):
-        settings.cross_process_id = "305249#35857"
-
     def test_no_incoming_cross_pid(self):
         # Make request without HTTP_X_NEWRELIC_ID.
         # Make sure X-NewRelic-App-Data is absent in response.
-        settings.cross_process.enabled = True
-        print "Running QXY"
+        pass
 
     def test_cross_pid(self):
         # Make request with HTTP_X_NEWRELIC_ID.
@@ -28,21 +23,49 @@ class TestCrossProcess(unittest.TestCase):
         # Make sure the metric was created.
         pass
 
-    def test_response_header_content(self):
-        # Unpack the X-NewRelic-App-Data and check the contents of json.
-        environ = { "HTTP_X_NEWRELIC_ID": "VwYCU1JaGwAFXFRW",
-                "CONTENT_LENGTH":"5"}
-        transaction = newrelic.api.web_transaction.WebTransaction(application,
-                environ)
-        with transaction:
-            print newrelic.api.web_transaction.build_nr_response_header(transaction, environ)
-            
-
     def test_blank_incoming_cross_pid(self):
         # Make request with HTTP_X_NEWRELIC_ID but leave the value blank.
         # Check to make sure response header is still present.
         # Check to make sure the metric wasn't added.
         pass
+
+class TestResponseHeaderJSON(unittest.TestCase):
+    """Tests the build_cross_process_header() function."""
+
+    def test_response_header_content_with_content_length(self):
+        # Unpack the X-NewRelic-App-Data and check the contents of json.
+
+        environ = {"CONTENT_LENGTH":"5", "REQUEST_URI": "/web_transaction" }
+
+        expected_header = u'["305249#35857", "WebTransaction/Uri/<undefined>"'\
+                ', 0.000000, 0.010000, 5]'
+        response_time = 0.01
+
+        transaction = WebTransaction(application, environ)
+        transaction._settings = newrelic.core.config.create_settings_snapshot(
+                {"cross_process_id":"305249#35857"})
+        transaction.enabled = True
+        with transaction:
+            header = WSGIApplicationWrapper.build_cross_process_header(
+                    transaction, environ, response_time)
+        self.assertEqual(header, expected_header)
+
+    def test_response_header_content_without_content_length(self):
+        # Unpack the X-NewRelic-App-Data and check the contents of json.
+
+        environ = { "REQUEST_URI": "/web_transaction" }
+
+        expected_header = u'["305249#35857", "WebTransaction/Uri/<undefined>", 0.000000, 0.010000, -1]'
+        response_time = 0.01
+
+        transaction = WebTransaction(application, environ)
+        transaction._settings = newrelic.core.config.create_settings_snapshot(
+                {"cross_process_id":"305249#35857"})
+        transaction.enabled = True
+        with transaction:
+            header = WSGIApplicationWrapper.build_cross_process_header(
+                    transaction, environ, response_time)
+        self.assertEqual(header, expected_header)
 
 
 if __name__ == '__main__':

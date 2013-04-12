2cf5be6288eb11e9553bb231bbc9631a861b4c97
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2013-04-12 12:01:55 +1000

    Add adapter for wrapping WSGI application when using wsgiref. PYTHON-869

--- newrelic/config.py
+++ newrelic/config.py
@@ -1486,6 +1486,10 @@ def _process_module_builtin_defaults():
             'newrelic.hooks.adapter_waitress',
             'instrument_waitress_server')
 
+    _process_module_definition('wsgiref.simple_server',
+            'newrelic.hooks.adapter_wsgiref',
+            'instrument_wsgiref_simple_server')
+
     _process_module_definition('cherrypy.wsgiserver.wsgiserver2',
             'newrelic.hooks.adapter_cherrypy',
             'instrument_cherrypy_wsgiserver_wsgiserver2')

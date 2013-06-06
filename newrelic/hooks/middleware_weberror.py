ee3d3b1ae27b3082d18a5e4e4f8f670ceaf64b1f
Amjith Ramanujam <amjith@newrelic.com>
2013-06-05 22:19:02 -0700

    Change weberror instrumentation to use newrelic.agent instead of reaching into newrelic.api PYTHON-971

--- newrelic/hooks/middleware_weberror.py
+++ newrelic/hooks/middleware_weberror.py
@@ -1,5 +1,5 @@
-from newrelic.api.function_trace import wrap_function_trace
-from newrelic.api.external_trace import wrap_external_trace
+from newrelic.agent import wrap_function_trace
+from newrelic.agent import wrap_external_trace
 
 def instrument_weberror_errormiddleware(module):
 
@@ -7,8 +7,8 @@ def instrument_weberror_errormiddleware(module):
 
 def instrument_weberror_reporter(module):
 
-    def smtp_url(obj, *args, **kwargs):
-        return 'smtp://' + obj.smtp_server
+    def smtp_url(reporter, *args, **kwargs):
+        return 'smtp://' + reporter.smtp_server
 
     wrap_external_trace(module, 'EmailReporter.report', 'weberror', smtp_url)
     wrap_function_trace(module, 'EmailReporter.report')

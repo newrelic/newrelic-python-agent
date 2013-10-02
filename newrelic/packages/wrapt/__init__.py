98cb2f717fff90d42f73b61bdd657f4a09bb37b2
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2013-10-02 14:11:45 +1000

    Update bundled wrapt version for Object.__getattr__() issue in C implementation. PYTHON-1057

--- newrelic/packages/wrapt/__init__.py
+++ newrelic/packages/wrapt/__init__.py
@@ -1,5 +1,6 @@
-__version_info__ = ('1', '1', '4')
+__version_info__ = ('1', '2', '0')
 __version__ = '.'.join(__version_info__)
 
 from .wrappers import ObjectProxy, FunctionWrapper, WeakFunctionProxy
 from .decorators import decorator, synchronized
+from .importer import register_post_import_hook, when_imported

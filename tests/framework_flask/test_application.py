3d62edcbb7928ef6e84f2a4051a747a47f38baf5
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2013-12-18 17:22:24 +1100

    Move Flask test applications into separate modules so not nested within function. PYTHON-1156

--- tests/framework_flask/test_application.py
+++ tests/framework_flask/test_application.py
@@ -1,45 +1,14 @@
-import webtest
-
-from flask import Flask, render_template_string, render_template, abort
-
-_test_application = None
-
 def test_application():
     # We need to delay Flask application creation because of ordering
     # issues whereby the agent needs to be initialised before Flask is
     # imported and the routes configured. Normally pytest only runs the
     # global fixture which will initialise the agent after each test
-    # file is imported, which is too late.
+    # file is imported, which is too late. We also can't do application
+    # creation within a function as we will then get view handler
+    # functions are different between Python 2 and 3, with the latter
+    # showing <local> scope in path.
 
-    global _test_application
-
-    if _test_application:
-        return _test_application
-
-    application = Flask(__name__)
-
-    @application.route('/index')
-    def index_page():
-        return 'INDEX RESPONSE'
-
-    @application.route('/error')
-    def error_page():
-        raise RuntimeError('RUNTIME ERROR')
-
-    @application.route('/abort_404')
-    def abort_404_page():
-        abort(404)
-
-    @application.route('/template_string')
-    def template_string():
-        return render_template_string('<body><p>INDEX RESPONSE</p></body>')
-
-    @application.route('/template_not_found')
-    def template_not_found():
-        return render_template('not_found')
-
-    _test_application = webtest.TestApp(application)
-
+    from _test_application import _test_application
     return _test_application
 
 def test_application_index():
--- tests/framework_flask/test_user_exceptions.py
+++ tests/framework_flask/test_user_exceptions.py
@@ -1,7 +1,4 @@
 import pytest
-import webtest
-
-from flask import Flask, redirect
 
 try:
     # The __version__ attribute was only added in 0.7.0.
@@ -13,37 +10,17 @@ except ImportError:
 requires_error_handler = pytest.mark.skipif(not is_gt_flask060,
         reason="The error handler decorator is not supported.")
 
-class UserException(Exception):
-    pass
-
-_test_application = None
-
 def test_application():
     # We need to delay Flask application creation because of ordering
     # issues whereby the agent needs to be initialised before Flask is
     # imported and the routes configured. Normally pytest only runs the
     # global fixture which will initialise the agent after each test
-    # file is imported, which is too late.
+    # file is imported, which is too late. We also can't do application
+    # creation within a function as we will then get view handler
+    # functions are different between Python 2 and 3, with the latter
+    # showing <local> scope in path.
 
-    global _test_application
-
-    if _test_application:
-        return _test_application
-
-    application = Flask(__name__)
-
-    application = Flask(__name__)
-
-    @application.errorhandler(UserException)
-    def page_not_found(error):
-        return 'USER EXCEPTION', 500
-
-    @application.route('/user_exception')
-    def error_page():
-        raise UserException('User exception.')
-
-    _test_application = webtest.TestApp(application)
-
+    from _test_user_exceptions import _test_application
     return _test_application
 
 @requires_error_handler

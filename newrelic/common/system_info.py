02b45c55ad42ae45add3ee23405b53f5e7a084b3
Amjith Ramanujam <amjith@newrelic.com>
2013-12-27 15:43:56 -0800

    Add cpuinfo and meminfo parser.

--- newrelic/common/system_info.py
+++ newrelic/common/system_info.py
@@ -1,195 +1,134 @@
-"""This module implements functions for querying properties of the operating
-system or for the specific process the code is running in.
-
-"""
+# Requirements: Capture the number of physical cores, number of logical
+# processors and total physical memory. When a value can't be reliably obtained
+# return 'null'
+#
+# Cores:
+#        Processor               Physical    Logical
+# 1 processor 1 core No HT          1           1
+# 1 processor 2 core No HT          2           2
+# 2 processor 2 core No HT          4           8
+# 1 processor 1 core with HT        1           2
+# 1 processor 2 core with HT        2           4
+# 2 processor 2 core with HT        4           8
+#
+# Linux - Parse a file
+# CPU : Parse /proc/cpuinfo
+# MEM : Parse /proc/meminfo
+#
+# OS X - Run a shell command
+# CPU : /usr/sbin/sysctl -n hw.physicalcpu
+# MEM : /usr/sbin/sysctl -n hw.memsize
 
 import os
-import sys
+from subprocess import Popen, PIPE
 import multiprocessing
-import re
 
-try:
-    import resource
-except ImportError:
-    pass
+LINUX_CPU = '/proc/cpuinfo'
+LINUX_MEM = '/proc/meminfo'
+
+DARWIN_CPU = '/usr/sbin/sysctl'
+DARWIN_MEM = '/usr/sbin/sysctl'
+
+def _extract_shell_results(*command):
+    """
+    Run a shell command and cast the output to an integer. Returns None on
+    failure.
+    """
+    result = None
+    output, err = Popen(command, stdout=PIPE, stderr=PIPE).communicate()
+    if not err:
+        try:
+            result = int(output)
+        except ValueError:
+            pass
+    return result
+
+class CpuInfo(object):
+    """
+    Handles the parsing of a cpuinfo file to obtain the number of physical
+    cores.
+    """
+    def __init__(self, filename=None):
+        self.filename = filename or '/proc/cpuinfo'
+        self.processors = 0
+        self.physical_cores = {}
+        self._reset()
+        self._parse_file()
+
+    def _reset(self):
+        self.physical_id = None
+        self.cores = None
+
+    def _parse_file(self):
+        # A line that starts with 'processor' marks the beginning of a
+        # section.
+        #
+        # Multi-core processors will have a 'processor' section for each core.
+        # Thereis usually a 'physical id' field and a 'cpu cores' field as
+        # well.  The 'physical id' field in each 'processor' section will have
+        # the same value for all cores. The 'cpu cores' field for each
+        # 'processor' section will provide the total number of cores for that
+        # physical processor.
+
+        try:
+            for original_line in open(self.filename):
+                line = original_line.lower()
+                if line.startswith('processor'):
+                    self.processors += 1
+                    if (self.cores and self.physical_id):
+                        self.physical_cores[self.physical_id] = self.cores
+                        self._reset()
+                elif line.startswith('physical id'):
+                    self.physical_id = line.split(':')[1]
+                elif line.startswith('cpu cores'):
+                    self.cores = int(line.split(':')[1])
+        except:
+            pass
+
+    def number_of_cores(self):
+        return sum(self.physical_cores.values()) or self.processors or None
+
+def _parse_meminfo(meminfo):
+    # Parse /proc/meminfo for 'memtotal'. The value is 'meminfo' file is in
+    # MB. Return the result in MB.
+
+    normalizer = {'kb': 1024, 'mb': 1}
+    try:
+        for line in open(meminfo):
+            if line.lower().startswith('memtotal'):
+                mem = line.split(":")[1]
+                value, units = mem.split()
+                return int(value)/normalizer[units.lower()]
+    except:
+        return None
 
 def cpu_count():
-    """Returns the number of CPUs in the system.
-
     """
-
-    # The multiprocessing module provides support for Windows,
-    # BSD systems (including MacOS X) and systems which support
-    # the POSIX API for querying the number of CPUs.
-
-    try:
-        return multiprocessing.cpu_count()
-    except NotImplementedError:
-        pass
-
-    # For Jython, we need to query the Java runtime environment.
-
-    try:
-        from java.lang import Runtime
-        runtime = Runtime.getRuntime()
-        res = runtime.availableProcessors()
-        if res > 0:
-            return res
-    except ImportError:
-        pass
-
-    # Assuming that Solaris will support POSIX API for querying
-    # the number of CPUs. Just in case though, work it out by
-    # looking at the devices corresponding to the available CPUs.
-
-    try:
-        pseudoDevices = os.listdir('/devices/pseudo/')
-        expr = re.compile('^cpuid@[0-9]+$')
-
-        res = 0
-        for pd in pseudoDevices:
-            if expr.match(pd) != None:
-                res += 1
-
-        if res > 0:
-            return res
-    except OSError:
-        pass
-
-    # Fallback to assuming only a single CPU.
-
-    return 1
-
-def memory_total():
-    """Returns the total physical memory available in the system.
-
+    Return a tuple (physical_cores, logical_cores)
     """
+    if os.path.isfile(LINUX_CPU):
+        cpu_info = CpuInfo(LINUX_CPU)
+        physical_cores = cpu_info.number_of_cores()
+    elif os.path.isfile(DARWIN_CPU):
+        command = (DARWIN_CPU, "-n", "hw.physicalcpu")
+        physical_cores = _extract_shell_results(*command)
 
-    # For Linux we can determine it from the proc filesystem.
+    logical_cores = multiprocessing.cpu_count()
 
-    if sys.platform == 'linux2':
+    return (physical_cores, logical_cores)
+
+def total_ram():
+    """
+    Return the size of RAM in MB.
+    """
+    ram = None
+    if os.path.isfile(DARWIN_MEM):
+        command = (DARWIN_CPU, "-n", "hw.memsize")
         try:
-            parser = re.compile(r'^(?P<key>\S*):\s*(?P<value>\d*)\s*kB')
-
-            fp = None
-
-            try:
-                fp = open('/proc/meminfo')
-
-                for line in fp.readlines():
-                    match = parser.match(line)
-                    if not match:
-                        continue
-                    key, value = match.groups(['key', 'value'])
-                    if key == 'MemTotal':
-                        memory_bytes = float(value) * 1024
-                        return memory_bytes / (1024*1024)
-
-            except Exception:
-                pass
-
-            finally:
-                if fp:
-                    fp.close()
-
-        except IOError:
+            ram = _extract_shell_results(*command)/(1024 * 1024)
+        except TypeError:
             pass
+    elif os.path.isfile(LINUX_MEM):
+        ram = _parse_meminfo(LINUX_MEM)
 
-    # For other platforms, how total system memory is calculated
-    # varies and can't always be done using just what is
-    # available in the Python standard library. Take a punt here
-    # and see if 'psutil' is available and use the value it
-    # generates for total memory.
-
-    try:
-        import psutil
-        return psutil.virtual_memory().total
-    except (ImportError, AttributeError):
-        pass
-
-    return 0
-
-def memory_used():
-    """Returns the memory used in MBs. Calculated differently depending
-    on the platform and designed for informational purposes only.
-
-    """
-
-    # For Linux use the proc filesystem. Use 'statm' as easier
-    # to parse than 'status' file.
-    #
-    #   /proc/[number]/statm
-    #          Provides information about memory usage, measured
-    #          in pages. The columns are:
-    #
-    #              size       total program size
-    #                         (same as VmSize in /proc/[number]/status)
-    #              resident   resident set size
-    #                         (same as VmRSS in /proc/[number]/status)
-    #              share      shared pages (from shared mappings)
-    #              text       text (code)
-    #              lib        library (unused in Linux 2.6)
-    #              data       data + stack
-    #              dt         dirty pages (unused in Linux 2.6)
-
-    if sys.platform == 'linux2':
-        pid = os.getpid()
-        statm = '/proc/%d/statm' % pid
-        fp = None
-
-        try:
-            fp = open(statm, 'r')
-            rss_pages = float(fp.read().split()[1])
-            memory_bytes = rss_pages * resource.getpagesize()
-            return memory_bytes / (1024*1024)
-        except Exception:
-            pass
-        finally:
-            if fp:
-                fp.close()
-
-    # Try using getrusage() if we have the resource module
-    # available. The units returned can differ based on
-    # platform. Assume 1024 byte blocks as default. Some
-    # platforms such as Solaris will report zero for
-    # 'ru_maxrss', so we skip those.
-
-
-    try:
-        rusage = resource.getrusage(resource.RUSAGE_SELF)
-    except NameError:
-        pass
-    else:
-        if sys.platform == 'darwin':
-            # On MacOS X, despite the manual page saying the
-            # value is in kilobytes, it is actually in bytes.
-
-            memory_bytes = float(rusage.ru_maxrss)
-            return memory_bytes / (1024*1024)
-
-        elif rusage.ru_maxrss > 0:
-            memory_kbytes = float(rusage.ru_maxrss)
-            return memory_kbytes / 1024
-
-    # For other platforms, how used memory is calculated varies
-    # and can't always be done using just what is available in
-    # the Python standard library. Take a punt here and see if
-    # 'psutil' is available and use the value it generates for
-    # total memory.
-
-    # NOTE This is currently not used as this isn't generally
-    # going to return resident memory usage (RSS) and what it
-    # does return can be very much greater than what one would
-    # expect. As a result it can be misleading/confusing and so
-    # figure it is best not to use it.
-
-    #try:
-    #    import psutil
-    #    memory_bytes = psutil.virtual_memory().used
-    #    return memory_bytes / (1024*1024)
-    #except ImportError, AttributeError:
-    #    pass
-
-    # Fallback to indicating no memory usage.
-
-    return 0
+    return ram
--- newrelic/common/tests/test_system_info.py
+++ newrelic/common/tests/test_system_info.py
@@ -1,22 +1,21 @@
-import unittest
+from ..system_info import CpuInfo, _parse_meminfo
 
-from newrelic.common.system_info import cpu_count, memory_total, memory_used
+def test_linux_cpu():
+    assert 1 == CpuInfo('cpuinfo_samples/1proc1coreNoHt.txt').number_of_cores()
+    assert 1 == CpuInfo('cpuinfo_samples/1proc1coreNoHtCentos.txt').number_of_cores()
+    assert 1 == CpuInfo('cpuinfo_samples/1proc1coreNoHtAws.txt').number_of_cores()
+    assert 1 == CpuInfo('cpuinfo_samples/1proc1coreWithHt.txt').number_of_cores()
+    assert 2 == CpuInfo('cpuinfo_samples/1proc2coreNoHt.txt').number_of_cores()
+    assert 4 == CpuInfo('cpuinfo_samples/1proc4coreNoHt.txt').number_of_cores()
+    assert 2 == CpuInfo('cpuinfo_samples/2proc1coreWithHt.txt').number_of_cores()
+    assert 4 == CpuInfo('cpuinfo_samples/2proc2coreNoHt.txt').number_of_cores()
+    assert 8 == CpuInfo('cpuinfo_samples/2proc4coreNoHt.txt').number_of_cores()
+    assert 1 == CpuInfo('cpuinfo_samples/1proc1coreNoHtARMv6.txt').number_of_cores()
+    assert 4 == CpuInfo('cpuinfo_samples/UnknownProc4coreUnknownHtVmwareCentOS.txt').number_of_cores()
+    assert None == CpuInfo('cpuinfo_samples/non-existent.txt').number_of_cores()
+    assert None == CpuInfo('cpuinfo_samples/malformed-file.txt').number_of_cores()
 
-class TestCpuCount(unittest.TestCase):
-
-    def test_cpu_count(self):
-        self.assertTrue(cpu_count() >= 1)
-
-class TestSystemMemory(unittest.TestCase):
-
-    def test_memory_total(self):
-        self.assertTrue(memory_total() >= 0)
-
-    def test_memory_used(self):
-        self.assertTrue(memory_used() >= 0)
-
-        if memory_total() > 0:
-            self.assertTrue(memory_used() <= memory_total())
-
-if __name__ == '__main__':
-    unittest.main()
+def test_linux_ram():
+    assert 2012 == _parse_meminfo('cpuinfo_samples/meminfo.txt')
+    assert None == _parse_meminfo('cpuinfo_samples/non-exitent.txt')
+    assert None == _parse_meminfo('cpuinfo_samples/malformed-file.txt')

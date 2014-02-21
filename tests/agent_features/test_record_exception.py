5c3a3e826a3950be5cb268a04d056284187e2e6e
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2014-02-21 16:56:27 +1100

    Separate tests for multiple of type versus distinct type for record_exception(). CORE-12868

--- tests/agent_features/test_record_exception.py
+++ tests/agent_features/test_record_exception.py
@@ -5,6 +5,7 @@ from testing_support.fixtures import validate_transaction_errors
 from newrelic.agent import background_task, record_exception
 
 _runtime_error_name = (RuntimeError.__module__ + ':' + RuntimeError.__name__)
+_type_error_name = (TypeError.__module__ + ':' + TypeError.__name__)
 
 _test_record_exception_sys_exc_info = [
         (_runtime_error_name, 'one')]
@@ -28,13 +29,30 @@ def test_record_exception_no_exc_info():
     except RuntimeError: 
       record_exception()
 
-_test_record_exception_multiple_errors = [
+_test_record_exception_multiple_different_type = [
+        (_runtime_error_name, 'one'),
+        (_type_error_name, 'two')]
+
+@validate_transaction_errors(errors=_test_record_exception_multiple_different_type)
+@background_task()
+def test_record_exception_multiple_different_type():
+    try: 
+      raise RuntimeError('one') 
+    except RuntimeError: 
+      record_exception()
+
+    try: 
+      raise TypeError('two') 
+    except TypeError: 
+      record_exception()
+
+_test_record_exception_multiple_same_type = [
         (_runtime_error_name, 'one'),
         (_runtime_error_name, 'two')]
 
-@validate_transaction_errors(errors=_test_record_exception_multiple_errors)
+@validate_transaction_errors(errors=_test_record_exception_multiple_same_type)
 @background_task()
-def test_record_exception_multiple():
+def test_record_exception_multiple_same_type():
     try: 
       raise RuntimeError('one') 
     except RuntimeError: 

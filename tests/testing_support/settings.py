299047d2dd75d7704ea11e489aa556fdf397c3bf
Tom Offermann <toffermann@newrelic.com>
2014-03-28 10:44:53 -0700

    Add function to set mysql env vars. PYTHON-1227
    mysql_settings() is a new function that works in the same way as the
    existing postgresql_settings() function. It's purpose is to centralize
    the database configuration in one place for all of the mysql tests.
    
    It will return the correct configuration depending on which environment
    it is running in: Local, Tddium, or in the test docker container.

--- tests/testing_support/settings.py
+++ tests/testing_support/settings.py
@@ -38,3 +38,36 @@ def postgresql_settings():
 	settings['port'] = int(_e('POSTGRESQL-9.1_PORT_5432_TCP_PORT', settings['port']))
 
 	return settings
+
+
+def mysql_settings():
+	"""
+	Return a dict of settings for connecting to mysql.
+
+	Will return the correct settings, depending on which of the
+	three environments it is running in. It attempts to set variables
+	in the following order, where later environments override earlier
+	ones.
+
+		1. Local
+		2. Tddium
+		3. Test Docker container
+
+	"""
+	settings = {}
+
+	# Use local defaults, if TDDIUM vars aren't present.
+	settings['name'] = _e('TDDIUM_DB_MYSQL_NAME', USER)
+	settings['user'] = _e('TDDIUM_DB_MYSQL_USER', USER)
+	settings['password'] = _e('TDDIUM_DB_MYSQL_PASSWORD', '')
+	settings['host'] = _e('TDDIUM_DB_MYSQL_HOST', 'localhost')
+	settings['port'] = int(_e('TDDIUM_DB_MYSQL_PORT', '5432'))
+
+	# Look for env vars in test docker container.
+	settings['name'] = _e('PACKNSEND_DB_USER', settings['name'])
+	settings['user'] = _e('PACKNSEND_DB_USER', settings['user'])
+	settings['password'] = _e('PACKNSEND_DB_USER', settings['password'])
+	settings['host'] = _e('MYSQL-5.5_PORT_3306_TCP_ADDR', settings['host'])
+	settings['port'] = int(_e('MYSQL-5.5_PORT_3306_TCP_PORT', settings['port']))
+
+	return settings

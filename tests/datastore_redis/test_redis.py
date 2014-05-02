77ff58e5a260a42b76c05dfca22b9f718b26eabe
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2014-05-02 16:02:04 +1000

    Wrong environment variable name for redis host on tddium. PYTHON-1285

--- tests/datastore_redis/test_redis.py
+++ tests/datastore_redis/test_redis.py
@@ -7,7 +7,7 @@ from testing_support.fixtures import validate_transaction_metrics
 
 from newrelic.agent import background_task
 
-REDIS_HOST = os.environ.get('TDIUM_REDIS_PORT', 'localhost')
+REDIS_HOST = os.environ.get('TDIUM_REDIS_HOST', 'localhost')
 REDIS_PORT = int(os.environ.get('TDIUM_REDIS_PORT', '6379'))
 
 REDIS_HOST = os.environ.get('REDIS_PORT_6379_TCP_ADDR', REDIS_HOST)

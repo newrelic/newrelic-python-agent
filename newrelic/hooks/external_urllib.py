c6682f92dab8c911170e703a7acce80b124489bb
Tom Offermann <toffermann@newrelic.com>
2014-06-20 15:22:32 -0700

    Reduce repetitiveness in external_urllib.py. PYTHON-1220
    Rather than having 3 separate wrapper functions that vary only in
    the bind_params function, the function trace name, and the external
    trace name, I added a _nr_wrapper_factory function to create the
    wrapper functions.

--- newrelic/hooks/external_urllib.py
+++ newrelic/hooks/external_urllib.py
@@ -8,81 +8,67 @@ import newrelic.packages.six as six
 from newrelic.agent import (current_transaction,
     wrap_function_wrapper, ExternalTrace, FunctionTrace)
 
-def _nr_wrapper_urlretrieve_(wrapped, instance, args, kwargs):
-    transaction = current_transaction()
+def _nr_wrapper_factory(bind_params_fn, function_name, external_name):
+    # Wrapper functions will be similar for monkeypatching the different
+    # urllib functions and methods, so a factory function to create them is
+    # used to reduce repetitiveness.
 
-    if transaction is None:
-        return wrapped(*args, **kwargs)
+    # Parameters:
+    #
+    # bind_params_fn: Function that returns the URL.
+    # function_name: String. The name to be used by FunctionTrace.
+    # external_name: String. The name to be used by ExternalTrace.
 
-    def _bind_params(url, *args, **kwargs):
-        return url
+    def _nr_wrapper(wrapped, instance, args, kwargs):
+        transaction = current_transaction()
 
-    url = _bind_params(*args, **kwargs)
-
-    details = urlparse.urlparse(url)
-
-    if details.hostname is None:
-        with FunctionTrace(transaction, 'urllib:urlretrieve'):
+        if transaction is None:
             return wrapped(*args, **kwargs)
 
-    with ExternalTrace(transaction, 'urllib', url):
-        return wrapped(*args, **kwargs)
+        url = bind_params_fn(*args, **kwargs)
 
-def _nr_wrapper_url_opener_open_(wrapped, instance, args, kwargs):
-    transaction = current_transaction()
+        details = urlparse.urlparse(url)
 
-    if transaction is None:
-        return wrapped(*args, **kwargs)
+        if details.hostname is None:
+            with FunctionTrace(transaction, function_name):
+                return wrapped(*args, **kwargs)
 
-    def _bind_params(fullurl, *args, **kwargs):
-        if isinstance(fullurl, six.string_types):
-            return fullurl
-        else:
-            return fullurl.get_full_url()
-
-    url = _bind_params(*args, **kwargs)
-
-    details = urlparse.urlparse(url)
-
-    if details.hostname is None:
-        with FunctionTrace(transaction, 'urllib:URLopener.open'):
+        with ExternalTrace(transaction, external_name, url):
             return wrapped(*args, **kwargs)
 
-    with ExternalTrace(transaction, 'urllib', url):
-        return wrapped(*args, **kwargs)
+    return _nr_wrapper
 
-def _nr_wrapper_opener_director_open_(wrapped, instance, args, kwargs):
-    transaction = current_transaction()
+def bind_params_urlretrieve(url, *args, **kwargs):
+    return url
 
-    if transaction is None:
-        return wrapped(*args, **kwargs)
+def bind_params_open(fullurl, *args, **kwargs):
 
-    def _bind_params(fullurl, *args, **kwargs):
-        if isinstance(fullurl, six.string_types):
-            return fullurl
-        else:
-            return fullurl.get_full_url()
-
-    url = _bind_params(*args, **kwargs)
-
-    details = urlparse.urlparse(url)
-
-    if details.hostname is None:
-        with FunctionTrace(transaction, 'urllib2:OpenerDirector.open'):
-            return wrapped(*args, **kwargs)
-
-    with ExternalTrace(transaction, 'urllib2', url):
-        return wrapped(*args, **kwargs)
+    if isinstance(fullurl, six.string_types):
+        return fullurl
+    else:
+        return fullurl.get_full_url()
 
 def instrument(module):
 
     if hasattr(module, 'urlretrieve'):
+
+        _nr_wrapper_urlretrieve_ = _nr_wrapper_factory(
+            bind_params_urlretrieve, 'urllib:urlretrieve', 'urllib')
+
         wrap_function_wrapper(module, 'urlretrieve', _nr_wrapper_urlretrieve_)
 
     if hasattr(module, 'URLopener'):
+
+        _nr_wrapper_url_opener_open_ = _nr_wrapper_factory(
+            bind_params_open, 'urllib:URLopener.open', 'urllib')
+
         wrap_function_wrapper(module, 'URLopener.open',
             _nr_wrapper_url_opener_open_)
 
     if hasattr(module, 'OpenerDirector'):
+
+        _nr_wrapper_opener_director_open_ = _nr_wrapper_factory(
+            bind_params_open, 'urllib2:OpenerDirector.open', 'urllib2')
+
         wrap_function_wrapper(module, 'OpenerDirector.open',
             _nr_wrapper_opener_director_open_)

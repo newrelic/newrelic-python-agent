6088a09fff8991aae7eb37dddea89f41092a89d4
nschlechter <nschlechter@newrelic.com>
2014-07-18 11:57:35 -0700

    HTML rum header insertion along with unit testing. PYTHON-1143
    Created a insert_html_snippet.py file to search a given string for
    certain html tags with regex. Loops through matched tags to decide
    correct insertion location.
    
    test_insert_html_snippet.py supplies a unit test for this new function
    using different html scenarios saved into the htmls directory from the
    php cross_agent_tests/rum_loader_insertion_location.

---
+++ newrelic/api/tests/test_insert_html_snippet.py
@@ -0,0 +1,59 @@
+import pytest
+from newrelic.api.insert_html_snippet import insert_html_snippet
+import os
+from newrelic.api.verify_body_exists import verify_body_exists
+
+path = os.path.dirname(os.path.abspath(__file__)) + "/htmls/"
+
+@pytest.mark.parametrize("input", [
+		(path + "basic.html"),
+		(path + "body_with_attributes.html"),
+		(path + "charset_tag.html"),
+		(path + "charset_tag_after_x_ua_tag.html"),
+		(path + "charset_tag_before_x_ua_tag.html"),
+		(path + "charset_tag_with_spaces.html"),
+		(path + "comments1.html"),
+		(path + "comments2.html"),
+		(path + "content_type_charset_tag.html"),
+		(path + "content_type_charset_tag_after_x_ua_tag.html"),
+		(path + "content_type_charset_tag_before_x_ua_tag.html"),
+		(path + "gt_in_quotes1.html"),
+		(path + "gt_in_quotes2.html"),
+		(path + "gt_in_quotes_mismatch.html"),
+		(path + "gt_in_single_quotes1.html"),
+		(path + "gt_in_single_quotes_mismatch.html"),
+		(path + "head_with_attributes.html"),
+		(path + "incomplete_non_meta_tags.html"),
+		(path + "no_end_header.html"),
+		(path + "no_header.html"),
+		(path + "no_html_and_no_header.html"),
+		(path + "no_start_header.html"),
+		(path + "script1.html"),
+		(path + "script2.html"),
+		(path + "x_ua_meta_tag.html"),
+		(path + "x_ua_meta_tag_multiline.html"),
+		(path + "x_ua_meta_tag_spaces_around_equals.html"),
+		(path + "x_ua_meta_tag_with_others.html"),
+		(path + "x_ua_meta_tag_with_spaces.html"),
+		(path + "x_ua_meta_with_uppercase.html"),
+
+		# The following test file fails due to duplicate script tags -- very poor html.
+		# Decided to ignore in testing.
+
+		# (path + "x_ua_meta_tag_multiple_tags.html"),
+
+		])
+
+def test_rum_insertion(input):
+	header = "EXPECTED_RUM_LOADER_LOCATION"
+
+	content = open(input, 'r').read()
+	modified_content = content.replace("EXPECTED_RUM_LOADER_LOCATION", "")
+
+	new_content = insert_html_snippet(modified_content, get_header)
+	print new_content[0]
+	print open(input, 'r').read()
+	assert new_content[0] == open(input, 'r').read()
+
+def get_header():
+	return "EXPECTED_RUM_LOADER_LOCATION"

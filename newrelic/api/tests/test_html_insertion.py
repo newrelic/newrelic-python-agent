20412bc05254d0acb4c7a88be2dd55845cad21df
nschlechter <nschlechter@newrelic.com>
2014-07-23 10:08:42 -0700

    Fix html_insertion for Python 3 formatting byte strings. PYTHON-1143
    Reworked the regex expressions in html_insertion to work around
    % formatting that doesn't work on byte strings in Python 3
    
    Also, reformatted test_html_insertion according to Graham's
    suggestions. Cleaned up the process of importing files and
    parametrizing.

--- newrelic/api/tests/test_html_insertion.py
+++ newrelic/api/tests/test_html_insertion.py
@@ -1,59 +1,64 @@
-import pytest
 import os
+import pytest
 
-from newrelic.api.html_insertion import insert_html_snippet
+from newrelic.api.html_insertion import (insert_html_snippet,
+        verify_body_exists)
 
-path = os.path.dirname(os.path.abspath(__file__)) + "/htmls/"
+input_files_directory = os.path.join(os.path.dirname(
+   os.path.abspath(__file__)), 'htmls')
 
-@pytest.mark.parametrize("input", [
-    (path + "basic.html"),
-    (path + "body_with_attributes.html"),
-    (path + "charset_tag.html"),
-    (path + "charset_tag_after_x_ua_tag.html"),
-    (path + "charset_tag_before_x_ua_tag.html"),
-    (path + "charset_tag_with_spaces.html"),
-    (path + "comments1.html"),
-    (path + "comments2.html"),
-    (path + "content_type_charset_tag.html"),
-    (path + "content_type_charset_tag_after_x_ua_tag.html"),
-    (path + "content_type_charset_tag_before_x_ua_tag.html"),
-    (path + "gt_in_quotes1.html"),
-    (path + "gt_in_quotes2.html"),
-    (path + "gt_in_quotes_mismatch.html"),
-    (path + "gt_in_single_quotes1.html"),
-    (path + "gt_in_single_quotes_mismatch.html"),
-    (path + "head_with_attributes.html"),
-    (path + "incomplete_non_meta_tags.html"),
-    (path + "no_end_header.html"),
-    (path + "no_header.html"),
-    (path + "no_html_and_no_header.html"),
-    (path + "no_start_header.html"),
-    (path + "script1.html"),
-    (path + "script2.html"),
-    (path + "x_ua_meta_tag.html"),
-    (path + "x_ua_meta_tag_multiline.html"),
-    (path + "x_ua_meta_tag_spaces_around_equals.html"),
-    (path + "x_ua_meta_tag_with_others.html"),
-    (path + "x_ua_meta_tag_with_spaces.html"),
-    (path + "x_ua_meta_with_uppercase.html"),
+input_files = [
+   'basic.html',
+   'body_with_attributes.html',
+   'charset_tag.html',
+   'charset_tag_after_x_ua_tag.html',
+   'charset_tag_before_x_ua_tag.html',
+   'charset_tag_with_spaces.html',
+   'comments1.html',
+   'comments2.html',
+   'content_type_charset_tag.html',
+   'content_type_charset_tag_after_x_ua_tag.html',
+   'content_type_charset_tag_before_x_ua_tag.html',
+   'gt_in_quotes1.html',
+   'gt_in_quotes2.html',
+   'gt_in_quotes_mismatch.html',
+   'gt_in_single_quotes1.html',
+   'gt_in_single_quotes_mismatch.html',
+   'head_with_attributes.html',
+   'incomplete_non_meta_tags.html',
+   'no_end_header.html',
+   'no_header.html',
+   'no_html_and_no_header.html',
+   'no_start_header.html',
+   'script1.html',
+   'script2.html',
+   'x_ua_meta_tag.html',
+   'x_ua_meta_tag_multiline.html',
+   'x_ua_meta_tag_spaces_around_equals.html',
+   'x_ua_meta_tag_with_others.html',
+   'x_ua_meta_tag_with_spaces.html',
+   'x_ua_meta_with_uppercase.html',
 
-    # The following test file fails due to duplicate script tags -- very poor html.
-    # Decided to ignore in testing.
+   # The following test file fails as the insertion code
+   # will insert after the second meta tag. Given that is
+   # just how the code works, would probably suggest the
+   # test input be modified to match what we actually
+   # calculate.
 
-    # (path + "x_ua_meta_tag_multiple_tags.html"),
+   # 'x_ua_meta_tag_multiple_tags.html',
+]
 
-    ])
+@pytest.mark.parametrize('input_file', input_files)
+def test_rum_insertion(input_file):
+   marker = b'EXPECTED_RUM_LOADER_LOCATION'
 
-def test_rum_insertion(input):
-    header = "EXPECTED_RUM_LOADER_LOCATION"
+   def get_header():
+       return marker
 
-    content = open(input, 'r').read()
-    modified_content = content.replace("EXPECTED_RUM_LOADER_LOCATION", "")
+   input_file = os.path.join(input_files_directory, input_file)
+   expected = open(input_file, 'rb').read()
+   original = expected.replace(marker, b'')
 
-    new_content = insert_html_snippet(modified_content, get_header)
-    print new_content
-    print open(input, 'r').read()
-    assert new_content == open(input, 'r').read()
+   content = insert_html_snippet(original, get_header)
 
-def get_header():
-    return "EXPECTED_RUM_LOADER_LOCATION"
+   assert content == expected

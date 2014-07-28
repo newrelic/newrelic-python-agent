b840152ee2dcf2651f0e50facc9f657c9077fc6f
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2014-07-28 14:32:11 +1000

    RUM insertion match for body should be required, not optional. PYTHON-1380

--- newrelic/api/html_insertion.py
+++ newrelic/api/html_insertion.py
@@ -21,46 +21,42 @@ _insertion_point_re = re.compile(b'('
    b'(.*' + _charset_meta_p(b'3') + b')|'
    b'(.*' + _xua_meta_p(b'3') + b')|'
    b'(.*' + _head_start_p + b'))?'
-   b'(.*' + _body_start_p + b'.*)?',
+   b'(.*' + _body_start_p + b'.*)',
    re.IGNORECASE | re.DOTALL)
 
 def insert_html_snippet(data, html_to_be_inserted):
    matchobj = _insertion_point_re.match(data)
 
-   if not matchobj:
-       return data
+   # If no match at all we return. This is dependent on the regex
+   # at least matching the body element. We cannot therefore make
+   # the body element optional in the insertion point regex.
 
-   if matchobj.start('body') == -1:
+   if not matchobj:
        return None
 
-   # Use the best match we have from the head element for
-   # the place to insert the text.
+   # Since body element is not optional, gauranteed of a place
+   # to insert the HTML snippet. Calculate the HTML snippet, but
+   # don't bother if is empty for some reason.
+
+   text = html_to_be_inserted()
+
+   if not text:
+       return data
+
+   # Use the best match we have from the head element for the
+   # place to insert the text after.
 
    for name in ('xua3', 'charset3', 'xua2', 'charset2', 'head'):
        end = matchobj.end(name)
 
        if end != -1:
-           text = html_to_be_inserted()
-
-           if not text:
-               return data
-
            return text.join((data[:end], data[end:]))
 
    # Fallback to inserting text prior to the body element.
 
    start = matchobj.start('body')
-   if start != -1:
-       text = html_to_be_inserted()
 
-       if not text:
-           return data
-
-       return text.join((data[:start], data[start:]))
-
-   # Something went very wrong as we should never get here.
-
-   return None
+   return text.join((data[:start], data[start:]))
 
 def verify_body_exists(data):
     return _body_start_re.match(data) != -1

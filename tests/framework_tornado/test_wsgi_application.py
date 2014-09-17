f7ccd8ed520114ff6002f71d4a34588b03f3fb7d
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2014-09-17 17:35:31 +1000

    Fix Python 3 issues with Tornado tests. PYTHON-1375

--- tests/framework_tornado/test_wsgi_application.py
+++ tests/framework_tornado/test_wsgi_application.py
@@ -6,6 +6,11 @@ from testing_support.fixtures import (validate_transaction_metrics,
 import tornado.web
 import tornado.wsgi
 
+from newrelic.packages import six
+
+def select_python_version(py2, py3):
+    return six.PY3 and py3 or py2
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("GET RESPONSE")
@@ -26,10 +31,16 @@ _test_wsgi_application_main_get_scoped_metrics = [
         ('Function/tornado.wsgi:WSGIApplication.__call__', 1),
         ('Function/tornado.httputil:parse_body_arguments', 1),
         ('Python/Tornado/Request/Process', 1),
-        ('Function/test_wsgi_application:MainHandler.prepare', 1),
+        (select_python_version(
+            py2='Function/test_wsgi_application:MainHandler.prepare',
+            py3='Function/tornado.web:RequestHandler.prepare'), 1),
         ('Function/test_wsgi_application:MainHandler.get', 1),
-        ('Function/test_wsgi_application:MainHandler.finish', 1),
-        ('Function/test_wsgi_application:MainHandler.on_finish', 1)]
+        (select_python_version(
+            py2='Function/test_wsgi_application:MainHandler.finish',
+            py3='Function/tornado.web:RequestHandler.finish'), 1),
+        (select_python_version(
+            py2='Function/test_wsgi_application:MainHandler.on_finish',
+            py3='Function/tornado.web:RequestHandler.on_finish'), 1)]
 
 @validate_transaction_errors(errors=[])
 @validate_transaction_metrics('test_wsgi_application:MainHandler.get',
@@ -45,10 +56,16 @@ _test_wsgi_application_main_post_scoped_metrics = [
         ('Function/tornado.wsgi:WSGIApplication.__call__', 1),
         ('Function/tornado.httputil:parse_body_arguments', 1),
         ('Python/Tornado/Request/Process', 1),
-        ('Function/test_wsgi_application:MainHandler.prepare', 1),
+        (select_python_version(
+            py2='Function/test_wsgi_application:MainHandler.prepare',
+            py3='Function/tornado.web:RequestHandler.prepare'), 1),
         ('Function/test_wsgi_application:MainHandler.post', 1),
-        ('Function/test_wsgi_application:MainHandler.finish', 1),
-        ('Function/test_wsgi_application:MainHandler.on_finish', 1)]
+        (select_python_version(
+            py2='Function/test_wsgi_application:MainHandler.finish',
+            py3='Function/tornado.web:RequestHandler.finish'), 1),
+        (select_python_version(
+            py2='Function/test_wsgi_application:MainHandler.on_finish',
+            py3='Function/tornado.web:RequestHandler.on_finish'), 1)]
 
 @validate_transaction_errors(errors=[])
 @validate_transaction_metrics('test_wsgi_application:MainHandler.post',

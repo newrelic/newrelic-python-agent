c7d0d80007c8dac583321f08a38a0156a97193d5
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2014-10-28 09:53:58 +1100

    Use current preferred way of checking for linux using sys.platform. PYTHON-1476

--- newrelic/common/system_info.py
+++ newrelic/common/system_info.py
@@ -184,7 +184,7 @@ def physical_processor_count():
 
     """
 
-    if sys.platform in ('linux', 'linux2', 'linux3'):
+    if sys.platform.startswith('linux'):
         return _linux_physical_processor_count()
     elif sys.platform == 'darwin':
         return _darwin_physical_processor_count()
@@ -234,7 +234,7 @@ def total_physical_memory():
 
     """
 
-    if sys.platform in ('linux', 'linux2', 'linux3'):
+    if sys.platform.startswith('linux'):
         return _linux_total_physical_memory()
     elif sys.platform == 'darwin':
         return _darwin_total_physical_memory()
@@ -279,7 +279,7 @@ def physical_memory_used():
     # can be used in metrics. As such has traditionally always been
     # returned as an integer to avoid checks at the point is used.
 
-    if sys.platform in ('linux', 'linux2', 'linux3'):
+    if sys.platform.startswith('linux'):
         return _linux_physical_memory_used()
 
     # For all other platforms try using getrusage() if we have the

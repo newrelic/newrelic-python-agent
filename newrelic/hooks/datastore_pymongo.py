cf3b60872010a5ee6a56ec6fb7e777ae7eeae332
Tom Offermann <toffermann@newrelic.com>
2014-11-21 09:36:39 -0800

    Clean up imports. PYTHON-1464

--- newrelic/hooks/datastore_pymongo.py
+++ newrelic/hooks/datastore_pymongo.py
@@ -1,4 +1,5 @@
-import newrelic.api.datastore_trace
+from newrelic.api.datastore_trace import wrap_datastore_trace
+from newrelic.api.function_trace import wrap_function_trace
 
 _methods = ['save', 'insert', 'update', 'drop', 'remove', 'find_one',
             'find', 'count', 'create_index', 'ensure_index', 'drop_indexes',
@@ -11,9 +12,8 @@ def instrument_pymongo_connection(module):
     # Must name function explicitly as pymongo overrides the
     # __getattr__() method in a way that breaks introspection.
 
-    newrelic.api.function_trace.wrap_function_trace(
-        module, 'Connection.__init__',
-        name='%s:Connection.__init__' % module.__name__)
+    wrap_function_trace(module, 'Connection.__init__',
+            name='%s:Connection.__init__' % module.__name__)
 
 def instrument_pymongo_collection(module):
 
@@ -25,7 +25,6 @@ def instrument_pymongo_collection(module):
 
     for method in _methods:
         if hasattr(module.Collection, method):
-            newrelic.api.datastore_trace.wrap_datastore_trace(
-                    module, 'Collection.%s' % method,
+            wrap_datastore_trace(module, 'Collection.%s' % method,
                     product='MongoDB', target=_collection_name,
                     operation=method)

2a39883e972937b91bf039be4deb13d00bb94da2
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2015-01-09 15:10:27 +1100

    Created Datastore metric variation of MySQLdb tox test for MySQL. PYTHON-1541

---
+++ tests/datastore_mysqldb/test_database.py
@@ -0,0 +1,167 @@
+import MySQLdb
+
+import pwd
+import os
+
+from testing_support.fixtures import (validate_transaction_metrics,
+    validate_database_trace_inputs)
+
+from testing_support.settings import mysql_settings
+
+from newrelic.agent import (background_task, current_transaction,
+    transient_function_wrapper)
+
+from newrelic.common.object_wrapper import resolve_path
+
+DB_SETTINGS = mysql_settings()
+
+_test_execute_via_cursor_scoped_metrics = [
+        ('Function/MySQLdb:Connect', 1),
+        ('Function/MySQLdb.connections:Connection.__enter__', 1),
+        ('Function/MySQLdb.connections:Connection.__exit__', 1),
+        ('Datastore/statement/MySQL/database_mysqldb/select', 1),
+        ('Datastore/statement/MySQL/database_mysqldb/insert', 1),
+        ('Datastore/statement/MySQL/database_mysqldb/update', 1),
+        ('Datastore/statement/MySQL/database_mysqldb/delete', 1),
+        ('Datastore/statement/MySQL/other/other', 6)]
+
+_test_execute_via_cursor_rollup_metrics = [
+        ('Datastore/all', 11),
+        ('Datastore/allOther', 11),
+        ('Datastore/MySQL/all', 11),
+        ('Datastore/MySQL/allOther', 11),
+        ('Datastore/operation/MySQL/select', 1),
+        ('Datastore/statement/MySQL/database_mysqldb/select', 1),
+        ('Datastore/operation/MySQL/insert', 1),
+        ('Datastore/statement/MySQL/database_mysqldb/insert', 1),
+        ('Datastore/operation/MySQL/update', 1),
+        ('Datastore/statement/MySQL/database_mysqldb/update', 1),
+        ('Datastore/operation/MySQL/delete', 1),
+        ('Datastore/statement/MySQL/database_mysqldb/delete', 1),
+        ('Datastore/instance/MySQL/localhost/database_mysqldb', 4),
+        ('Datastore/operation/MySQL/other', 6),
+        ('Datastore/statement/MySQL/other/other', 6)]
+
+@validate_transaction_metrics('test_database:test_execute_via_cursor',
+        scoped_metrics=_test_execute_via_cursor_scoped_metrics,
+        rollup_metrics=_test_execute_via_cursor_rollup_metrics,
+        background_task=True)
+@validate_database_trace_inputs(tuple)
+@background_task()
+def test_execute_via_cursor():
+    connection = MySQLdb.connect(db=DB_SETTINGS['name'],
+            user=DB_SETTINGS['user'], passwd=DB_SETTINGS['password'],
+            host=DB_SETTINGS['host'], port=DB_SETTINGS['port'])
+
+    with connection as cursor:
+        cursor.execute("""drop table if exists database_mysqldb""")
+
+        cursor.execute("""create table database_mysqldb """
+                """(a integer, b real, c text)""")
+
+        cursor.executemany("""insert into database_mysqldb """
+                """values (%s, %s, %s)""", [(1, 1.0, '1.0'),
+                (2, 2.2, '2.2'), (3, 3.3, '3.3')])
+
+        cursor.execute("""select * from database_mysqldb""")
+
+        for row in cursor: pass
+
+        cursor.execute("""update database_mysqldb set a=%s, b=%s, """
+                """c=%s where a=%s""", (4, 4.0, '4.0', 1))
+
+        cursor.execute("""delete from database_mysqldb where a=2""")
+
+    connection.commit()
+    connection.rollback()
+    connection.commit()
+
+_test_connect_using_alias_scoped_metrics = [
+        ('Function/MySQLdb:Connect', 1),
+        ('Datastore/statement/MySQL/database_mysqldb/select', 1),
+        ('Datastore/statement/MySQL/database_mysqldb/insert', 1),
+        ('Datastore/statement/MySQL/database_mysqldb/update', 1),
+        ('Datastore/statement/MySQL/database_mysqldb/delete', 1),
+        ('Datastore/statement/MySQL/other/other', 6)]
+
+_test_connect_using_alias_rollup_metrics = [
+        ('Datastore/all', 11),
+        ('Datastore/allOther', 11),
+        ('Datastore/MySQL/all', 11),
+        ('Datastore/MySQL/allOther', 11),
+        ('Datastore/operation/MySQL/select', 1),
+        ('Datastore/statement/MySQL/database_mysqldb/select', 1),
+        ('Datastore/operation/MySQL/insert', 1),
+        ('Datastore/statement/MySQL/database_mysqldb/insert', 1),
+        ('Datastore/operation/MySQL/update', 1),
+        ('Datastore/statement/MySQL/database_mysqldb/update', 1),
+        ('Datastore/operation/MySQL/delete', 1),
+        ('Datastore/statement/MySQL/database_mysqldb/delete', 1),
+        ('Datastore/instance/MySQL/localhost/database_mysqldb', 4),
+        ('Datastore/operation/MySQL/other', 6),
+        ('Datastore/statement/MySQL/other/other', 6)]
+
+@validate_transaction_metrics('test_database:test_connect_using_alias',
+        scoped_metrics=_test_connect_using_alias_scoped_metrics,
+        rollup_metrics=_test_connect_using_alias_rollup_metrics,
+        background_task=True)
+@validate_database_trace_inputs(tuple)
+@background_task()
+def test_connect_using_alias():
+    connection = MySQLdb.connect(db=DB_SETTINGS['name'],
+            user=DB_SETTINGS['user'], passwd=DB_SETTINGS['password'],
+            host=DB_SETTINGS['host'], port=DB_SETTINGS['port'])
+
+    with connection as cursor:
+        cursor.execute("""drop table if exists database_mysqldb""")
+
+        cursor.execute("""create table database_mysqldb """
+                """(a integer, b real, c text)""")
+
+        cursor.executemany("""insert into database_mysqldb """
+                """values (%s, %s, %s)""", [(1, 1.0, '1.0'),
+                (2, 2.2, '2.2'), (3, 3.3, '3.3')])
+
+        cursor.execute("""select * from database_mysqldb""")
+
+        for row in cursor: pass
+
+        cursor.execute("""update database_mysqldb set a=%s, b=%s, """
+                """c=%s where a=%s""", (4, 4.0, '4.0', 1))
+
+        cursor.execute("""delete from database_mysqldb where a=2""")
+
+    connection.commit()
+    connection.rollback()
+    connection.commit()
+
+_test_rollback_on_exception_scoped_metrics = [
+        ('Function/MySQLdb:Connect', 1),
+        ('Function/MySQLdb.connections:Connection.__enter__', 1),
+        ('Function/MySQLdb.connections:Connection.__exit__', 1),
+        ('Datastore/statement/MySQL/other/other', 1)]
+
+_test_rollback_on_exception_rollup_metrics = [
+        ('Datastore/all', 2),
+        ('Datastore/allOther', 2),
+        ('Datastore/MySQL/all', 2),
+        ('Datastore/MySQL/allOther', 2),
+        ('Datastore/operation/MySQL/other', 1),
+        ('Datastore/statement/MySQL/other/other', 1)]
+
+@validate_transaction_metrics('test_database:test_rollback_on_exception',
+        scoped_metrics=_test_rollback_on_exception_scoped_metrics,
+        rollup_metrics=_test_rollback_on_exception_rollup_metrics,
+        background_task=True)
+@validate_database_trace_inputs(sql_parameters_type=tuple)
+@background_task()
+def test_rollback_on_exception():
+    try:
+        with MySQLdb.connect(
+            db=DB_SETTINGS['name'], user=DB_SETTINGS['user'],
+            passwd=DB_SETTINGS['password'], host=DB_SETTINGS['host'],
+            port=DB_SETTINGS['port']) as connection:
+
+            raise RuntimeError('error')
+    except RuntimeError:
+        pass

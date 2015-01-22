7a5dc7c0c5b85c0792ac051aaa5b5f8d9af65357
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2015-01-22 18:21:10 +1100

    Clean up wrapping of methods in pymongo instrumentation. PYTHON-1557

--- newrelic/hooks/datastore_pymongo.py
+++ newrelic/hooks/datastore_pymongo.py
@@ -1,10 +1,10 @@
 from newrelic.agent import wrap_function_trace, wrap_datastore_trace
 
-_methods = ['save', 'insert', 'update', 'drop', 'remove', 'find_one',
-            'find', 'count', 'create_index', 'ensure_index', 'drop_indexes',
-            'drop_index', 'reindex', 'index_information', 'options',
-            'group', 'rename', 'distinct', 'map_reduce', 'inline_map_reduce',
-            'find_and_modify']
+_pymongo_client_methods = ('save', 'insert', 'update', 'drop', 'remove',
+    'find_one', 'find', 'count', 'create_index', 'ensure_index',
+    'drop_indexes', 'drop_index', 'reindex', 'index_information',
+    'options', 'group', 'rename', 'distinct', 'map_reduce',
+    'inline_map_reduce', 'find_and_modify')
 
 def instrument_pymongo_connection(module):
     # Must name function explicitly as pymongo overrides the
@@ -21,14 +21,10 @@ def instrument_pymongo_mongo_client(module):
             name='%s:MongoClient.__init__' % module.__name__)
 
 def instrument_pymongo_collection(module):
-    # Must name function explicitly as pymongo overrides the
-    # __getattr__() method in a way that breaks introspection.
-
     def _collection_name(collection, *args, **kwargs):
         return collection.name
 
-    for method in _methods:
-        if hasattr(module.Collection, method):
-            wrap_datastore_trace(module, 'Collection.%s' % method,
-                    product='MongoDB', target=_collection_name,
-                    operation=method)
+    for name in _pymongo_client_methods:
+        if hasattr(module.Collection, name):
+            wrap_datastore_trace(module.Collection, name, product='MongoDB',
+                    target=_collection_name, operation=name)

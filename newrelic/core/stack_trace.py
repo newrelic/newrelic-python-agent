e5e36dd54d824517fce58bb5774fddc601ead15e
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2015-03-10 20:22:54 +1100

    Restore more complex algorithm for extracting exception traceback as simplified version incorrect in some cases. Ensure using line number of traceback object and not frame. PYTHON-1590

--- newrelic/core/stack_trace.py
+++ newrelic/core/stack_trace.py
@@ -57,11 +57,40 @@ def _extract_tb(tb, limit=None):
     if tb is None:
         return []
 
-    while tb is not None:
-        last = tb
-        tb = tb.tb_next
+    # The first traceback object is actually connected to the top most
+    # frame where the exception was caught. As the limit needs to apply
+    # to the bottom most, we need to traverse the complete list of
+    # traceback objects and calculate as we go what would be the top
+    # traceback object. Only once we have done that can we then work
+    # out what frames to return.
 
-    return _extract_stack(last.tb_frame, limit=limit)
+    n = 0
+    top = tb
+
+    while tb is not None:
+        if n >= limit:
+            top = top.tb_next
+
+        tb = tb.tb_next
+        n += 1
+
+    n = 0
+    l = []
+
+    # We have now the top traceback object for the limit of what we are
+    # to return. The bottom most will be that where the error occurred.
+
+    tb = top
+
+    while tb is not None and n < limit:
+        f = tb.tb_frame
+        l.append(dict(source=f.f_code.co_filename,
+                line=tb.tb_lineno, name=f.f_code.co_name))
+
+        tb = tb.tb_next
+        n += 1
+
+    return l
 
 def exception_stack(tb, limit=None):
     if tb is None:

f2555fe9906c14b567baa9e5792b9c83e961b940
Tom Offermann <toffermann@newrelic.com>
2015-03-31 14:56:10 -0700

    Add psycopg2 register_range test. PYTHON-1567

--- tests/datastore_psycopg2/test_database.py
+++ tests/datastore_psycopg2/test_database.py
@@ -217,3 +217,35 @@ def test_register_json():
         loads = lambda x: json.loads(x, parse_float=Decimal)
         psycopg2.extras.register_json(connection, loads=loads)
         psycopg2.extras.register_json(cursor, loads=loads)
+
+@pytest.mark.skipif(POSTGRESQL_VERSION < (9, 2),
+        reason="Range types were introduced in Postgres 9.2")
+@validate_transaction_metrics('test_database:test_register_range',
+        background_task=True)
+@validate_transaction_errors(errors=[])
+@background_task()
+def test_register_range():
+    with psycopg2.connect(
+            database=DB_SETTINGS['name'], user=DB_SETTINGS['user'],
+            password=DB_SETTINGS['password'], host=DB_SETTINGS['host'],
+            port=DB_SETTINGS['port']) as connection:
+
+        create_sql = ('CREATE TYPE floatrange AS RANGE ('
+                      'subtype = float8,'
+                      'subtype_diff = float8mi)')
+
+        cursor = connection.cursor()
+
+        cursor.execute("DROP TYPE if exists floatrange")
+        cursor.execute(create_sql)
+
+        psycopg2.extras.register_range('floatrange',
+                psycopg2.extras.NumericRange, connection)
+
+        cursor.execute("DROP TYPE if exists floatrange")
+        cursor.execute(create_sql)
+
+        psycopg2.extras.register_range('floatrange',
+                psycopg2.extras.NumericRange, cursor)
+
+        cursor.execute("DROP TYPE if exists floatrange")

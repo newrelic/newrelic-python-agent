beabc1a7264a945b8ab118bb65a4cdc4ed2d0c85
Amjith Ramanujam <amjith.r@gmail.com>
2015-04-21 10:15:43 -0700

    Remove the admin commands from solrpy instrumentation. PYTHON-1644

--- newrelic/hooks/datastore_solrpy.py
+++ newrelic/hooks/datastore_solrpy.py
@@ -3,9 +3,6 @@ from newrelic.agent import wrap_datastore_trace
 _solrpy_client_methods = ('query', 'add', 'add_many', 'delete', 'delete_many',
 'delete_query', 'commit', 'optimize', 'raw_query')
 
-_solrpy_admin_methods = ('status', 'create', 'reload', 'rename', 'swap',
-    'unload', 'load')
-
 def instrument_solrpy(module):
     for name in _solrpy_client_methods:
         if hasattr(module.SolrConnection, name):

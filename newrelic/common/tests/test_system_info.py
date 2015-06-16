48b0659aa0e61088e75298ea09cf58ecc20ecc75
Tom Offermann <toffermann@newrelic.com>
2015-06-16 10:48:42 -0700

    Import functions by name in test_system_info.py. PYTHON-1663
    Line wrapping problems are easier to solve when you don't have to include
    module path.

--- newrelic/common/tests/test_system_info.py
+++ newrelic/common/tests/test_system_info.py
@@ -1,7 +1,9 @@
 import sys
 import unittest
 
-import newrelic.common.system_info
+from newrelic.common.system_info import (docker_container_id,
+        _process_cgroup_info, _validate_docker_container_id)
+
 
 class TestsDockerContainerId(unittest.TestCase):
 
@@ -10,8 +12,7 @@ class TestsDockerContainerId(unittest.TestCase):
 
     def test_not_linux(self):
         sys.platform = 'darwin'
-        self.assertEqual(None,
-                newrelic.common.system_info.docker_container_id())
+        self.assertEqual(None, docker_container_id())
 
     # Test we parse ids we expect to see
     def test_native_no_systemd(self):
@@ -28,7 +29,7 @@ class TestsDockerContainerId(unittest.TestCase):
             "2:cpuset:/"]
         self.assertEqual(
                 "2a4f870e24a3b52eb9fe7f3e02858c31855e213e568cfa6c76cb046ffa5b8a28",
-                newrelic.common.system_info._process_cgroup_info(cgroup_info))
+                _process_cgroup_info(cgroup_info))
 
     def test_native_systemd(self):
         cgroup_info = [
@@ -44,7 +45,7 @@ class TestsDockerContainerId(unittest.TestCase):
             "1:name=systemd:/system.slice/docker-67f98c9e6188f9c1818672a15dbe46237b6ee7e77f834d40d41c5fb3c2f84a2f.scope"]
         self.assertEqual(
                 "67f98c9e6188f9c1818672a15dbe46237b6ee7e77f834d40d41c5fb3c2f84a2f",
-                newrelic.common.system_info._process_cgroup_info(cgroup_info))
+                _process_cgroup_info(cgroup_info))
 
     def test_lxc(self):
         cgroup_info = [
@@ -60,41 +61,33 @@ class TestsDockerContainerId(unittest.TestCase):
             "2:cpuset:/lxc/cb8c113e5f3cf8332f5231f8154adc429ea910f7c29995372de4f571c55d3159"]
         self.assertEqual(
                 'cb8c113e5f3cf8332f5231f8154adc429ea910f7c29995372de4f571c55d3159',
-                newrelic.common.system_info._process_cgroup_info(cgroup_info))
+                _process_cgroup_info(cgroup_info))
 
     def test_not_in_cgroup(self):
         cgroup_info = ["1:cpu:/"]
-        self.assertEqual(None,
-                newrelic.common.system_info._process_cgroup_info(cgroup_info))
+        self.assertEqual(None, _process_cgroup_info(cgroup_info))
 
     # Test for bad ids
     def test_id_has_unexpected_characters(self):
         cgroup_info = ["1:cpu:/docker/asdf1234#@%"]
-        self.assertEqual(None,
-                newrelic.common.system_info._process_cgroup_info(cgroup_info))
+        self.assertEqual(None, _process_cgroup_info(cgroup_info))
 
     def test_no_cpu_in_map(self):
         cgroup_info = [
             "11:hugetlb:/lxc/p1",
             "10:perf_event:/lxc/p1",
             "9:blkio:/lxc/p1"]
-        self.assertEqual(None,
-                newrelic.common.system_info._process_cgroup_info(cgroup_info))
+        self.assertEqual(None, _process_cgroup_info(cgroup_info))
 
     def test_unrecognized_id_format(self):
         cgroup_info = ["BAD_FORMAT"]
-        self.assertEqual(None,
-                newrelic.common.system_info._process_cgroup_info(cgroup_info))
+        self.assertEqual(None, _process_cgroup_info(cgroup_info))
 
     def test_validate_docker_container_id_length(self):
         container_id = 'a' * 64
-        self.assertTrue(
-                newrelic.common.system_info._validate_docker_container_id(
-                        container_id))
+        self.assertTrue(_validate_docker_container_id(container_id))
         container_id = 'a' * 65
-        self.assertFalse(
-                newrelic.common.system_info._validate_docker_container_id(
-                        container_id))
+        self.assertFalse(_validate_docker_container_id(container_id))
 
 if __name__ == '__main__':
     unittest.main()

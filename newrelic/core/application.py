664fcf317c68db3c6815e7680abd9ce804858590
Tom Offermann <toffermann@newrelic.com>
2015-08-23 23:01:55 -0700

    Move attribute filter to application settings. PYTHON-1701
    Given that we need access to the attribute filter in various places
    (like StatsEngine, or TransactionNode), it's easier to get at it if it
    is stored in the application settings object, rather than stored as an
    attribute on the application object. That's because the application
    settings object is also stored as an attribute of StatsEngine,
    Transaction, and TransactionNode.
    
    Note that the application settings object is different than the global
    settings object, since server-side settings have been applied and the
    attribute filter has been created. Both of those modifications to the
    settings object take place in a new function:
    
        newrelic.core.config:finalize_application_settings

--- newrelic/core/application.py
+++ newrelic/core/application.py
@@ -16,9 +16,7 @@ import newrelic.packages.six as six
 
 from newrelic.samplers.data_sampler import DataSampler
 
-from newrelic.core.attribute_filter import AttributeFilter
-from newrelic.core.config import (global_settings_dump, global_settings,
-        flatten_settings)
+from newrelic.core.config import global_settings_dump, global_settings
 from newrelic.core.data_collector import create_session
 from newrelic.network.exceptions import (ForceAgentRestart,
         ForceAgentDisconnect, DiscardDataForRequest, RetryDataForRequest)
@@ -113,8 +111,6 @@ class Application(object):
         # value = XraySession object
 
         self._active_xrays = {}
-
-        self.attribute_filter = None
 
     @property
     def name(self):
@@ -433,12 +429,6 @@ class Application(object):
 
             with self._stats_custom_lock:
                 self._stats_custom_engine.reset_stats(configuration)
-
-            # Now that configuration has been finalized, we can create the
-            # AttributeFilter.
-
-            flat_settings = flatten_settings(configuration)
-            self.attribute_filter = AttributeFilter(flat_settings)
 
             # Record an initial start time for the reporting period and
             # clear record of last transaction processed.

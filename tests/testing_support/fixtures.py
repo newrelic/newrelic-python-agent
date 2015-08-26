1135059ff541340509f7210c93b2a74d0a860c9e
Amy Boyle <aboyle@newrelic.com>
2015-08-26 14:18:06 -0700

    Test error json sent to collector. PYTHON-1649

--- tests/testing_support/fixtures.py
+++ tests/testing_support/fixtures.py
@@ -5,6 +5,7 @@ import sys
 import pwd
 import threading
 import time
+import json
 
 from newrelic.packages import six
 
@@ -13,7 +14,7 @@ from newrelic.agent import (initialize, register_application,
         transient_function_wrapper, function_wrapper, application_settings,
         wrap_function_wrapper, ObjectProxy, application, callable_name)
 
-from newrelic.common.encoding_utils import unpack_field
+from newrelic.common.encoding_utils import unpack_field, json_encode
 
 from newrelic.core.config import (apply_config_setting, flatten_settings)
 from newrelic.core.database_utils import SQLConnections
@@ -651,6 +652,41 @@ def check_error_attributes(parameters, required_params={}, forgone_params={},
 
         for param in forgone_params['user']:
             assert param not in parameters['userAttributes']
+
+def validate_error_trace_collector_json():
+    @transient_function_wrapper('newrelic.core.stats_engine',
+            'StatsEngine.record_transaction')
+    def _validate_error_trace_collector_json(wrapped, instance, args, kwargs):
+        try:
+            result = wrapped(*args, **kwargs)
+        except:
+            raise
+        errors = instance.error_data()
+
+        # recreate what happens right before data is sent to the collector in
+        # data_collector.py via ApplicationSession.send_errors
+        agent_run_id = 666
+        payload = (agent_run_id, errors)
+        collector_json = json_encode(payload)
+
+        decoded_json = json.loads(collector_json)
+
+        assert decoded_json[0] == agent_run_id
+        err = decoded_json[1][0]
+        assert len(err) == 5
+        assert isinstance(err[0], (int, float))
+        assert isinstance(err[1], six.string_types) # path
+        assert isinstance(err[2], six.string_types) # error message
+        assert isinstance(err[3], six.string_types) # exception name
+        parameters = err[4]
+
+        parameter_fields = ['userAttributes', 'stack_trace', 'agentAttributes',
+                'intrinsics', 'request_uri']
+
+        for field in parameter_fields:
+            assert field in parameters
+
+    return _validate_error_trace_collector_json
 
 def validate_tt_parameters(required_params={},
         forgone_params={}):

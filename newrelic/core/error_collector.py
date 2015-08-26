dd0d711c1056323f8535995d17608a8dad006cac
Amy Boyle <aboyle@newrelic.com>
2015-08-26 11:06:29 -0700

    Remove obsolete error_parameters. PYTHON-1649

--- newrelic/core/error_collector.py
+++ newrelic/core/error_collector.py
@@ -2,16 +2,3 @@ from collections import namedtuple
 
 TracedError = namedtuple('TracedError',
          ['start_time','path','message','type','parameters'])
-
-def error_parameters(uri,backtrace=[],request_params={},custom_params={}):
-    params = {}
-
-    params["request_uri"] = uri
-    params["stack_trace"] = backtrace
-    if len(request_params) > 0:
-        params["request_params"] = request_params
-
-    if len(custom_params) > 0:
-        params["custom_params"] = custom_params
-
-    return params

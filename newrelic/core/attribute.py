9233596d02d2cbafdfc097229f637e3456bfa7d8
Amy Boyle <aboyle@newrelic.com>
2015-09-16 14:33:35 -0700

    Reformat attribute destinations dict. PYTHON-1651
    Change how the attribute default destinations are created
    and applied to improve clarity.

--- newrelic/core/attribute.py
+++ newrelic/core/attribute.py
@@ -1,4 +1,4 @@
-from collections import namedtuple, defaultdict
+from collections import namedtuple
 
 from .attribute_filter import (DST_ALL, DST_ERROR_COLLECTOR,
         DST_TRANSACTION_TRACER, DST_NONE, DST_TRANSACTION_EVENTS)
@@ -6,26 +6,23 @@ from .attribute_filter import (DST_ALL, DST_ERROR_COLLECTOR,
 _Attribute = namedtuple('_Attribute',
         ['name', 'value', 'destinations'])
 
-# The following defaults map, _DEFAULT_DESTINATIONS is created here, never
-# changed, and only used in create_agent_attributes. It is placed at the module
-# level here as an optimization
+# The following destinations are created here, never changed, and only used in
+# create_agent_attributes. It is placed at the module level here as an optimization
 
 # All agent attributes go to transaction traces and error traces by default
 
-_DEFAULT_DESTINATIONS = defaultdict(lambda:
-        DST_ERROR_COLLECTOR | DST_TRANSACTION_TRACER)
+_DESTINATIONS = DST_ERROR_COLLECTOR | DST_TRANSACTION_TRACER
+_DESTINATIONS_WITH_EVENTS = _DESTINATIONS | DST_TRANSACTION_EVENTS
 
 # The following subset goes to transaction events by default
 
-trans_event_default = [
+_TRANSACTION_EVENT_DEFAULT_ATTRIBUTES = [
         'request.method',
         'request.headers.content-type',
         'request.headers.content-length',
         'response.status',
         'response.content-length'
 ]
-for attr in trans_event_default:
-    _DEFAULT_DESTINATIONS[attr] |= DST_TRANSACTION_EVENTS
 
 class Attribute(_Attribute):
 
@@ -46,7 +43,11 @@ def create_agent_attributes(attr_dict, attribute_filter):
     attributes = []
 
     for k, v in attr_dict.items():
-        dest = attribute_filter.apply(k, _DEFAULT_DESTINATIONS[k])
+        if k in _TRANSACTION_EVENT_DEFAULT_ATTRIBUTES:
+            dest = attribute_filter.apply(k, _DESTINATIONS_WITH_EVENTS)
+        else:
+            dest = attribute_filter.apply(k, _DESTINATIONS)
+
         attributes.append(Attribute(k, v, dest))
 
     return attributes

f97ab91c9f2d484ecd30cedbac610a5029d7f9a2
Tom Offermann <toffermann@newrelic.com>
2015-11-04 15:57:42 -0800

    Add deploy-to-pypi.sh script. PYTHON-1760
    This is a simple "glue" script that just calls the two scripts
    that do all the real work:
    
        download-from-artifactory.sh
        upload-to-pypi.sh

---
+++ deploy/deploy-to-pypi.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Deploy a source distribution package to PyPI.
+#
+# Glue script that calls two other scripts to:
+#
+#   1. Download package from Artifactory.
+#   2. Upload package to PyPI.
+#
+# Required environment variables:
+#
+#   1. Either PYPI_TEST_PASSWORD or PYPI_PRODUCTION_PASSWORD
+#   2. PYPI_REPOSITORY
+#   3. AGENT_VERSION
+#
+# Requires: git, md5sum, curl, and twine.
+
+set -e
+
+GIT_REPO_ROOT=$(git rev-parse --show-toplevel)
+
+cd $GIT_REPO_ROOT
+
+deploy/download-from-artifactory.sh
+deploy/upload-to-pypi.sh

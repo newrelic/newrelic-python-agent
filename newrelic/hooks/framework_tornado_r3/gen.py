80f08c6ace9fe58aad59b82c85d330528745c759
Bill Dirks <bdirks@newrelic.com>
2015-11-11 14:49:22 -0800

    Update comments in our gen.py instrumenation. PYTHON-1718

--- newrelic/hooks/framework_tornado_r3/gen.py
+++ newrelic/hooks/framework_tornado_r3/gen.py
@@ -29,29 +29,28 @@ except:
 class NoneProxy(object):
     pass
 
-# If I do this wrapper on _make_coroutine_wrapper do I get gen.engine also?
-# Or do i need to do the wrapping at the coroutine and the gen.engine level?
-
 def _coroutine_name(func):
     # Because of how coroutines get scheduled they will look like plain
-    # functions (and not methods) and will not have a class associated with
-    # them. In particular, func will not have the attribute im_class.
-    # This means callable_name will return the function name without the class
-    # prefix.
+    # functions (and not methods) in python 2 and will not have a class
+    # associated with them. In particular, func will not have the attribute
+    # im_class. This means callable_name will return the function name without
+    # the class prefix. See PYTHON-1798.
     return '%s %s' % (callable_name(func), '(coroutine)')
 
 def _nr_wrapper_Runner__init__(wrapped, instance, args, kwargs):
-    # We want the associate a function name from _make_coroutine_wrapper with
+    # We want to associate a function name from _make_coroutine_wrapper with
     # a call to Runner.__init__. This way we know the name of the function
     # running in Runner and can associate metrics with it.
     # One strategy would be to store the function name on the transaction.
-    # This can be problematic because an asynchronous/blocking action can occur
-    # in the call to _make_coroutine_wrapper.wrapper before Runner.__init__.
+    # This can be problematic because an asynchronous/blocking call can occur
+    # in _make_coroutine_wrapper.wrapper before the call to Runner.__init__.
     # This means other coroutines could run in the transaction between when we
     # record the function name in the transaction and before Runner.__init__
     # is called. Since these coroutines run asynchronously and don't nest, we
     # can't use a stack to keep track of which _make_coroutine_wrapper.wrapper
     # call belongs to which Runner.__init__ call.
+    # Instead of storing the name in the transaction, we will look up the call
+    # stack to get the function name.
 
     transaction = retrieve_current_transaction()
     if transaction is None:

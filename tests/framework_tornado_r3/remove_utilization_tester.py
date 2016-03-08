112a325bfe071f17f96917cd895fe087c1a3bad5
Amy Boyle <aboyle@newrelic.com>
2016-03-08 11:27:35 -0800

    Allow assert statements to fail and print trace. PYTHON-1521

--- tests/framework_tornado_r3/remove_utilization_tester.py
+++ tests/framework_tornado_r3/remove_utilization_tester.py
@@ -4,6 +4,11 @@ import newrelic.agent
 from newrelic.core.agent import agent_instance
 from newrelic.core.thread_utilization import _utilization_trackers
 
+# This test script is intended to be ran manually. This is because it deals with
+# code that can run a couple different ways for agent initialization/
+# registration, and the test code deals with this already by the time you get to
+# a test case.
+
 def remove_utilization_tester(now=True):
 
     newrelic.agent.initialize('newrelic.ini')
@@ -17,19 +22,15 @@ def remove_utilization_tester(now=True):
 
     agent = agent_instance()
 
-    try:
-        source_names = [s[0].__name__ for s in agent._data_sources[None]]
-        assert 'thread_utilization_data_source' not in source_names
+    source_names = [s[0].__name__ for s in agent._data_sources[None]]
+    assert 'thread_utilization_data_source' not in source_names
 
-        for app in agent._applications.values():
-            sampler_names = [x.name for x in app._data_samplers]
-            assert 'Thread Utilization' not in sampler_names
+    for app in agent._applications.values():
+        sampler_names = [x.name for x in app._data_samplers]
+        assert 'Thread Utilization' not in sampler_names
 
-        assert len(_utilization_trackers) == 0
-    except:
-        return False
+    assert len(_utilization_trackers) == 0
 
-    return True
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'later':
@@ -37,4 +38,4 @@ if __name__ == '__main__':
     else:
         passed = remove_utilization_tester(True)
 
-    print('PASSED?', passed)
+    print("PASSED!")

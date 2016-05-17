da293ab832726b7c10045bc74ed94cdca973d174
Reina Abolofia <rabolofia@newrelic.com>
2016-05-17 10:36:26 -0700

    Tests for utilization configuration settings
    Tests that the settings object is created correctly. Tests that the data is
    correctly sent back to the collector.

---
+++ tests/settings_config/test_utilization_settings.py
@@ -0,0 +1,115 @@
+import os
+import pytest
+import tempfile
+
+from newrelic.agent import function_wrapper, global_settings, initialize
+from newrelic.core.data_collector import ApplicationSession
+
+# these will be reloaded for each test
+import newrelic.config
+import newrelic.core.config
+
+try:
+    # python 2.x
+    reload
+except NameError:
+    # python 3.x
+    from imp import reload
+
+try:
+    import ConfigParser
+except ImportError:
+    import configparser as ConfigParser
+
+INI_FILE_WITHOUT_UTIL_CONF = b"""
+[newrelic]
+"""
+
+INI_FILE_WITH_UTIL_CONF = b"""
+[newrelic]
+
+utilization.billing_hostname = file-hostname
+"""
+
+ENV_WITHOUT_UTIL_CONF = {}
+ENV_WITH_UTIL_CONF = {'NEW_RELIC_UTILIZATION_BILLING_HOSTNAME': 'env-hostname'}
+
+INITIAL_ENV = os.environ
+
+def reset_agent_config(ini_contents, env_dict):
+    @function_wrapper
+    def reset(wrapped, instance, args, kwargs):
+        os.environ.update(env_dict)
+
+        # wrap in try/except so env vars are always reset
+        try:
+            ini_file = tempfile.NamedTemporaryFile()
+            ini_file.write(ini_contents)
+            ini_file.seek(0)
+
+            # clean settings cache and reload env vars
+            # Note that reload can at times work in unexpected ways. All that
+            # is required here is that the globals (such as
+            # newrelic.core.config._settings) be reset.
+            #
+            # From python docs (2.x and 3.x)
+            # "When a module is reloaded, its dictionary (containing the
+            # module's global variables) is retained. Redefinitions of names
+            # will override the old definitions, so this is generally not a
+            # problem."
+            reload(newrelic.core.config)
+            reload(newrelic.config)
+            initialize(ini_file.name)
+
+            returned = wrapped(*args, **kwargs)
+        except:
+            raise
+        finally:
+            os.environ.clear()
+            os.environ = INITIAL_ENV
+
+        return returned
+    return reset
+
+@reset_agent_config(INI_FILE_WITHOUT_UTIL_CONF, ENV_WITH_UTIL_CONF)
+def test_billing_hostname_from_env_vars():
+    settings = global_settings()
+    assert settings.utilization.billing_hostname == 'env-hostname'
+
+    local_config, = ApplicationSession._create_connect_payload(
+            '', [], [], newrelic.core.config.global_settings_dump())
+    util_conf = local_config['utilization'].get('config')
+    assert util_conf == {'hostname': 'env-hostname'}
+
+@reset_agent_config(INI_FILE_WITH_UTIL_CONF, ENV_WITH_UTIL_CONF)
+def test_billing_hostname_precedence():
+    # ini-file takes precedence over env vars
+    settings = global_settings()
+    assert settings.utilization.billing_hostname == 'file-hostname'
+
+    local_config, = ApplicationSession._create_connect_payload(
+            '', [], [], newrelic.core.config.global_settings_dump())
+    util_conf = local_config['utilization'].get('config')
+    assert util_conf == {'hostname': 'file-hostname'}
+
+@reset_agent_config(INI_FILE_WITHOUT_UTIL_CONF, ENV_WITHOUT_UTIL_CONF)
+def test_billing_hostname_with_blank_ini_file_no_env():
+    settings = global_settings()
+    assert settings.utilization.billing_hostname == None
+
+    # if no utilization config settings are set, the 'config' section is not in
+    # the payload at all
+    local_config, = ApplicationSession._create_connect_payload(
+            '', [], [], newrelic.core.config.global_settings_dump())
+    util_conf = local_config['utilization'].get('config')
+    assert util_conf == None
+
+@reset_agent_config(INI_FILE_WITH_UTIL_CONF, ENV_WITHOUT_UTIL_CONF)
+def test_billing_hostname_with_set_in_ini_not_in_env():
+    settings = global_settings()
+    assert settings.utilization.billing_hostname == 'file-hostname'
+
+    local_config, = ApplicationSession._create_connect_payload(
+            '', [], [], newrelic.core.config.global_settings_dump())
+    util_conf = local_config['utilization'].get('config')
+    assert util_conf == {'hostname': 'file-hostname'}

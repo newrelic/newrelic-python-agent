be81b6c4cd9654598f731cfbc8ce74f446fe0037
Tom Offermann <toffermann@newrelic.com>
2016-08-23 14:53:18 -0700

    Add separate Django middleware config for v1.10 test. PYTHON-2028

--- tests/framework_django/wsgi.py
+++ tests/framework_django/wsgi.py
@@ -19,6 +19,9 @@ import os
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "sample.settings"
+#
+# Note that DJANGO_SETTINGS_MODULE may be set in tox.ini!
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 # This application object is used by any WSGI server configured to use this

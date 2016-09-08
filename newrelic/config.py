4306c078384f00da14adbfc51c82ec16f563c02f
Tom Offermann <toffermann@newrelic.com>
2016-09-08 14:50:42 -0700

    Remove database.instrumentation.r2 feature flag. PYTHON-2075
    This feature flag is no longer used anywhere.

--- newrelic/config.py
+++ newrelic/config.py
@@ -55,7 +55,6 @@ _FEATURE_FLAGS = set([
     'tornado.instrumentation.r2',
     'tornado.instrumentation.r3',
     'django.instrumentation.inclusion-tags.r1',
-    'database.instrumentation.r2',
 ])
 
 # Names of configuration file and deployment environment. This

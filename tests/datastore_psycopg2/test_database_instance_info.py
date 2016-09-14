a769d3f9a9ed0d089fb2c0232e5f3368995c30e8
Allan Feldman <afeldman@newrelic.com>
2016-09-14 13:05:50 -0700

    Add testing for psycopg2 URI parsing. PYTHON-2082
    This commit adds more extensive testing of the psycopg2 URI support. URI
    parsing is required as part of instance metric collection for psycopg2.

--- tests/datastore_psycopg2/test_database_instance_info.py
+++ tests/datastore_psycopg2/test_database_instance_info.py
@@ -21,6 +21,16 @@ def test_arg_str_missing_port():
     output = instance_info(*connect_params)
     assert output == ('foobar', None)
 
+def test_arg_str_multiple_host():
+    connect_params = (("host=foobar host=barbaz",), {})
+    output = instance_info(*connect_params)
+    assert output == ('barbaz', None)
+
+def test_arg_str_multiple_port():
+    connect_params = (("port=5555 port=7777",), {})
+    output = instance_info(*connect_params)
+    assert output == (None, '7777')
+
 def test_arg_str_missing_host():
     connect_params = (("port=5555",), {})
     output = instance_info(*connect_params)
@@ -41,3 +51,51 @@ def test_str_in_port_arg_str():
     output = instance_info(*connect_params)
     assert output == (None, 'foobar')
 
+@pytest.mark.parametrize('connect_params,expected', [
+    ((('postgresql://',), {}),
+        (None, None)),
+    ((('postgresql://localhost',), {}),
+        ('localhost', None)),
+    ((('postgresql://localhost:5433',), {}),
+        ('localhost', '5433')),
+    ((('postgresql://localhost/mydb',), {}),
+        ('localhost', None)),
+    ((('postgresql://user@localhost',), {}),
+        ('localhost', None)),
+    ((('postgresql://user:secret@localhost',), {}),
+        ('localhost', None)),
+    ((('postgresql://[2001:db8::1234]/database',), {}),
+        ('[2001:db8::1234]', None)),
+    ((('postgresql://[2001:db8::1234]:2222/database',), {}),
+        ('[2001:db8::1234]', '2222')),
+    ((('postgresql:///dbname?host=/var/lib/postgresql',), {}),
+        ('/var/lib/postgresql',  None)),
+    ((('postgresql://%2Fvar%2Flib%2Fpostgresql/dbname',), {}),
+        ('/var/lib/postgresql',  None)),
+    ((('postgresql://other@localhost/otherdb?c=10&a=myapp',), {}),
+        ('localhost', None)),
+    ((('postgresql:///',), {}),
+        (None, None)),
+    ((('postgresql:///dbname?host=foo',), {}),
+        ('foo', None)),
+    ((('postgresql:///dbname?port=1234',), {}),
+        (None, '1234')),
+    ((('postgresql:///dbname?host=foo&port=1234',), {}),
+        ('foo', '1234')),
+    ((('postgres:///dbname?host=foo&port=1234',), {}),
+        ('foo', '1234')),
+    ((('postgres://localhost:5444/blah?host=::1',), {}),
+        ('::1', '5444')),
+    ((('postgresql:///dbname?host=foo&port=1234&host=bar',), {}),
+        ('bar', '1234')),
+    ((('postgresql://%2Ftmp:1234',), {}),
+        ('/tmp', '1234')),
+])
+def test_uri(connect_params, expected):
+    output = instance_info(*connect_params)
+    assert output == expected
+
+def test_bad_uri():
+    connect_params = (("blah:///foo",), {})
+    output = instance_info(*connect_params)
+    assert output == ('unknown', 'unknown')

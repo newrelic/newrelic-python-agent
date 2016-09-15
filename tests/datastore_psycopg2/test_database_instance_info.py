51fff8d656879e93c13da0f0452043bc70dc287e
Allan Feldman <afeldman@newrelic.com>
2016-09-15 13:43:31 -0700

    Change to more explicit naming. PYTHON-2085

--- tests/datastore_psycopg2/test_database_instance_info.py
+++ tests/datastore_psycopg2/test_database_instance_info.py
@@ -2,42 +2,42 @@ import pytest
 from newrelic.hooks.database_psycopg2 import instance_info
 
 def test_kwargs():
-    args_kwargs = ((), {'database': 'foo', 'host': '1.2.3.4', 'port': 1234})
-    output = instance_info(*args_kwargs)
+    connect_params = ((), {'database': 'foo', 'host': '1.2.3.4', 'port': 1234})
+    output = instance_info(*connect_params)
     assert output == ('1.2.3.4', '1234')
 
 def test_arg_str():
-    args_kwargs = (("host=foobar port=9876",), {})
-    output = instance_info(*args_kwargs)
+    connect_params = (("host=foobar port=9876",), {})
+    output = instance_info(*connect_params)
     assert output == ('foobar', '9876')
 
 def test_kwargs_str_for_port():
-    args_kwargs = ((), {'database': 'foo', 'host': '1.2.3.4', 'port': '1234'})
-    output = instance_info(*args_kwargs)
+    connect_params = ((), {'database': 'foo', 'host': '1.2.3.4', 'port': '1234'})
+    output = instance_info(*connect_params)
     assert output == ('1.2.3.4', '1234')
 
 def test_arg_str_missing_port():
-    args_kwargs = (("host=foobar",), {})
-    output = instance_info(*args_kwargs)
+    connect_params = (("host=foobar",), {})
+    output = instance_info(*connect_params)
     assert output == ('foobar', None)
 
 def test_arg_str_missing_host():
-    args_kwargs = (("port=5555",), {})
-    output = instance_info(*args_kwargs)
+    connect_params = (("port=5555",), {})
+    output = instance_info(*connect_params)
     assert output == (None, '5555')
 
 def test_arg_str_missing_host_and_port():
-    args_kwargs = (("nothing=here",), {})
-    output = instance_info(*args_kwargs)
+    connect_params = (("nothing=here",), {})
+    output = instance_info(*connect_params)
     assert output == (None, None)
 
 def test_malformed_arg_str():
-    args_kwargs = (("this_is_malformed",), {})
-    output = instance_info(*args_kwargs)
+    connect_params = (("this_is_malformed",), {})
+    output = instance_info(*connect_params)
     assert output == ('unknown', 'unknown')
 
 def test_str_in_port_arg_str():
-    args_kwargs = (("port=foobar",), {})
-    output = instance_info(*args_kwargs)
+    connect_params = (("port=foobar",), {})
+    output = instance_info(*connect_params)
     assert output == (None, 'foobar')
 

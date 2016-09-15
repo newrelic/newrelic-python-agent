6d4c030f35c33eeb08d408c4352dd29cf4e31425
Allan Feldman <afeldman@newrelic.com>
2016-09-15 11:01:04 -0700

    Add more descriptive test names. PYTHON-2085

--- tests/datastore_psycopg2/test_database_instance_info.py
+++ tests/datastore_psycopg2/test_database_instance_info.py
@@ -11,27 +11,32 @@ def test_arg_str():
     output = instance_info(*args_kwargs)
     assert output == ('foobar', '9876')
 
-def test_missing_port():
+def test_kwargs_str_for_port():
+    args_kwargs = ((), {'database': 'foo', 'host': '1.2.3.4', 'port': '1234'})
+    output = instance_info(*args_kwargs)
+    assert output == ('1.2.3.4', '1234')
+
+def test_arg_str_missing_port():
     args_kwargs = (("host=foobar",), {})
     output = instance_info(*args_kwargs)
     assert output == ('foobar', None)
 
-def test_missing_host():
+def test_arg_str_missing_host():
     args_kwargs = (("port=5555",), {})
     output = instance_info(*args_kwargs)
     assert output == (None, '5555')
 
-def test_missing_host_and_port():
+def test_arg_str_missing_host_and_port():
     args_kwargs = (("nothing=here",), {})
     output = instance_info(*args_kwargs)
     assert output == (None, None)
 
-def test_malformed_arg():
+def test_malformed_arg_str():
     args_kwargs = (("this_is_malformed",), {})
     output = instance_info(*args_kwargs)
     assert output == ('unknown', 'unknown')
 
-def test_str_in_port():
+def test_str_in_port_arg_str():
     args_kwargs = (("port=foobar",), {})
     output = instance_info(*args_kwargs)
     assert output == (None, 'foobar')

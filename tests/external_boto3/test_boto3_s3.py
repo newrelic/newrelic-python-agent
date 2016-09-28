45aa70e11696ec1f115c619fd8c4e3c3098a0b9b
Reina Abolofia <rabolofia@newrelic.com>
2016-09-28 11:51:38 -0700

    Add boto3 tests for S3. PYTHON-2105

---
+++ tests/external_boto3/test_boto3_s3.py
@@ -0,0 +1,73 @@
+import uuid
+
+import boto3
+import moto
+
+from newrelic.agent import background_task
+from testing_support.fixtures import validate_transaction_metrics
+
+AWS_ACCESS_KEY_ID = 'AAAAAAAAAAAACCESSKEY'
+AWS_SECRET_ACCESS_KEY = 'AAAAAASECRETKEY'
+
+TEST_USER = 'python-agent-test-%s' % uuid.uuid4()
+TEST_BUCKET = 'python-agent-test-%s' % uuid.uuid4()
+
+_s3_scoped_metrics = [
+    ('External/s3-us-west-2.amazonaws.com/botocore/GET', 2),
+    ('External/s3-us-west-2.amazonaws.com/botocore/PUT', 2),
+    ('External/s3-us-west-2.amazonaws.com/botocore/DELETE', 2),
+]
+
+_s3_rollup_metrics = [
+    ('External/all', 6),
+    ('External/allOther', 6),
+    ('External/s3-us-west-2.amazonaws.com/all', 6),
+    ('External/s3-us-west-2.amazonaws.com/botocore/GET', 2),
+    ('External/s3-us-west-2.amazonaws.com/botocore/PUT', 2),
+    ('External/s3-us-west-2.amazonaws.com/botocore/DELETE', 2),
+]
+
+@validate_transaction_metrics(
+        'test_boto3_s3:test_s3',
+        scoped_metrics=_s3_scoped_metrics,
+        rollup_metrics=_s3_rollup_metrics,
+        background_task=True)
+@background_task()
+@moto.mock_s3
+def test_s3():
+    client = boto3.client(
+            's3',
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    )
+
+    # Create bucket
+    resp = client.create_bucket(Bucket=TEST_BUCKET)
+    assert resp['ResponseMetadata']['HTTPStatusCode'] == 200
+
+    # Put object
+    resp = client.put_object(
+            Bucket=TEST_BUCKET,
+            Key='hello_world',
+            Body=b'hello_world_content'
+    )
+    assert resp['ResponseMetadata']['HTTPStatusCode'] == 200
+
+    # List bucket
+    resp = client.list_objects(Bucket=TEST_BUCKET)
+    assert resp['ResponseMetadata']['HTTPStatusCode'] == 200
+    assert len(resp['Contents']) == 1
+    assert resp['Contents'][0]['Key'] == 'hello_world'
+
+    # Get object
+    resp = client.get_object(Bucket=TEST_BUCKET, Key='hello_world')
+    assert resp['ResponseMetadata']['HTTPStatusCode'] == 200
+    assert resp['Body'].read() == b'hello_world_content'
+
+    # Delete object
+    resp = client.delete_object(Bucket=TEST_BUCKET, Key='hello_world')
+    assert resp['ResponseMetadata']['HTTPStatusCode'] == 204
+
+    # Delete bucket
+    resp = client.delete_bucket(Bucket=TEST_BUCKET)
+    assert resp['ResponseMetadata']['HTTPStatusCode'] == 204

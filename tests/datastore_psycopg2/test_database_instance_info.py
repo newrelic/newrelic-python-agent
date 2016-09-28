711a4888670381536aa15eff3e86edd5dd96ac76
Allan Feldman <afeldman@newrelic.com>
2016-09-28 13:56:31 -0700

    Revert "change hostaddr reporting precedence. PYTHON-2099"

--- tests/datastore_psycopg2/test_database_instance_info.py
+++ tests/datastore_psycopg2/test_database_instance_info.py
@@ -54,12 +54,12 @@ def test_str_in_port_arg_str():
 def test_host_and_hostaddr_in_arg_str():
     connect_params = (("host=foobar hostaddr=1.2.3.4",), {})
     output = instance_info(*connect_params)
-    assert output == ('foobar', None, None)
+    assert output == ('1.2.3.4', None, None)
 
 def test_host_and_hostaddr_in_kwarg():
     connect_params = ((), {'host':'foobar', 'hostaddr':'1.2.3.4'})
     output = instance_info(*connect_params)
-    assert output == ('foobar', None, None)
+    assert output == ('1.2.3.4', None, None)
 
 def test_only_hostaddr_in_kwarg():
     connect_params = ((), {'hostaddr':'1.2.3.4'})

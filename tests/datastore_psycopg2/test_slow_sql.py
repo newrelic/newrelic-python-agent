4f1937a0dd253c10af8539c39ca7da6a9fc526ef
Tom Offermann <toffermann@newrelic.com>
2016-10-07 13:37:22 -0700

    Split out test_slow_sql.py in psycopg2 tests. PYTHON-2127

---
+++ tests/datastore_psycopg2/test_slow_sql.py
@@ -0,0 +1,34 @@
+import psycopg2
+
+from testing_support.fixtures import validate_slow_sql_collector_json
+from utils import DB_SETTINGS, POSTGRESQL_VERSION
+
+from newrelic.agent import background_task, global_settings
+
+
+settings = global_settings()
+
+slow_sql_json_required = set()
+slow_sql_json_forgone = set()
+if 'datastore.instances.r1' in settings.feature_flag:
+    # instance/database_name should be reported
+    slow_sql_json_required.add('host')
+    slow_sql_json_required.add('port_path_or_id')
+    slow_sql_json_required.add('database_name')
+else:
+    # instance/database_name should not be reported
+    slow_sql_json_forgone.add('host')
+    slow_sql_json_forgone.add('port_path_or_id')
+    slow_sql_json_forgone.add('database_name')
+
+@validate_slow_sql_collector_json(required_params=slow_sql_json_required,
+        forgone_params=slow_sql_json_forgone)
+@background_task()
+def test_slow_sql_json():
+    with psycopg2.connect(
+            database=DB_SETTINGS['name'], user=DB_SETTINGS['user'],
+            password=DB_SETTINGS['password'], host=DB_SETTINGS['host'],
+            port=DB_SETTINGS['port']) as connection:
+        cursor = connection.cursor()
+        cursor.execute("""SELECT setting from pg_settings where name=%s""",
+                ('server_version',))

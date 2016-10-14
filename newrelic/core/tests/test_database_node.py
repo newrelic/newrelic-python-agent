9b45a72c37e97cf2d46b907d8a177f1e9a769041
Allan Feldman <afeldman@newrelic.com>
2016-10-14 15:44:46 -0700

    Add testing for instance_hostname property. PYTHON-2129

--- newrelic/core/tests/test_database_node.py
+++ newrelic/core/tests/test_database_node.py
@@ -1,38 +1,51 @@
-import unittest
 import newrelic.core.database_node
+from newrelic.common import system_info
 
-class TestDatabaseNodeProperties(unittest.TestCase):
+HOST='cookiemonster'
 
-    def setUp(self):
-        self.db_node = newrelic.core.database_node.DatabaseNode(
-                dbapi2_module=None,
-                sql='COMMIT',
-                children=[],
-                start_time=0.1,
-                end_time=0.9,
-                duration=0.8,
-                exclusive=0.8,
-                stack_trace=[],
-                sql_format='obfuscated',
-                connect_params=((), {'host':'foo', 'port':1234}),
-                cursor_params=None,
-                sql_parameters=None,
-                execute_params=None,
-                host='1.2.3.4',
-                port_path_or_id='1234',
-                database_name='bar')
+_backup_methods = {}
 
-    def test_product_property(self):
-        assert self.db_node.product is None
+def setup_module(module):
 
-    def test_operation(self):
-        assert self.db_node.operation == 'commit'
+    # Mock out the calls used to create the connect payload.
+    def gethostname():
+        return HOST
+    _backup_methods['gethostname'] = system_info.gethostname
+    system_info.gethostname = gethostname
 
-    def test_target(self):
-        assert self.db_node.target == ''
+def teardown_module(module):
+    system_info.gethostname = _backup_methods['gethostname']
 
-    def test_formatted(self):
-        assert self.db_node.formatted == 'COMMIT'
+_db_node = newrelic.core.database_node.DatabaseNode(
+        dbapi2_module=None,
+        sql='COMMIT',
+        children=[],
+        start_time=0.1,
+        end_time=0.9,
+        duration=0.8,
+        exclusive=0.8,
+        stack_trace=[],
+        sql_format='obfuscated',
+        connect_params=((), {'host':'localhost', 'port':1234}),
+        cursor_params=None,
+        sql_parameters=None,
+        execute_params=None,
+        host='localhost',
+        port_path_or_id='1234',
+        database_name='bar')
 
-if __name__ == '__main__':
-    unittest.main()
+def test_product_property():
+    assert _db_node.product is None
+
+def test_operation():
+    assert _db_node.operation == 'commit'
+
+def test_target():
+    assert _db_node.target == ''
+
+def test_formatted():
+    assert _db_node.formatted == 'COMMIT'
+
+def test_instance_hostname():
+    assert _db_node.instance_hostname == HOST
+

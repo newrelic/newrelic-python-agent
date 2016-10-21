76dfdf0a3fae4a398548e43ac1560fe3ff0e4bcb
Reina Abolofia <rabolofia@newrelic.com>
2016-10-21 15:46:01 -0700

    Fix tests for execute_command. PYTHON-2141
    Fixes two issues based on redis-py version.
    
    1. The command 'CLIENT LIST' is not supported until v2.10
    2. Before v2.10 the kwargs `parse` was required if you wanted to parse the
    results of the query.

---
+++ tests/datastore_redis/test_execute_command.py
@@ -0,0 +1,76 @@
+import os
+
+import pytest
+import redis
+
+from testing_support.fixtures import validate_transaction_metrics
+
+from newrelic.agent import background_task, set_background_task
+
+REDIS_HOST = os.environ.get('TDIUM_REDIS_HOST', 'localhost')
+REDIS_PORT = int(os.environ.get('TDIUM_REDIS_PORT', '6379'))
+
+REDIS_HOST = os.environ.get('REDIS_PORT_6379_TCP_ADDR', REDIS_HOST)
+REDIS_PORT = int(os.environ.get('REDIS_PORT_6379_TCP_PORT', REDIS_PORT))
+
+REDIS_PY_VERSION = redis.VERSION
+
+_test_execute_command_scoped_metrics = [
+        ('Datastore/operation/Redis/client_list', 1),
+]
+
+_test_execute_command_rollup_metrics = [
+        ('Datastore/all', 1),
+        ('Datastore/allOther', 1),
+        ('Datastore/Redis/all', 1),
+        ('Datastore/Redis/allOther', 1),
+        ('Datastore/operation/Redis/client_list', 1),
+]
+
+@validate_transaction_metrics(
+        'test_execute_command:test_strict_redis_execute_command_two_args',
+        scoped_metrics=_test_execute_command_scoped_metrics,
+        rollup_metrics=_test_execute_command_rollup_metrics,
+        background_task=True)
+@background_task()
+def test_strict_redis_execute_command_two_args():
+    r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0)
+
+    r.execute_command('CLIENT', 'LIST', parse='LIST')
+
+@validate_transaction_metrics(
+        'test_execute_command:test_redis_execute_command_two_args',
+        scoped_metrics=_test_execute_command_scoped_metrics,
+        rollup_metrics=_test_execute_command_rollup_metrics,
+        background_task=True)
+@background_task()
+def test_redis_execute_command_two_args():
+    r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
+
+    r.execute_command('CLIENT', 'LIST', parse='LIST')
+
+@pytest.mark.skipif(REDIS_PY_VERSION < (2, 10),
+        reason='This command is not implemented yet')
+@validate_transaction_metrics(
+        'test_execute_command:test_strict_redis_execute_command_as_one_arg',
+        scoped_metrics=_test_execute_command_scoped_metrics,
+        rollup_metrics=_test_execute_command_rollup_metrics,
+        background_task=True)
+@background_task()
+def test_strict_redis_execute_command_as_one_arg():
+    r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0)
+
+    r.execute_command('CLIENT LIST')
+
+@pytest.mark.skipif(REDIS_PY_VERSION < (2, 10),
+        reason='This command is not implemented yet')
+@validate_transaction_metrics(
+        'test_execute_command:test_redis_execute_command_as_one_arg',
+        scoped_metrics=_test_execute_command_scoped_metrics,
+        rollup_metrics=_test_execute_command_rollup_metrics,
+        background_task=True)
+@background_task()
+def test_redis_execute_command_as_one_arg():
+    r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
+
+    r.execute_command('CLIENT LIST')
---
+++ tests/datastore_redis/test_get_and_set.py
@@ -0,0 +1,51 @@
+import os
+
+import redis
+
+from testing_support.fixtures import validate_transaction_metrics
+
+from newrelic.agent import background_task, set_background_task
+
+REDIS_HOST = os.environ.get('TDIUM_REDIS_HOST', 'localhost')
+REDIS_PORT = int(os.environ.get('TDIUM_REDIS_PORT', '6379'))
+
+REDIS_HOST = os.environ.get('REDIS_PORT_6379_TCP_ADDR', REDIS_HOST)
+REDIS_PORT = int(os.environ.get('REDIS_PORT_6379_TCP_PORT', REDIS_PORT))
+
+_test_redis_scoped_metrics = [
+        ('Datastore/operation/Redis/get', 1),
+        ('Datastore/operation/Redis/set', 1),
+]
+
+_test_redis_rollup_metrics = [
+        ('Datastore/all', 2),
+        ('Datastore/allOther', 2),
+        ('Datastore/Redis/all', 2),
+        ('Datastore/Redis/allOther', 2),
+        ('Datastore/operation/Redis/get', 1),
+        ('Datastore/operation/Redis/get', 1),
+]
+
+@validate_transaction_metrics(
+        'test_get_and_set:test_strict_redis_operation',
+        scoped_metrics=_test_redis_scoped_metrics,
+        rollup_metrics=_test_redis_rollup_metrics,
+        background_task=True)
+@background_task()
+def test_strict_redis_operation():
+    r = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=0)
+
+    r.set('key', 'value')
+    r.get('key')
+
+@validate_transaction_metrics(
+        'test_get_and_set:test_redis_operation',
+        scoped_metrics=_test_redis_scoped_metrics,
+        rollup_metrics=_test_redis_rollup_metrics,
+        background_task=True)
+@background_task()
+def test_redis_operation():
+    r = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
+
+    r.set('key', 'value')
+    r.get('key')

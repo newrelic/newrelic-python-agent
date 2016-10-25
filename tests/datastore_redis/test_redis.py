e4e41a192d2f81c8a78afccd5d5d325dec60ad41
Tom Offermann <toffermann@newrelic.com>
2016-10-25 14:28:30 -0700

    Create Datastore instance metrics. PYTHON-2136
    Gate creation of instance metrics on the configuration setting:
    
        datastore_tracer.instance_reporting.enabled
    
    Add tests to confirm that enabling/disabling this setting will
    control whether or not the instance metrics get created.

--- tests/datastore_redis/test_redis.py
+++ tests/datastore_redis/test_redis.py
@@ -1,58 +1,124 @@
 import pytest
-
 import redis
 
-from testing_support.fixtures import validate_transaction_metrics
-from testing_support.settings import redis_multiple_settings
-
 from newrelic.agent import background_task
+
+from testing_support.fixtures import (validate_transaction_metrics,
+    override_application_settings)
+from testing_support.settings import redis_multiple_settings
+from testing_support.util import instance_hostname
 
 DB_MULTIPLE_SETTINGS = redis_multiple_settings()
 DB_SETTINGS = DB_MULTIPLE_SETTINGS[0]
 
-_test_redis_scoped_metrics = [
+# Settings
+
+_enable_instance_settings = {
+    'datastore_tracer.instance_reporting.enabled': True,
+}
+_disable_instance_settings = {
+    'datastore_tracer.instance_reporting.enabled': False,
+}
+
+# Metrics
+
+_base_scoped_metrics = (
         ('Datastore/operation/Redis/get', 1),
         ('Datastore/operation/Redis/set', 1),
-        ('Datastore/operation/Redis/client_list', 2)]
+        ('Datastore/operation/Redis/client_list', 2),
+)
 
-_test_redis_rollup_metrics = [
+_base_rollup_metrics = (
         ('Datastore/all', 4),
         ('Datastore/allOther', 4),
         ('Datastore/Redis/all', 4),
         ('Datastore/Redis/allOther', 4),
         ('Datastore/operation/Redis/get', 1),
         ('Datastore/operation/Redis/get', 1),
-        ('Datastore/operation/Redis/client_list', 2)]
+        ('Datastore/operation/Redis/client_list', 2),
+)
 
+_disable_scoped_metrics = list(_base_scoped_metrics)
+_disable_rollup_metrics = list(_base_rollup_metrics)
+
+_enable_scoped_metrics = list(_base_scoped_metrics)
+_enable_rollup_metrics = list(_base_rollup_metrics)
+
+_host = instance_hostname(DB_SETTINGS['host'])
+_port = DB_SETTINGS['port']
+
+_instance_metric_name = 'Datastore/instance/Redis/%s/%s' % (_host, _port)
+
+_enable_rollup_metrics.append(
+        (_instance_metric_name, 2)
+)
+
+_disable_rollup_metrics.append(
+        (_instance_metric_name, None)
+)
+
+# Operations
+
+def exercise_redis(client):
+    client.set('key', 'value')
+    client.get('key')
+
+    client.execute_command('CLIENT LIST')
+    client.execute_command('CLIENT', 'LIST')
+
+# Tests
+
+@override_application_settings(_enable_instance_settings)
 @validate_transaction_metrics(
-        'test_redis:test_strict_redis_operation',
-        scoped_metrics=_test_redis_scoped_metrics,
-        rollup_metrics=_test_redis_rollup_metrics,
+        'test_redis:test_strict_redis_operation_enable_instance',
+        scoped_metrics=_enable_scoped_metrics,
+        rollup_metrics=_enable_rollup_metrics,
         background_task=True)
 @background_task()
-def test_strict_redis_operation():
-    r = redis.StrictRedis(host=DB_SETTINGS['host'], port=DB_SETTINGS['port'], db=0)
+def test_strict_redis_operation_enable_instance():
+    client = redis.StrictRedis(host=DB_SETTINGS['host'],
+            port=DB_SETTINGS['port'], db=0)
+    exercise_redis(client)
 
-    r.set('key', 'value')
-    r.get('key')
 
-    r.execute_command('CLIENT LIST')
-    r.execute_command('CLIENT', 'LIST')
-
+@override_application_settings(_disable_instance_settings)
 @validate_transaction_metrics(
-        'test_redis:test_redis_operation',
-        scoped_metrics=_test_redis_scoped_metrics,
-        rollup_metrics=_test_redis_rollup_metrics,
+        'test_redis:test_strict_redis_operation_disable_instance',
+        scoped_metrics=_disable_scoped_metrics,
+        rollup_metrics=_disable_rollup_metrics,
         background_task=True)
 @background_task()
-def test_redis_operation():
-    r = redis.Redis(host=DB_SETTINGS['host'], port=DB_SETTINGS['port'], db=0)
+def test_strict_redis_operation_disable_instance():
+    client = redis.StrictRedis(host=DB_SETTINGS['host'],
+            port=DB_SETTINGS['port'], db=0)
+    exercise_redis(client)
 
-    r.set('key', 'value')
-    r.get('key')
 
-    r.execute_command('CLIENT LIST')
-    r.execute_command('CLIENT', 'LIST')
+@override_application_settings(_enable_instance_settings)
+@validate_transaction_metrics(
+        'test_redis:test_redis_operation_enable_instance',
+        scoped_metrics=_enable_scoped_metrics,
+        rollup_metrics=_enable_rollup_metrics,
+        background_task=True)
+@background_task()
+def test_redis_operation_enable_instance():
+    client = redis.StrictRedis(host=DB_SETTINGS['host'],
+            port=DB_SETTINGS['port'], db=0)
+    exercise_redis(client)
+
+
+@override_application_settings(_disable_instance_settings)
+@validate_transaction_metrics(
+        'test_redis:test_redis_operation_disable_instance',
+        scoped_metrics=_disable_scoped_metrics,
+        rollup_metrics=_disable_rollup_metrics,
+        background_task=True)
+@background_task()
+def test_redis_operation_disable_instance():
+    client = redis.StrictRedis(host=DB_SETTINGS['host'],
+            port=DB_SETTINGS['port'], db=0)
+    exercise_redis(client)
+
 
 _test_multiple_databases_scoped_metrics = [
         ('Datastore/operation/Redis/get', 1),

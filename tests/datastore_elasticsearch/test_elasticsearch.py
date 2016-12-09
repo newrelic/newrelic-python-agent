ec0067dcc22a2b36b7e77241f43e48297fcb680b
Reina Abolofia <rabolofia@newrelic.com>
2016-12-08 16:50:05 -0800

    Multi-db tests for elasticsearch driver. PYTHON-2172
    Adds two new test files.
    
    Test 1. Given two elasticsearch instances, create a client for each and add
    something to the index.
    
    Test 2. Given two elasticsearch instances, create a single client for both, add
    an item to each instance, then go a bulk get for those two items.

--- tests/datastore_elasticsearch/test_elasticsearch.py
+++ tests/datastore_elasticsearch/test_elasticsearch.py
@@ -5,12 +5,12 @@ import elasticsearch.client
 
 from testing_support.fixtures import (validate_transaction_metrics,
     validate_transaction_errors, validate_database_duration)
-from testing_support.settings import elasticsearch_settings
+from testing_support.settings import elasticsearch_multiple_settings
 
 from newrelic.agent import background_task
 
-ES_HOST, ES_PORT = elasticsearch_settings()
-ES_URL = 'http://%s:%s' % (ES_HOST, ES_PORT)
+ES_SETTINGS = elasticsearch_multiple_settings()[0]
+ES_URL = 'http://%s:%s' % (ES_SETTINGS['host'], ES_SETTINGS['port'])
 
 def _exercise_es(es):
     es.index("contacts", "person",

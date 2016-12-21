abab01c485939f1b2271404004d3c3229101280e
Allan Feldman <afeldman@newrelic.com>
2016-12-20 17:33:59 -0800

    Update devpi packages. PYTHON-2186

--- docker/devpi/pre-build.sh
+++ docker/devpi/pre-build.sh
@@ -4,8 +4,8 @@ OUT_DIR=docker/devpi/package-lists/
 mkdir -p $OUT_DIR
 
 EXCLUDE_ALL="mysql-connector-python"
-SOURCE_ONLY="importlib==1.0.4"
-EXTRA_PACKAGES="CherryPy==8.1.3 cffi==1.9.1 docutils==0.13.1 httpretty==0.8.10 importlib==1.0.4 ordereddict==1.1 pyramid==1.4.9 pyramid==1.5b1 simplejson==3.3.0 tornado==2.4.1"
+SOURCE_ONLY="importlib==1.0.4 psycopg2cffi"
+EXTRA_PACKAGES="importlib==1.0.4 ordereddict==1.1 cffi==1.9.1 httpretty==0.8.10 docutils==0.13.1 simplejson==3.3.0 pyramid==1.5b1 nose==1.3.7"
 
 test -n "$EXCLUDE_ALL" && EXCLUDE_ALL="-e $EXCLUDE_ALL"
 test -n "$SOURCE_ONLY" && SOURCE_ONLY="-s $SOURCE_ONLY"

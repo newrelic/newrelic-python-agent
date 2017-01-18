3db976e3038e9f6cf58c2d7f55f8b32f47bc1a61
Rey Abolofia <rabolofia@newrelic.com>
2017-01-18 14:28:36 -0800

    Fix long lines for readability. PYTHON-2207

--- docker/devpi/pre-build.sh
+++ docker/devpi/pre-build.sh
@@ -4,9 +4,20 @@ OUT_DIR=docker/devpi/package-lists/
 mkdir -p $OUT_DIR
 rm -rf $OUT_DIR/*
 
-EXCLUDE_ALL="mysql-connector-python"
-SOURCE_ONLY="importlib==1.0.4 psycopg2cffi"
-EXTRA_PACKAGES="importlib==1.0.4 ordereddict==1.1 cffi==1.9.1 httpretty==0.8.10 docutils==0.13.1 simplejson==3.3.0 pyramid==1.5b1 nose==1.3.7"
+EXCLUDE_ALL="
+    mysql-connector-python"
+SOURCE_ONLY="
+    importlib==1.0.4
+    psycopg2cffi"
+EXTRA_PACKAGES="
+    importlib==1.0.4
+    ordereddict==1.1
+    cffi==1.9.1
+    httpretty==0.8.10
+    docutils==0.13.1
+    simplejson==3.3.0
+    pyramid==1.5b1
+    nose==1.3.7"
 
 if [[ -f tests/base_requirements.txt ]]
 then
@@ -18,4 +29,9 @@ test -n "$SOURCE_ONLY" && SOURCE_ONLY="-s $SOURCE_ONLY"
 test -n "$EXTRA_PACKAGES" && EXTRA_PACKAGES="-x $EXTRA_PACKAGES"
 
 TOX_FILES=$(git ls-files '*tox*.ini')
-$(python docker/devpi/parseconfig.py $TOX_FILES -o $OUT_DIR $EXCLUDE_ALL $SOURCE_ONLY $EXTRA_PACKAGES)
+python docker/devpi/parseconfig.py \
+    "$TOX_FILES" \
+    -o "$OUT_DIR" \
+    "$EXCLUDE_ALL" \
+    "$SOURCE_ONLY" \
+    "$EXTRA_PACKAGES"

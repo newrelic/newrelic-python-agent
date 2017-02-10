2821528cc5281304c9616ebda0e7a0ba59f4b053
Matthew Sneeden <msneeden@newrelic.com>
2017-02-10 06:41:13 -0800

    Fetch snakeyaml instead of 'grab'

--- jenkins/test-global.groovy
+++ jenkins/test-global.groovy
@@ -107,6 +107,7 @@ use(extensions) {
             }
 
             steps {
+                shell(readFileFromWorkspace('./jenkins/fetch_snakeyaml.sh'))
                 reseedFrom('jenkins/test-integration.groovy')
             }
         }

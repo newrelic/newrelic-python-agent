c03756aa68a1a83368e480e2972017bb6f8a3cd8
Rey Abolofia <rabolofia@newrelic.com>
2017-03-27 14:12:53 -0700

    Flake8. PYTHON-2279

--- tests/agent_features/test_function_trace.py
+++ tests/agent_features/test_function_trace.py
@@ -6,6 +6,7 @@ from newrelic.agent import background_task, FunctionTrace, current_transaction
 _test_function_trace_default_group_scoped_metrics = [
         ('Function/FunctionTrace', 1)]
 
+
 @validate_transaction_metrics(
         'test_function_trace:test_function_trace_default_group',
         scoped_metrics=_test_function_trace_default_group_scoped_metrics,
@@ -16,8 +17,10 @@ def test_function_trace_default_group():
     with FunctionTrace(transaction, 'FunctionTrace'):
         pass
 
+
 _test_function_trace_valid_override_scoped_metrics = [
         ('Custom/FunctionTrace', 1)]
+
 
 @validate_transaction_metrics(
         'test_function_trace:test_function_trace_valid_override',
@@ -29,8 +32,10 @@ def test_function_trace_valid_override():
     with FunctionTrace(transaction, 'FunctionTrace', group='Custom'):
         pass
 
+
 _test_function_trace_empty_group_scoped_metrics = [
         ('Function/FunctionTrace', 1)]
+
 
 @validate_transaction_metrics(
         'test_function_trace:test_function_trace_empty_group',
@@ -42,13 +47,15 @@ def test_function_trace_empty_group():
     with FunctionTrace(transaction, 'FunctionTrace', group=''):
         pass
 
+
 _test_function_trace_leading_slash_on_group_scoped_metrics = [
         ('Function/Group/FunctionTrace', 1)]
 
+
 @validate_transaction_metrics(
-        'test_function_trace:test_function_trace_leading_slash_on_group',
-        scoped_metrics=_test_function_trace_leading_slash_on_group_scoped_metrics,
-        background_task=True)
+    'test_function_trace:test_function_trace_leading_slash_on_group',
+    scoped_metrics=_test_function_trace_leading_slash_on_group_scoped_metrics,
+    background_task=True)
 @background_task()
 def test_function_trace_leading_slash_on_group():
     transaction = current_transaction()

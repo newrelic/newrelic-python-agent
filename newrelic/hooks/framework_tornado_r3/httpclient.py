876eb06f656ad1e5e5f64416d1bd75137716a832
Allan Feldman <afeldman@newrelic.com>
2017-05-11 10:59:57 -0700

    Fix tornado exception handling for external requests. PYTHON-2343

--- newrelic/hooks/framework_tornado_r3/httpclient.py
+++ newrelic/hooks/framework_tornado_r3/httpclient.py
@@ -1,3 +1,4 @@
+import sys
 from tornado.httpclient import HTTPRequest
 
 from newrelic.agent import ExternalTrace, wrap_function_wrapper
@@ -6,38 +7,18 @@ from .util import retrieve_current_transaction
 
 def _prepare_request(*args, **kwargs):
 
-    def _extract_request(request, *_args, **_kwargs):
-        return request, _args, _kwargs
+    def _extract_request(request, callback=None, raise_error=True, **_kwargs):
+        return request, callback, raise_error, _kwargs
 
-    request, _args, _kwargs = _extract_request(*args, **kwargs)
+    request, callback, raise_error, _kwargs = _extract_request(*args,
+            **kwargs)
 
     # request is either a string or a HTTPRequest object
     if not isinstance(request, HTTPRequest):
         url = request
-        request = HTTPRequest(url, *_args, **_kwargs)
+        request = HTTPRequest(url, **_kwargs)
 
-    return request
-
-
-def _nr_wrapper_httpclient_HTTPClient_fetch_(wrapped, instance, args, kwargs):
-
-    transaction = retrieve_current_transaction()
-
-    if transaction is None:
-        return wrapped(*args, **kwargs)
-
-    req = _prepare_request(*args, **kwargs)
-
-    # Prepare outgoing CAT headers
-    outgoing_headers = ExternalTrace.generate_request_headers(transaction)
-    for header_name, header_value in outgoing_headers:
-        req.headers.add(header_name, header_value)
-
-    with ExternalTrace(transaction, 'tornado.httpclient', req.url) as trace:
-        response = wrapped(req)
-        # Process CAT response headers
-        trace.process_response_headers(response.headers.get_all())
-        return response
+    return request, callback, raise_error
 
 
 def _nr_wrapper_httpclient_AsyncHTTPClient_fetch_(
@@ -48,10 +29,7 @@ def _nr_wrapper_httpclient_AsyncHTTPClient_fetch_(
     if transaction is None:
         return wrapped(*args, **kwargs)
 
-    def _prepare_async_req(request, callback=None, raise_error=True, **kwargs):
-        return _prepare_request(request, **kwargs), callback, raise_error
-
-    req, _cb, _raise_error = _prepare_async_req(*args, **kwargs)
+    req, _cb, _raise_error = _prepare_request(*args, **kwargs)
 
     # Prepare outgoing CAT headers
     outgoing_headers = ExternalTrace.generate_request_headers(transaction)
@@ -73,14 +51,16 @@ def _nr_wrapper_httpclient_AsyncHTTPClient_fetch_(
 
     transaction._ref_count += 1
     trace.__enter__()
-
-    future = wrapped(req, _cb, _raise_error)
-    future.add_done_callback(external_trace_done)
+    try:
+        future = wrapped(req, _cb, _raise_error)
+        future.add_done_callback(external_trace_done)
+    except Exception:
+        transaction._ref_count -= 1
+        trace.__exit__(*sys.exc_info())
+        raise
     return future
 
 
 def instrument_tornado_httpclient(module):
-    wrap_function_wrapper(module, 'HTTPClient.fetch',
-            _nr_wrapper_httpclient_HTTPClient_fetch_)
     wrap_function_wrapper(module, 'AsyncHTTPClient.fetch',
             _nr_wrapper_httpclient_AsyncHTTPClient_fetch_)

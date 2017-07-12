e66a5edfdea5ff0e582b03e745f7b94792bbc11c
Mike McConnell <mmcconnell@newrelic.com>
2017-07-12 16:53:32 -0700

    Add GCP vendor utilization. PYTHON-2392

---
+++ newrelic/common/utilization_gcp.py
@@ -0,0 +1,28 @@
+
+from newrelic.common.utilization_common import CommonUtilization
+
+
+class GCPUtilization(CommonUtilization):
+    EXPECTED_KEYS = ('id', 'machineType', 'name', 'zone')
+    HEADERS = {'Metadata-Flavor': 'Google'}
+    METADATA_URL = 'http://%s/computeMetadata/v1/instance/?recursive=true' % (
+            'metadata.google.internal')
+    VENDOR_NAME = 'gcp'
+
+    @staticmethod
+    def normalize(key, data):
+        if key in ('machineType', 'zone'):
+            formatted = data.strip().split('/')[-1]
+        elif key == 'id':
+            formatted = str(data)
+        else:
+            formatted = data.strip()
+
+        if GCPUtilization.valid_length(formatted) and \
+                GCPUtilization.valid_chars(formatted):
+            return formatted
+
+
+def gcp_data():
+    gcp = GCPUtilization()
+    return gcp.detect()

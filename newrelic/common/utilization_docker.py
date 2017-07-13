635ed4198c3a0e94768eb714323d812766170af7
Allan Feldman <afeldman@newrelic.com>
2017-07-13 11:16:53 -0700

    Add docker utilization detection. PYTHON-2407

---
+++ newrelic/common/utilization_docker.py
@@ -0,0 +1,61 @@
+import string
+from newrelic.common.utilization_common import CommonUtilization
+
+
+class DockerUtilization(CommonUtilization):
+    VENDOR_NAME = 'docker'
+    EXPECTED_KEYS = ['id']
+    METADATA_FILE = '/proc/self/cgroup'
+
+    @classmethod
+    def fetch(cls):
+        try:
+            with open(cls.METADATA_FILE, 'rb') as f:
+                for line in f:
+                    stripped = line.strip()
+                    if stripped.startswith('2:cpu'):
+                        return stripped
+        except:
+            # There are all sorts of exceptions that can occur here
+            # (i.e. permissions, non-existent file, etc)
+            pass
+
+    @classmethod
+    def get_values(cls, contents):
+        if contents is None:
+            return
+
+        # parse the line
+        if '/' not in contents:
+            return
+
+        value = contents.split('/')[-1]
+
+        return {'id': value}
+
+    @classmethod
+    def valid_chars(cls, data):
+        if data is None:
+            return False
+
+        hex_digits = set(string.hexdigits)
+
+        valid = all((c in hex_digits for c in data))
+        if valid:
+            return True
+
+        cls.record_error(cls.METADATA_FILE, data)
+        return False
+
+    @classmethod
+    def valid_length(cls, data):
+        if data is None:
+            return False
+
+        # Must be exactly 64 characters
+        valid = len(data) == 64
+        if valid:
+            return True
+
+        cls.record_error(cls.METADATA_FILE, data)
+        return False

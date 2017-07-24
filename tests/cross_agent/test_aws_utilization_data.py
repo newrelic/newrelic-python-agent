9d59cd84a9e79b4222e82aafc30f5a0d45938971
Rey Abolofia <rabolofia@newrelic.com>
2017-07-24 16:52:22 -0700

    Update test_aws_utilization_data test for new fixture. PYTHON-2418

--- tests/cross_agent/test_aws_utilization_data.py
+++ tests/cross_agent/test_aws_utilization_data.py
@@ -4,13 +4,14 @@ import os
 import pytest
 
 from newrelic.packages import requests
-from newrelic.common.utilization import aws_data
+from newrelic.common.utilization_aws import AWSUtilization
 
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
-FIXTURE = os.path.normpath(os.path.join(CURRENT_DIR, 'fixtures', 'aws.json'))
+FIXTURE = os.path.normpath(os.path.join(CURRENT_DIR, 'fixtures',
+    'utilization_vendor_specific', 'aws.json'))
 
-_parameters_list = ['testname', 'uris', 'expected_vendors_hash',
+_parameters_list = ['testname', 'uri', 'expected_vendors_hash',
         'expected_metrics']
 
 _parameters = ','.join(_parameters_list)
@@ -28,13 +29,6 @@ def _parametrize_test(test):
 
 _aws_tests = [_parametrize_test(t) for t in _load_tests()]
 
-_metadata_urls = {
-    'id_url': 'http://169.254.169.254/2008-02-01/meta-data/instance-id',
-    'type_url': 'http://169.254.169.254/2008-02-01/meta-data/instance-type',
-    'zone_url': ('http://169.254.169.254/2008-02-01/meta-data/'
-            'placement/availability-zone'),
-}
-
 
 class MockResponse(object):
 
@@ -42,13 +36,15 @@ class MockResponse(object):
         self.code = code
         self.text = body
 
+    def raise_for_status(self):
+        assert str(self.code) == '200'
+
+    def json(self):
+        return self.text
+
 
 @pytest.mark.parametrize(_parameters, _aws_tests)
-def test_aws(testname, uris, expected_vendors_hash, expected_metrics):
-
-    # Sanity check that the uris in the test match what we expect
-
-    assert sorted(_metadata_urls.values()) == sorted(uris.keys())
+def test_aws(testname, uri, expected_vendors_hash, expected_metrics):
 
     # Generate mock responses for requests.Session.get
 
@@ -58,19 +54,17 @@ def test_aws(testname, uris, expected_vendors_hash, expected_metrics):
         else:
             return MockResponse('200', api_result['response'])
 
-    mock_return_values = [_get_mock_return_value(api_result) for api_result in
-            (uris[_metadata_urls['id_url']],
-             uris[_metadata_urls['type_url']],
-             uris[_metadata_urls['zone_url']])]
+    mock_return_values = (
+            _get_mock_return_value(uri[AWSUtilization.METADATA_URL]),)
 
     # Define function that actually runs the test
 
-    @mock.patch('newrelic.common.utilization.internal_metric')
+    @mock.patch('newrelic.common.utilization_common.internal_metric')
     @mock.patch.object(requests.Session, 'get')
     def _test_aws_data(mock_get, mock_internal_metric):
         mock_get.side_effect = mock_return_values
 
-        data = aws_data()
+        data = AWSUtilization.detect()
 
         if data:
             aws_vendor_hash = {'aws': data}

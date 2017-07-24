30100acd6bc61d434b764cc71d6cabc43ba0c512
Allan Feldman <afeldman@newrelic.com>
2017-07-24 13:55:29 -0700

    Update PCF to disable default invalid data messages. PYTHON-2431

--- newrelic/common/utilization_pivotal.py
+++ newrelic/common/utilization_pivotal.py
@@ -7,14 +7,20 @@ class PCFUtilization(CommonUtilization):
     EXPECTED_KEYS = ('cf_instance_guid', 'cf_instance_ip', 'memory_limit')
     VENDOR_NAME = 'pcf'
 
-    @classmethod
-    def fetch(cls):
-        return {
-            'cf_instance_guid': os.environ.get('CF_INSTANCE_GUID'),
-            'cf_instance_ip': os.environ.get('CF_INSTANCE_IP'),
-            'memory_limit': os.environ.get('MEMORY_LIMIT'),
-        }
+    @staticmethod
+    def fetch():
+        cf_instance_guid = os.environ.get('CF_INSTANCE_GUID')
+        cf_instance_ip = os.environ.get('CF_INSTANCE_IP')
+        memory_limit = os.environ.get('MEMORY_LIMIT')
+        pcf_vars = (cf_instance_guid, cf_instance_ip, memory_limit)
+        if all(pcf_vars):
+            return pcf_vars
 
-    @classmethod
-    def get_values(cls, response):
-        return response
+    @staticmethod
+    def get_values(response):
+        if response is not None and len(response) == 3:
+            return {
+                'cf_instance_guid': response[0],
+                'cf_instance_ip': response[1],
+                'memory_limit': response[2],
+            }

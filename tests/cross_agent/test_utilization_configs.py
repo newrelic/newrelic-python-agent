4c0b392d4057dcee9bf778b320b8872a89159fcf
Rey Abolofia <rabolofia@newrelic.com>
2017-07-26 15:54:09 -0700

    Fix utilization tests to not use a mock http server. PYTHON-2418

--- tests/cross_agent/test_utilization_configs.py
+++ tests/cross_agent/test_utilization_configs.py
@@ -8,9 +8,12 @@ import tempfile
 # methods in newrelic.core.data_collector and does not put them back!
 from newrelic.core.data_collector import ApplicationSession
 from newrelic.common.system_info import BootIdUtilization
+from newrelic.common.utilization_aws import AWSUtilization
+from newrelic.common.utilization_azure import AzureUtilization
+from newrelic.common.utilization_gcp import GCPUtilization
+from newrelic.common.object_wrapper import (transient_function_wrapper,
+        function_wrapper)
 import newrelic.core.config
-
-from testing_support.mock_external_http_server import MockExternalHTTPServer
 
 try:
     # python 2.x
@@ -106,86 +109,140 @@ class UpdatedSettings(object):
         newrelic.core.config._settings = self.initial_settings
 
 
-def assert_dicts_less_than_or_equal(dict1, dict2):
-    """Assert that all the key, value pairs in dict1 match those found in
-    dict2. There can be some keys in dict2 that are not in dict1, but if there
-    are keys in dict1 that are not in dict2, raise AssertionError.
-
-    """
-    for key in dict1.keys():
-        val1 = dict1.get(key)
-        val2 = dict2.get(key)
-        if isinstance(val1, dict) and isinstance(val2, dict):
-            assert_dicts_less_than_or_equal(val1, val2)
-        elif val1 != val2:
-            raise AssertionError('For key %s: %s != %s' % (key, val2, val1))
+def _get_effected_url_for_test(test):
+    if test.get('input_aws_id'):
+        return AWSUtilization.METADATA_URL
+    elif test.get('input_azure_id'):
+        return AzureUtilization.METADATA_URL
+    elif test.get('input_gcp_id'):
+        return GCPUtilization.METADATA_URL
+    elif test.get('input_boot_id'):
+        return BootIdUtilization.METADATA_URL
 
 
-class MockMetadataEndpoints(object):
-    def __init__(self, external, test):
-        self.external = external
-        self.test = test
-        self.boot_id_file = None
+def _get_response_body_for_test(test):
+    if test.get('input_aws_id'):
+        return json.dumps({
+            'instanceId': test.get('input_aws_id'),
+            'instanceType': test.get('input_aws_type'),
+            'availabilityZone': test.get('input_aws_zone'),
+        }).encode('utf8')
+    if test.get('input_azure_id'):
+        return json.dumps({
+            'location': test.get('input_azure_location'),
+            'name': test.get('input_azure_name'),
+            'vmId': test.get('input_azure_id'),
+            'vmSize': test.get('input_azure_size'),
+        }).encode('utf8')
+    if test.get('input_gcp_id'):
+        return json.dumps({
+            'id': test.get('input_gcp_id'),
+            'machineType': test.get('input_gcp_type'),
+            'name': test.get('input_gcp_name'),
+            'zone': test.get('input_gcp_zone'),
+        }).encode('utf8')
 
-    def __enter__(self):
-        dc = newrelic.core.data_collector
 
-        # mock metadata urls
-        if self.test.get('input_aws_id'):
-            MockExternalHTTPServer.RESPONSE = json.dumps({
-                'instanceId': self.test.get('input_aws_id'),
-                'instanceType': self.test.get('input_aws_type'),
-                'availabilityZone': self.test.get('input_aws_zone'),
-            }).encode('utf8')
-            dc.AWSUtilization.METADATA_URL = (
-                    'http://localhost:%s' % self.external.port)
-        if self.test.get('input_azure_id'):
-            MockExternalHTTPServer.RESPONSE = json.dumps({
-                'location': self.test.get('input_azure_location'),
-                'name': self.test.get('input_azure_name'),
-                'vmId': self.test.get('input_azure_id'),
-                'vmSize': self.test.get('input_azure_size'),
-            }).encode('utf8')
-            dc.AzureUtilization.METADATA_URL = (
-                    'http://localhost:%s' % self.external.port)
-        if self.test.get('input_gcp_id'):
-            MockExternalHTTPServer.RESPONSE = json.dumps({
-                'id': self.test.get('input_gcp_id'),
-                'machineType': self.test.get('input_gcp_type'),
-                'name': self.test.get('input_gcp_name'),
-                'zone': self.test.get('input_gcp_zone'),
-            }).encode('utf8')
-            dc.GCPUtilization.METADATA_URL = (
-                    'http://localhost:%s' % self.external.port)
-        if self.test.get('input_boot_id'):
-            self.boot_id_file = tempfile.NamedTemporaryFile()
-            self.boot_id_file.write(self.test.get('input_boot_id'))
-            self.boot_id_file.seek(0)
-            BootIdUtilization.METADATA_URL = self.boot_id_file.name
+class MockResponse(object):
+
+    def __init__(self, code, body):
+        self.code = code
+        self.text = body
+
+    def raise_for_status(self):
+        assert str(self.code) == '200'
+
+    def json(self):
+        if hasattr(self.text, 'decode'):
+            self.text = self.text.decode('utf-8')
+        return json.loads(self.text)
+
+
+def patch_Session_get(test):
+    @transient_function_wrapper('newrelic.packages.requests', 'Session.get')
+    def _patch_Session_get(wrapped, instance, args, kwargs):
+        def _bind_params(url, *args, **kwargs):
+            return url
+
+        url = _bind_params(*args, **kwargs)
+        effected_url = _get_effected_url_for_test(test)
+        if url != effected_url:
+            return MockResponse('500', 'Not the correct url, this is fine')
+
+        body = _get_response_body_for_test(test)
+        return MockResponse('200', body)
+    return _patch_Session_get
+
+
+def patch_boot_id_file(test):
+    @function_wrapper
+    def _patch_boot_id_file(wrapped, instance, args, kwargs):
+        boot_id_file = None
+        initial_sys_platform = sys.platform
+
+        if test.get('input_boot_id'):
+            boot_id_file = tempfile.NamedTemporaryFile()
+            boot_id_file.write(test.get('input_boot_id'))
+            boot_id_file.seek(0)
+            BootIdUtilization.METADATA_URL = boot_id_file.name
             sys.platform = 'linux-mock-testing'  # ensure boot_id is gathered
+        else:
+            # do not gather boot_id at all, this will ensure there is nothing
+            # extra in the gathered utilizations data
+            sys.platform = 'not-linux'
 
-        # mock the methods that derive the data for the payload
+        try:
+            return wrapped(*args, **kwargs)
+        finally:
+            del boot_id_file  # close and thus delete the tempfile
+            sys.platform = initial_sys_platform
+
+    return _patch_boot_id_file
+
+
+def patch_system_info(test):
+    @function_wrapper
+    def _patch_system_info(wrapped, instance, args, kwargs):
+        dc = newrelic.core.data_collector
+        initial_logical_processor_count = dc.logical_processor_count
+        initial_total_physical_memory = dc.total_physical_memory
+        initial_system_info_gethostname = dc.system_info.gethostname
+
         dc.logical_processor_count = _mock_logical_processor_count(
-                self.test.get('input_logical_processors'))
+                test.get('input_logical_processors'))
         dc.total_physical_memory = _mock_total_physical_memory(
-                self.test.get('input_total_ram_mib'))
+                test.get('input_total_ram_mib'))
         dc.system_info.gethostname = _mock_gethostname(
-                self.test.get('input_hostname'))
+                test.get('input_hostname'))
 
-    def __exit__(self, *args, **kwargs):
-        del self.boot_id_file  # close and thus delete the tempfile
+        try:
+            return wrapped(*args, **kwargs)
+        finally:
+            dc.logical_processor_count = initial_logical_processor_count
+            dc.total_physical_memory = initial_total_physical_memory
+            dc.system_info.gethostname = initial_system_info_gethostname
+
+    return _patch_system_info
 
 
 @pytest.mark.parametrize('test', _load_tests())
 def test_utilization_settings(test):
-    with MockExternalHTTPServer() as external:
-        with MockMetadataEndpoints(external, test):
-            with UpdatedSettings(test) as settings:
-                local_config, = ApplicationSession._create_connect_payload(
-                        '', [], [], settings)
-                util_output = local_config['utilization']
-                expected_output = test['expected_output_json']
-                # assert x == y does not work when running these tests within a
-                # docker container b/c util_output will have extra values not
-                # found in expected_output
-                assert_dicts_less_than_or_equal(expected_output, util_output)
+
+    @patch_Session_get(test)
+    @patch_boot_id_file(test)
+    @patch_system_info(test)
+    def _test_utilization_data():
+        with UpdatedSettings(test) as settings:
+
+            # Ignoring docker will ensure that there is nothing extra in the
+            # gathered utilizations data
+            settings['utilization.detect_docker'] = False
+
+            local_config, = ApplicationSession._create_connect_payload(
+                    '', [], [], settings)
+            util_output = local_config['utilization']
+            expected_output = test['expected_output_json']
+
+            assert expected_output == util_output
+
+    _test_utilization_data()

1573492b26ab75ed3117ce9a509317a977f1c6d9
Rey Abolofia <rabolofia@newrelic.com>
2017-08-11 16:23:22 -0700

    Sample application for testing gRPC. PYTHON-2459

--- tests/external_grpc/conftest.py
+++ tests/external_grpc/conftest.py
@@ -4,7 +4,6 @@ from testing_support.fixtures import (code_coverage_fixture,
         collector_agent_registration_fixture, collector_available_fixture)
 
 _coverage_source = [
-    'newrelic.api.external_trace',
     'newrelic.hooks.external_grpc',
 ]
 

805b5389e6945a6623c2e7e91b89879303278313
Rey Abolofia <rabolofia@newrelic.com>
2017-08-24 18:49:59 -0700

    Test when server unavailable metric still recorded. PYTHON-2468

--- tests/external_grpc/test_clients.py
+++ tests/external_grpc/test_clients.py
@@ -284,3 +284,56 @@ def test_future_timeout_error(service_method_type, service_method_method_name,
         assert error.value.code() == grpc.StatusCode.DEADLINE_EXCEEDED
 
     _test_future_timeout_error()
+
+
+@pytest.mark.parametrize(*_test_matrix)
+def test_server_down(service_method_type, service_method_method_name,
+        future_response):
+    port = 1234
+
+    service_method_class_name = 'Do%s' % (
+            service_method_type.title().replace('_', ''))
+    streaming_request = service_method_type.split('_')[0] == 'stream'
+
+    _test_scoped_metrics = [
+            ('External/localhost:%s/gRPC/%s' % (port, service_method_type), 1),
+    ]
+    _test_rollup_metrics = [
+            ('External/localhost:%s/gRPC/%s' % (port, service_method_type), 1),
+            ('External/localhost:%s/all' % port, 1),
+            ('External/allOther', 1),
+            ('External/all', 1),
+    ]
+
+    if six.PY2:
+        _test_transaction_name = 'test_clients:_test_server_down'
+    else:
+        _test_transaction_name = (
+                'test_clients:test_server_down.<locals>._test_server_down')
+
+    @validate_transaction_errors(errors=[])
+    @validate_transaction_metrics(_test_transaction_name,
+            scoped_metrics=_test_scoped_metrics,
+            rollup_metrics=_test_rollup_metrics,
+            background_task=True)
+    @background_task()
+    def _test_server_down():
+        channel = grpc.insecure_channel('localhost:%s' % port)
+        stub = SampleApplicationStub(channel)
+
+        service_method_class = getattr(stub, service_method_class_name)
+        service_method_method = getattr(service_method_class,
+                service_method_method_name)
+
+        if streaming_request:
+            request = _message_stream(count=1)
+        else:
+            request = Message(text='Hello World', count=1)
+
+        with pytest.raises(grpc.RpcError) as error:
+            reply = service_method_method(request)
+            list(reply)
+
+        assert error.value.code() == grpc.StatusCode.UNAVAILABLE
+
+    _test_server_down()

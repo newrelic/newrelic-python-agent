5be45874563ce770a1bf9d2bf7fc1642638ebc20
Rey Abolofia <rabolofia@newrelic.com>
2017-08-30 16:37:59 -0700

    Do not run mock gRPC server in thread. PYTHON-2475

--- tests/testing_support/mock_external_grpc_server.py
+++ tests/testing_support/mock_external_grpc_server.py
@@ -1,6 +1,5 @@
 from concurrent import futures
 import grpc
-import threading
 
 
 # This defines an external grpc server test apps can use for testing.
@@ -11,35 +10,17 @@ import threading
 #       add_SampleApplicationServicer_to_server(SampleApplicationServicer(),
 #               server)
 #       ... test stuff ...
-#
-# The server runs on a separate thread meaning it won't block the test app.
 
 
-class MockExternalgRPCServer(threading.Thread):
+class MockExternalgRPCServer(object):
 
     def __init__(self, port=50051, *args, **kwargs):
-        super(MockExternalgRPCServer, self).__init__(*args, **kwargs)
-
-        self.port = port
-        self.daemon = True
-
         self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-        self.server.add_insecure_port('[::]:%s' % self.port)
-
-        self.shutdown_event = threading.Event()
+        self.server.port = self.server.add_insecure_port('[::]:%s' % port)
 
     def __enter__(self):
-        self.start()
+        self.server.start()
         return self.server
 
     def __exit__(self, type, value, tb):
-        self.stop()
-
-    def run(self):
-        self.server.start()
-        self.shutdown_event.wait()
-
-    def stop(self):
         self.server.stop(grace=0.1)
-        self.shutdown_event.set()
-        self.join()

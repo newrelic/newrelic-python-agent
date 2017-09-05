cc4117c52e5357d2be01dfd92ed0929452a34a59
Allan Feldman <afeldman@newrelic.com>
2017-09-05 11:09:27 -0700

    Always test result on non-streaming gRPC responses. PYTHON-2483

--- tests/external_grpc/test_clients.py
+++ tests/external_grpc/test_clients.py
@@ -206,8 +206,11 @@ def test_client(service_method_type, service_method_method_name,
         try:
             # If the reply was canceled or the server code raises an exception,
             # this will raise an exception which will be recorded by the agent
-            reply = list(reply) or [reply.result()]
-        except TypeError:
+            if streaming_response or cancel:
+                reply = list(reply)
+            else:
+                reply = [reply.result()]
+        except (AttributeError, TypeError):
             reply = [reply]
 
         expected_text = '%s: Hello World' % service_method_type

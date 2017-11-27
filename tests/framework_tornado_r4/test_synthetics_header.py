1d52fdf91a9a3fa78ffb0dbf5570503d447c7c7c
Rey Abolofia <rabolofia@newrelic.com>
2017-11-27 14:37:43 -0800

    Test synthetics headers passed to external requests. PYTHON-2547

--- tests/framework_tornado_r4/test_synthetics_header.py
+++ tests/framework_tornado_r4/test_synthetics_header.py
@@ -4,7 +4,10 @@ import sys
 from tornado.ioloop import IOLoop
 
 from testing_support.fixtures import (validate_synthetics_event,
-        override_application_settings, make_synthetics_header)
+        override_application_settings, make_synthetics_header,
+        validate_transaction_metrics)
+from testing_support.mock_external_http_server import (
+        MockExternalHTTPHResponseHeadersServer)
 
 
 ENCODING_KEY = '1234567890123456789012345678901234567890'
@@ -25,6 +28,13 @@ if (sys.version_info < (3, 4) or
 else:
     loops = [None, 'tornado.platform.asyncio.AsyncIOLoop',
             'zmq.eventloop.ioloop.ZMQIOLoop']
+
+
+@pytest.fixture(scope='module')
+def external():
+    external = MockExternalHTTPHResponseHeadersServer()
+    with external:
+        yield external
 
 
 def _make_synthetics_header(version='1', account_id=ACCOUNT_ID,
@@ -51,5 +61,60 @@ def test_valid_synthetics_event(app, ioloop):
         headers = _make_synthetics_header()
         response = app.fetch('/simple/fast', headers=headers)
         assert response.code == 200
+
+    _test()
+
+
+@pytest.mark.parametrize('client_class',
+        ['AsyncHTTPClient', 'CurlAsyncHTTPClient', 'HTTPClient'])
+@pytest.mark.parametrize('cat_enabled', [True, False])
+@pytest.mark.parametrize('synthetics_enabled', [True, False])
+@pytest.mark.parametrize('request_type', ['uri', 'class'])
+@override_application_settings(_override_settings)
+def test_synthetics_headers_sent_on_external_requests(app, cat_enabled,
+        synthetics_enabled, request_type, client_class, external):
+
+    if cat_enabled or ('Async' not in client_class):
+        port = external.port
+    else:
+        port = app.get_http_port()
+
+    uri = '/async-client/%s/%s/%s' % (port, request_type, client_class)
+
+    expected_metrics = [
+        ('External/localhost:%s/tornado.httpclient/GET' % port, 1)
+    ]
+
+    @override_application_settings({
+        'cross_application_tracer.enabled': cat_enabled,
+        'synthetics.enabled': synthetics_enabled})
+    @validate_transaction_metrics(
+        '_target_application:AsyncExternalHandler.get',
+        rollup_metrics=expected_metrics,
+        scoped_metrics=expected_metrics)
+    def _test():
+        synthetics_headers = _make_synthetics_header()
+        response = app.fetch(uri, headers=synthetics_headers)
+        assert response.code == 200
+
+        sent_headers = response.body
+        if hasattr(sent_headers, 'decode'):
+            sent_headers = sent_headers.decode('utf-8')
+
+        sent_headers_lower = sent_headers.lower()
+
+        if cat_enabled:
+            assert 'X-NewRelic-ID' in sent_headers
+            assert 'X-NewRelic-Transaction' in sent_headers
+            assert 'X-NewRelic-App-Data' not in sent_headers
+        else:
+            assert 'x-newrelic-id' not in sent_headers_lower
+            assert 'x-newrelic-transaction' not in sent_headers_lower
+            assert 'x-newrelic-app-data' not in sent_headers_lower
+
+        if synthetics_enabled:
+            assert 'X-NewRelic-Synthetics' in sent_headers
+        else:
+            assert 'x-newrelic-synthetics' not in sent_headers_lower
 
     _test()

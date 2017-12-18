d1d7819e9cf42b90cd1cb5c1bd8841b7b80c9e19
Rey Abolofia <rabolofia@newrelic.com>
2017-12-18 15:28:32 -0800

    Do not rely on imported packages for tests. PYTHON-2616

--- tests/cross_agent/test_sql_obfuscation.py
+++ tests/cross_agent/test_sql_obfuscation.py
@@ -2,7 +2,7 @@ import json
 import os
 import pytest
 
-from newrelic.core.database_utils import SQLDatabase, SQLStatement
+from newrelic.core.database_utils import SQLStatement
 
 
 CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
@@ -27,47 +27,32 @@ def load_tests():
     return result
 
 
-_dbapi2modules = None
+_quoting_styles = {
+    'sqlite': 'single',
+    'mysql': 'single+double',
+    'postgres': 'single+dollar',
+    'oracle': 'single+oracle',
+    'cassandra': 'single',
+}
 
 
-def _load_dbapi2modules():
-
-    # TODO: is importing these the right thing to do? or should I just create a
-    # dummy db that is either single, double or single+double?
-
-    import sqlite3, mysql.connector as mysql, psycopg2, cx_Oracle  # noqa
-
-    return {
-        'sqlite': [sqlite3],
-        'mysql': [mysql],
-        'postgres': [psycopg2],
-        'oracle': [cx_Oracle],
-        'cassandra': [],
-    }
+def get_quoting_styles(dialects):
+    return set([_quoting_styles.get(dialect) for dialect in dialects])
 
 
-def get_dbpi2modules(dialects):
-    global _dbapi2modules
-    if not _dbapi2modules:
-        _dbapi2modules = _load_dbapi2modules()
-
-    modules = []
-
-    for dialect in dialects:
-        modules_for_dialect = _dbapi2modules.get(dialect)
-        modules.extend(modules_for_dialect)
-
-    return modules
+class DummyDB(object):
+    def __init__(self, quoting_style):
+        self.quoting_style = quoting_style
 
 
 @pytest.mark.xfail()
 @pytest.mark.parametrize(_parameters, load_tests())
 def test_sql_obfuscation(obfuscated, dialects, sql, malformed, pathological):
 
-    dbapi2modules = get_dbpi2modules(dialects)
+    quoting_styles = get_quoting_styles(dialects)
 
-    for dbapi2module in dbapi2modules:
-        database = SQLDatabase(dbapi2module)
+    for quoting_style in quoting_styles:
+        database = DummyDB(quoting_style)
         statement = SQLStatement(sql, database)
         actual_obfuscated = statement.obfuscated
         assert actual_obfuscated in obfuscated

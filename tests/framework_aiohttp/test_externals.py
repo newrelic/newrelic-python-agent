4a975dcd56abed0e95d9c0dc28cfdd89a6823154
Rey Abolofia <rabolofia@newrelic.com>
2018-02-27 09:41:41 -0800

    ClientSession uses `async with` in aiohttp 3.x. PYTHON-2674

--- tests/framework_aiohttp/test_externals.py
+++ tests/framework_aiohttp/test_externals.py
@@ -1,14 +1,8 @@
-import pytest
-import aiohttp
 import asyncio
 
 from testing_support.fixtures import validate_transaction_metrics
 
-version_info = tuple(int(_) for _ in aiohttp.__version__.split('.')[:2])
 
-
-@pytest.mark.xfail(version_info >= (3, 0), reason='aiohttp 3.x PYTHON-2674',
-        strict=True)
 @validate_transaction_metrics('_target_application:multi_fetch_handler',
         rollup_metrics=[('External/all', 2)])
 def test_multiple_requests_within_transaction(aiohttp_app):

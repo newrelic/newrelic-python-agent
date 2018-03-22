51e15cf78851dbf1428db0e56a71c05bc1f22ed5
Allan Feldman <afeldman@newrelic.com>
2018-03-22 15:59:33 -0700

    Add unittests for full URIs in request line. PYTHON-2712

---
+++ tests/agent_unittests/test_full_uri_payloads.py
@@ -0,0 +1,76 @@
+import os
+import pytest
+import time
+
+from newrelic.core.config import global_settings, global_settings_dump
+from newrelic.core.data_collector import create_session
+from newrelic.packages.requests.adapters import HTTPAdapter, urldefragauth
+
+
+class FullURIAdapter(HTTPAdapter):
+    def request_url(self, request, proxies):
+        return urldefragauth(request.url)
+
+
+@pytest.fixture(scope='module')
+def base_settings():
+    settings = global_settings()
+
+    settings.license_key = os.environ.get('NEW_RELIC_LICENSE_KEY',
+            '84325f47e9dec80613e262be4236088a9983d501')
+    settings.host = os.environ.get('NEW_RELIC_HOST',
+            'staging-collector.newrelic.com')
+    settings.port = int(os.environ.get('NEW_RELIC_PORT', '0'))
+
+    return settings
+
+
+@pytest.fixture(scope='module')
+def session(base_settings):
+    environment = ()
+    linked_apps = []
+    app_name = 'Python Agent Test'
+
+    session = create_session(
+            None, app_name, linked_apps, environment, global_settings_dump())
+
+    # Mount an adapter that will force the full URI to be sent
+    assert session._requests_session is session.requests_session
+    original_adapters = session.requests_session.adapters.copy()
+
+    session.requests_session.mount('https://', FullURIAdapter())
+    session.requests_session.mount('http://', FullURIAdapter())
+
+    yield session
+
+    # Restore the original HTTP adapters
+    session.requests_session.adapters = original_adapters
+
+    session.shutdown_session()
+
+
+NOW = time.time()
+EMPTY_SAMPLES = {
+    'reservoir_size': 100,
+    'events_seen': 0,
+}
+
+
+@pytest.mark.parametrize('method,args', [
+    ('send_metric_data', (NOW, NOW + 1, ())),
+    ('send_transaction_events', ((),)),
+    ('send_custom_events', (EMPTY_SAMPLES, ())),
+    ('send_error_events', (EMPTY_SAMPLES, ())),
+    ('send_transaction_traces', ([[]],)),
+    ('send_sql_traces', ([[]],)),
+    ('get_agent_commands', ()),
+    ('send_profile_data', ([[]],)),
+    ('get_xray_metadata', (0,)),
+    ('send_errors', ([[]],)),
+    ('send_agent_command_results', ({0: {}},)),
+])
+def test_full_uri_payload(session, method, args):
+    sender = getattr(session, method)
+
+    # An exception will be raised here if there's a problem with the response
+    sender(*args)

1561d4980c5177255b4421669948cdcbc9ca32bf
Allan Feldman <afeldman@newrelic.com>
2018-04-13 11:38:24 -0700

    Add functional tests for distributed trace payload. PYTHON-2733

--- newrelic/api/tests/test_transaction.py
+++ newrelic/api/tests/test_transaction.py
@@ -13,6 +13,10 @@ settings = newrelic.api.settings.settings()
 application = application_instance()
 
 
+DISTRIBUTED_TRACE_KEYS_REQUIRED = (
+        'ty', 'ac', 'ap', 'id', 'tr', 'pr', 'sa', 'ti')
+
+
 class TestTraceEndsAfterTransaction(newrelic.tests.test_cases.TestCase):
 
     requires_collector = True
@@ -121,6 +125,51 @@ class TestTransactionApis(newrelic.tests.test_cases.TestCase):
         with self.transaction:
             payload = self.transaction.create_distributed_tracing_payload()
             assert type(payload.http_safe()) is str
+
+    def test_distributed_trace_no_referring_transaction(self):
+        with self.transaction:
+            payload = self.transaction.create_distributed_tracing_payload()
+            assert payload['v'] == (0, 1)
+
+            data = payload['d']
+
+            # Check required keys
+            assert all(k in data for k in DISTRIBUTED_TRACE_KEYS_REQUIRED)
+
+            # Type is always App
+            assert data['ty'] == 'App'
+
+            # IDs should be the transaction GUID
+            assert data['id'] == self.transaction.guid
+            assert data['tr'] == self.transaction.guid
+
+            # Parent should be excluded
+            assert 'pa' not in data
+
+    def test_distributed_trace_referring_transaction(self):
+        with self.transaction:
+            self.transaction.referring_transaction_guid = 'abcde'
+            self.transaction._trace_id = 'qwerty'
+            self.transaction.priority = 0.0
+
+            payload = self.transaction.create_distributed_tracing_payload()
+            assert payload['v'] == (0, 1)
+
+            data = payload['d']
+
+            # Type is always App
+            assert data['ty'] == 'App'
+
+            # Check required keys
+            assert all(k in data for k in DISTRIBUTED_TRACE_KEYS_REQUIRED)
+
+            # ID should be the transaction GUID
+            assert data['id'] == self.transaction.guid
+
+            # Parent data should be forwarded
+            assert data['pa'] == 'abcde'
+            assert data['tr'] == 'qwerty'
+            assert data['pr'] == 0.0
 
 
 if __name__ == '__main__':

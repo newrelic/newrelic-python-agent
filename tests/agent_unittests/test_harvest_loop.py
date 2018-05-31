b99fd37cbed3d85bb0d3263d6da78f3b082de891
Allan Feldman <afeldman@newrelic.com>
2018-05-31 13:51:04 -0700

    Add integration test for adaptive sampling. PYTHON-2793

--- tests/agent_unittests/test_harvest_loop.py
+++ tests/agent_unittests/test_harvest_loop.py
@@ -147,3 +147,34 @@ def test_transaction_count(transaction_node):
 
     # Harvest resets the transaction count
     assert app._transaction_count == 0
+
+
+def test_adaptive_sampling(transaction_node):
+    settings = global_settings()
+    settings.developer_mode = True
+    settings.collect_custom_events = False
+    settings.license_key = '**NOT A LICENSE KEY**'
+
+    app = Application('Python Agent Test (Harvest Loop)')
+
+    # Should always return false for sampling prior to connect
+    assert app.compute_sampled(1.0) is False
+
+    app.connect_to_data_collector()
+
+    # First harvest, first N should be sampled
+    for _ in range(settings.agent_limits.sampling_target):
+        assert app.compute_sampled(1.0) is True
+
+    assert app.compute_sampled(1.0) is False
+
+    # Multiple harvests should behave the same
+    for _ in range(2):
+        app.harvest()
+
+        # Subsequent harvests should allow sampling of 2X the target
+        for _ in range(2 * settings.agent_limits.sampling_target):
+            assert app.compute_sampled(1.0) is True
+
+        # No further samples should be saved
+        assert app.compute_sampled(1.0) is False

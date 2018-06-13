bb1e913f14d2660256396ca2c01300e5296fbe92
Joe Ryan <jryan@newrelic.com>
2018-06-13 15:21:13 -0700

    Initial jenkins benchmark job for develop. PYTHON-2817

---
+++ jenkins/benchmark.groovy
@@ -0,0 +1,30 @@
+import newrelic.jenkins.extensions
+
+String organization = 'python-agent'
+String repoGHE = 'python_agent'
+String repoFull = "${organization}/${repoGHE}"
+String testSuffix = "__integration-test"
+String slackChannel = '#python-agent'
+
+use(extensions) {
+    baseJob("_BENCHMARKS-develop_") {
+        label('py-ec2-linux')
+        repo(repoFull)
+        branch('develop')
+
+        configure {
+            description('Run benchmarks on develop, nightly at 5am.')
+
+            triggers {
+                // run daily on cron
+                cron('H 5 * * 1-5')
+            }
+
+            steps {
+                shell('docker/packnsend run jenkins/scripts/run_benchmark.sh')
+            }
+
+            slackQuiet(slackChannel)
+        }
+    }
+}

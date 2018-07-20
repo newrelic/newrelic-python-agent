1f213aedd6d79223c2a353deed79e31dc98bc214
Allan Feldman <afeldman@newrelic.com>
2018-07-20 16:58:04 -0700

    Add environment variable support for distributed tracing. PYTHON-2873

--- newrelic/core/config.py
+++ newrelic/core/config.py
@@ -464,7 +464,8 @@ _settings.transaction_events.attributes.include = []
 _settings.custom_insights_events.enabled = True
 _settings.custom_insights_events.max_samples_stored = DEFAULT_RESERVOIR_SIZE
 
-_settings.distributed_tracing.enabled = False
+_settings.distributed_tracing.enabled = _environ_as_bool(
+        'NEW_RELIC_DISTRIBUTED_TRACING_ENABLED', default=False)
 _settings.span_events.enabled = True
 _settings.span_events.max_samples_stored = SPAN_EVENT_RESERVOIR_SIZE
 

36211605477145b8067dcb8f542814f6ef10eb4b
Rey Abolofia <rabolofia@newrelic.com>
2018-08-15 10:39:53 -0700

    Benchmarking for TimeTrace. PYTHON-2898

--- benchmarks/time_traces.py
+++ benchmarks/time_traces.py
@@ -1,6 +1,7 @@
 import sys
 
 from newrelic.api.function_trace import FunctionTrace
+from newrelic.api.time_trace import TimeTrace
 from newrelic.api.transaction import Transaction
 
 from benchmarks.util import MockApplication
@@ -20,6 +21,9 @@ class TimeTraceInit(object):
     def teardown(self):
         self.transaction.__exit__(None, None, None)
 
+    def time_time_trace_init(self):
+        TimeTrace(self.transaction)
+
     def time_function_trace_init(self):
         FunctionTrace(self.transaction, **_function_trace_kwargs)
 
@@ -30,14 +34,13 @@ class TimeTraceEnter(object):
         app = MockApplication()
         self.transaction = Transaction(app)
         self.transaction.__enter__()
-        self.function_trace = FunctionTrace(self.transaction,
-                **_function_trace_kwargs)
+        self.time_trace = TimeTrace(self.transaction)
 
     def teardown(self):
         self.transaction.__exit__(None, None, None)
 
-    def time_function_trace_enter(self):
-        self.function_trace.__enter__()
+    def time_time_trace_enter(self):
+        self.time_trace.__enter__()
 
 
 class TimeTraceExit(object):
@@ -46,8 +49,10 @@ class TimeTraceExit(object):
         app = MockApplication()
         self.transaction = Transaction(app)
         self.transaction.__enter__()
+        self.time_trace = TimeTrace(self.transaction)
         self.function_trace = FunctionTrace(self.transaction,
                 **_function_trace_kwargs)
+        self.time_trace.__enter__()
         self.function_trace.__enter__()
 
         try:
@@ -58,6 +63,12 @@ class TimeTraceExit(object):
     def teardown(self):
         self.transaction.__exit__(None, None, None)
         self.exc_info = None
+
+    def time_time_trace_exit_no_error(self):
+        self.time_trace.__exit__(None, None, None)
+
+    def time_time_trace_exit_with_error(self):
+        self.time_trace.__exit__(*self.exc_info)
 
     def time_function_trace_exit_no_error(self):
         self.function_trace.__exit__(None, None, None)

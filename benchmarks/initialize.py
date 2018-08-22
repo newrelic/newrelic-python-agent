a1a6d50a7eb28793708ea31e163d6c61007607ec
Rey Abolofia <rabolofia@newrelic.com>
2018-08-22 14:21:52 -0700

    Add note about pkg_resources impact on overhead. PYTHON-2931

--- benchmarks/initialize.py
+++ benchmarks/initialize.py
@@ -14,6 +14,15 @@ class TimeInitialize(object):
     params = (None, _newrelic_ini.name)
     param_names = ('config_file', )
 
+    # NOTE: A good portion of the time taken up during initialize is in
+    # importing pkg_resources. Unfortunately however it is quite difficult to
+    # benchmark this. First we would need to run benchmarks both with and
+    # without pkg_resources installed. Second, because python caches modules,
+    # we would need to disable cache lookup. In lieu of doing this, we have
+    # decided to only test with pkg_resources and let the caching happen with
+    # the knowledge that if we could avoid the import of pkg_resources
+    # altogether we would get a pretty big overhead reduction win.
+
     def time_initialize(self, config_file):
         newrelic.common.log_file._initialized = False
         newrelic.config._configuration_done = False

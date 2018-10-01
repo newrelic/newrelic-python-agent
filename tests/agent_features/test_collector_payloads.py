ab7d584cd9251e9f515f273eade2bd728b8e8625
Rey Abolofia <rabolofia@newrelic.com>
2018-10-01 16:52:59 -0700

    Flake8. PYTHON-2993

--- tests/agent_features/test_collector_payloads.py
+++ tests/agent_features/test_collector_payloads.py
@@ -13,28 +13,33 @@ exceptional_application = webtest.TestApp(simple_exceptional_app)
 normal_application = webtest.TestApp(simple_app)
 custom_event_application = webtest.TestApp(simple_custom_event_app)
 
+
 @validate_error_trace_collector_json()
 def test_error_trace_json():
     try:
-        response = exceptional_application.get('/')
+        exceptional_application.get('/')
     except ValueError:
         pass
+
 
 @validate_error_event_collector_json()
 def test_error_event_json():
     try:
-        response = exceptional_application.get('/')
+        exceptional_application.get('/')
     except ValueError:
         pass
 
+
 @validate_tt_collector_json()
 def test_transaction_trace_json():
-    response = normal_application.get('/')
+    normal_application.get('/')
+
 
 @validate_transaction_event_collector_json()
 def test_transaction_event_json():
-    response = normal_application.get('/')
+    normal_application.get('/')
+
 
 @validate_custom_event_collector_json()
 def test_custom_event_json():
-    response = custom_event_application.get('/')
+    custom_event_application.get('/')

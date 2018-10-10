b8c1e25610ee40a9ff84813399ad20bec384a4c9
Allan Feldman <afeldman@newrelic.com>
2018-10-10 10:30:15 -0700

    Flake8. PYTHON-3000

--- newrelic/core/config.py
+++ newrelic/core/config.py
@@ -449,7 +449,8 @@ _settings.transaction_segment_terms = []
 
 _settings.account_id = os.environ.get('NEW_RELIC_ACCOUNT_ID')
 _settings.cross_process_id = None
-_settings.primary_application_id = os.environ.get('NEW_RELIC_PRIMARY_APPLICATION_ID')
+_settings.primary_application_id = \
+        os.environ.get('NEW_RELIC_PRIMARY_APPLICATION_ID')
 _settings.trusted_account_ids = []
 _settings.trusted_account_key = os.environ.get('NEW_RELIC_TRUSTED_ACCOUNT_KEY')
 _settings.encoding_key = None

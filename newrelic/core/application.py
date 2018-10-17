3a0d3be95640a9cace53ce1bfc8d3c01fb3dc2fb
Allan Feldman <afeldman@newrelic.com>
2018-10-17 13:34:26 -0700

    Change SampledDataSet to an iterable. PYTHON-3032
    Our audit logging in serverless mode simply prints out the object prior
    to json encoding. This causes our audit logs to print the repr of the
    generator object for structures using our SampledDataSet.
    
    In order to fix this problem, SampledDataSet has been converted into an
    iterable and application now constructs a list for each data type so
    that data is finalized before sending to data_collector.

--- newrelic/core/application.py
+++ newrelic/core/application.py
@@ -1420,11 +1420,14 @@ class Application(object):
                             configuration.distributed_tracing.enabled):
                         spans = stats.span_events
                         if spans.num_samples > 0:
+                            span_samples = list(spans)
+
                             _logger.debug('Sending span event data '
                                     'for harvest of %r.', self._app_name)
 
                             self._active_session.send_span_events(
-                                spans.sampling_info, spans.samples)
+                                spans.sampling_info, span_samples)
+                            span_samples = None
 
                         # As per spec
                         spans_seen = spans.num_seen
@@ -1445,12 +1448,15 @@ class Application(object):
                         error_events = stats.error_events
                         num_error_samples = error_events.num_samples
                         if num_error_samples > 0:
+                            error_event_samples = list(error_events)
+
                             _logger.debug('Sending error event data '
                                     'for harvest of %r.', self._app_name)
 
                             samp_info = error_events.sampling_info
                             self._active_session.send_error_events(samp_info,
-                                    error_events.samples)
+                                    error_event_samples)
+                            error_event_samples = None
 
                         # As per spec
                         internal_count_metric('Supportability/Events/'
@@ -1468,11 +1474,14 @@ class Application(object):
                         customs = stats.custom_events
 
                         if customs.num_samples > 0:
+                            custom_samples = list(customs)
+
                             _logger.debug('Sending custom event data '
                                     'for harvest of %r.', self._app_name)
 
                             self._active_session.send_custom_events(
-                                    customs.sampling_info, customs.samples)
+                                    customs.sampling_info, custom_samples)
+                            custom_samples = None
 
                         # As per spec
                         internal_count_metric('Supportability/Events/'

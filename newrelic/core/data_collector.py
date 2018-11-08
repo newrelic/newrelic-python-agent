57a2cda695e2588bf5f609192c899af69998d8db
Joe Ryan <jryan@newrelic.com>
2018-11-07 16:05:10 -0800

    Change Session.send_request to http_request. PYTHON-3048
    Also add retry_after param to RetryDataForRequest exception.

--- newrelic/core/data_collector.py
+++ newrelic/core/data_collector.py
@@ -700,6 +700,7 @@ class ApplicationSession(object):
         self.request_headers_map = configuration.request_headers_map
 
         self._requests_session = None
+        self.timed_out_endpoints = {}
 
     @property
     def requests_session(self):
@@ -716,8 +717,30 @@ class ApplicationSession(object):
     def max_payload_size_in_bytes(self):
         return self.configuration.max_payload_size_in_bytes
 
+    def send_request(self, session, url, method, license_key,
+            agent_run_id=None, request_headers_map=None, payload=(),
+            max_payload_size_in_bytes=None):
+
+        if method in self.timed_out_endpoints:
+            current_time = time.time()
+            if current_time > self.timed_out_endpoints[method]:
+                self.timed_out_endpoints.pop(method)
+            else:
+                raise RetryDataForRequest
+
+        try:
+            return self.http_request(session, url, method, license_key,
+                agent_run_id, request_headers_map, payload,
+                max_payload_size_in_bytes)
+
+        except RetryDataForRequest as e:
+            if e.retry_after:
+                self.timed_out_endpoints[method] = (time.time() +
+                        e.retry_after)
+            raise
+
     @classmethod
-    def send_request(cls, session, url, method, license_key,
+    def http_request(cls, session, url, method, license_key,
             agent_run_id=None, request_headers_map=None, payload=(),
             max_payload_size_in_bytes=None):
         return send_request(session, url, method, license_key,
@@ -970,7 +993,7 @@ class ApplicationSession(object):
 
             url = collector_url()
 
-            result = cls.send_request(None, url,
+            result = cls.http_request(None, url,
                     'preconnect', license_key)
             redirect_host = result['redirect_host']
 
@@ -989,7 +1012,7 @@ class ApplicationSession(object):
 
             url = collector_url(redirect_host)
 
-            server_config = cls.send_request(None, url, 'connect',
+            server_config = cls.http_request(None, url, 'connect',
                     license_key, None, None, payload)
 
             # Apply High Security Mode to server_config, so the local
@@ -1216,9 +1239,8 @@ _developer_mode_responses = {
 
 
 class DeveloperModeSession(ApplicationSession):
-
     @classmethod
-    def send_request(cls, session, url, method, license_key,
+    def http_request(cls, session, url, method, license_key,
             agent_run_id=None, request_headers_map=None, payload=(),
             max_payload_size_in_bytes=None):
 
@@ -1309,7 +1331,7 @@ class ServerlessModeSession(ApplicationSession):
             'arn': settings.aws_arn,
         })
 
-    def send_request(self, session, url, method, license_key,
+    def http_request(self, session, url, method, license_key,
             agent_run_id=None, request_headers_map=None, payload=(),
             max_payload_size_in_bytes=None):
 

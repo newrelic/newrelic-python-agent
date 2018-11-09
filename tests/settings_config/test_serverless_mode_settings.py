fb663fdff1ffd4a2a4e7208dc789ed22cf13ee19
Rey Abolofia <rabolofia@newrelic.com>
2018-11-09 11:54:29 -0800

    Fix overloaded test. PYTHON-3057
    This test was testing two things: distributed tracing environment
    variables _and_ serverless mode settings. This commit updates it to only
    be testing distributed tracing.

--- tests/settings_config/test_serverless_mode_settings.py
+++ tests/settings_config/test_serverless_mode_settings.py
@@ -93,12 +93,10 @@ def test_serverless_apdex_t(ini, env, value, global_settings):
 
 
 @pytest.mark.parametrize('ini,env,values', [
-    (INI_FILE_SERVERLESS_MODE, LAMBDA_ENV, (
-            None, 'Unknown', None, True)),
     (INI_FILE_EMPTY, NON_SERVERLESS_MODE_ENV, (
-            None, 'Unknown', None, False)),
+            None, 'Unknown', None)),
     (INI_FILE_SERVERLESS_MODE, DT_ENV, (
-            'account_id', 'application_id', 'trusted_key', True)),
+            'account_id', 'application_id', 'trusted_key')),
 ])
 def test_serverless_dt_environment(ini, env, values, global_settings):
     settings = global_settings()
@@ -106,8 +104,6 @@ def test_serverless_dt_environment(ini, env, values, global_settings):
     assert settings.account_id == values[0]
     assert settings.primary_application_id == values[1]
     assert settings.trusted_account_key == values[2]
-
-    assert settings.serverless_mode == values[3]
 
 
 # The purpose of test_serverless_app_names is to ensure that if the customer

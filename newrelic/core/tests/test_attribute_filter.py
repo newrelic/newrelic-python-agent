cf00ad07e19b038c027368ff7c1f651c02fb9a83
Allan Feldman <afeldman@newrelic.com>
2018-12-03 17:09:05 -0800

    Add testing for attribute filter destinations. PYTHON-3092

---
+++ newrelic/core/tests/test_attribute_filter.py
@@ -0,0 +1,60 @@
+import newrelic.tests.test_cases
+from newrelic.core.attribute_filter import (DST_ALL, DST_SPAN_EVENTS,
+        DST_BROWSER_MONITORING, DST_TRANSACTION_SEGMENTS, DST_ERROR_COLLECTOR,
+        DST_TRANSACTION_TRACER, DST_TRANSACTION_EVENTS)
+from newrelic.core.config import finalize_application_settings
+
+
+class TestAttributeFilter(newrelic.tests.test_cases.TestCase):
+    def test_default_all_destinations_enabled(self):
+        settings = finalize_application_settings({
+            'browser_monitoring.attributes.enabled': True,
+        })
+        destinations = settings.attribute_filter.apply('foo', DST_ALL)
+        assert destinations == DST_ALL
+
+    def test_destination_transaction_segments_disabled(self):
+        settings = finalize_application_settings({
+            'browser_monitoring.attributes.enabled': True,
+            'transaction_segments.attributes.enabled': False,
+        })
+        destinations = settings.attribute_filter.apply('foo', DST_ALL)
+        assert destinations == (DST_ALL & ~DST_TRANSACTION_SEGMENTS)
+
+    def test_destination_span_events_disabled(self):
+        settings = finalize_application_settings({
+            'browser_monitoring.attributes.enabled': True,
+            'span_events.attributes.enabled': False,
+        })
+        destinations = settings.attribute_filter.apply('foo', DST_ALL)
+        assert destinations == (DST_ALL & ~DST_SPAN_EVENTS)
+
+    def test_destination_browser_monitoring_disabled(self):
+        settings = finalize_application_settings({
+        })
+        destinations = settings.attribute_filter.apply('foo', DST_ALL)
+        assert destinations == (DST_ALL & ~DST_BROWSER_MONITORING)
+
+    def test_destination_error_collector_disabled(self):
+        settings = finalize_application_settings({
+            'browser_monitoring.attributes.enabled': True,
+            'error_collector.attributes.enabled': False,
+        })
+        destinations = settings.attribute_filter.apply('foo', DST_ALL)
+        assert destinations == (DST_ALL & ~DST_ERROR_COLLECTOR)
+
+    def test_destination_transaction_tracer_disabled(self):
+        settings = finalize_application_settings({
+            'browser_monitoring.attributes.enabled': True,
+            'transaction_tracer.attributes.enabled': False,
+        })
+        destinations = settings.attribute_filter.apply('foo', DST_ALL)
+        assert destinations == (DST_ALL & ~DST_TRANSACTION_TRACER)
+
+    def test_destination_transaction_events_disabled(self):
+        settings = finalize_application_settings({
+            'browser_monitoring.attributes.enabled': True,
+            'transaction_events.attributes.enabled': False,
+        })
+        destinations = settings.attribute_filter.apply('foo', DST_ALL)
+        assert destinations == (DST_ALL & ~DST_TRANSACTION_EVENTS)

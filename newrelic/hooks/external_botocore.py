ea423e024d7bb29aa5832bd25fd85891d0d6f622
Joe Ryan <jryan@newrelic.com>
2018-12-06 15:31:25 -0800

    Add instrumentation for fine-grained traces for boto3. PYTHON-3077

--- newrelic/hooks/external_botocore.py
+++ newrelic/hooks/external_botocore.py
@@ -1,6 +1,8 @@
+from newrelic.api.coroutine_trace import return_value_fn
 from newrelic.api.external_trace import ExternalTrace
 from newrelic.api.transaction import current_transaction
-from newrelic.common.object_wrapper import wrap_function_wrapper
+from newrelic.common.object_wrapper import (wrap_function_wrapper,
+        FunctionWrapper)
 
 try:
     import urlparse
@@ -8,8 +10,39 @@ except ImportError:
     import urllib.parse as urlparse
 
 
-def _nr_endpoint_make_request_(wrapped, instance, args, kwargs):
+CUSTOM_TRACE_POINTS = {
+}
 
+
+def bind__create_api_method(py_operation_name, operation_name, service_model):
+    return (py_operation_name, operation_name, service_model)
+
+
+def _nr_clientcreator__create_api_method_(wrapped, instance, args, kwargs):
+    (py_operation_name, operation_name, service_model) = \
+            bind__create_api_method(*args)
+
+    api_method = wrapped(*args, **kwargs)
+    service_name = service_model._service_name.lower()
+
+    if hasattr(CUSTOM_TRACE_POINTS.get(service_name), py_operation_name):
+        tracer = getattr(CUSTOM_TRACE_POINTS[service_name], py_operation_name)
+        return_value = return_value_fn(wrapped)
+
+        def wrap_service_method(_wrapped, _instance, _args, _kwargs):
+            transaction = current_transaction()
+            if transaction is None:
+                return _wrapped(*_args, **_kwargs)
+
+            trace = tracer(transaction, operation_name, _instance, _kwargs)
+            return return_value(trace, lambda: _wrapped(*_args, **_kwargs))
+
+        return FunctionWrapper(api_method, wrap_service_method)
+
+    return api_method
+
+
+def _nr_endpoint_make_request_(wrapped, instance, args, kwargs):
     transaction = current_transaction()
 
     if transaction is None:
@@ -36,3 +69,8 @@ def _nr_endpoint_make_request_(wrapped, instance, args, kwargs):
 def instrument_botocore_endpoint(module):
     wrap_function_wrapper(module, 'Endpoint.make_request',
             _nr_endpoint_make_request_)
+
+
+def instrument_botocore_client(module):
+    wrap_function_wrapper(module, 'ClientCreator._create_api_method',
+            _nr_clientcreator__create_api_method_)

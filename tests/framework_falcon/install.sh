c48c95d194f95a96257be2cb4a3a14d09bba763e
Allan Feldman <afeldman@newrelic.com>
2019-01-15 10:45:10 -0800

    Disable cython builds for falcon since they take too long.

--- tests/framework_falcon/install.sh
+++ tests/framework_falcon/install.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 # Install cython only when the CYTHON env variable is set
-[[ $CYTHON ]] && pip install cython
-
-pip install --no-binary :all: $*
+if [ -n "$CYTHON" ]; then
+    echo "CYTHON"
+    pip install cython
+    pip install --no-binary :all: $*
+else
+    echo "PYTHON"
+    pip install $*
+fi

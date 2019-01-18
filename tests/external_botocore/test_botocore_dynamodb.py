a2f9cde4d89f9f117cf957dff9ae2ecc47e2dbc1
Robert Lewis <rlewis@newrelic.com>
2019-01-18 09:31:18 -0800

    Add requestID span and segment check for dynamodb. PYTHON-3074

--- tests/external_botocore/test_botocore_dynamodb.py
+++ tests/external_botocore/test_botocore_dynamodb.py
@@ -6,7 +6,9 @@ import moto
 
 from newrelic.api.background_task import background_task
 from testing_support.fixtures import (validate_transaction_metrics,
-        override_application_settings)
+        validate_tt_segment_params, override_application_settings)
+from testing_support.validators.validate_span_events import (
+        validate_span_events)
 
 MOTO_VERSION = tuple(int(v) for v in moto.__version__.split('.'))
 
@@ -43,6 +45,8 @@ _dynamodb_rollup_metrics = [
 
 
 @override_application_settings({'distributed_tracing.enabled': True})
+@validate_span_events(expected_agents=('aws.requestId',), count=8)
+@validate_tt_segment_params(present_params=('aws.requestId',))
 @validate_transaction_metrics(
         'test_botocore_dynamodb:test_dynamodb',
         scoped_metrics=_dynamodb_scoped_metrics,

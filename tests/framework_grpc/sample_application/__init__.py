ffee7c1c73b24f37df8652b967838dc42bf71637
Loren Arthur <larthur@newrelic.com>
2019-04-09 13:28:27 -0700

    Ignore server when testing client. PYTHON-3242

--- tests/framework_grpc/sample_application/__init__.py
+++ tests/framework_grpc/sample_application/__init__.py
@@ -1,4 +1,5 @@
 import time
+from newrelic.api.transaction import current_transaction
 
 from sample_application_pb2 import Message
 from sample_application_pb2_grpc import (
@@ -51,6 +52,40 @@ class SampleApplicationServicer(_SampleApplicationServicer):
     def DoStreamStreamRaises(self, request_iter, context):
         for request in request_iter:
             raise AssertionError('stream_stream: %s' % request.text)
+
+    def NoTxnUnaryUnaryRaises(self, request, context):
+        current_transaction().ignore_transaction = True
+        raise AssertionError('unary_unary: %s' % request.text)
+
+    def NoTxnUnaryStreamRaises(self, request, context):
+        current_transaction().ignore_transaction = True
+        raise AssertionError('unary_stream: %s' % request.text)
+
+    def NoTxnStreamUnaryRaises(self, request_iter, context):
+        current_transaction().ignore_transaction = True
+        for request in request_iter:
+            raise AssertionError('stream_unary: %s' % request.text)
+
+    def NoTxnStreamStreamRaises(self, request_iter, context):
+        current_transaction().ignore_transaction = True
+        for request in request_iter:
+            raise AssertionError('stream_stream: %s' % request.text)
+
+    def NoTxnUnaryUnary(self, request, context):
+        current_transaction().ignore_transaction = True
+        return self.DoUnaryUnary(request, context)
+
+    def NoTxnUnaryStream(self, request, context):
+        current_transaction().ignore_transaction = True
+        return self.DoUnaryStream(request, context)
+
+    def NoTxnStreamUnary(self, request_iter, context):
+        current_transaction().ignore_transaction = True
+        return self.DoStreamUnary(request_iter, context)
+
+    def NoTxnStreamStream(self, request_iter, context):
+        current_transaction().ignore_transaction = True
+        return self.DoStreamStream(request_iter, context)
 
     def extract_dt_value(self, metadata):
         for k, v in metadata:

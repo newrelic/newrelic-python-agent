053b152f9f5c117ce7a3d574387461c955530c2d
Allan Feldman <afeldman@newrelic.com>
2019-04-12 13:57:10 -0700

    Mark py37 only pika failure. PYTHON-3245

--- tests/messagebroker_pika/test_pika_blocking_connection_consume_generator.py
+++ tests/messagebroker_pika/test_pika_blocking_connection_consume_generator.py
@@ -1,4 +1,4 @@
-from minversion import new_pika_skip, new_pika_xfail
+from minversion import new_pika_skip, new_pika_xfail, new_pika_xfail_py37
 import pika
 
 from newrelic.api.background_task import background_task
@@ -43,7 +43,7 @@ def test_blocking_connection_consume_break(producer):
             break
 
 
-@new_pika_xfail
+@new_pika_xfail_py37
 @validate_transaction_metrics(
         ('test_pika_blocking_connection_consume_generator:'
                 'test_blocking_connection_consume_connection_close'),

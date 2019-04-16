fcca5cf9e871b367cc03f8cc12429a17e1b98237
Allan Feldman <afeldman@newrelic.com>
2019-04-16 16:31:07 -0700

    Enable consume generator tests. PYTHON-3248

--- tests/messagebroker_pika/test_pika_blocking_connection_consume_generator.py
+++ tests/messagebroker_pika/test_pika_blocking_connection_consume_generator.py
@@ -1,4 +1,3 @@
-from minversion import new_pika_skip, new_pika_xfail
 import pika
 
 from newrelic.api.background_task import background_task
@@ -68,7 +67,6 @@ def test_blocking_connection_consume_connection_close(producer):
         connection.close()
 
 
-@new_pika_xfail
 @validate_transaction_metrics(
         ('test_pika_blocking_connection_consume_generator:'
                 'test_blocking_connection_consume_timeout'),
@@ -161,7 +159,6 @@ _test_blocking_connection_consume_many_metrics = [
 ]
 
 
-@new_pika_skip
 @validate_transaction_metrics(
         ('test_pika_blocking_connection_consume_generator:'
                 'test_blocking_connection_consume_many'),
@@ -184,7 +181,6 @@ def test_blocking_connection_consume_many(produce_five):
                 break
 
 
-@new_pika_xfail
 @validate_transaction_metrics(
         ('test_pika_blocking_connection_consume_generator:'
                 'test_blocking_connection_consume_using_methods'),
@@ -220,7 +216,6 @@ def test_blocking_connection_consume_using_methods(producer):
         assert result is None
 
 
-@new_pika_skip
 @validate_transaction_metrics(
         'Named/%s' % EXCHANGE,
         scoped_metrics=_test_blocking_connection_consume_metrics,
@@ -278,7 +273,6 @@ def test_blocking_connection_consume_many_outside_txn(produce_five):
                 consumer.close()
 
 
-@new_pika_xfail
 @validate_transaction_metrics(
         'Named/%s' % EXCHANGE,
         scoped_metrics=_test_blocking_connection_consume_metrics,

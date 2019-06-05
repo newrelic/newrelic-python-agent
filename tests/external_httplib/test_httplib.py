551c50a2a23c74e25ebf48c3642a83adde251f4a
Loren Arthur <larthur@newrelic.com>
2019-06-05 14:59:34 -0700

    Update httplib tests to use MockHTTPServer. Python-3236

--- tests/external_httplib/test_httplib.py
+++ tests/external_httplib/test_httplib.py
@@ -21,17 +21,23 @@ def select_python_version(py2, py3):
     return six.PY3 and py3 or py2
 
 
+@pytest.fixture()
+def mock_server():
+    with MockExternalHTTPServer() as server:
+        yield server
+
+
 _test_httplib_http_request_scoped_metrics = [select_python_version(
-        py2=('External/www.example.com/httplib/', 1),
-        py3=('External/www.example.com/http/', 1))]
+        py2=('External/localhost:8989/httplib/', 1),
+        py3=('External/localhost:8989/http/', 1))]
 
 
 _test_httplib_http_request_rollup_metrics = [
         ('External/all', 1),
         ('External/allOther', 1),
-        ('External/www.example.com/all', 1),
-        select_python_version(py2=('External/www.example.com/httplib/', 1),
-                              py3=('External/www.example.com/http/', 1))]
+        ('External/localhost:8989/all', 1),
+        select_python_version(py2=('External/localhost:8989/httplib/', 1),
+                              py3=('External/localhost:8989/http/', 1))]
 
 
 @validate_transaction_metrics(
@@ -40,18 +46,31 @@ _test_httplib_http_request_rollup_metrics = [
         rollup_metrics=_test_httplib_http_request_rollup_metrics,
         background_task=True)
 @background_task()
-def test_httplib_http_request():
-    connection = httplib.HTTPConnection('www.example.com', 80)
+def test_httplib_http_request(mock_server):
+    connection = httplib.HTTPConnection('localhost', 8989)
     connection.request('GET', '/')
     response = connection.getresponse()
     response.read()
     connection.close()
 
 
+_test_httplib_https_request_scoped_metrics = [select_python_version(
+        py2=('External/www.example.com/httplib/', 1),
+        py3=('External/www.example.com/http/', 1))]
+
+
+_test_httplib_https_request_rollup_metrics = [
+        ('External/all', 1),
+        ('External/allOther', 1),
+        ('External/www.example.com/all', 1),
+        select_python_version(py2=('External/www.example.com/httplib/', 1),
+                              py3=('External/www.example.com/http/', 1))]
+
+
 @validate_transaction_metrics(
         'test_httplib:test_httplib_https_request',
-        scoped_metrics=_test_httplib_http_request_scoped_metrics,
-        rollup_metrics=_test_httplib_http_request_rollup_metrics,
+        scoped_metrics=_test_httplib_https_request_scoped_metrics,
+        rollup_metrics=_test_httplib_https_request_rollup_metrics,
         background_task=True)
 @background_task()
 def test_httplib_https_request():
@@ -81,13 +100,12 @@ _test_httplib_http_request_with_port_rollup_metrics = [
         rollup_metrics=_test_httplib_http_request_with_port_rollup_metrics,
         background_task=True)
 @background_task()
-def test_httplib_http_with_port_request():
-    with MockExternalHTTPServer():
-        connection = httplib.HTTPConnection('localhost', 8989)
-        connection.request('GET', '/')
-        response = connection.getresponse()
-        response.read()
-        connection.close()
+def test_httplib_http_with_port_request(mock_server):
+    connection = httplib.HTTPConnection('localhost', 8989)
+    connection.request('GET', '/')
+    response = connection.getresponse()
+    response.read()
+    connection.close()
 
 
 @pytest.mark.parametrize('distributed_tracing,span_events', (
@@ -95,12 +113,12 @@ def test_httplib_http_with_port_request():
     (True, False),
     (False, False),
 ))
-def test_httplib_cross_process_request(distributed_tracing, span_events):
+def test_httplib_cross_process_request(distributed_tracing, span_events, mock_server):
     @background_task(name='test_httplib:test_httplib_cross_process_request')
     @cache_outgoing_headers
     @validate_cross_process_headers
     def _test():
-        connection = httplib.HTTPConnection('www.example.com', 80)
+        connection = httplib.HTTPConnection('localhost', 8989)
         connection.request('GET', '/')
         response = connection.getresponse()
         response.read()
@@ -150,29 +168,28 @@ def test_httplib_cross_process_response():
         connection.close()
 
 
-def test_httplib_multiple_requests_cross_process_response():
-    with MockExternalHTTPServer():
-        connection = httplib.HTTPConnection('localhost', 8989)
+def test_httplib_multiple_requests_cross_process_response(mock_server):
+    connection = httplib.HTTPConnection('localhost', 8989)
 
-        @validate_transaction_metrics(
-                'test_httplib:test_transaction',
-                scoped_metrics=_test_httplib_cross_process_response_scoped_metrics,
-                rollup_metrics=_test_httplib_cross_process_response_rollup_metrics,
-                background_task=True)
-        @insert_incoming_headers
-        @validate_external_node_params(
-                params=_test_httplib_cross_process_response_external_node_params)
-        @background_task(name='test_httplib:test_transaction')
-        def test_transaction():
-            connection.request('GET', '/')
-            response = connection.getresponse()
-            response.read()
+    @validate_transaction_metrics(
+            'test_httplib:test_transaction',
+            scoped_metrics=_test_httplib_cross_process_response_scoped_metrics,
+            rollup_metrics=_test_httplib_cross_process_response_rollup_metrics,
+            background_task=True)
+    @insert_incoming_headers
+    @validate_external_node_params(
+            params=_test_httplib_cross_process_response_external_node_params)
+    @background_task(name='test_httplib:test_transaction')
+    def test_transaction():
+        connection.request('GET', '/')
+        response = connection.getresponse()
+        response.read()
 
-        # make multiple requests with the same connection
-        for _ in range(2):
-            test_transaction()
+    # make multiple requests with the same connection
+    for _ in range(2):
+        test_transaction()
 
-        connection.close()
+    connection.close()
 
 
 def process_response(response):

e0bd5b3d19e8b194b3ca56831169d468cdfbb65c
Allan Feldman <afeldman@newrelic.com>
2019-06-10 16:57:02 -0400

    Add asyncio_transactions interface. PYTHON-3294

--- tests/agent_features/conftest.py
+++ tests/agent_features/conftest.py
@@ -39,4 +39,5 @@ def requires_data_collector(collector_available_fixture):
 
 
 if six.PY2:
-    collect_ignore = ['test_coroutine_transaction.py', 'test_async_timing.py']
+    collect_ignore = ['test_coroutine_transaction.py', 'test_async_timing.py',
+            'test_asyncio_transactions.py']

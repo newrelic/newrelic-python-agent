675cb221818e53f68b2340da65fa623fc6335368
Robert Lewis <rlewis@newrelic.com>
2019-06-28 12:42:13 -0400

    Restore settings as part of cat header tests. PYTHON-3321

--- newrelic/api/tests/test_cat_header_mixin.py
+++ newrelic/api/tests/test_cat_header_mixin.py
@@ -25,10 +25,12 @@ class TestProcessResponseHeaders(newrelic.tests.test_cases.TestCase):
 
     def test_process_response_headers_message_trace_without_settings(self):
         with self.transaction:
+            settings = self.transaction._settings
             self.transaction._settings = None
             with MessageTrace(self.transaction, 'library', 'operation',
                     'destination_type', 'destination_name') as trace:
                 trace.process_response_headers([])
+            self.transaction._settings = settings
 
     def test_process_response_headers_message_trace_without_transaction(self):
         with MessageTrace(None, 'library', 'operation', 'destination_type',

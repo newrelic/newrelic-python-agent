cb286132522112f3c3e9c5053f160c37cb7ad602
Allan Feldman <afeldman@newrelic.com>
2019-07-23 12:00:48 -0700

    Add a test for the blocking transaction remaining active. PYTHON-3295

--- tests/agent_features/test_io_loop_wait_time.py
+++ tests/agent_features/test_io_loop_wait_time.py
@@ -1,3 +1,4 @@
+import pytest
 import asyncio
 import time
 from newrelic.api.background_task import background_task
@@ -6,10 +7,13 @@ from testing_support.fixtures import validate_transaction_metrics
 
 @background_task(name="block")
 @asyncio.coroutine
-def block_loop(ready, done):
+def block_loop(ready, done, blocking_transaction_active):
     yield from ready.wait()
     time.sleep(0.1)
     done.set()
+    if blocking_transaction_active:
+        ready.clear()
+        yield from ready.wait()
 
 
 _wait_metrics_scoped = (
@@ -26,20 +30,29 @@ _wait_metrics_rollup = (
 def wait_for_loop(ready, done):
     ready.set()
     yield from done.wait()
+    ready.set()
 
 
-@validate_transaction_metrics(
-    "wait",
-    scoped_metrics=_wait_metrics_scoped,
-    rollup_metrics=_wait_metrics_rollup,
-    background_task=True,
-)
-def test_record_io_loop_wait():
+@pytest.mark.parametrize('blocking_transaction_active', (True, False))
+def test_record_io_loop_wait(blocking_transaction_active):
     import asyncio
 
     ready, done = (asyncio.Event(), asyncio.Event())
     future = asyncio.gather(
         wait_for_loop(ready, done),
-        block_loop(ready, done),
+        block_loop(ready, done, blocking_transaction_active),
     )
-    asyncio.get_event_loop().run_until_complete(future)
+
+    index = 0 if blocking_transaction_active else -1
+
+    @validate_transaction_metrics(
+        "wait",
+        scoped_metrics=_wait_metrics_scoped,
+        rollup_metrics=_wait_metrics_rollup,
+        background_task=True,
+        index=index,
+    )
+    def _test():
+        asyncio.get_event_loop().run_until_complete(future)
+
+    _test()

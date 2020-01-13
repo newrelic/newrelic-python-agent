c59ae64a055cf392e5eed3cf629153b73988ba07
Loren Arthur <larthur@newrelic.com>
2020-01-13 14:16:36 -0800

    Add exclude_newrelic_header setting. PYTHON-3508

--- tests/settings_config/test_distributed_tracing_settings.py
+++ tests/settings_config/test_distributed_tracing_settings.py
@@ -7,15 +7,15 @@ INI_FILE_EMPTY = b"""
 
 INI_FILE_W3C = b"""
 [newrelic]
-distributed_tracing.format = w3c
+distributed_tracing.exclude_newrelic_header = true
 """
 
 # Tests for loading settings and testing for values precedence
 @pytest.mark.parametrize('ini,env,expected_format', (
-    (INI_FILE_EMPTY, {}, 'newrelic',),
-    (INI_FILE_W3C, {}, 'w3c'),
+    (INI_FILE_EMPTY, {}, False),
+    (INI_FILE_W3C, {}, True),
 ))
-def test_region_aware_license_keys(ini, env, expected_format, global_settings):
+def test_distributed_trace_setings(ini, env, expected_format, global_settings):
 
     settings = global_settings()
-    assert settings.distributed_tracing.format == expected_format
+    assert settings.distributed_tracing.exclude_newrelic_header == expected_format

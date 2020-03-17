edb1848f06a7521dc7ed80bd17012868a9f99660
Carrie Edwards <carrieedwards@newrelic.com>
2020-03-17 16:38:22 -0700

    Add data collector grpc stream test. PYTHON-3576

---
+++ tests/backend_mtb/test_data_collector.py
@@ -0,0 +1,13 @@
+from newrelic.core.data_collector import ApplicationSession
+from newrelic.core.config import finalize_application_settings
+
+
+def test_stream_connect(mock_grpc_server):
+    settings = finalize_application_settings(
+        {'mtb.endpoint': 'http://localhost:%d' % mock_grpc_server}
+    )
+    session = ApplicationSession('12345', settings.license_key, settings)
+    responses = session.connect_span_stream(iter(()))
+    responses = list(responses)
+    assert len(responses) == 1
+    assert responses[0].messages_seen == 0

6b9fd51a60eb1908ebb716c75f22287559f9e0fb
Loren Arthur <larthur@newrelic.com>
2020-03-18 14:54:29 -0700

    Use TerminatingDeque in test. PYTHON-3576

--- tests/backend_mtb/test_data_collector.py
+++ tests/backend_mtb/test_data_collector.py
@@ -1,4 +1,5 @@
 from newrelic.core.data_collector import ApplicationSession
+from newrelic.common.streaming_utils import TerminatingDeque
 from newrelic.core.config import finalize_application_settings
 
 
@@ -7,7 +8,11 @@ def test_stream_connect(mock_grpc_server):
         {'mtb.endpoint': 'http://localhost:%d' % mock_grpc_server}
     )
     session = ApplicationSession('12345', settings.license_key, settings)
-    responses = session.connect_span_stream(iter(()))
+    responses = session.connect_span_stream(TerminatingDeque(0))
+    try:
+        session.shutdown_session()
+    except:
+        pass
     responses = list(responses)
     assert len(responses) == 1
     assert responses[0].messages_seen == 0

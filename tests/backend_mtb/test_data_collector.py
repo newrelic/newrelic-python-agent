c40c63af807352a8651b20ea5eca3a375f9677ac
Loren Arthur <larthur@newrelic.com>
2020-03-18 15:13:45 -0700

    Use fake requests session. PYTHON-3576

--- tests/backend_mtb/test_data_collector.py
+++ tests/backend_mtb/test_data_collector.py
@@ -1,18 +1,36 @@
+import json
+
 from newrelic.core.data_collector import ApplicationSession
 from newrelic.common.streaming_utils import TerminatingDeque
 from newrelic.core.config import finalize_application_settings
+import newrelic.packages.requests as requests
+
+
+class FakeRequestsSession(requests.Session):
+    def __init__(self, status, text):
+        self.status = status
+        self.text = text
+
+    def request(self, *args, **kwargs):
+        response = requests.Response()
+        response.status_code = self.status
+        response._content_consumed = True
+        response._content = self.text.encode('utf-8')
+
+        return response
 
 
 def test_stream_connect(mock_grpc_server):
     settings = finalize_application_settings(
         {'mtb.endpoint': 'http://localhost:%d' % mock_grpc_server}
     )
-    session = ApplicationSession('12345', settings.license_key, settings)
+    requests_session = FakeRequestsSession(
+            200, json.dumps({'return_value': ''}))
+    session = ApplicationSession('http://', settings.license_key, settings)
+    session._requests_session = requests_session
     responses = session.connect_span_stream(TerminatingDeque(0))
-    try:
-        session.shutdown_session()
-    except:
-        pass
+    # Calling shutdown will terminate the RPC
+    session.shutdown_session()
     responses = list(responses)
     assert len(responses) == 1
     assert responses[0].messages_seen == 0

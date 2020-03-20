07f4b24d651dac77e484b72e2135d1e4022efd46
Allan Feldman <afeldman@newrelic.com>
2020-03-20 14:37:30 -0400

    Terminate stream immediately on shutdown. PYTHON-3577

--- newrelic/common/tests/test_streaming_utils.py
+++ newrelic/common/tests/test_streaming_utils.py
@@ -3,48 +3,48 @@ import newrelic.tests.test_cases
 import newrelic.common.streaming_utils as streaming_utils
 
 
-class TestTerminatingDeque(newrelic.tests.test_cases.TestCase):
+class TestStreamBuffer(newrelic.tests.test_cases.TestCase):
     def setUp(self):
-        self.terminating_deque = streaming_utils.TerminatingDeque(2)
-        self.lock = self.terminating_deque._notify
+        self.buffer = streaming_utils.StreamBuffer(2)
+        self.lock = self.buffer._notify
 
     def test_simple_put(self):
         item = object()
 
         # Set up a put operation on a separate thread
         thread = threading.Thread(
-                target=self.terminating_deque.put, args=(item,))
+                target=self.buffer.put, args=(item,))
         thread.daemon = True
 
         with self.lock:
             thread.start()
 
             # The lock should prevent the item from being added to the queue
-            assert len(self.terminating_deque._queue) == 0
+            assert len(self.buffer._queue) == 0
 
         # The thread should terminate after the lock is released
         thread.join(timeout=0.1)
         assert not thread.is_alive()
 
         # The item should be added to the queue
-        assert len(self.terminating_deque._queue) == 1
+        assert len(self.buffer._queue) == 1
 
     def test_shutdown_prevents_additional_puts(self):
         item = object()
 
         # Call shutdown
-        self.terminating_deque.shutdown()
+        self.buffer.shutdown()
 
         # Attempt to put item into the deque
-        self.terminating_deque.put(item)
+        self.buffer.put(item)
 
         # The item should not be in the queue
-        assert len(self.terminating_deque._queue) == 0
+        assert len(self.buffer._queue) == 0
 
     def test_shutdown_terminates_iterator(self):
         # Set up an iterator thread
         thread = threading.Thread(
-                target=lambda: [_ for _ in self.terminating_deque])
+                target=lambda: [_ for _ in self.buffer])
         thread.daemon = True
 
         # Force the thread to start
@@ -52,32 +52,32 @@ class TestTerminatingDeque(newrelic.tests.test_cases.TestCase):
         assert thread.is_alive()
 
         # Call shutdown
-        self.terminating_deque.shutdown()
+        self.buffer.shutdown()
 
         # Thread terminates immediately
         thread.join(timeout=0.1)
         assert not thread.is_alive()
 
-    def test_queue_drains_before_shutdown(self):
+    def test_shutdown_terminates_without_draining_queue(self):
         items = [object() for _ in range(2)]
 
         # Put items into the queue and shutdown
         for item in items:
-            self.terminating_deque.put(item)
-        self.terminating_deque.shutdown()
+            self.buffer.put(item)
+        self.buffer.shutdown()
 
         # Set up consumer to record items consumed in a background thread
         consumed = []
         thread = threading.Thread(
                 target=lambda: consumed.extend(
-                    item for item in self.terminating_deque))
+                    item for item in self.buffer))
         thread.daemon = True
         thread.start()
 
         # The thread should shut down only after consuming
         thread.join(timeout=0.1)
         assert not thread.is_alive()
-        assert consumed == items
+        assert consumed == []
 
 
 class AttributeValue(object):

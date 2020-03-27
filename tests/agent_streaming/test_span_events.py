80c15e88c8c3c23e943a20de0495e43ef1d8abd1
Allan Feldman <afeldman@newrelic.com>
2020-03-27 16:51:32 -0400

    Add testing for DT disabled. PYTHON-3581

--- tests/agent_streaming/test_span_events.py
+++ tests/agent_streaming/test_span_events.py
@@ -1,22 +1,26 @@
+from newrelic.api.background_task import background_task
+from newrelic.api.transaction import current_transaction
+
 from testing_support.fixtures import (override_application_settings,
         core_application_stats_engine)
 from testing_support.validators.validate_span_events import (
         validate_span_events)
-from newrelic.api.background_task import background_task
-from newrelic.api.transaction import current_transaction
 
 
-@override_application_settings(
-    {
-        "distributed_tracing.enabled": True,
-        "span_events.enabled": True,
-    }
-)
+@override_application_settings({'distributed_tracing.enabled': True})
 @validate_span_events(count=1)
-@background_task(name="test_mtb_span_events")
-def test_mtb_span_events():
+@background_task(name='test_span_events_dt_enabled')
+def test_span_events_dt_enabled():
     transaction = current_transaction()
-    transaction._sampled = True
+    transaction._sampled = False
+
+
+@override_application_settings({'distributed_tracing.enabled': False})
+@validate_span_events(count=0)
+@background_task(name='test_span_events_dt_disabled')
+def test_span_events_dt_disabled():
+    transaction = current_transaction()
+    transaction._sampled = False
 
 
 def test_span_stream_is_singleton():

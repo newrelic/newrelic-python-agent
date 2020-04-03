4cc9e1a4444b4b2e1837a96c0f8c12112638e07d
Allan Feldman <afeldman@newrelic.com>
2020-04-03 13:37:40 -0400

    Update 8t settings in tests. PYTHON-3601

--- tests/agent_streaming/test_data_collector.py
+++ tests/agent_streaming/test_data_collector.py
@@ -4,10 +4,11 @@ from newrelic.core.config import finalize_application_settings
 
 
 def test_stream_connect(mock_grpc_server):
-    settings = finalize_application_settings(
-        {'infinite_tracing.trace_observer_url':
-        'http://localhost:%d' % mock_grpc_server}
-    )
+    settings = finalize_application_settings({
+        'infinite_tracing.trace_observer_host': 'localhost',
+        'infinite_tracing.trace_observer_port': mock_grpc_server,
+        'infinite_tracing.ssl': False,
+    })
     session = ApplicationSession('http://', settings.license_key, settings)
     stream_buffer = StreamBuffer(0)
     responses = session.connect_span_stream(stream_buffer)

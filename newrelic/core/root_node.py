f3810a13834fc975749774dcb3f7cc69fd40f9eb
Carrie Edwards <carrieedwards@newrelic.com>
2020-05-12 13:46:51 -0700

    Add transaction name intrinsic attr to root span. PYTHON-3636

--- newrelic/core/root_node.py
+++ newrelic/core/root_node.py
@@ -16,6 +16,7 @@ class RootNode(_RootNode, GenericNodeMixin):
     def span_event(self, *args, **kwargs):
         span = super(RootNode, self).span_event(*args, **kwargs)
         i_attrs = span[0]
+        i_attrs['transaction.name'] = self.path
         i_attrs['nr.entryPoint'] = True
         if self.trusted_parent_span:
             i_attrs['trustedParentId'] = self.trusted_parent_span

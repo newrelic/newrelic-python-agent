b47c782ad3e3eef91b116b919918a331769a2cef
Allan Feldman <afeldman@newrelic.com>
2020-08-05 11:10:40 -0400

    Fix license packaging for urllib3.

--- setup.py
+++ setup.py
@@ -147,6 +147,7 @@ kwargs = dict(
         python_requires = '>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*',
         package_data = {'newrelic': ['newrelic.ini',
                                      'version.txt',
+                                     'packages/urllib3/LICENSE.txt',
                                      'common/cacert.pem'],
         },
         scripts = [ 'scripts/newrelic-admin' ],

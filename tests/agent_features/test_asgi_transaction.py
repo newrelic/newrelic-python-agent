d70b74f129f9fab692fdcfe51ea4a33af2264eb5
Uma Annamalai <uannamalai@newrelic.com>
2020-08-20 16:18:35 -0700

    Update asgi app decorator test. PYTHON-3706

--- tests/agent_features/test_asgi_transaction.py
+++ tests/agent_features/test_asgi_transaction.py
@@ -1,3 +1,17 @@
+# Copyright 2010 New Relic, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import pytest
 from testing_support.sample_asgi_applications import simple_app_v2_raw, simple_app_v3_raw, simple_app_v2, simple_app_v3
 from testing_support.fixtures import validate_transaction_metrics, override_application_settings, function_not_called
@@ -37,48 +51,41 @@ def test_single_callable_naming_scheme(naming_scheme):
 #Test the default naming scheme logic and ASGIApplicationWrapper for a double callable
 @validate_transaction_metrics(name="", group="Uri")
 def test_double_callable_default_naming_scheme():
-
-    def _test():
-        response = simple_app_v2_wrapped.make_request("GET", "/")
-        assert response.status == 200
-        assert response.headers == {}
-        assert response.body == b""
-
-    _test()
+    response = simple_app_v2_wrapped.make_request("GET", "/")
+    assert response.status == 200
+    assert response.headers == {}
+    assert response.body == b""
 
 
 #No harm test on single callable asgi app with agent disabled to ensure proper response
 def test_single_callable_raw():
-
-    def _test():
-        response = simple_app_v3_original.make_request("GET", "/")
-        assert response.status == 200
-        assert response.headers == {}
-        assert response.body == b""
-
-    _test()
+    response = simple_app_v3_original.make_request("GET", "/")
+    assert response.status == 200
+    assert response.headers == {}
+    assert response.body == b""
 
 
 #No harm test on double callable asgi app with agent disabled to ensure proper response
 def test_double_callable_raw():
-
-    def _test():
-        response = simple_app_v3_original.make_request("GET", "/")
-        assert response.status == 200
-        assert response.headers == {}
-        assert response.body == b""
-
-    _test()
+    response = simple_app_v3_original.make_request("GET", "/")
+    assert response.status == 200
+    assert response.headers == {}
+    assert response.body == b""
 
 
 #Test asgi_application decorator with parameters passed in on a single callable
-@pytest.mark.parametrize("group", (None, "group", ""))
-@pytest.mark.parametrize("framework", (None, "function", ""))
-def test_asgi_application_decorator_single_callable(group, framework):
+@pytest.mark.parametrize("name, group", ((None, "group"), ("name", "group"), ("", "group")))
+def test_asgi_application_decorator_single_callable(name, group):
+    if name:
+        expected_name = name
+        expected_group = group
+    else:
+        expected_name = ""
+        expected_group = "Uri"
 
-    @validate_transaction_metrics(name="", group="Uri")
+    @validate_transaction_metrics(name=expected_name, group=expected_group)
     def _test():
-        asgi_decorator = asgi_application(name="", group=group, framework=framework)
+        asgi_decorator = asgi_application(name=name, group=group)
         decorated_application = asgi_decorator(simple_app_v3_raw)
         application = AsgiTest(decorated_application)
         response = application.make_request("GET", "/")
@@ -90,16 +97,13 @@ def test_asgi_application_decorator_single_callable(group, framework):
 
 
 #Test asgi_application decorator using default values on a double callable
+@validate_transaction_metrics(name="", group="Uri")
 def test_asgi_application_decorator_no_params_double_callable():
+    asgi_decorator = asgi_application()
+    decorated_application = asgi_decorator(simple_app_v2_raw)
+    application = AsgiTest(decorated_application)
+    response = application.make_request("GET", "/")
+    assert response.status == 200
+    assert response.headers == {}
+    assert response.body == b""
 
-    @validate_transaction_metrics(name="", group="Uri")
-    def _test():
-        asgi_decorator = asgi_application()
-        decorated_application = asgi_decorator(simple_app_v2_raw)
-        application = AsgiTest(decorated_application)
-        response = application.make_request("GET", "/")
-        assert response.status == 200
-        assert response.headers == {}
-        assert response.body == b""
-
-    _test()

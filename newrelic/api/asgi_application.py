f9c42262fd9c358ad32bd0a3b68bc3031d42db48
Allan Feldman <afeldman@newrelic.com>
2020-08-26 10:36:13 -0400

    Update ASGI APIs to use wrapt. PYTHON-3715

--- newrelic/api/asgi_application.py
+++ newrelic/api/asgi_application.py
@@ -19,9 +19,29 @@ import newrelic.packages.six as six
 from newrelic.api.application import application_instance
 from newrelic.api.web_transaction import WebTransaction
 from newrelic.common.object_names import callable_name
-from newrelic.common.object_wrapper import wrap_object
+from newrelic.common.object_wrapper import wrap_object, FunctionWrapper, function_wrapper
 from newrelic.common.async_proxy import CoroutineProxy, LoopContext
 from newrelic.api.html_insertion import insert_html_snippet, verify_body_exists
+
+
+def _bind_scope(scope, *args, **kwargs):
+    return scope
+
+
+def _bind_receive_send(scope, receive, send):
+    return receive, send
+
+
+async def coro_function_wrapper(coro_function, receive, send):
+    return await coro_function(receive, send)
+
+
+@function_wrapper
+def double_to_single_callable(wrapped, instance, args, kwargs):
+    scope = _bind_scope(*args, **kwargs)
+    receive, send = _bind_receive_send(*args, **kwargs)
+    coro_function = wrapped(scope)
+    return coro_function_wrapper(coro_function, receive, send)
 
 
 class ASGIBrowserMiddleware(object):
@@ -219,97 +239,96 @@ class ASGIWebTransaction(WebTransaction):
             headers=headers,
         )
 
+        if self._settings:
+            self.capture_params = self._settings.capture_params
+
     async def send(self, event):
         if event["type"] == "http.response.start":
             self.process_response(event["status"], event.get("headers", ()))
         return await self._send(event)
 
 
-def make_single_callable(original, wrapped, application, name, group, framework):
-    async def nr_asgi_wrapper(scope, receive, send):
-        if scope["type"] != "http":
-            return await wrapped(scope, receive, send)
-
-        _application = application_instance(application)
-
-        with ASGIWebTransaction(
-            application=_application, scope=scope, receive=receive, send=send
-        ) as transaction:
-
-            # Record details of framework against the transaction for later
-            # reporting as supportability metrics.
-            if framework:
-                transaction.add_framework_info(name=framework[0], version=framework[1])
-
-            # Override the initial web transaction name to be the supplied
-            # name, or the name of the wrapped callable if wanting to use
-            # the callable as the default. This will override the use of a
-            # raw URL which can result in metric grouping issues where a
-            # framework is not instrumented or is leaking URLs.
-            #
-            # Note that at present if default for naming scheme is still
-            # None and we aren't specifically wrapping a designated
-            # framework, then we still allow old URL based naming to
-            # override. When we switch to always forcing a name we need to
-            # check for naming scheme being None here.
-
-            settings = transaction._settings
-
-            if name is None and settings:
-                naming_scheme = settings.transaction_name.naming_scheme
-
-                if framework is not None:
-                    if naming_scheme in (None, "framework"):
-                        transaction.set_transaction_name(
-                            callable_name(original), priority=1
-                        )
-
-                elif naming_scheme in ("component", "framework"):
-                    transaction.set_transaction_name(
-                        callable_name(original), priority=1
-                    )
-
-            elif name:
-                transaction.set_transaction_name(name, group, priority=1)
-
-            if (
-                settings
-                and settings.browser_monitoring.enabled
-                and not transaction.autorum_disabled
-            ):
-                app = ASGIBrowserMiddleware(wrapped, transaction)
-            else:
-                app = wrapped
-            coro = app(scope, transaction.receive, transaction.send)
-            coro = CoroutineProxy(coro, LoopContext())
-            return await coro
-
-    return nr_asgi_wrapper
-
-
-def make_double_callable(wrapped, *args, **kwargs):
-    class NRAsgiWrapper(object):
-        def __init__(self, scope):
-            self.scope = scope
-            _wrapped = asgiref_compatibility.double_to_single_callable(wrapped)
-            self.wrapped = make_single_callable(wrapped, _wrapped, *args, **kwargs)
-
-        async def __call__(self, receive, send):
-            return await self.wrapped(self.scope, receive, send)
-
-    return NRAsgiWrapper
-
-
 def ASGIApplicationWrapper(
     wrapped, application=None, name=None, group=None, framework=None
 ):
 
-    if asgiref_compatibility.is_double_callable(wrapped):
-        return make_double_callable(wrapped, application, name, group, framework)
-    else:
-        return make_single_callable(
-            wrapped, wrapped, application, name, group, framework
-        )
+    def nr_asgi_wrapper(wrapped, instance, args, kwargs):
+        double_callable = asgiref_compatibility.is_double_callable(wrapped)
+        if double_callable:
+            is_v2_signature = (len(args) + len(kwargs)) == 1
+            if not is_v2_signature:
+                return wrapped(*args, **kwargs)
+            wrapped = double_to_single_callable(wrapped)
+
+        scope = _bind_scope(*args, **kwargs)
+
+        if scope["type"] != "http":
+            return wrapped(*args, **kwargs)
+
+        async def nr_async_asgi(receive, send):
+            with ASGIWebTransaction(
+                application=application_instance(application),
+                scope=scope,
+                receive=receive,
+                send=send,
+            ) as transaction:
+
+                # Record details of framework against the transaction for later
+                # reporting as supportability metrics.
+                if framework:
+                    transaction.add_framework_info(
+                        name=framework[0], version=framework[1]
+                    )
+
+                # Override the initial web transaction name to be the supplied
+                # name, or the name of the wrapped callable if wanting to use
+                # the callable as the default. This will override the use of a
+                # raw URL which can result in metric grouping issues where a
+                # framework is not instrumented or is leaking URLs.
+                #
+                # Note that at present if default for naming scheme is still
+                # None and we aren't specifically wrapping a designated
+                # framework, then we still allow old URL based naming to
+                # override. When we switch to always forcing a name we need to
+                # check for naming scheme being None here.
+
+                settings = transaction._settings
+
+                if name is None and settings:
+                    naming_scheme = settings.transaction_name.naming_scheme
+
+                    if framework is not None:
+                        if naming_scheme in (None, "framework"):
+                            transaction.set_transaction_name(
+                                callable_name(wrapped), priority=1
+                            )
+
+                    elif naming_scheme in ("component", "framework"):
+                        transaction.set_transaction_name(
+                            callable_name(wrapped), priority=1
+                        )
+
+                elif name:
+                    transaction.set_transaction_name(name, group, priority=1)
+
+                if (
+                    settings
+                    and settings.browser_monitoring.enabled
+                    and not transaction.autorum_disabled
+                ):
+                    app = ASGIBrowserMiddleware(wrapped, transaction)
+                else:
+                    app = wrapped
+                coro = app(scope, transaction.receive, transaction.send)
+                coro = CoroutineProxy(coro, LoopContext())
+                return await coro
+
+        if double_callable:
+            return nr_async_asgi
+        else:
+            return nr_async_asgi(*_bind_receive_send(*args, **kwargs))
+
+    return FunctionWrapper(wrapped, nr_asgi_wrapper)
 
 
 def asgi_application(application=None, name=None, group=None, framework=None):

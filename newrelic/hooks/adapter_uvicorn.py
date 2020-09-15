729b60425e99087dc74e309641219fb1a39f30fb
Allan Feldman <afeldman@newrelic.com>
2020-09-15 10:48:10 -0400

    Fix uvicorn auto detection. PYTHON-3731

--- newrelic/hooks/adapter_uvicorn.py
+++ newrelic/hooks/adapter_uvicorn.py
@@ -17,6 +17,13 @@ from newrelic.api.asgi_application import ASGIApplicationWrapper
 
 @property
 def loaded_app(self):
+    # Always use the original application until the interface is resolved in
+    # auto mode
+    if getattr(self, "interface", "") == "auto":
+        app = self._nr_loaded_app
+        while hasattr(app, "__wrapped__"):
+            app = app.__wrapped__
+        return app
     return self._nr_loaded_app
 
 

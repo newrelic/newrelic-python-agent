5f8f2f6cf69b649bb4fe3c603131b1f344825ece
Uma Annamalai <uannamalai@newrelic.com>
2020-09-22 13:20:47 -0700

    Remove use of code_coverage. PYTHON-3726

--- tests/framework_starlette/test_application.py
+++ tests/framework_starlette/test_application.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 def test_application_index(target_application):
-    print("Hello World!")
+    pass
+
+
+

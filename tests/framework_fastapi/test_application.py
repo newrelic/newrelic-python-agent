2f12e3a3801ecb36181cbe109a5751d1e3b08792
Timothy Pansino <11214426+TimPansino@users.noreply.github.com>
2020-12-02 16:02:13 -0800

    PYTHON-3826: Adding fastapi tests (#37)
    * Adding fastapi tests [PYTHON-3826]
    
    * Adding fastapi to test matrix [PYTHON-3826]
    
    * Adding passenv to tox config [PYTHON-3826]

---
+++ tests/framework_fastapi/test_application.py
@@ -0,0 +1,16 @@
+import pytest
+from testing_support.fixtures import validate_transaction_metrics
+
+
+@pytest.mark.parametrize("endpoint,transaction_name", (
+    ("/sync", "_target_application:sync"),
+    ("/async", "_target_application:non_sync"),
+))
+def test_application(app, endpoint, transaction_name):
+    @validate_transaction_metrics(transaction_name,
+        scoped_metrics=[("Function/" + transaction_name, 1)])
+    def _test():
+        response = app.get(endpoint)
+        assert response.status == 200
+
+    _test()

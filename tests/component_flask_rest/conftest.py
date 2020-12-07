ff510fed6a5f7d489a5ebf93f3107dd4129faf36
Allan Feldman <6374032+a-feld@users.noreply.github.com>
2020-12-07 18:24:48 -0500

    Add flask restful/plus/x tests. (#49)
    * Add component_flask_rest tests.
    
    * Add support for flask_restx.
    
    * Add component_flask_rest to test matrix.

---
+++ tests/component_flask_rest/conftest.py
@@ -0,0 +1,34 @@
+import pytest
+
+from testing_support.fixtures import (code_coverage_fixture,  # noqa
+        collector_agent_registration_fixture, collector_available_fixture)
+
+_coverage_source = [
+    'newrelic.hooks.framework_flask',
+    'newrelic.hooks.component_flask_rest',
+]
+
+code_coverage = code_coverage_fixture(source=_coverage_source)
+
+_default_settings = {
+    'transaction_tracer.explain_threshold': 0.0,
+    'transaction_tracer.transaction_threshold': 0.0,
+    'transaction_tracer.stack_trace_threshold': 0.0,
+    'debug.log_data_collector_payloads': True,
+    'debug.record_transaction_failure': True,
+    'debug.log_autorum_middleware': True,
+}
+
+collector_agent_registration = collector_agent_registration_fixture(
+        app_name='Python Agent Test (component_flask_rest)',
+        default_settings=_default_settings)
+
+
+@pytest.fixture(scope='session')
+def session_initialization(code_coverage, collector_agent_registration):
+    pass
+
+
+@pytest.fixture(scope='function')  # noqa
+def requires_data_collector(collector_available_fixture):
+    pass

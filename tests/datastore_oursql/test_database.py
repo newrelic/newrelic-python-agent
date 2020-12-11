350f6701fcf20b007fbbd74df641a632d41d2546
Tim Pansino <timpansino@gmail.com>
2020-12-11 15:28:13 -0800

    Cleaning up tests [PYTHON-3795]

--- tests/datastore_oursql/test_database.py
+++ tests/datastore_oursql/test_database.py
@@ -3,20 +3,21 @@ import oursql
 from testing_support.fixtures import (validate_transaction_metrics,
             validate_database_trace_inputs)
 
-from testing_support.settings import mysql_settings
+from testing_support.db_settings import mysql_settings
 
 from newrelic.api.background_task import background_task
 
-DB_SETTINGS = mysql_settings()
+DB_SETTINGS = mysql_settings()[0]
+DB_NAMESPACE = DB_SETTINGS["namespace"]
 
 _test_execute_via_cursor_scoped_metrics = [
         ('Function/oursql:Connection', 1),
         ('Function/oursql:Connection.__enter__', 1),
         ('Function/oursql:Connection.__exit__', 1),
-        ('Datastore/statement/MySQL/datastore_oursql/select', 2),
-        ('Datastore/statement/MySQL/datastore_oursql/insert', 1),
-        ('Datastore/statement/MySQL/datastore_oursql/update', 1),
-        ('Datastore/statement/MySQL/datastore_oursql/delete', 1),
+        ('Datastore/statement/MySQL/datastore_oursql_%s/select' % DB_NAMESPACE, 2),
+        ('Datastore/statement/MySQL/datastore_oursql_%s/insert' % DB_NAMESPACE, 1),
+        ('Datastore/statement/MySQL/datastore_oursql_%s/update' % DB_NAMESPACE, 1),
+        ('Datastore/statement/MySQL/datastore_oursql_%s/delete' % DB_NAMESPACE, 1),
         ('Datastore/operation/MySQL/create', 1),
         ('Datastore/operation/MySQL/drop', 1),
         ('Datastore/operation/MySQL/commit', 3),
@@ -28,13 +29,13 @@ _test_execute_via_cursor_rollup_metrics = [
         ('Datastore/MySQL/all', 12),
         ('Datastore/MySQL/allOther', 12),
         ('Datastore/operation/MySQL/select', 2),
-        ('Datastore/statement/MySQL/datastore_oursql/select', 2),
+        ('Datastore/statement/MySQL/datastore_oursql_%s/select' % DB_NAMESPACE, 2),
         ('Datastore/operation/MySQL/insert', 1),
-        ('Datastore/statement/MySQL/datastore_oursql/insert', 1),
+        ('Datastore/statement/MySQL/datastore_oursql_%s/insert' % DB_NAMESPACE, 1),
         ('Datastore/operation/MySQL/update', 1),
-        ('Datastore/statement/MySQL/datastore_oursql/update', 1),
+        ('Datastore/statement/MySQL/datastore_oursql_%s/update' % DB_NAMESPACE, 1),
         ('Datastore/operation/MySQL/delete', 1),
-        ('Datastore/statement/MySQL/datastore_oursql/delete', 1),
+        ('Datastore/statement/MySQL/datastore_oursql_%s/delete' % DB_NAMESPACE, 1),
         ('Datastore/operation/MySQL/create', 1),
         ('Datastore/operation/MySQL/drop', 1),
         ('Datastore/operation/MySQL/commit', 3),
@@ -46,22 +47,22 @@ _test_execute_via_cursor_rollup_metrics = [
         background_task=True)
 @validate_database_trace_inputs(sql_parameters_type=tuple)
 @background_task()
-def test_execute_via_cursor():
+def test_execute_via_cursor(table_name):
 
     connection = oursql.connect(db=DB_SETTINGS['name'],
             user=DB_SETTINGS['user'], passwd=DB_SETTINGS['password'],
             host=DB_SETTINGS['host'], port=DB_SETTINGS['port'])
 
     with connection as cursor:
-        cursor.execute("""drop table if exists datastore_oursql""")
+        cursor.execute("""drop table if exists %s""" % table_name)
 
-        cursor.execute("""create table datastore_oursql """
+        cursor.execute("""create table %s """ % table_name +
                 """(a integer, b real, c text)""")
 
-        cursor.executemany("""insert into datastore_oursql values (?, ?, ?)""",
+        cursor.executemany("""insert into %s values (?, ?, ?)""" % table_name,
                 [(1, 1.0, '1.0'), (2, 2.2, '2.2'), (3, 3.3, '3.3')])
 
-        cursor.execute("""select * from datastore_oursql""")
+        cursor.execute("""select * from %s""" % table_name)
 
         # The oursql cursor execute() method takes a non DBAPI2
         # argument to disable parameter interpolation. Also
@@ -69,14 +70,14 @@ def test_execute_via_cursor():
         # speedup in execution because the default way creates a
         # prepared statement every time and then throws it away.
 
-        cursor.execute("""select * from datastore_oursql""", plain_query=True)
+        cursor.execute("""select * from %s""" % table_name, plain_query=True)
 
         for row in cursor: pass
 
-        cursor.execute("""update datastore_oursql set a=?, b=?, c=? """
+        cursor.execute("""update %s set a=?, b=?, c=? """ % table_name +
                 """where a=?""", (4, 4.0, '4.0', 1))
 
-        cursor.execute("""delete from datastore_oursql where a=2""")
+        cursor.execute("""delete from %s where a=2""" % table_name)
 
     connection.commit()
     connection.rollback()

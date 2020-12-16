9ee1a7d048ad022a20f591693a9303b0a339587d
Tim Pansino <timpansino@gmail.com>
2020-12-16 11:08:21 -0800

    Parameterize solr tests for concurrency

--- tests/datastore_solrpy/test_solr.py
+++ tests/datastore_solrpy/test_solr.py
@@ -1,21 +1,24 @@
 from solr import SolrConnection
 
 from testing_support.fixtures import validate_transaction_metrics
-from testing_support.settings import solr_settings
+from testing_support.db_settings import solr_settings
 
 from newrelic.api.background_task import background_task
 
-SOLR_HOST, SOLR_PORT = solr_settings()
-SOLR_URL = 'http://%s:%s/solr/collection' % (SOLR_HOST, SOLR_PORT)
+DB_SETTINGS = solr_settings()[0]
+SOLR_HOST = DB_SETTINGS["host"]
+SOLR_PORT = DB_SETTINGS["port"]
+SOLR_URL = 'http://%s:%s/solr/collection' % (DB_SETTINGS["host"], DB_SETTINGS["port"])
 
 def _exercise_solr(solr):
-    solr.add_many([
-        {"id": "doc_1"},
-        {"id": "doc_2"},
-        ])
+    # Construct document names within namespace
+    documents = ["pysolr_doc_1", "pysolr_doc_2"]
+    documents = [x + "_" + DB_SETTINGS["namespace"] for x in documents]
+
+    solr.add_many([{"id": x} for x in documents])
     solr.commit()
-    solr.query('id:doc_1').results
-    solr.delete('id:doc_1')
+    solr.query('id:%s' % documents[0]).results
+    solr.delete('id:*_%s' % DB_SETTINGS["namespace"])
     solr.commit()
 
 _test_solr_search_scoped_metrics = [

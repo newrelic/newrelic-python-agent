f137bc86f30e783e19327d6c4c9336e44fa8e094
carrieedwards <edwrdscarrie@gmail.com>
2020-12-17 15:49:58 -0800

    Add http test suite. (#77)
    * Add HTTP test suite.
    
    * Update test workflow.
    
    * Add fixtures for HTTP library.
    
    * Update tox test matrix.
    
    * Update test fixtures.

---
+++ tests/external_http/test_http.py
@@ -0,0 +1,132 @@
+import pytest
+import six
+
+from testing_support.fixtures import (validate_transaction_metrics,
+        override_application_settings)
+from testing_support.external_fixtures import (cache_outgoing_headers,
+    insert_incoming_headers)
+from testing_support.validators.validate_cross_process_headers import (
+    validate_cross_process_headers)
+from testing_support.validators.validate_external_node_params import (
+    validate_external_node_params)
+
+from newrelic.api.background_task import background_task
+
+if six.PY2:
+    import httplib
+else:
+    import http.client as httplib
+
+@pytest.fixture(scope='session')
+def metrics(server):
+    if six.PY2:
+        _external_metric = 'External/localhost:%s/httplib/' % server.port
+    else:
+        _external_metric = 'External/localhost:%s/http/' % server.port
+
+    scoped = [
+            (_external_metric, 1)]
+
+    rollup = [
+            ('External/all', 1),
+            ('External/allOther', 1),
+            ('External/localhost:%s/all' % server.port, 1),
+            (_external_metric, 1)]
+
+    return scoped, rollup
+
+
+def test_http_http_request(server, metrics):
+    @validate_transaction_metrics(
+            'test_http:test_http_http_request',
+            scoped_metrics=metrics[0],
+            rollup_metrics=metrics[1],
+            background_task=True)
+    @background_task(name='test_http:test_http_http_request')
+    def _test():
+        connection = httplib.HTTPConnection('localhost', server.port)
+        connection.request('GET', '/')
+        response = connection.getresponse()
+        response.read()
+        connection.close()
+
+    _test()
+
+
+def test_http_https_request(server, metrics):
+    @validate_transaction_metrics(
+            'test_http:test_http_https_request',
+            scoped_metrics=metrics[0],
+            rollup_metrics=metrics[1],
+            background_task=True)
+    @background_task(name='test_http:test_http_https_request')
+    def _test():
+        connection = httplib.HTTPSConnection('localhost', server.port)
+        try:
+            connection.request('GET', '/')
+        except Exception:
+            pass
+        connection.close()
+    
+    _test()
+
+
+@pytest.mark.parametrize('distributed_tracing,span_events', (
+    (True, True),
+    (True, False),
+    (False, False),
+))
+def test_http_cross_process_request(distributed_tracing, span_events, server):
+
+    @background_task(name='test_http:test_http_cross_process_request')
+    @cache_outgoing_headers
+    @validate_cross_process_headers
+    def _test():
+        connection = httplib.HTTPConnection('localhost', server.port)
+        connection.request('GET', '/')
+        response = connection.getresponse()
+        response.read()
+        connection.close()
+
+    _test = override_application_settings({
+        'distributed_tracing.enabled': distributed_tracing,
+        'span_events.enabled': span_events,
+    })(_test)
+
+    _test()
+
+
+def test_http_cross_process_response(server):
+    _test_http_cross_process_response_scoped_metrics = [
+            ('ExternalTransaction/localhost:%s/1#2/test' % server.port, 1)]
+
+    _test_http_cross_process_response_rollup_metrics = [
+            ('External/all', 1),
+            ('External/allOther', 1),
+            ('External/localhost:%s/all' % server.port, 1),
+            ('ExternalApp/localhost:%s/1#2/all' % server.port, 1),
+            ('ExternalTransaction/localhost:%s/1#2/test' % server.port, 1)]
+
+    _test_http_cross_process_response_external_node_params = [
+            ('cross_process_id', '1#2'),
+            ('external_txn_name', 'test'),
+            ('transaction_guid', '0123456789012345')]
+
+
+    @validate_transaction_metrics(
+            'test_http:test_http_cross_process_response',
+            scoped_metrics=_test_http_cross_process_response_scoped_metrics,
+            rollup_metrics=_test_http_cross_process_response_rollup_metrics,
+            background_task=True)
+    @insert_incoming_headers
+    @validate_external_node_params(
+            params=_test_http_cross_process_response_external_node_params)
+    @background_task(name='test_http:test_http_cross_process_response')
+    def _test():
+        connection = httplib.HTTPConnection('localhost', server.port)
+        connection.request('GET', '/')
+        response = connection.getresponse()
+        response.read()
+        connection.close()
+
+    _test()

c23304d2648a4b591009ae859b43e2749d92595f
Tim Pansino <timpansino@gmail.com>
2020-12-16 16:00:41 -0800

    Fix missing imports

--- tests/testing_support/external_fixtures.py
+++ tests/testing_support/external_fixtures.py
@@ -14,6 +14,7 @@
 
 from newrelic.api.external_trace import ExternalTrace
 from newrelic.common.object_wrapper import transient_function_wrapper
+from newrelic.common.encoding_utils import json_encode, obfuscate
 
 
 def create_incoming_headers(transaction):

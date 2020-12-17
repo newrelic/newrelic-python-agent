40b3c85ce9930670361fd6e1196ece415740b076
Tim Pansino <timpansino@gmail.com>
2020-12-17 13:05:48 -0800

    Importing grpc tests

---
+++ tests/framework_grpc/_test_common.py
@@ -0,0 +1,69 @@
+import grpc
+import threading
+import functools
+from newrelic.api.application import application_instance
+
+
+def create_stub(port):
+    return create_stub_and_channel(port)[0]
+
+
+def create_stub_and_channel(port):
+    from sample_application.sample_application_pb2_grpc import (
+            SampleApplicationStub)
+    channel = grpc.insecure_channel('localhost:%s' % port)
+    stub = SampleApplicationStub(channel)
+    return stub, channel
+
+
+def create_request(streaming_request, count=1, timesout=False):
+    from sample_application.sample_application_pb2 import Message
+
+    def _message_stream():
+        for i in range(count):
+            yield Message(text='Hello World', count=count, timesout=timesout)
+
+    if streaming_request:
+        request = _message_stream()
+    else:
+        request = Message(text='Hello World', count=count, timesout=timesout)
+
+    return request
+
+
+def get_result(method, request, *args, **kwargs):
+    try:
+        from grpc._channel import _InactiveRpcError as Error
+    except ImportError:
+        from grpc._channel import _Rendezvous as Error
+    result = None
+    try:
+        result = method(request, *args, **kwargs)
+        list(result)
+    except Error as e:
+        result = e
+    except Exception:
+        pass
+    return result
+
+
+def wait_for_transaction_completion(fn):
+    CALLED = threading.Event()
+    application = application_instance()
+    record_transaction = application.record_transaction
+
+    def record_transaction_wrapper(*args, **kwargs):
+        record_transaction(*args, **kwargs)
+        CALLED.set()
+
+    @functools.wraps(fn)
+    def _waiter(*args, **kwargs):
+        application.record_transaction = record_transaction_wrapper
+        try:
+            result = fn(*args, **kwargs)
+            CALLED.wait(timeout=1)
+            return result
+        finally:
+            application.record_transaction = record_transaction
+
+    return _waiter
---
+++ tests/framework_grpc/conftest.py
@@ -0,0 +1,71 @@
+import gc
+import pytest
+import random
+
+from testing_support.fixtures import (code_coverage_fixture,
+        collector_agent_registration_fixture, collector_available_fixture)
+from testing_support.mock_external_grpc_server import MockExternalgRPCServer
+
+_coverage_source = [
+    'newrelic.hooks.framework_grpc',
+]
+
+code_coverage = code_coverage_fixture(source=_coverage_source)
+
+_default_settings = {
+    'transaction_tracer.explain_threshold': 0.0,
+    'transaction_tracer.transaction_threshold': 0.0,
+    'transaction_tracer.stack_trace_threshold': 0.0,
+    'debug.log_data_collector_payloads': True,
+    'debug.record_transaction_failure': True,
+}
+
+collector_agent_registration = collector_agent_registration_fixture(
+        app_name='Python Agent Test (framework_grpc)',
+        default_settings=_default_settings)
+
+
+@pytest.fixture(scope='module')
+def grpc_app_server():
+    port = random.randint(50000, 50100)
+    with MockExternalgRPCServer(port=port) as server:
+        yield server, port
+
+
+@pytest.fixture(scope='module')
+def mock_grpc_server(grpc_app_server):
+    from sample_application.sample_application_pb2_grpc import (
+            add_SampleApplicationServicer_to_server)
+    from sample_application import SampleApplicationServicer
+    server, port = grpc_app_server
+    add_SampleApplicationServicer_to_server(
+            SampleApplicationServicer(), server)
+    return port
+
+
+@pytest.fixture(scope='session')
+def session_initialization(code_coverage, collector_agent_registration):
+    pass
+
+
+@pytest.fixture(scope='function')
+def requires_data_collector(collector_available_fixture):
+    pass
+
+
+@pytest.fixture(scope='function')
+def gc_garbage_empty():
+    yield
+
+    # garbage collect until everything is reachable
+    while gc.collect():
+        pass
+
+    from grpc._channel import _Rendezvous
+    rendezvous_stored = sum(1 for o in gc.get_objects()
+            if hasattr(o, '__class__') and isinstance(o, _Rendezvous))
+
+    assert rendezvous_stored == 0
+
+    # make sure that even python knows there isn't any garbage remaining
+    assert not gc.garbage

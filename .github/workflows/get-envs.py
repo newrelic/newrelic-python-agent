3cce51da5589552ecc25423000e6a29598df033a
Uma Annamalai <uannamalai@newrelic.com>
2021-01-04 17:02:07 -0800

    Consolidate test suites into single tox.ini file

---
+++ .github/workflows/get-envs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3.8
+import fileinput
+import os
+
+GROUP_NUMBER = int(os.environ["GROUP_NUMBER"])
+TOTAL_GROUPS = int(os.environ["TOTAL_GROUPS"])
+
+
+def main(f):
+    environments = [e.rstrip() for e in f]
+    total_environments = len(environments)
+    envs_per_group = total_environments // TOTAL_GROUPS
+    start = GROUP_NUMBER * envs_per_group
+    end = start + envs_per_group
+    print(",".join(environments[start:end]))
+
+
+if __name__ == "__main__":
+    with fileinput.input() as f:
+        main(f)

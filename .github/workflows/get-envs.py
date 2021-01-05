ff9e701ec5a0d6b6a354188ae496302ffa1db19b
Tim Pansino <timpansino@gmail.com>
2021-01-05 11:07:45 -0800

    Tweak get-envs to use slicing for grouping

--- .github/workflows/get-envs.py
+++ .github/workflows/get-envs.py
@@ -8,11 +8,8 @@ TOTAL_GROUPS = int(os.environ["TOTAL_GROUPS"])
 
 def main(f):
     environments = [e.rstrip() for e in f]
-    total_environments = len(environments)
-    envs_per_group = total_environments // TOTAL_GROUPS
-    start = GROUP_NUMBER * envs_per_group
-    end = start + envs_per_group
-    print(",".join(environments[start:end]))
+    filtered_envs = environments[GROUP_NUMBER::TOTAL_GROUPS]
+    print(",".join(filtered_envs))
 
 
 if __name__ == "__main__":

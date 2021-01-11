010d680236264d19b5c79500c5223ad18a0f7c9d
Allan Feldman <afeldman@newrelic.com>
2021-01-11 16:56:55 -0500

    Continue porting tests.

--- tests/framework_django/test_asgi_application.py
+++ tests/framework_django/test_asgi_application.py
@@ -2,7 +2,9 @@ import os
 import pytest
 import django
 
-from testing_support.fixtures import validate_transaction_metrics
+from testing_support.fixtures import (validate_transaction_metrics,
+    validate_transaction_errors, override_application_settings,
+    override_generic_settings, override_ignore_status_codes)
 
 DJANGO_VERSION = tuple(map(int, django.get_version().split('.')[:2]))
 
@@ -12,74 +14,16 @@ if DJANGO_VERSION[0] < 3:
 from testing_support.asgi_testing import AsgiTest
 
 
-# The middleware scoped metrics are dependent on the MIDDLEWARE_CLASSES or
-# MIDDLEWARE defined in the version-specific Django settings.py file.
-
-_test_django_pre_1_10_middleware_scoped_metrics = [
-    (('Function/django.middleware.common:'
-            'CommonMiddleware.process_request'), 1),
-    (('Function/django.contrib.sessions.middleware:'
-            'SessionMiddleware.process_request'), 1),
-    (('Function/django.contrib.auth.middleware:'
-            'AuthenticationMiddleware.process_request'), 1),
-    (('Function/django.contrib.messages.middleware:'
-            'MessageMiddleware.process_request'), 1),
-    (('Function/django.middleware.csrf:'
-            'CsrfViewMiddleware.process_view'), 1),
-    (('Function/django.contrib.messages.middleware:'
-            'MessageMiddleware.process_response'), 1),
-    (('Function/django.middleware.csrf:'
-            'CsrfViewMiddleware.process_response'), 1),
-    (('Function/django.contrib.sessions.middleware:'
-            'SessionMiddleware.process_response'), 1),
-    (('Function/django.middleware.common:'
-            'CommonMiddleware.process_response'), 1),
-    (('Function/django.middleware.gzip:'
-            'GZipMiddleware.process_response'), 1),
-    (('Function/newrelic.hooks.framework_django:'
-            'browser_timing_insertion'), 1),
-]
-
-_test_django_post_1_10_middleware_scoped_metrics = [
-    ('Function/django.middleware.security:SecurityMiddleware', 1),
+scoped_metrics = [
     ('Function/django.contrib.sessions.middleware:SessionMiddleware', 1),
     ('Function/django.middleware.common:CommonMiddleware', 1),
     ('Function/django.middleware.csrf:CsrfViewMiddleware', 1),
     ('Function/django.contrib.auth.middleware:AuthenticationMiddleware', 1),
     ('Function/django.contrib.messages.middleware:MessageMiddleware', 1),
-    ('Function/django.middleware.clickjacking:XFrameOptionsMiddleware', 1),
     ('Function/django.middleware.gzip:GZipMiddleware', 1),
-]
-
-_test_django_pre_1_10_url_resolver_scoped_metrics = [
-    ('Function/django.core.urlresolvers:RegexURLResolver.resolve', 'present'),
-]
-
-_test_django_post_1_10_url_resolver_scoped_metrics = [
-    ('Function/django.urls.resolvers:RegexURLResolver.resolve', 'present'),
-]
-
-_test_django_post_2_0_url_resolver_scoped_metrics = [
+    ('Function/middleware:ExceptionTo410Middleware', 1),
     ('Function/django.urls.resolvers:URLResolver.resolve', 'present'),
 ]
-
-_test_application_index_scoped_metrics = [
-    ('Function/views:index', 1),
-]
-
-if DJANGO_VERSION < (1, 10):
-    _test_application_index_scoped_metrics.extend(
-        _test_django_pre_1_10_url_resolver_scoped_metrics)
-elif DJANGO_VERSION >= (2, 0):
-    _test_application_index_scoped_metrics.extend(
-        _test_django_post_2_0_url_resolver_scoped_metrics)
-else:
-    _test_application_index_scoped_metrics.extend(
-        _test_django_post_1_10_url_resolver_scoped_metrics)
-
-if DJANGO_VERSION < (1, 10):
-    _test_application_index_scoped_metrics.extend(
-        _test_django_pre_1_10_middleware_scoped_metrics)
 
 
 @pytest.fixture(scope="module")
@@ -91,7 +35,65 @@ def application():
 
 
 @validate_transaction_metrics('views:index',
-        scoped_metrics=_test_application_index_scoped_metrics)
+        scoped_metrics=[('Function/views:index', 1)] + scoped_metrics)
 def test_asgi_index(application):
     response = application.get('/')
     assert response.status == 200
+
+
+@validate_transaction_metrics('views:exception',
+        scoped_metrics=[('Function/views:exception', 1)] + scoped_metrics)
+def test_asgi_exception(application):
+    response = application.get('/exception')
+    assert response.status == 500
+
+
+@override_ignore_status_codes([410])
+@validate_transaction_errors(errors=[])
+@validate_transaction_metrics('views:middleware_410',
+        scoped_metrics=[('Function/views:middleware_410', 1)] + scoped_metrics)
+def test_asgi_middleware_ignore_status_codes(application):
+    response = application.get('/middleware_410')
+    assert response.status == 410
+
+
+@override_ignore_status_codes([403])
+@validate_transaction_errors(errors=[])
+@validate_transaction_metrics('views:permission_denied',
+        scoped_metrics=[('Function/views:permission_denied', 1)] + scoped_metrics)
+def test_asgi_ignored_status_code(application):
+    response = application.get('/permission_denied')
+    assert response.status == 403
+
+
+@pytest.mark.parametrize('url,view_name', (
+    ('/cbv', 'views:MyView.get'),
+    ('/deferred_cbv', 'views:deferred_cbv'),
+))
+def test_asgi_class_based_view(application, url, view_name):
+
+    @validate_transaction_errors(errors=[])
+    @validate_transaction_metrics(view_name,
+            scoped_metrics=[('Function/' + view_name, 1)] + scoped_metrics)
+    def _test():
+        response = application.get(url)
+        assert response.status == 200
+
+    _test()
+
+
+@pytest.mark.parametrize('url', (
+    '/html_insertion',
+    '/html_insertion_content_length',
+))
+@override_application_settings({
+    'browser_monitoring.enabled': True,
+    'browser_monitoring.auto_instrument': True,
+    'js_agent_loader': u'<!-- NREUM HEADER -->',
+})
+def test_asgi_html_insertion_success(application, url):
+    response = application.get(url)
+    assert response.status == 200
+
+    assert b'NREUM HEADER' in response.body
+    assert b'NREUM.info' in response.body

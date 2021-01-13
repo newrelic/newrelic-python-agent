e6538170b054b049d75c2f3919d0775f284bfb98
Tim Pansino <timpansino@gmail.com>
2021-01-13 10:10:52 -0800

    Add tests for httpx DT headers. #101

--- tests/external_httpx/test_client.py
+++ tests/external_httpx/test_client.py
@@ -13,16 +13,18 @@
 # limitations under the License.
 
 import asyncio
+from newrelic.api.transaction import current_transaction
 import pytest
 from newrelic.api.background_task import background_task
-from testing_support.fixtures import validate_transaction_metrics
+from testing_support.fixtures import validate_transaction_metrics, validate_transaction_errors, override_application_settings
+from testing_support.validators.validate_cross_process_headers import validate_cross_process_headers
 
 
 SCOPED_METRICS = []
 ROLLUP_METRICS = [("External/all", 2), ("External/allOther", 2)]
 
 
-@pytest.fixture(autouse=True)
+@pytest.fixture()
 def populate_metrics(server, request):
     SCOPED_METRICS[:] = []
     method = request.getfixturevalue("method").upper()
@@ -47,7 +49,7 @@ def populate_metrics(server, request):
     background_task=True,
 )
 @background_task(name='test_sync_client')
-def test_sync_client(httpx, server, method):
+def test_sync_client(httpx, server, method, populate_metrics):
     with httpx.Client() as client:
         resolved_method = getattr(client, method)
         resolved_method("http://localhost:%s" % server.port)
@@ -74,7 +76,7 @@ def test_sync_client(httpx, server, method):
     background_task=True,
 )
 @background_task(name='test_async_client')
-def test_async_client(httpx, server, loop, method):
+def test_async_client(httpx, server, loop, method, populate_metrics):
     async def test_async_client():
         async with httpx.AsyncClient() as client:
             resolved_method = getattr(client, method)
@@ -87,3 +89,57 @@ def test_async_client(httpx, server, loop, method):
 
     responses = loop.run_until_complete(test_async_client())
     assert all(response.status_code == 200 for response in responses)
+
+
+@pytest.mark.parametrize('distributed_tracing,span_events', (
+    (True, True),
+    (True, False),
+    (False, False),
+))
+def test_sync_cross_process_request(httpx, server, distributed_tracing, span_events):
+
+    @override_application_settings({
+        'distributed_tracing.enabled': distributed_tracing,
+        'span_events.enabled': span_events,
+    })
+    @validate_transaction_errors(errors=[])
+    @background_task()
+    @validate_cross_process_headers
+    def _test():  
+        transaction = current_transaction()
+
+        with httpx.Client() as client:
+            response = client.get("http://localhost:%s" % server.port)
+        
+        transaction._test_request_headers = response.request.headers
+
+        assert response.status_code == 200
+
+    _test()
+
+
+@pytest.mark.parametrize('distributed_tracing,span_events', (
+    (True, True),
+    (True, False),
+    (False, False),
+))
+@validate_transaction_errors(errors=[])
+@background_task()
+@validate_cross_process_headers
+def test_async_cross_process_request(httpx, server, loop, distributed_tracing, span_events):
+
+    @override_application_settings({
+        'distributed_tracing.enabled': distributed_tracing,
+        'span_events.enabled': span_events,
+    })
+    async def _test():
+        async with httpx.AsyncClient() as client:
+            response = await client.get("http://localhost:%s" % server.port)
+
+        return response
+        
+    transaction = current_transaction()
+    response = loop.run_until_complete(_test())
+    transaction._test_request_headers = response.request.headers
+
+    assert response.status_code == 200

1dfe3eb99cbc696ce4c0e8310d4b33266209ba95
Uma Annamalai <uannamalai@newrelic.com>
2021-01-29 10:50:00 -0800

    Add garbage collector object count by type metrics. #137 (#138)
    * Add object count by type metrics. #137
    
    * Update object count metric format. #137

--- newrelic/samplers/gc_data.py
+++ newrelic/samplers/gc_data.py
@@ -15,8 +15,10 @@
 import gc
 import os
 import time
+from collections import Counter
 from newrelic.samplers.decorators import data_source_factory
 from newrelic.core.stats_engine import CustomMetrics
+from newrelic.common.object_names import callable_name
 
 
 
@@ -63,11 +65,19 @@ class _GCDataSource(object):
 
 
     def __call__(self):
+        # Record object count in total and per generation
         if hasattr(gc, "get_count"):
             counts = gc.get_count()
             yield ("GC/objects/%d/all" % self.pid, {"count": sum(counts)})
             for gen, count in enumerate(counts):
-                yield ("GC/objects/%d/%d" % (self.pid, gen), {"count": count})
+                yield ("GC/objects/%d/generation/%d" % (self.pid, gen), {"count": count})
+
+        # Record object count for top five types with highest count
+        if hasattr(gc, "get_objects"):
+            object_types = map(type, gc.get_objects())
+            highest_types = Counter(object_types).most_common(5)
+            for obj_type, count in highest_types:
+                yield ("GC/objects/%d/type/%s" % (self.pid, callable_name(obj_type)), {"count": count})
 
         if hasattr(gc, "get_stats"):
             stats_by_gen = gc.get_stats()

d64e0f5edf9f2ff721f3f0e1eea8126a92d0696d
Tim Pansino <timpansino@gmail.com>
2021-04-23 15:56:41 -0700

    Correct tests to follow new logic in complete_root

--- tests/agent_features/test_background_task.py
+++ tests/agent_features/test_background_task.py
@@ -29,26 +29,19 @@ def test_nested_context_managers():
 def test_sentinel_exited_complete_root_exception():
     """
     This test forces a transaction to exit while it still has an active trace
-    this causes an exception to be raised in TraceCache complete_root(). It
-    verifies that the sentinel.exited property is set to true if an exception
+    It verifies that the sentinel.exited property is set to true if an exception
     is raised in complete_root()
     """
-    expected_error = "not the current trace"
 
-    try:
-        txn = None
-        sentinel = None
-        txn = BackgroundTask(application_instance(), "Parent")
-        txn.__enter__()
-        sentinel = txn.root_span
-        trace = FunctionTrace("trace")
-        trace.__enter__()
-        txn.__exit__(None, None, None)
-        assert False, "Did not raise exception"
-    except RuntimeError as e:
-        assert str(e) == expected_error
-    finally:
-        assert sentinel.exited
-        # Make sure to exit properly so cleanup is performed
-        trace.__exit__(None, None, None)
-        txn.__exit__(None, None, None)
+    txn = None
+    sentinel = None
+    txn = BackgroundTask(application_instance(), "Parent")
+    txn.__enter__()
+    sentinel = txn.root_span
+    trace = FunctionTrace("trace")
+    trace.__enter__()
+    txn.__exit__(None, None, None)
+    assert sentinel.exited
+    # Make sure to exit properly so cleanup is performed
+    trace.__exit__(None, None, None)
+    txn.__exit__(None, None, None)

05f00e98ef727e9a1c4af97efd0121e88d94a1ca
Uma <uannamalai@newrelic.com>
2021-05-11 22:08:10 -0700

    Update error trace with status code and expected error.

--- newrelic/api/error_trace.py
+++ newrelic/api/error_trace.py
@@ -14,24 +14,25 @@
 
 import functools
 
-from newrelic.api.time_trace import current_trace, record_exception
+from newrelic.api.time_trace import current_trace, notice_error
 from newrelic.common.object_wrapper import FunctionWrapper, wrap_object
 
 
 class ErrorTrace(object):
-
-    def __init__(self, ignore_errors=[], **kwargs):
+    def __init__(self, ignore=None, expected=None, status_code=None, **kwargs):
         parent = None
         if kwargs:
             if len(kwargs) > 1:
                 raise TypeError("Invalid keyword arguments:", kwargs)
-            parent = kwargs['parent']
+            parent = kwargs["parent"]
 
         if parent is None:
             parent = current_trace()
 
         self._transaction = parent and parent.transaction
-        self._ignore_errors = ignore_errors
+        self._ignore_errors = ignore
+        self._expected_errors = expected
+        self._status_code = status_code
 
     def __enter__(self):
         return self
@@ -43,27 +44,32 @@ class ErrorTrace(object):
         if self._transaction is None:
             return
 
-        record_exception(exc=exc, value=value, tb=tb,
-                ignore_errors=self._ignore_errors)
+        notice_error(
+            error=(exc, value, tb),
+            ignore=self._ignore_errors,
+            expected=self._expected_errors,
+            status_code=self._status_code,
+        )
 
 
-def ErrorTraceWrapper(wrapped, ignore_errors=[]):
-
+def ErrorTraceWrapper(wrapped, ignore=None, expected=None, status_code=None):
     def wrapper(wrapped, instance, args, kwargs):
         parent = current_trace()
 
         if parent is None:
             return wrapped(*args, **kwargs)
 
-        with ErrorTrace(ignore_errors, parent=parent):
+        with ErrorTrace(ignore, expected, status_code, parent=parent):
             return wrapped(*args, **kwargs)
 
     return FunctionWrapper(wrapped, wrapper)
 
 
-def error_trace(ignore_errors=[]):
-    return functools.partial(ErrorTraceWrapper, ignore_errors=ignore_errors)
+def error_trace(ignore=None, expected=None, status_code=None):
+    return functools.partial(
+        ErrorTraceWrapper, ignore=ignore, expected=expected, status_code=status_code
+    )
 
 
-def wrap_error_trace(module, object_path, ignore_errors=[]):
-    wrap_object(module, object_path, ErrorTraceWrapper, (ignore_errors, ))
+def wrap_error_trace(module, object_path, ignore=None, expected=None, status_code=None):
+    wrap_object(module, object_path, ErrorTraceWrapper, (ignore, expected, status_code, ))

1b7dee7be3ccf09a8ecd6d1fc1c18a9e1190c2d3
Tim Pansino <timpansino@gmail.com>
2021-05-20 12:15:34 -0700

    Add str method to trace cache

--- newrelic/core/trace_cache.py
+++ newrelic/core/trace_cache.py
@@ -100,6 +100,12 @@ class TraceCache(object):
     def __init__(self):
         self._cache = weakref.WeakValueDictionary()
 
+    def __str__(self):
+        return "<newrelic.core.trace_cache.TraceCache object at 0x%x %s>" % (id(self), str(dict(self._cache.items())))
+
+    def __repr__(self):
+        return str(self)
+
     def current_thread_id(self):
         """Returns the thread ID for the caller.
 

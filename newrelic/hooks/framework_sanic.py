d7ad07cfdf84b25dea07759cf3bc77205028d0d9
Allan Feldman <afeldman@newrelic.com>
2021-05-21 18:13:07 -0400

    Fix sanic response header parsing.

--- newrelic/hooks/framework_sanic.py
+++ newrelic/hooks/framework_sanic.py
@@ -158,10 +158,11 @@ def _sanic_app_init(wrapped, instance, args, kwargs):
 
 
 def _nr_sanic_response_parse_headers(wrapped, instance, args, kwargs):
+    result = wrapped(*args, **kwargs)
     transaction = current_transaction()
 
     if transaction is None:
-        return wrapped(*args, **kwargs)
+        return result
 
     # instance is the response object
     cat_headers = transaction.process_response(str(instance.status),
@@ -171,7 +172,7 @@ def _nr_sanic_response_parse_headers(wrapped, instance, args, kwargs):
         if header_name not in instance.headers:
             instance.headers[header_name] = header_value
 
-    return wrapped(*args, **kwargs)
+    return result
 
 
 def _nr_wrapper_middleware_(attach_to):
@@ -235,5 +236,12 @@ def instrument_sanic_app(module):
 
 
 def instrument_sanic_response(module):
-    wrap_function_wrapper(module, 'BaseHTTPResponse._parse_headers',
-        _nr_sanic_response_parse_headers)
+    if hasattr(module.BaseHTTPResponse, 'get_headers'):
+        wrap_function_wrapper(module, 'BaseHTTPResponse.get_headers',
+            _nr_sanic_response_parse_headers)
+    elif hasattr(module.BaseHTTPResponse, '_parse_headers'):
+        wrap_function_wrapper(module, 'BaseHTTPResponse._parse_headers',
+            _nr_sanic_response_parse_headers)
+    else:
+        wrap_function_wrapper(module, 'BaseHTTPResponse.send',
+            _nr_sanic_response_parse_headers)

4a1ea9be94c82ea8d7fbf590f78fd9567e2f6a5e
Uma Annamalai <uannamalai@newrelic.com>
2021-08-31 09:55:12 -0700

    Fix agent_features tests.

--- tests/messagebroker_pika/test_cat.py
+++ tests/messagebroker_pika/test_cat.py
@@ -34,8 +34,6 @@ _override_settings = {
     'encoding_key': ENCODING_KEY,
     'trusted_account_ids': [1],
     'browser_monitoring.enabled': False,
-    'cross_application_tracer.enabled': True,
-    'distributed_tracing.enabled': False,
 }
 
 

18580a806caf68c469d866d0b62c831b03790b22
Timothy Pansino <11214426+TimPansino@users.noreply.github.com>
2021-09-10 11:30:32 -0700

    Ariadne WSGI/ASGI Support (#356)
    * Ariadne wsgi/asgi support
    
    * Format and lint

--- tests/testing_support/validators/validate_span_events.py
+++ tests/testing_support/validators/validate_span_events.py
@@ -12,11 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import six
 import time
 
-from newrelic.common.object_wrapper import (transient_function_wrapper,
-        function_wrapper)
+import six
+
+from newrelic.common.object_wrapper import function_wrapper, transient_function_wrapper
 
 try:
     from newrelic.core.infinite_tracing_pb2 import AttributeValue, Span
@@ -25,10 +25,19 @@ except:
     Span = None
 
 
-def validate_span_events(count=1,
-        exact_intrinsics={}, expected_intrinsics=[], unexpected_intrinsics=[],
-        exact_agents={}, expected_agents=[], unexpected_agents=[],
-        exact_users={}, expected_users=[], unexpected_users=[]):
+def validate_span_events(
+    count=1,
+    exact_intrinsics={},
+    expected_intrinsics=[],
+    unexpected_intrinsics=[],
+    exact_agents={},
+    expected_agents=[],
+    unexpected_agents=[],
+    exact_users={},
+    expected_users=[],
+    unexpected_users=[],
+    index=-1,
+):
 
     # Used for validating a single span event.
     #
@@ -44,14 +53,11 @@ def validate_span_events(count=1,
         record_transaction_called = []
         recorded_span_events = []
 
-        @transient_function_wrapper('newrelic.core.stats_engine',
-                'StatsEngine.record_transaction')
+        @transient_function_wrapper("newrelic.core.stats_engine", "StatsEngine.record_transaction")
         def capture_span_events(wrapped, instance, args, kwargs):
             events = []
 
-            @transient_function_wrapper(
-                    'newrelic.common.streaming_utils',
-                    'StreamBuffer.put')
+            @transient_function_wrapper("newrelic.common.streaming_utils", "StreamBuffer.put")
             def stream_capture(wrapped, instance, args, kwargs):
                 event = args[0]
                 events.append(event)
@@ -64,8 +70,7 @@ def validate_span_events(count=1,
                 raise
             else:
                 if not instance.settings.infinite_tracing.enabled:
-                    events = [event for priority, seen_at, event
-                                    in instance.span_events.pq]
+                    events = [event for priority, seen_at, event in instance.span_events.pq]
 
                 recorded_span_events.append(events)
 
@@ -74,7 +79,7 @@ def validate_span_events(count=1,
         _new_wrapper = capture_span_events(wrapped)
         val = _new_wrapper(*args, **kwargs)
         assert record_transaction_called
-        captured_events = recorded_span_events.pop()
+        captured_events = recorded_span_events.pop(index)
 
         mismatches = []
         matching_span_events = 0
@@ -88,25 +93,23 @@ def validate_span_events(count=1,
 
             _check_span_intrinsics(intrinsics)
 
-            intrinsics_ok = _check_span_attributes(intrinsics,
-                    exact_intrinsics, expected_intrinsics,
-                    unexpected_intrinsics, mismatches)
-            agent_attr_ok = _check_span_attributes(agent_attrs,
-                    exact_agents, expected_agents,
-                    unexpected_agents, mismatches)
-            user_attr_ok = _check_span_attributes(user_attrs,
-                    exact_users, expected_users,
-                    unexpected_users, mismatches)
+            intrinsics_ok = _check_span_attributes(
+                intrinsics, exact_intrinsics, expected_intrinsics, unexpected_intrinsics, mismatches
+            )
+            agent_attr_ok = _check_span_attributes(
+                agent_attrs, exact_agents, expected_agents, unexpected_agents, mismatches
+            )
+            user_attr_ok = _check_span_attributes(user_attrs, exact_users, expected_users, unexpected_users, mismatches)
 
             if intrinsics_ok and agent_attr_ok and user_attr_ok:
                 matching_span_events += 1
 
         def _span_details():
             details = [
-                'matching_span_events=%d' % matching_span_events,
-                'count=%d' % count,
-                'mismatches=%s' % mismatches,
-                'captured_events=%s' % captured_events
+                "matching_span_events=%d" % matching_span_events,
+                "count=%d" % count,
+                "mismatches=%s" % mismatches,
+                "captured_events=%s" % captured_events,
             ]
 
             return "\n".join(details)
@@ -132,11 +135,11 @@ def check_value_equals(dictionary, key, expected_value):
 def assert_isinstance(value, expected_type):
     if AttributeValue and isinstance(value, AttributeValue):
         if expected_type is six.string_types:
-            assert value.HasField('string_value')
+            assert value.HasField("string_value")
         elif expected_type is float:
-            assert value.HasField('double_value')
+            assert value.HasField("double_value")
         elif expected_type is int:
-            assert value.HasField('int_value')
+            assert value.HasField("int_value")
         else:
             assert False
     else:
@@ -154,8 +157,7 @@ def _check_span_attributes(attrs, exact, expected, unexpected, mismatches):
         else:
             for key, value in exact.items():
                 if not check_value_equals(attrs, key, value):
-                    mismatches.append('key: %s, value:<%s><%s>' % (
-                        key, value, attrs.get(key)))
+                    mismatches.append("key: %s, value:<%s><%s>" % (key, value, attrs.get(key)))
                     break
             else:
                 return True
@@ -164,21 +166,21 @@ def _check_span_attributes(attrs, exact, expected, unexpected, mismatches):
 
 
 def _check_span_intrinsics(intrinsics):
-    assert check_value_equals(intrinsics, 'type', 'Span')
-    assert_isinstance(intrinsics['traceId'], six.string_types)
-    assert_isinstance(intrinsics['guid'], six.string_types)
-    if 'parentId' in intrinsics:
-        assert_isinstance(intrinsics['parentId'], six.string_types)
-    assert_isinstance(intrinsics['transactionId'], six.string_types)
-    intrinsics['sampled'] is True
-    assert_isinstance(intrinsics['priority'], float)
-    assert_isinstance(intrinsics['timestamp'], int)
-    ts = intrinsics['timestamp']
+    assert check_value_equals(intrinsics, "type", "Span")
+    assert_isinstance(intrinsics["traceId"], six.string_types)
+    assert_isinstance(intrinsics["guid"], six.string_types)
+    if "parentId" in intrinsics:
+        assert_isinstance(intrinsics["parentId"], six.string_types)
+    assert_isinstance(intrinsics["transactionId"], six.string_types)
+    intrinsics["sampled"] is True
+    assert_isinstance(intrinsics["priority"], float)
+    assert_isinstance(intrinsics["timestamp"], int)
+    ts = intrinsics["timestamp"]
     if AttributeValue and isinstance(ts, AttributeValue):
         ts = ts.double_value
     assert ts <= int(time.time() * 1000)
-    assert_isinstance(intrinsics['duration'], float)
-    assert_isinstance(intrinsics['name'], six.string_types)
-    assert_isinstance(intrinsics['category'], six.string_types)
-    if 'nr.entryPoint' in intrinsics:
-        assert check_value_equals(intrinsics, 'nr.entryPoint', True)
+    assert_isinstance(intrinsics["duration"], float)
+    assert_isinstance(intrinsics["name"], six.string_types)
+    assert_isinstance(intrinsics["category"], six.string_types)
+    if "nr.entryPoint" in intrinsics:
+        assert check_value_equals(intrinsics, "nr.entryPoint", True)

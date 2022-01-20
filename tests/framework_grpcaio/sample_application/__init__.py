e96cfb89c8a3bb0ebf66f7cc47b5f9570b41e792
Tim Pansino <timpansino@gmail.com>
2022-01-19 16:18:48 -0800

    Fix issues in grpcaio sample app

--- tests/framework_grpcaio/sample_application/__init__.py
+++ tests/framework_grpcaio/sample_application/__init__.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 import json
-import time
+import asyncio
 from newrelic.api.transaction import current_transaction
 import grpc
 
@@ -33,15 +33,15 @@ class SampleApplicationServicer(_SampleApplicationServicer):
     def DoUnaryUnary(self, request, context):
         context.set_trailing_metadata([('content-type', 'text/plain')])
         if request.timesout:
-            while context.is_active():
-                time.sleep(0.1)
+            while context.time_remaining():
+                asyncio.sleep(0.1)
         return Message(text='unary_unary: %s' % request.text)
 
     def DoUnaryStream(self, request, context):
         context.set_trailing_metadata([('content-type', 'text/plain')])
         if request.timesout:
-            while context.is_active():
-                time.sleep(0.1)
+            while context.time_remaining():
+                asyncio.sleep(0.1)
         for i in range(request.count):
             yield Message(text='unary_stream: %s' % request.text)
 
@@ -49,16 +49,16 @@ class SampleApplicationServicer(_SampleApplicationServicer):
         context.set_trailing_metadata([('content-type', 'text/plain')])
         for request in request_iter:
             if request.timesout:
-                while context.is_active():
-                    time.sleep(0.1)
+                while context.time_remaining():
+                    asyncio.sleep(0.1)
             return Message(text='stream_unary: %s' % request.text)
 
     def DoStreamStream(self, request_iter, context):
         context.set_trailing_metadata([('content-type', 'text/plain')])
         for request in request_iter:
             if request.timesout:
-                while context.is_active():
-                    time.sleep(0.1)
+                while context.time_remaining():
+                    asyncio.sleep(0.1)
             yield Message(text='stream_stream: %s' % request.text)
 
     def DoUnaryUnaryRaises(self, request, context):
@@ -76,37 +76,51 @@ class SampleApplicationServicer(_SampleApplicationServicer):
             raise AssertionError('stream_stream: %s' % request.text)
 
     def NoTxnUnaryUnaryRaises(self, request, context):
-        current_transaction().ignore_transaction = True
+        txn = current_transaction()
+        if txn: txn.ignore_transaction = True
         raise AssertionError('unary_unary: %s' % request.text)
 
     def NoTxnUnaryStreamRaises(self, request, context):
-        current_transaction().ignore_transaction = True
+        txn = current_transaction()
+        if txn: txn.ignore_transaction = True
         raise AssertionError('unary_stream: %s' % request.text)
 
     def NoTxnStreamUnaryRaises(self, request_iter, context):
-        current_transaction().ignore_transaction = True
+        txn = current_transaction()
+        if txn: txn.ignore_transaction = True
+        
         for request in request_iter:
             raise AssertionError('stream_unary: %s' % request.text)
 
-    def NoTxnStreamStreamRaises(self, request_iter, context):
-        current_transaction().ignore_transaction = True
-        for request in request_iter:
-            raise AssertionError('stream_stream: %s' % request.text)
+    async def NoTxnStreamStreamRaises(self, request_iter, context):
+        txn = current_transaction()
+        if txn: txn.ignore_transaction = True
+
+        try:
+            async for request in request_iter:
+                raise AssertionError('stream_stream: %s' % request.text)
+        except TypeError as e:
+            for request in request_iter:
+                raise AssertionError('stream_stream: %s' % request.text)
 
     def NoTxnUnaryUnary(self, request, context):
-        current_transaction().ignore_transaction = True
+        txn = current_transaction()
+        if txn: txn.ignore_transaction = True
         return self.DoUnaryUnary(request, context)
 
     def NoTxnUnaryStream(self, request, context):
-        current_transaction().ignore_transaction = True
+        txn = current_transaction()
+        if txn: txn.ignore_transaction = True
         return self.DoUnaryStream(request, context)
 
     def NoTxnStreamUnary(self, request_iter, context):
-        current_transaction().ignore_transaction = True
+        txn = current_transaction()
+        if txn: txn.ignore_transaction = True
         return self.DoStreamUnary(request_iter, context)
 
     def NoTxnStreamStream(self, request_iter, context):
-        current_transaction().ignore_transaction = True
+        txn = current_transaction()
+        if txn: txn.ignore_transaction = True
         return self.DoStreamStream(request_iter, context)
 
     def DoUnaryUnaryAbort(self, request, context):
@@ -138,19 +152,23 @@ class SampleApplicationServicer(_SampleApplicationServicer):
         return Message(text=json.dumps(d))
 
     def DtNoTxnUnaryUnary(self, request, context):
-        current_transaction().ignore_transaction = True
+        txn = current_transaction()
+        if txn: txn.ignore_transaction = True
         return self.extract_dt_value(context.invocation_metadata())
 
     def DtNoTxnUnaryStream(self, request, context):
-        current_transaction().ignore_transaction = True
+        txn = current_transaction()
+        if txn: txn.ignore_transaction = True
         yield self.extract_dt_value(context.invocation_metadata())
 
     def DtNoTxnStreamUnary(self, request_iter, context):
-        current_transaction().ignore_transaction = True
+        txn = current_transaction()
+        if txn: txn.ignore_transaction = True
         list(request_iter)  # consume iterator
         return self.extract_dt_value(context.invocation_metadata())
 
     def DtNoTxnStreamStream(self, request_iter, context):
-        current_transaction().ignore_transaction = True
+        txn = current_transaction()
+        if txn: txn.ignore_transaction = True
         for request in request_iter:
             yield self.extract_dt_value(context.invocation_metadata())

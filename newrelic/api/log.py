b7ea5ec3b2b38ca5b786e196a5708a1ba35dc47c
iamr0b0x <abduladeneye@gmail.com>
2022-03-06 13:07:43 +0100

    Updated gitignore;impl handler for newrelic api

--- newrelic/api/log.py
+++ newrelic/api/log.py
@@ -13,6 +13,10 @@
 # limitations under the License.
 
 import json
+import logging
+import time
+
+from urllib.request import urlopen, Request
 from logging import Formatter, LogRecord
 
 from newrelic.api.time_trace import get_linking_metadata
@@ -78,3 +82,87 @@ class NewRelicContextFormatter(Formatter):
                 return "<unprintable %s object>" % type(object).__name__
 
         return json.dumps(self.log_record_to_dict(record), default=safe_str, separators=(",", ":"))
+
+
+class NewRelicLogHandler(logging.Handler):
+    """
+    Implementation was derived from: https://pypi.org/project/new-relic-logger-for-python/0.2.0/
+    file: newrelic_logger.handlers.py
+    A class which sends records to a New Relic via its API.
+    """
+
+    def __init__(
+            self,
+            level=logging.INFO,
+            app_id: int = 0,
+            app_name: str = None,
+            license_key: str = None,
+            region: str = "US",
+    ):
+        """
+        Initialize the instance with the region and license_key
+        """
+        super(NewRelicLogHandler, self).__init__(level=level)
+        self.app_id = app_id
+        self.app_name = app_name
+        self.host_us = "log-api.newrelic.com"
+        self.host_eu = "log-api.eu.newrelic.com"
+        self.url = "/log/v1"
+        self.region = region.upper()
+        self.license_key = license_key
+        self.setFormatter(NewRelicContextFormatter())
+
+    def prepare(self, record):
+        self.format(record)
+
+        record.msg = record.message
+        record.args = get_linking_metadata()
+        record.exc_info = None
+        return record
+
+    def emit(self, record):
+        """
+        Emit a record.
+        Send the record to the New Relic API
+        """
+        try:
+            record = self.prepare(record)
+            print(f"{record.getMessage()}")
+            data_formatted_dict = json.loads(self.format(record))
+
+            data = {
+                **data_formatted_dict,
+                "appId": self.app_id,
+                "labels": {"app": self.app_name},
+                **record.args,
+            }
+            self.send_log(data=data)
+
+        except Exception:
+            self.handleError(record)
+
+    def send_log(self, data: {}):
+        host = self.host_us if self.region == "US" else self.host_eu
+        req = Request(
+            url="https://" + host + self.url,
+            data=json.dumps(data).encode(),
+            headers={
+                'X-License-Key': self.license_key,
+                'Content-Type': "application/json",
+            },
+            method="POST"
+        )
+        resp = urlopen(req)
+
+        if resp.status // 100 != 2:
+            if resp.status == 429:
+                print(f"New Relic API Response: Retry-After")
+                time.sleep(1)
+                self.send_log(data=data)
+                return
+            print(f"Error sending log to new relic")
+            print(f"Status Code: {resp.status}")
+            print(f"Reason: {resp.reason}")
+            print(f"url: {resp.url}")
+            print(resp.read().decode())
+            print(f"data: {data}")

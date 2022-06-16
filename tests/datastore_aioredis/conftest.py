69a6060eefa2011de33230994f8c4cf2331a1ed9
Tim Pansino <timpansino@gmail.com>
2022-06-16 10:24:47 -0700

    Move fixture import

--- tests/datastore_aioredis/conftest.py
+++ tests/datastore_aioredis/conftest.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from testing_support.fixture.event_loop import event_loop as loop  # noqa: F401
 from testing_support.fixtures import (  # noqa: F401
     code_coverage_fixture,
     collector_agent_registration_fixture,

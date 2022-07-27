28ef55f46d9d5fdfde1b2bc5e78427a055e000c7
Tim Pansino <timpansino@gmail.com>
2022-07-27 10:42:07 -0700

    Fix version gating again

--- tests/datastore_mysql/test_database.py
+++ tests/datastore_mysql/test_database.py
@@ -26,7 +26,7 @@ DB_NAMESPACE = DB_SETTINGS["namespace"]
 DB_PROCEDURE = "hello_" + DB_NAMESPACE
 
 mysql_version = tuple(int(x) for x in mysql.connector.__version__.split(".")[:3])
-if mysql_version >= (8, 0, 40):
+if mysql_version >= (8, 0, 30):
     _connector_metric_name = 'Function/mysql.connector.pooling:connect'
 else:
     _connector_metric_name = 'Function/mysql.connector:connect'

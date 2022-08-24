8b8fdfde5a2618adbacaad3276fa2d27c85cec82
Tim Pansino <timpansino@gmail.com>
2022-08-24 09:56:34 -0700

    Add testing for kafka producer. Co-authored-by: Lalleh Rafeei <lrafeei@users.noreply.github.com>

--- tests/messagebroker_kafkapython/test_kafka_produce.py
+++ tests/messagebroker_kafkapython/test_kafka_produce.py
@@ -13,14 +13,19 @@
 # limitations under the License.
 
 import time
+import kafka
+from newrelic.api.background_task import background_task
+from testing_support.fixtures import validate_transaction_metrics
 
+from conftest import cache_kafka_headers
+from testing_support.validators.validate_messagebroker_headers import validate_messagebroker_headers
 
 def test_no_harm(topic, producer, consumer):
     MESSAGES = [
         {"foo": "bar"},
         {"baz": "bat"},
     ]
-
+    
     for msg in MESSAGES:
         time.sleep(1)
         producer.send(topic, value=msg)
@@ -28,3 +33,24 @@ def test_no_harm(topic, producer, consumer):
 
     for msg in consumer:
         assert msg.topic == topic
+
+
+def test_producer(topic, producer, consumer):
+    scoped_metrics = [
+        ("MessageBroker/Kafka/Topic/Produce/Named/%s" % topic, 3)
+    ]
+    unscoped_metrics = scoped_metrics
+
+
+    @validate_transaction_metrics("test_kafka_produce:test_producer.<locals>.test", scoped_metrics=scoped_metrics, rollup_metrics=unscoped_metrics, background_task=True)
+    @background_task()
+    @cache_kafka_headers
+    @validate_messagebroker_headers
+    def test():
+        messages = [1, 2, 3]
+        for message in messages:
+            producer.send(topic, message)
+
+        producer.flush()
+
+    test()

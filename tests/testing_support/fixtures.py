f3d788f129f9ddef6d67dc4a17f9f51388a4d07f
Lalleh Rafeei <lrafeei@newrelic.com>
2022-08-30 11:38:22 -0700

    Comment out original validate_transaction_metrics from fixtures.py

--- tests/testing_support/fixtures.py
+++ tests/testing_support/fixtures.py
@@ -385,119 +385,119 @@ def make_synthetics_header(account_id, resource_id, job_id, monitor_id, encoding
     return {"X-NewRelic-Synthetics": value}
 
 
-def validate_transaction_metrics(
-    name,
-    group="Function",
-    background_task=False,
-    scoped_metrics=None,
-    rollup_metrics=None,
-    custom_metrics=None,
-    index=-1,
-):
-    scoped_metrics = scoped_metrics or []
-    rollup_metrics = rollup_metrics or []
-    custom_metrics = custom_metrics or []
+# def validate_transaction_metrics(
+#     name,
+#     group="Function",
+#     background_task=False,
+#     scoped_metrics=None,
+#     rollup_metrics=None,
+#     custom_metrics=None,
+#     index=-1,
+# ):
+#     scoped_metrics = scoped_metrics or []
+#     rollup_metrics = rollup_metrics or []
+#     custom_metrics = custom_metrics or []
 
-    if background_task:
-        unscoped_metrics = [
-            "OtherTransaction/all",
-            "OtherTransaction/%s/%s" % (group, name),
-            "OtherTransactionTotalTime",
-            "OtherTransactionTotalTime/%s/%s" % (group, name),
-        ]
-        transaction_scope_name = "OtherTransaction/%s/%s" % (group, name)
-    else:
-        unscoped_metrics = [
-            "WebTransaction",
-            "WebTransaction/%s/%s" % (group, name),
-            "WebTransactionTotalTime",
-            "WebTransactionTotalTime/%s/%s" % (group, name),
-            "HttpDispatcher",
-        ]
-        transaction_scope_name = "WebTransaction/%s/%s" % (group, name)
+#     if background_task:
+#         unscoped_metrics = [
+#             "OtherTransaction/all",
+#             "OtherTransaction/%s/%s" % (group, name),
+#             "OtherTransactionTotalTime",
+#             "OtherTransactionTotalTime/%s/%s" % (group, name),
+#         ]
+#         transaction_scope_name = "OtherTransaction/%s/%s" % (group, name)
+#     else:
+#         unscoped_metrics = [
+#             "WebTransaction",
+#             "WebTransaction/%s/%s" % (group, name),
+#             "WebTransactionTotalTime",
+#             "WebTransactionTotalTime/%s/%s" % (group, name),
+#             "HttpDispatcher",
+#         ]
+#         transaction_scope_name = "WebTransaction/%s/%s" % (group, name)
 
-    @function_wrapper
-    def _validate_wrapper(wrapped, instance, args, kwargs):
+#     @function_wrapper
+#     def _validate_wrapper(wrapped, instance, args, kwargs):
 
-        record_transaction_called = []
-        recorded_metrics = []
+#         record_transaction_called = []
+#         recorded_metrics = []
 
-        @transient_function_wrapper("newrelic.core.stats_engine", "StatsEngine.record_transaction")
-        @catch_background_exceptions
-        def _validate_transaction_metrics(wrapped, instance, args, kwargs):
-            record_transaction_called.append(True)
-            try:
-                result = wrapped(*args, **kwargs)
-            except:
-                raise
-            else:
-                metrics = instance.stats_table
-                # Record a copy of the metric value so that the values aren't
-                # merged in the future
-                _metrics = {}
-                for k, v in metrics.items():
-                    _metrics[k] = copy.copy(v)
-                recorded_metrics.append(_metrics)
+#         @transient_function_wrapper("newrelic.core.stats_engine", "StatsEngine.record_transaction")
+#         @catch_background_exceptions
+#         def _validate_transaction_metrics(wrapped, instance, args, kwargs):
+#             record_transaction_called.append(True)
+#             try:
+#                 result = wrapped(*args, **kwargs)
+#             except:
+#                 raise
+#             else:
+#                 metrics = instance.stats_table
+#                 # Record a copy of the metric value so that the values aren't
+#                 # merged in the future
+#                 _metrics = {}
+#                 for k, v in metrics.items():
+#                     _metrics[k] = copy.copy(v)
+#                 recorded_metrics.append(_metrics)
 
-            return result
+#             return result
 
-        def _validate(metrics, name, scope, count):
-            key = (name, scope)
-            metric = metrics.get(key)
+#         def _validate(metrics, name, scope, count):
+#             key = (name, scope)
+#             metric = metrics.get(key)
 
-            def _metrics_table():
-                out = [""]
-                out.append("Expected: {0}: {1}".format(key, count))
-                for metric_key, metric_value in metrics.items():
-                    out.append("{0}: {1}".format(metric_key, metric_value[0]))
-                return "\n".join(out)
+#             def _metrics_table():
+#                 out = [""]
+#                 out.append("Expected: {0}: {1}".format(key, count))
+#                 for metric_key, metric_value in metrics.items():
+#                     out.append("{0}: {1}".format(metric_key, metric_value[0]))
+#                 return "\n".join(out)
 
-            def _metric_details():
-                return "metric=%r, count=%r" % (key, metric.call_count)
+#             def _metric_details():
+#                 return "metric=%r, count=%r" % (key, metric.call_count)
 
-            if count is not None:
-                assert metric is not None, _metrics_table()
-                if count == "present":
-                    assert metric.call_count > 0, _metric_details()
-                else:
-                    assert metric.call_count == count, _metric_details()
+#             if count is not None:
+#                 assert metric is not None, _metrics_table()
+#                 if count == "present":
+#                     assert metric.call_count > 0, _metric_details()
+#                 else:
+#                     assert metric.call_count == count, _metric_details()
 
-                assert metric.total_call_time >= 0, (key, metric)
-                assert metric.total_exclusive_call_time >= 0, (key, metric)
-                assert metric.min_call_time >= 0, (key, metric)
-                assert metric.sum_of_squares >= 0, (key, metric)
+#                 assert metric.total_call_time >= 0, (key, metric)
+#                 assert metric.total_exclusive_call_time >= 0, (key, metric)
+#                 assert metric.min_call_time >= 0, (key, metric)
+#                 assert metric.sum_of_squares >= 0, (key, metric)
 
-            else:
-                assert metric is None, _metrics_table()
+#             else:
+#                 assert metric is None, _metrics_table()
 
-        _new_wrapper = _validate_transaction_metrics(wrapped)
-        val = _new_wrapper(*args, **kwargs)
-        assert record_transaction_called
-        metrics = recorded_metrics[index]
+#         _new_wrapper = _validate_transaction_metrics(wrapped)
+#         val = _new_wrapper(*args, **kwargs)
+#         assert record_transaction_called
+#         metrics = recorded_metrics[index]
 
-        record_transaction_called[:] = []
-        recorded_metrics[:] = []
+#         record_transaction_called[:] = []
+#         recorded_metrics[:] = []
 
-        for unscoped_metric in unscoped_metrics:
-            _validate(metrics, unscoped_metric, "", 1)
+#         for unscoped_metric in unscoped_metrics:
+#             _validate(metrics, unscoped_metric, "", 1)
 
-        for scoped_name, scoped_count in scoped_metrics:
-            _validate(metrics, scoped_name, transaction_scope_name, scoped_count)
+#         for scoped_name, scoped_count in scoped_metrics:
+#             _validate(metrics, scoped_name, transaction_scope_name, scoped_count)
 
-        for rollup_name, rollup_count in rollup_metrics:
-            _validate(metrics, rollup_name, "", rollup_count)
+#         for rollup_name, rollup_count in rollup_metrics:
+#             _validate(metrics, rollup_name, "", rollup_count)
 
-        for custom_name, custom_count in custom_metrics:
-            _validate(metrics, custom_name, "", custom_count)
+#         for custom_name, custom_count in custom_metrics:
+#             _validate(metrics, custom_name, "", custom_count)
 
-        custom_metric_names = {name for name, _ in custom_metrics}
-        for name, _ in metrics:
-            if name not in custom_metric_names:
-                assert not name.startswith("Supportability/api/"), name
+#         custom_metric_names = {name for name, _ in custom_metrics}
+#         for name, _ in metrics:
+#             if name not in custom_metric_names:
+#                 assert not name.startswith("Supportability/api/"), name
 
-        return val
+#         return val
 
-    return _validate_wrapper
+#     return _validate_wrapper
 
 
 def validate_time_metrics_outside_transaction(time_metrics=None, index=-1):

3b89bbf3084797bc906eff91b3bc4e57a504105a
Timothy Pansino <11214426+TimPansino@users.noreply.github.com>
2022-09-02 13:39:02 -0700

    Kafka Internal Metrics Instrumentation (#613)
    * Kafka Metrics data source
    
    Co-authored-by: Lalleh Rafeei <lrafeei@users.noreply.github.com>
    Co-authored-by: Hannah Stepanek <hmstepanek@users.noreply.github.com>
    
    * [Mega-Linter] Apply linters fixes
    
    * Remove breakpoint
    
    * Run linter
    
    * Fix black issues
    
    * Swap @classmethod decorator order.
    
    Co-authored-by: Hannah Stepanek <hmstepanek@users.noreply.github.com>
    
    * Remove variable named variable
    
    Co-authored-by: Lalleh Rafeei <lrafeei@users.noreply.github.com>
    Co-authored-by: Hannah Stepanek <hmstepanek@users.noreply.github.com>
    Co-authored-by: TimPansino <TimPansino@users.noreply.github.com>
    Co-authored-by: Lalleh Rafeei <lrafeei@newrelic.com>
    Co-authored-by: Hannah Stepanek <hstepanek@newrelic.com>

--- newrelic/hooks/messagebroker_kafkapython.py
+++ newrelic/hooks/messagebroker_kafkapython.py
@@ -12,11 +12,22 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import logging
+import math
+import threading
+
+from kafka.metrics.metrics_reporter import AbstractMetricsReporter
+
+import newrelic.core.agent
 from newrelic.api.application import application_instance
 from newrelic.api.message_trace import MessageTrace
 from newrelic.api.time_trace import notice_error
 from newrelic.api.transaction import current_transaction
 from newrelic.common.object_wrapper import wrap_function_wrapper
+from newrelic.packages import six
+from newrelic.samplers.decorators import data_source_factory
+
+_logger = logging.getLogger(__name__)
 
 HEARTBEAT_POLL = "MessageBroker/Kafka/Heartbeat/Poll"
 HEARTBEAT_SENT = "MessageBroker/Kafka/Heartbeat/Sent"
@@ -53,11 +64,6 @@ def wrap_KafkaProducer_send(wrapped, instance, args, kwargs):
         except Exception:
             notice_error()
             raise
-
-
-def instrument_kafka_producer(module):
-    if hasattr(module, "KafkaProducer"):
-        wrap_function_wrapper(module, "KafkaProducer.send", wrap_KafkaProducer_send)
 
 
 def metric_wrapper(metric_name, check_result=False):
@@ -101,3 +107,144 @@ def instrument_kafka_heartbeat(module):
             wrap_function_wrapper(
                 module, "Heartbeat.poll_timeout_expired", metric_wrapper(HEARTBEAT_POLL_TIMEOUT, check_result=True)
             )
+
+
+class KafkaMetricsDataSource(object):
+    _instance = None
+
+    def __init__(self):
+        self.reporters = []
+
+    @classmethod
+    @data_source_factory(name="Kafka Metrics Reporter")
+    def factory(cls, settings=None, environ=None):
+        return cls.singleton()
+
+    @classmethod
+    def singleton(cls, register=True):
+        # If already initialized, exit early
+        if cls._instance:
+            return cls._instance
+
+        # Init and register instance on class
+        instance = cls()
+        cls._instance = instance
+
+        # register_data_source takes a callable so let it rerun singleton to retrieve the instance
+        if register:
+            try:
+                _logger.debug("Registering kafka metrics data source.")
+                newrelic.core.agent.agent_instance().register_data_source(cls.factory)
+            except Exception:
+                _logger.exception(
+                    "Attempt to register kafka metrics data source has failed. Data source will be skipped."
+                )
+
+        return instance
+
+    def register(self, reporter):
+        self.reporters.append(reporter)
+
+    def unregister(self, reporter):
+        if reporter in self.reporters:
+            self.reporters.remove(reporter)
+
+    def start(self):
+        return
+
+    def stop(self):
+        # Clear references to reporters to prevent them from participating in a reference cycle.
+        self.reporters = []
+
+    def __call__(self):
+        for reporter in self.reporters:
+            for name, metric in six.iteritems(reporter.snapshot()):
+                yield name, metric
+
+
+class NewRelicMetricsReporter(AbstractMetricsReporter):
+    def __init__(self, *args, **kwargs):
+        super(NewRelicMetricsReporter, self).__init__(*args, **kwargs)
+
+        # Register with data source for harvesting
+        self.data_source = KafkaMetricsDataSource.singleton()
+        self.data_source.register(self)
+
+        self._metrics = {}
+        self._lock = threading.Lock()
+
+    def close(self, *args, **kwargs):
+        self.data_source.unregister(self)
+        with self._lock:
+            self._metrics = {}
+
+    def init(self, metrics):
+        for metric in metrics:
+            self.metric_change(metric)
+
+    @staticmethod
+    def invalid_metric_value(metric):
+        name, value = metric
+        return not any((math.isinf(value), math.isnan(value), value == 0))
+
+    def snapshot(self):
+        with self._lock:
+            # metric.value can only be called once, so care must be taken when filtering
+            metrics = ((name, metric.value()) for name, metric in six.iteritems(self._metrics))
+            return {
+                "MessageBroker/Kafka/Internal/%s" % name: {"count": value}
+                for name, value in filter(self.invalid_metric_value, metrics)
+            }
+
+    def get_metric_name(self, metric):
+        metric_name = metric.metric_name  # Get MetricName object to work with
+
+        name = metric_name.name
+        group = metric_name.group
+
+        if "topic" in metric_name.tags:
+            topic = metric_name.tags["topic"]
+            return "/".join((group, topic, name))
+        else:
+            return "/".join((group, name))
+
+    def metric_change(self, metric):
+        name = self.get_metric_name(metric)
+        with self._lock:
+            self._metrics[name] = metric
+
+    def metric_removal(self, metric):
+        name = self.get_metric_name(metric)
+        with self._lock:
+            if name in self._metrics:
+                self._metrics.pop(name)
+
+    def configure(self, configs):
+        return
+
+
+def wrap_KafkaProducerConsumer_init(wrapped, instance, args, kwargs):
+    try:
+        if "metric_reporters" in kwargs:
+            metric_reporters = list(kwargs.get("metric_reporters", []))
+            metric_reporters.append(NewRelicMetricsReporter)
+            kwargs["metric_reporters"] = [metric_reporters]
+        else:
+            kwargs["metric_reporters"] = [NewRelicMetricsReporter]
+    except Exception:
+        pass
+
+    return wrapped(*args, **kwargs)
+
+
+def instrument_kafka_producer(module):
+    if hasattr(module, "KafkaProducer"):
+        wrap_function_wrapper(module, "KafkaProducer.__init__", wrap_KafkaProducerConsumer_init)
+
+    if hasattr(module, "KafkaProducer"):
+        wrap_function_wrapper(module, "KafkaProducer.send", wrap_KafkaProducer_send)
+
+
+def instrument_kafka_consumer_group(module):
+    if hasattr(module, "KafkaConsumer"):
+        wrap_function_wrapper(module, "KafkaConsumer.__init__", wrap_KafkaProducerConsumer_init)

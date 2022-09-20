c7d1126e255a115475ec08db9d08b3b17288888c
Lalleh Rafeei <lrafeei@newrelic.com>
2022-09-20 10:39:15 -0700

    Move validate_custom_event_collector_json into validator directory

--- tests/testing_support/fixtures.py
+++ tests/testing_support/fixtures.py
@@ -628,50 +628,50 @@ def check_attributes(parameters, required_params=None, forgone_params=None, exac
 #     return _validate_transaction_event_collector_json
 
 
-def validate_custom_event_collector_json(num_events=1):
-    """Validate the format, types and number of custom events."""
+# def validate_custom_event_collector_json(num_events=1):
+#     """Validate the format, types and number of custom events."""
 
-    @transient_function_wrapper("newrelic.core.application", "Application.record_transaction")
-    def _validate_custom_event_collector_json(wrapped, instance, args, kwargs):
-        try:
-            result = wrapped(*args, **kwargs)
-        except:
-            raise
-        else:
-            stats = instance._stats_engine
-            settings = stats.settings
+#     @transient_function_wrapper("newrelic.core.application", "Application.record_transaction")
+#     def _validate_custom_event_collector_json(wrapped, instance, args, kwargs):
+#         try:
+#             result = wrapped(*args, **kwargs)
+#         except:
+#             raise
+#         else:
+#             stats = instance._stats_engine
+#             settings = stats.settings
 
-            agent_run_id = 666
-            sampling_info = stats.custom_events.sampling_info
-            samples = list(stats.custom_events)
+#             agent_run_id = 666
+#             sampling_info = stats.custom_events.sampling_info
+#             samples = list(stats.custom_events)
 
-            # Emulate the payload used in data_collector.py
+#             # Emulate the payload used in data_collector.py
 
-            payload = (agent_run_id, sampling_info, samples)
-            collector_json = json_encode(payload)
+#             payload = (agent_run_id, sampling_info, samples)
+#             collector_json = json_encode(payload)
 
-            decoded_json = json.loads(collector_json)
+#             decoded_json = json.loads(collector_json)
 
-            decoded_agent_run_id = decoded_json[0]
-            decoded_sampling_info = decoded_json[1]
-            decoded_events = decoded_json[2]
+#             decoded_agent_run_id = decoded_json[0]
+#             decoded_sampling_info = decoded_json[1]
+#             decoded_events = decoded_json[2]
 
-            assert decoded_agent_run_id == agent_run_id
-            assert decoded_sampling_info == sampling_info
+#             assert decoded_agent_run_id == agent_run_id
+#             assert decoded_sampling_info == sampling_info
 
-            max_setting = settings.event_harvest_config.harvest_limits.custom_event_data
-            assert decoded_sampling_info["reservoir_size"] == max_setting
+#             max_setting = settings.event_harvest_config.harvest_limits.custom_event_data
+#             assert decoded_sampling_info["reservoir_size"] == max_setting
 
-            assert decoded_sampling_info["events_seen"] == num_events
-            assert len(decoded_events) == num_events
+#             assert decoded_sampling_info["events_seen"] == num_events
+#             assert len(decoded_events) == num_events
 
-            for (intrinsics, attributes) in decoded_events:
-                assert isinstance(intrinsics, dict)
-                assert isinstance(attributes, dict)
+#             for (intrinsics, attributes) in decoded_events:
+#                 assert isinstance(intrinsics, dict)
+#                 assert isinstance(attributes, dict)
 
-        return result
+#         return result
 
-    return _validate_custom_event_collector_json
+#     return _validate_custom_event_collector_json
 
 
 def validate_tt_parameters(required_params=None, forgone_params=None):

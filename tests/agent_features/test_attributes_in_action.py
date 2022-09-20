3c44151346218466d0e2e460d51015fbc6918964
Lalleh Rafeei <lrafeei@newrelic.com>
2022-09-20 12:38:25 -0700

    Move validate_error_trace_attributes_outside_transaction into validators directory

--- tests/agent_features/test_attributes_in_action.py
+++ tests/agent_features/test_attributes_in_action.py
@@ -21,13 +21,15 @@ from testing_support.fixtures import (
     reset_core_stats_engine,
     validate_attributes,
     validate_error_event_attributes_outside_transaction,
-    validate_error_trace_attributes_outside_transaction,
 )
 from testing_support.validators.validate_browser_attributes import (
     validate_browser_attributes,
 )
 from testing_support.validators.validate_error_event_attributes import (
     validate_error_event_attributes,
+)
+from testing_support.validators.validate_error_trace_attributes_outside_transaction import (
+    validate_error_trace_attributes_outside_transaction,
 )
 from testing_support.validators.validate_span_events import validate_span_events
 from testing_support.validators.validate_transaction_error_trace_attributes import (
--- tests/testing_support/fixtures.py
+++ tests/testing_support/fixtures.py
@@ -433,61 +433,61 @@ def check_event_attributes(event_data, required_params=None, forgone_params=None
             assert intrinsics[param] == value, ((param, value), intrinsics)
 
 
-def check_error_attributes(
-    parameters, required_params=None, forgone_params=None, exact_attrs=None, is_transaction=True
-):
-    required_params = required_params or {}
-    forgone_params = forgone_params or {}
-    exact_attrs = exact_attrs or {}
+# def check_error_attributes(
+#     parameters, required_params=None, forgone_params=None, exact_attrs=None, is_transaction=True
+# ):
+#     required_params = required_params or {}
+#     forgone_params = forgone_params or {}
+#     exact_attrs = exact_attrs or {}
 
-    parameter_fields = ["userAttributes"]
-    if is_transaction:
-        parameter_fields.extend(["stack_trace", "agentAttributes", "intrinsics"])
+#     parameter_fields = ["userAttributes"]
+#     if is_transaction:
+#         parameter_fields.extend(["stack_trace", "agentAttributes", "intrinsics"])
 
-    for field in parameter_fields:
-        assert field in parameters
+#     for field in parameter_fields:
+#         assert field in parameters
 
-    # we can remove this after agent attributes transition is all over
-    assert "parameter_groups" not in parameters
-    assert "custom_params" not in parameters
-    assert "request_params" not in parameters
-    assert "request_uri" not in parameters
+#     # we can remove this after agent attributes transition is all over
+#     assert "parameter_groups" not in parameters
+#     assert "custom_params" not in parameters
+#     assert "request_params" not in parameters
+#     assert "request_uri" not in parameters
 
-    check_attributes(parameters, required_params, forgone_params, exact_attrs)
+#     check_attributes(parameters, required_params, forgone_params, exact_attrs)
 
 
-def check_attributes(parameters, required_params=None, forgone_params=None, exact_attrs=None):
-    required_params = required_params or {}
-    forgone_params = forgone_params or {}
-    exact_attrs = exact_attrs or {}
+# def check_attributes(parameters, required_params=None, forgone_params=None, exact_attrs=None):
+#     required_params = required_params or {}
+#     forgone_params = forgone_params or {}
+#     exact_attrs = exact_attrs or {}
 
-    intrinsics = parameters.get("intrinsics", {})
-    user_attributes = parameters.get("userAttributes", {})
-    agent_attributes = parameters.get("agentAttributes", {})
+#     intrinsics = parameters.get("intrinsics", {})
+#     user_attributes = parameters.get("userAttributes", {})
+#     agent_attributes = parameters.get("agentAttributes", {})
 
-    if required_params:
-        for param in required_params["agent"]:
-            assert param in agent_attributes, (param, agent_attributes)
-        for param in required_params["user"]:
-            assert param in user_attributes, (param, user_attributes)
-        for param in required_params["intrinsic"]:
-            assert param in intrinsics, (param, intrinsics)
+#     if required_params:
+#         for param in required_params["agent"]:
+#             assert param in agent_attributes, (param, agent_attributes)
+#         for param in required_params["user"]:
+#             assert param in user_attributes, (param, user_attributes)
+#         for param in required_params["intrinsic"]:
+#             assert param in intrinsics, (param, intrinsics)
 
-    if forgone_params:
-        for param in forgone_params["agent"]:
-            assert param not in agent_attributes, (param, agent_attributes)
-        for param in forgone_params["user"]:
-            assert param not in user_attributes, (param, user_attributes)
-        for param in forgone_params["intrinsic"]:
-            assert param not in intrinsics, (param, intrinsics)
+#     if forgone_params:
+#         for param in forgone_params["agent"]:
+#             assert param not in agent_attributes, (param, agent_attributes)
+#         for param in forgone_params["user"]:
+#             assert param not in user_attributes, (param, user_attributes)
+#         for param in forgone_params["intrinsic"]:
+#             assert param not in intrinsics, (param, intrinsics)
 
-    if exact_attrs:
-        for param, value in exact_attrs["agent"].items():
-            assert agent_attributes[param] == value, ((param, value), agent_attributes)
-        for param, value in exact_attrs["user"].items():
-            assert user_attributes[param] == value, ((param, value), user_attributes)
-        for param, value in exact_attrs["intrinsic"].items():
-            assert intrinsics[param] == value, ((param, value), intrinsics)
+#     if exact_attrs:
+#         for param, value in exact_attrs["agent"].items():
+#             assert agent_attributes[param] == value, ((param, value), agent_attributes)
+#         for param, value in exact_attrs["user"].items():
+#             assert user_attributes[param] == value, ((param, value), user_attributes)
+#         for param, value in exact_attrs["intrinsic"].items():
+#             assert intrinsics[param] == value, ((param, value), intrinsics)
 
 
 # def validate_tt_segment_params(forgone_params=(), present_params=(), exact_params=None):
@@ -655,29 +655,29 @@ def check_attributes(parameters, required_params=None, forgone_params=None, exac
 # t     return _validate_wrapper
 
 
-def validate_error_trace_attributes_outside_transaction(
-    err_name, required_params=None, forgone_params=None, exact_attrs=None
-):
-    required_params = required_params or {}
-    forgone_params = forgone_params or {}
-    exact_attrs = exact_attrs or {}
+# def validate_error_trace_attributes_outside_transaction(
+#     err_name, required_params=None, forgone_params=None, exact_attrs=None
+# ):
+#     required_params = required_params or {}
+#     forgone_params = forgone_params or {}
+#     exact_attrs = exact_attrs or {}
 
-    @transient_function_wrapper("newrelic.core.stats_engine", "StatsEngine.notice_error")
-    def _validate_error_trace_attributes_outside_transaction(wrapped, instance, args, kwargs):
-        try:
-            result = wrapped(*args, **kwargs)
-        except:
-            raise
-        else:
-            target_error = core_application_stats_engine_error(err_name)
+#     @transient_function_wrapper("newrelic.core.stats_engine", "StatsEngine.notice_error")
+#     def _validate_error_trace_attributes_outside_transaction(wrapped, instance, args, kwargs):
+#         try:
+#             result = wrapped(*args, **kwargs)
+#         except:
+#             raise
+#         else:
+#             target_error = core_application_stats_engine_error(err_name)
 
-            check_error_attributes(
-                target_error.parameters, required_params, forgone_params, exact_attrs, is_transaction=False
-            )
+#             check_error_attributes(
+#                 target_error.parameters, required_params, forgone_params, exact_attrs, is_transaction=False
+#             )
 
-        return result
+#         return result
 
-    return _validate_error_trace_attributes_outside_transaction
+#     return _validate_error_trace_attributes_outside_transaction
 
 
 def validate_error_event_attributes_outside_transaction(
@@ -1452,25 +1452,25 @@ def core_application_stats_engine(app_name=None):
     return core_application._stats_engine
 
 
-def core_application_stats_engine_error(error_type, app_name=None):
-    """Return a single error with the type of error_type, or None.
+# def core_application_stats_engine_error(error_type, app_name=None):
+#     """Return a single error with the type of error_type, or None.
 
-    In the core application StatsEngine, look in StatsEngine.error_data()
-    and return the first error with the type of error_type. If none found,
-    return None.
+#     In the core application StatsEngine, look in StatsEngine.error_data()
+#     and return the first error with the type of error_type. If none found,
+#     return None.
 
-    Useful for verifying that application.notice_error() works, since
-    the error is saved outside of a transaction. Must use a unique error
-    type per test in a single test file, so that it returns the error you
-    expect. (If you have 2 tests that record the same type of exception, then
-    StatsEngine.error_data() will contain 2 errors with the same type, but
-    this function will always return the first one it finds.)
+#     Useful for verifying that application.notice_error() works, since
+#     the error is saved outside of a transaction. Must use a unique error
+#     type per test in a single test file, so that it returns the error you
+#     expect. (If you have 2 tests that record the same type of exception, then
+#     StatsEngine.error_data() will contain 2 errors with the same type, but
+#     this function will always return the first one it finds.)
 
-    """
+#     """
 
-    stats = core_application_stats_engine(app_name)
-    errors = stats.error_data()
-    return next((e for e in errors if e.type == error_type), None)
+#     stats = core_application_stats_engine(app_name)
+#     errors = stats.error_data()
+#     return next((e for e in errors if e.type == error_type), None)
 
 
 def error_is_saved(error, app_name=None):

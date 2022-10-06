c4619a2c81e3a0352972ff190214efc1bc3e2ca7
Tim Pansino <timpansino@gmail.com>
2022-10-06 15:45:55 -0700

    Add tests for graphql trace types to unittests

--- tests/agent_features/test_span_events.py
+++ tests/agent_features/test_span_events.py
@@ -25,6 +25,7 @@ from newrelic.api.database_trace import DatabaseTrace
 from newrelic.api.datastore_trace import DatastoreTrace
 from newrelic.api.external_trace import ExternalTrace
 from newrelic.api.function_trace import FunctionTrace, function_trace
+from newrelic.api.graphql_trace import GraphQLOperationTrace, GraphQLResolverTrace
 from newrelic.api.memcache_trace import MemcacheTrace
 from newrelic.api.message_trace import MessageTrace
 from newrelic.api.solr_trace import SolrTrace
@@ -121,6 +122,8 @@ def test_span_events(dt_enabled, span_events_enabled, txn_sampled):
     (DatastoreTrace, ('db_product', 'db_target', 'db_operation')),
     (ExternalTrace, ('lib', 'url')),
     (FunctionTrace, ('name', )),
+    (GraphQLOperationTrace, ()),
+    (GraphQLResolverTrace, ()),
     (MemcacheTrace, ('command', )),
     (MessageTrace, ('lib', 'operation', 'dst_type', 'dst_name')),
     (SolrTrace, ('lib', 'command')),
@@ -592,6 +595,8 @@ _span_event_metrics = [("Supportability/SpanEvent/Errors/Dropped", None)]
     (DatastoreTrace, ('db_product', 'db_target', 'db_operation')),
     (ExternalTrace, ('lib', 'url')),
     (FunctionTrace, ('name', )),
+    (GraphQLOperationTrace, ()),
+    (GraphQLResolverTrace, ()),
     (MemcacheTrace, ('command', )),
     (MessageTrace, ('lib', 'operation', 'dst_type', 'dst_name')),
     (SolrTrace, ('lib', 'command')),
@@ -638,6 +643,8 @@ def test_span_event_error_attributes_notice_error(trace_type, args):
     (DatastoreTrace, ('db_product', 'db_target', 'db_operation')),
     (ExternalTrace, ('lib', 'url')),
     (FunctionTrace, ('name', )),
+    (GraphQLOperationTrace, ()),
+    (GraphQLResolverTrace, ()),
     (MemcacheTrace, ('command', )),
     (MessageTrace, ('lib', 'operation', 'dst_type', 'dst_name')),
     (SolrTrace, ('lib', 'command')),
@@ -678,6 +685,8 @@ def test_span_event_error_attributes_observed(trace_type, args):
     (DatastoreTrace, ('db_product', 'db_target', 'db_operation')),
     (ExternalTrace, ('lib', 'url')),
     (FunctionTrace, ('name', )),
+    (GraphQLOperationTrace, ()),
+    (GraphQLResolverTrace, ()),
     (MemcacheTrace, ('command', )),
     (MessageTrace, ('lib', 'operation', 'dst_type', 'dst_name')),
     (SolrTrace, ('lib', 'command')),
--- tests/agent_features/test_transaction_trace_segments.py
+++ tests/agent_features/test_transaction_trace_segments.py
@@ -21,6 +21,7 @@ from newrelic.api.database_trace import DatabaseTrace
 from newrelic.api.datastore_trace import DatastoreTrace
 from newrelic.api.external_trace import external_trace, ExternalTrace
 from newrelic.api.function_trace import FunctionTrace
+from newrelic.api.graphql_trace import GraphQLOperationTrace, GraphQLResolverTrace
 from newrelic.api.memcache_trace import MemcacheTrace
 from newrelic.api.message_trace import MessageTrace
 from newrelic.api.solr_trace import SolrTrace
@@ -110,6 +111,8 @@ def test_database_segment_attributes_disabled():
     (DatastoreTrace, ('db_product', 'db_target', 'db_operation')),
     (ExternalTrace, ('lib', 'url')),
     (FunctionTrace, ('name', )),
+    (GraphQLOperationTrace, ()),
+    (GraphQLResolverTrace, ()),
     (MemcacheTrace, ('command', )),
     (MessageTrace, ('lib', 'operation', 'dst_type', 'dst_name')),
     (SolrTrace, ('lib', 'command')),

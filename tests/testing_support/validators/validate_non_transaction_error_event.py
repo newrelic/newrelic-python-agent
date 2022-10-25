3e59665823425c2f21dcb638ccc02d0942ed9cd4
Lalleh Rafeei <lrafeei@newrelic.com>
2022-10-25 15:50:33 -0700

    Fix (more) import issues

--- tests/testing_support/validators/validate_non_transaction_error_event.py
+++ tests/testing_support/validators/validate_non_transaction_error_event.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from time import time
+import time
 
 from testing_support.fixtures import core_application_stats_engine
 

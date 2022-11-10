78e0f587612f7dfa44bf4e74af5bd162d195f7a5
Tim Pansino <timpansino@gmail.com>
2022-11-10 13:04:06 -0800

    Fix settings typo

--- newrelic/core/config.py
+++ newrelic/core/config.py
@@ -269,7 +269,7 @@ class ApplicationLoggingForwardingSettings(Settings):
     pass
 
 
-class ApplicationLoggingForwardinContextDataSettings(Settings):
+class ApplicationLoggingForwardingContextDataSettings(Settings):
     pass
 
 
@@ -361,7 +361,7 @@ class EventHarvestConfigHarvestLimitSettings(Settings):
 _settings = TopLevelSettings()
 _settings.application_logging = ApplicationLoggingSettings()
 _settings.application_logging.forwarding = ApplicationLoggingForwardingSettings()
-_settings.application_logging.forwarding.context_data = ApplicationLoggingForwardinContextDataSettings()
+_settings.application_logging.forwarding.context_data = ApplicationLoggingForwardingContextDataSettings()
 _settings.application_logging.metrics = ApplicationLoggingMetricsSettings()
 _settings.application_logging.local_decorating = ApplicationLoggingLocalDecoratingSettings()
 _settings.attributes = AttributesSettings()

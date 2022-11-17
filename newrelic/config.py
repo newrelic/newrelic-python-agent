f5568cad3dfb050223a9f3da18dafcdb757d0740
Hannah Stepanek <hstepanek@newrelic.com>
2022-11-16 17:57:52 -0800

    Fixup: add comments & organize

--- newrelic/config.py
+++ newrelic/config.py
@@ -2795,6 +2795,7 @@ def _process_module_builtin_defaults():
         "newrelic.hooks.component_sklearn",
         "instrument_sklearn_tree_models",
     )
+    # In scikit-learn < 0.21 the model classes are in tree.py instead of _classes.py.
     _process_module_definition(
         "sklearn.tree.tree",
         "newrelic.hooks.component_sklearn",

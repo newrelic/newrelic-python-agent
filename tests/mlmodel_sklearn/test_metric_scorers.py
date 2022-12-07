723cad4e7ee7ee7eac26f3675f4ceb95d38e2905
Hannah Stepanek <hstepanek@newrelic.com>
2022-12-07 12:42:54 -0800

    Add test for iterable score results

--- tests/mlmodel_sklearn/test_metric_scorers.py
+++ tests/mlmodel_sklearn/test_metric_scorers.py
@@ -20,15 +20,62 @@ from newrelic.api.background_task import background_task
 from newrelic.hooks.mlmodel_sklearn import _wrap_predict_return_type
 
 
+@pytest.mark.parametrize(
+    "metric_scorer_name",
+    (
+        "accuracy_score",
+        "balanced_accuracy_score",
+        "f1_score",
+        "precision_score",
+        "recall_score",
+        "roc_auc_score",
+        "r2_score",
+    ),
+)
 def test_metric_scorer_attributes(metric_scorer_name, run_metric_scorer):
     @validate_attributes("agent", ["DecisionTreeClassifier.%s" % metric_scorer_name])
     @background_task()
     def _test():
-        run_metric_scorer()
+        run_metric_scorer(metric_scorer_name)
 
     _test()
 
 
+@pytest.mark.parametrize(
+    "metric_scorer_name,kwargs",
+    [
+        ("f1_score", {"average": None}),
+        ("precision_score", {"average": None}),
+        ("recall_score", {"average": None}),
+    ],
+)
+def test_metric_scorer_iterable_score_attributes(metric_scorer_name, kwargs, run_metric_scorer):
+    @validate_attributes(
+        "agent",
+        [
+            "DecisionTreeClassifier.%s[0]" % metric_scorer_name,
+            "DecisionTreeClassifier.%s[1]" % metric_scorer_name,
+        ],
+    )
+    @background_task()
+    def _test():
+        run_metric_scorer(metric_scorer_name, kwargs)
+
+    _test()
+
+
+@pytest.mark.parametrize(
+    "metric_scorer_name",
+    [
+        "accuracy_score",
+        "balanced_accuracy_score",
+        "f1_score",
+        "precision_score",
+        "recall_score",
+        "roc_auc_score",
+        "r2_score",
+    ],
+)
 def test_metric_scorer_attributes_unknown_model(metric_scorer_name):
     @validate_attributes("agent", ["Unknown.%s" % metric_scorer_name])
     @background_task()
@@ -41,21 +88,6 @@ def test_metric_scorer_attributes_unknown_model(metric_scorer_name):
         getattr(metrics, metric_scorer_name)(y_test, y_pred)
 
     _test()
-
-
-@pytest.fixture(
-    params=[
-        "accuracy_score",
-        "balanced_accuracy_score",
-        "f1_score",
-        "precision_score",
-        "recall_score",
-        "roc_auc_score",
-        "r2_score",
-    ]
-)
-def metric_scorer_name(request):
-    return request.param
 
 
 @pytest.mark.parametrize("data", (np.array([0, 1]), "foo", 1, 1.0, True, [0, 1], {"foo": "bar"}, (0, 1), np.str_("F")))
@@ -76,8 +108,8 @@ def test_no_harm__wrap_predict_return_type():
 
 
 @pytest.fixture
-def run_metric_scorer(metric_scorer_name):
-    def _run():
+def run_metric_scorer():
+    def _run(metric_scorer_name, metric_scorer_kwargs=None):
         from sklearn import metrics, tree
 
         x_train = [[0, 0], [1, 1]]
@@ -89,6 +121,8 @@ def run_metric_scorer(metric_scorer_name):
         model = clf.fit(x_train, y_train)
 
         labels = model.predict(x_test)
-        return getattr(metrics, metric_scorer_name)(y_test, labels)
+
+        metric_scorer_kwargs = metric_scorer_kwargs or {}
+        return getattr(metrics, metric_scorer_name)(y_test, labels, **metric_scorer_kwargs)
 
     return _run

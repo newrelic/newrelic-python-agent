4e038ec4faa3bcb7576a45ea49830cde178301d5
Lalleh Rafeei <lrafeei@newrelic.com>
2022-12-20 14:06:17 -0800

    Add multiclass models

--- newrelic/hooks/mlmodel_sklearn.py
+++ newrelic/hooks/mlmodel_sklearn.py
@@ -238,6 +238,15 @@ def instrument_sklearn_cluster_kmeans_models(module):
     _instrument_sklearn_models(module, model_classes)
 
 
+def instrument_sklearn_multiclass_models(module):
+    model_classes = (
+        "OneVsRestClassifier",
+        "OneVsOneClassifier",
+        "OutputCodeClassifier",
+    )
+    _instrument_sklearn_models(module, model_classes)
+
+
 def instrument_sklearn_metrics(module):
     for scorer in METRIC_SCORERS:
         if hasattr(module, scorer):

15cabcbb563be7eb9387c0a9e727067f122af9c0
Tim Pansino <timpansino@gmail.com>
2023-01-04 14:24:52 -0800

    Add pytest parametrization from dict

--- tests/testing_support/util.py
+++ tests/testing_support/util.py
@@ -14,6 +14,8 @@
 
 import re
 import socket
+import pytest
+from newrelic.packages import six
 
 def _to_int(version_str):
     m = re.match(r'\d+', version_str)
@@ -50,3 +52,8 @@ def conditional_decorator(decorator, condition):
         return decorator(func)
 
     return _conditional_decorator
+
+
+def pytest_parametrize_from_dict(argnames, argvalues, *args, **kwargs):
+    pytest_params = [pytest.param(*values, id=key) for key, values in six.iteritems(argvalues)]
+    return pytest.mark.parametrize(argnames, pytest_params, *args, **kwargs)

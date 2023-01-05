16295bc42c7aeb13e6c145b02733e1e513ba27be
Uma Annamalai <uannamalai@newrelic.com>
2023-01-04 16:38:39 -0800

    Add ml_model wrapper and tests.

---
+++ tests/mlmodel_sklearn/test_ml_model.py
@@ -0,0 +1,129 @@
+# Copyright 2010 New Relic, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import logging
+import pandas
+from sklearn.tree import BaseDecisionTree
+from newrelic.api.ml_model import wrap_mlmodel
+
+from testing_support.fixtures import (reset_core_stats_engine,
+        validate_custom_event_count,
+        validate_custom_event_in_application_stats_engine,)
+
+from _validate_custom_events import validate_custom_events
+from newrelic.api.background_task import background_task
+
+_logger = logging.getLogger(__name__)
+
+class CustomTestModel(BaseDecisionTree):
+    def __init__(self,
+        criterion="poisson",
+        splitter="random",
+        max_depth=None,
+        min_samples_split=1,
+        min_samples_leaf=1,
+        min_weight_fraction_leaf=0.0,
+        max_features=None,
+        random_state=0,
+        max_leaf_nodes=None,
+        min_impurity_decrease=0.0,
+        class_weight=None,
+        ccp_alpha=0.0):
+
+        super().__init__(
+        criterion = criterion,
+        splitter = splitter,
+        max_depth = max_depth,
+        min_samples_split = min_samples_split,
+        min_samples_leaf = min_samples_leaf,
+        min_weight_fraction_leaf = min_weight_fraction_leaf,
+        max_features = max_features,
+        max_leaf_nodes = max_leaf_nodes,
+        class_weight = class_weight,
+        random_state = random_state,
+        min_impurity_decrease = min_impurity_decrease,
+        ccp_alpha = ccp_alpha,
+
+        )
+
+    def fit(self, X, y, sample_weight=None, check_input=True):
+        _logger.info("Redefining fit function in custom model")
+
+        super().fit(
+            X,
+            y,
+            sample_weight=sample_weight,
+            check_input=check_input,
+        )
+
+        return self
+
+    def predict(self, X, check_input=True):
+        _logger.info("Redefining fit function in custom model")
+        super().predict(X, check_input=check_input)
+
+
+int_list_recorded_custom_events = [
+    {"users": {"inference_id": None, 'model_name': 'MyCustomModel', "model_version": '1.2.3', 'feature_name': None, 'type': "numerical", 'value': 1.0}},
+    {"users": {"inference_id": None, 'model_name': 'MyCustomModel', "model_version": '1.2.3', 'feature_name': None, 'type': "numerical", 'value': 2.0}},
+    {"users": {"inference_id": None, 'model_name': 'MyCustomModel', "model_version": '1.2.3', 'feature_name': None, 'type': "numerical", 'value': 3.0}},
+    {"users": {"inference_id": None, 'model_name': 'MyCustomModel', "model_version": '1.2.3', 'feature_name': None, 'type': "numerical", 'value': 4.0}},
+]
+
+
+@reset_core_stats_engine()
+def test_wrapper_attrs_custom_model_int_list():
+    @validate_custom_event_count(count=4)
+    @validate_custom_events(int_list_recorded_custom_events)
+    @background_task()
+    def _test():
+        x_train = [[0, 0], [1, 1]]
+        y_train = [0, 1]
+        x_test = [[1.0, 2.0], [3.0, 4.0]]
+
+        model = CustomTestModel().fit(x_train, y_train)
+        wrap_mlmodel(model, name="MyCustomModel", version="1.2.3")
+
+        labels = model.predict(x_test)
+
+        return model
+
+    _test()
+
+
+pandas_df_recorded_custom_events = [
+    {"users": {"inference_id": None, 'model_name': 'PandasTestModel', "model_version": '1.5.0b1', 'feature_name': "feature1", 'type': "categorical", 'value': 5.0}},
+    {"users": {"inference_id": None, 'model_name': 'PandasTestModel', "model_version": '1.5.0b1', 'feature_name': "feature1", 'type': "categorical", 'value': 6.0}},
+    {"users": {"inference_id": None, 'model_name': 'PandasTestModel', "model_version": '1.5.0b1', 'feature_name': "feature2", 'type': "categorical", 'value': 7.0}},
+    {"users": {"inference_id": None, 'model_name': 'PandasTestModel', "model_version": '1.5.0b1', 'feature_name': "feature2", 'type': "categorical", 'value': 8.0}},
+]
+
+
+@reset_core_stats_engine()
+def test_wrapper_attrs_custom_model_pandas_df():
+    @validate_custom_event_count(count=4)
+    @validate_custom_events(pandas_df_recorded_custom_events)
+    @background_task()
+    def _test():
+        x_train = pandas.DataFrame({"col1": [0, 0], "col2": [1, 1]}, dtype='category')
+        y_train = pandas.DataFrame({"label": [0, 1]}, dtype='category')
+        x_test = pandas.DataFrame({"col1": [5.0, 6.0], "col2": [7.0, 8.0]}, dtype='category')
+
+        model = CustomTestModel().fit(x_train, y_train)
+        wrap_mlmodel(model, name="PandasTestModel", version="1.5.0b1", feature_names=["feature1", "feature2"])
+        labels = model.predict(x_test)
+
+        return model
+
+    _test()
+

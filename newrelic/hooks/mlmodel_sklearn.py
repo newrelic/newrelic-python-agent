d785977ee3067b010f8a336dcf4a069aee727dbd
Lalleh Rafeei <84813886+lrafeei@users.noreply.github.com>
2023-01-06 12:05:08 -0800

    Add neural network model function traces (#729)
    * Add neural network models
    
    * Fixup: merge conflict
    
    Co-authored-by: Hannah Stepanek <hstepanek@newrelic.com>

--- newrelic/hooks/mlmodel_sklearn.py
+++ newrelic/hooks/mlmodel_sklearn.py
@@ -243,6 +243,15 @@ def instrument_sklearn_cluster_kmeans_models(module):
     _instrument_sklearn_models(module, model_classes)
 
 
+def instrument_sklearn_neural_network_models(module):
+    model_classes = (
+        "BernoulliRBM",
+        "MLPClassifier",
+        "MLPRegressor",
+    )
+    _instrument_sklearn_models(module, model_classes)
+
+
 def instrument_sklearn_svm_models(module):
     model_classes = (
         "LinearSVC",

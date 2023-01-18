111e3d65fb3b4ac653b7e500f30291972af0984d
Lalleh Rafeei <lrafeei@newrelic.com>
2023-01-18 13:46:30 -0800

    Start to add try/except blocks in tests

--- tests/datastore_elasticsearch/test_connection.py
+++ tests/datastore_elasticsearch/test_connection.py
@@ -12,21 +12,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from elasticsearch.connection.base import Connection
+try:
+    from elasticsearch.connection.base import Connection
+except:
+    # Fill in
+    import elastic_transport  # noqa for now.  This will be used
 
 
 def test_connection_default():
     conn = Connection()
-    assert conn._nr_host_port == ('localhost', '9200')
+    assert conn._nr_host_port == ("localhost", "9200")
+
 
 def test_connection_host_arg():
-    conn = Connection('the_host')
-    assert conn._nr_host_port == ('the_host', '9200')
+    conn = Connection("the_host")
+    assert conn._nr_host_port == ("the_host", "9200")
+
 
 def test_connection_args():
-    conn = Connection('the_host', 9999)
-    assert conn._nr_host_port == ('the_host', '9999')
+    conn = Connection("the_host", 9999)
+    assert conn._nr_host_port == ("the_host", "9999")
+
 
 def test_connection_kwargs():
-    conn = Connection(host='foo', port=8888)
-    assert conn._nr_host_port == ('foo', '8888')
+    conn = Connection(host="foo", port=8888)
+    assert conn._nr_host_port == ("foo", "8888")

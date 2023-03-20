b3f2a9501e41a834c3ae09d4e9b77db91cf065ec
umaannamalai <umaannamalai@users.noreply.github.com>
2023-03-20 22:13:20 +0000

    [Mega-Linter] Apply linters fixes

--- tests/datastore_redis/test_instance_info.py
+++ tests/datastore_redis/test_instance_info.py
@@ -117,10 +117,11 @@ if REDIS_PY_VERSION >= (2, 10):
     )
 
 if REDIS_PY_VERSION >= (4, 5, 2):
-    _instance_info_from_url_tests_4_5_2 = _instance_info_from_url_tests[:-3] + \
-            [(("unix:///path/to/socket.sock",), {}, ("localhost", "6379", "0")),
-            (("unix:///path/to/socket.sock?db=2",), {}, ("localhost", "6379", "2")),
-            (("unix:///path/to/socket.sock",), {"db": 2}, ("localhost", "6379", "2")),]
+    _instance_info_from_url_tests_4_5_2 = _instance_info_from_url_tests[:-3] + [
+        (("unix:///path/to/socket.sock",), {}, ("localhost", "6379", "0")),
+        (("unix:///path/to/socket.sock?db=2",), {}, ("localhost", "6379", "2")),
+        (("unix:///path/to/socket.sock",), {"db": 2}, ("localhost", "6379", "2")),
+    ]
 
 
 @pytest.mark.skipif(REDIS_PY_VERSION < (2, 6), reason="from_url not yet implemented in this redis-py version")
@@ -140,7 +141,10 @@ def test_strict_redis_client_from_url(args, kwargs, expected):
 
 
 @pytest.mark.skipif(REDIS_PY_VERSION < (2, 6), reason="from_url not yet implemented in this redis-py version")
-@pytest.mark.parametrize("args,kwargs,expected", _instance_info_from_url_tests if REDIS_PY_VERSION < (4, 5, 2) else _instance_info_from_url_tests_4_5_2)
+@pytest.mark.parametrize(
+    "args,kwargs,expected",
+    _instance_info_from_url_tests if REDIS_PY_VERSION < (4, 5, 2) else _instance_info_from_url_tests_4_5_2,
+)
 def test_redis_connection_from_url(args, kwargs, expected):
     r = redis.Redis.from_url(*args, **kwargs)
     if r.connection_pool.connection_class is redis.Connection:
@@ -160,7 +164,10 @@ def test_redis_connection_from_url(args, kwargs, expected):
 
 
 @pytest.mark.skipif(REDIS_PY_VERSION < (2, 6), reason="from_url not yet implemented in this redis-py version")
-@pytest.mark.parametrize("args,kwargs,expected", _instance_info_from_url_tests if REDIS_PY_VERSION < (4, 5, 2) else _instance_info_from_url_tests_4_5_2)
+@pytest.mark.parametrize(
+    "args,kwargs,expected",
+    _instance_info_from_url_tests if REDIS_PY_VERSION < (4, 5, 2) else _instance_info_from_url_tests_4_5_2,
+)
 def test_strict_redis_connection_from_url(args, kwargs, expected):
     r = redis.StrictRedis.from_url(*args, **kwargs)
     if r.connection_pool.connection_class is redis.Connection:

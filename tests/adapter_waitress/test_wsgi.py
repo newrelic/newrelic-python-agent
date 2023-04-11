074e8718eadbca0747c0802aa07091f0b3e3558b
Lalleh Rafeei <lrafeei@newrelic.com>
2023-04-11 10:37:07 -0700

    Remove commented out code

--- tests/adapter_waitress/test_wsgi.py
+++ tests/adapter_waitress/test_wsgi.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# import pytest
-
 
 def test_wsgi_application_index(target_application):
     response = target_application.get("/")

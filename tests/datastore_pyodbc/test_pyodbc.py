f4137d8b2e5a947d5e773a9de1d53691e0d68b8c
Hannah Stepanek <hstepanek@newrelic.com>
2023-04-19 10:02:59 -0700

    Fixup: remove time import

--- tests/datastore_pyodbc/test_pyodbc.py
+++ tests/datastore_pyodbc/test_pyodbc.py
@@ -11,8 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import time
-
 import pytest
 from testing_support.db_settings import postgresql_settings
 from testing_support.validators.validate_database_trace_inputs import (

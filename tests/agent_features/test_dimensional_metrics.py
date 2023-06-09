14e5a4533a349cf642553cca9e534da12480e09c
Tim Pansino <timpansino@gmail.com>
2023-06-09 11:50:45 -0700

    Expand OTLP metrics testing

--- tests/agent_features/test_dimensional_metrics.py
+++ tests/agent_features/test_dimensional_metrics.py
@@ -21,7 +21,10 @@ from newrelic.common.metric_utils import create_metric_identity
 
 from testing_support.fixtures import reset_core_stats_engine
 from testing_support.validators.validate_transaction_metrics import validate_transaction_metrics
-from testing_support.validators.validate_dimensional_metrics_outside_transaction import validate_dimensional_metrics_outside_transaction
+from testing_support.validators.validate_dimensional_metrics_outside_transaction import (
+    validate_dimensional_metrics_outside_transaction,
+)
+from testing_support.validators.validate_dimensional_metric_payload import validate_dimensional_metric_payload
 
 
 _test_tags_examples = [
@@ -47,10 +50,15 @@ def test_create_metric_identity(tags, expected):
 
 
 @pytest.mark.parametrize("tags,expected", _test_tags_examples)
+@reset_core_stats_engine()
 def test_record_dimensional_metric_inside_transaction(tags, expected):
-    @validate_transaction_metrics("test_record_dimensional_metric_inside_transaction", background_task=True, dimensional_metrics=[
-        ("Metric", expected, 1),
-    ])
+    @validate_transaction_metrics(
+        "test_record_dimensional_metric_inside_transaction",
+        background_task=True,
+        dimensional_metrics=[
+            ("Metric", expected, 1),
+        ],
+    )
     @background_task(name="test_record_dimensional_metric_inside_transaction")
     def _test():
         record_dimensional_metric("Metric", 1, tags=tags)
@@ -70,11 +78,16 @@ def test_record_dimensional_metric_outside_transaction(tags, expected):
 
 
 @pytest.mark.parametrize("tags,expected", _test_tags_examples)
+@reset_core_stats_engine()
 def test_record_dimensional_metrics_inside_transaction(tags, expected):
-    @validate_transaction_metrics("test_record_dimensional_metrics_inside_transaction", background_task=True, dimensional_metrics=[("Metric/1", expected, 1), ("Metric/2", expected, 1)])
+    @validate_transaction_metrics(
+        "test_record_dimensional_metrics_inside_transaction",
+        background_task=True,
+        dimensional_metrics=[("Metric.1", expected, 1), ("Metric.2", expected, 1)],
+    )
     @background_task(name="test_record_dimensional_metrics_inside_transaction")
     def _test():
-        record_dimensional_metrics([("Metric/1", 1, tags), ("Metric/2", 1, tags)])
+        record_dimensional_metrics([("Metric.1", 1, tags), ("Metric.2", 1, tags)])
 
     _test()
 
@@ -82,25 +95,71 @@ def test_record_dimensional_metrics_inside_transaction(tags, expected):
 @pytest.mark.parametrize("tags,expected", _test_tags_examples)
 @reset_core_stats_engine()
 def test_record_dimensional_metrics_outside_transaction(tags, expected):
-    @validate_dimensional_metrics_outside_transaction([("Metric/1", expected, 1), ("Metric/2", expected, 1)])
+    @validate_dimensional_metrics_outside_transaction([("Metric.1", expected, 1), ("Metric.2", expected, 1)])
     def _test():
         app = application_instance()
-        record_dimensional_metrics([("Metric/1", 1, tags), ("Metric/2", 1, tags)], application=app)
+        record_dimensional_metrics([("Metric.1", 1, tags), ("Metric.2", 1, tags)], application=app)
 
     _test()
 
 
+@reset_core_stats_engine()
 def test_dimensional_metrics_different_tags():
-    @validate_transaction_metrics("test_dimensional_metrics_different_tags", background_task=True, dimensional_metrics=[
-        ("Metric", frozenset({("tag", 1)}), 1),
-        ("Metric", frozenset({("tag", 2)}), 2),
-    ])
+    @validate_transaction_metrics(
+        "test_dimensional_metrics_different_tags",
+        background_task=True,
+        dimensional_metrics=[
+            ("Metric", frozenset({("tag", 1)}), 1),
+            ("Metric", frozenset({("tag", 2)}), 2),
+        ],
+    )
     @background_task(name="test_dimensional_metrics_different_tags")
     def _test():
-        record_dimensional_metrics([
-            ("Metric", 1, {"tag": 1}),
-            ("Metric", 1, {"tag": 2}),
-        ])
+        record_dimensional_metrics(
+            [
+                ("Metric", 1, {"tag": 1}),
+                ("Metric", 1, {"tag": 2}),
+            ]
+        )
         record_dimensional_metric("Metric", 1, {"tag": 2})
 
     _test()
+
+
+@reset_core_stats_engine()
+@validate_dimensional_metric_payload(
+    summary_metrics=[
+        ("Metric.Summary", {"tag": 1}, 1),
+        ("Metric.Summary", {"tag": 2}, 1),
+        ("Metric.Summary", None, 1),
+        ("Metric.Mixed", {"tag": 1}, 1),
+        ("Metric.NotPresent", None, None),
+    ],
+    count_metrics=[
+        ("Metric.Count", {"tag": 1}, 1),
+        ("Metric.Count", {"tag": 2}, 2),
+        ("Metric.Count", None, 3),
+        ("Metric.Mixed", {"tag": 2}, 2),
+        ("Metric.NotPresent", None, None),
+    ],
+)
+def test_dimensional_metric_payload():
+    @background_task(name="test_dimensional_metric_payload")
+    def _test():
+        record_dimensional_metrics(
+            [
+                ("Metric.Summary", 1, {"tag": 1}),
+                ("Metric.Summary", 2, {"tag": 2}),
+                ("Metric.Summary", 3),  # No tags
+                ("Metric.Count", {"count": 1}, {"tag": 1}),
+                ("Metric.Count", {"count": 2}, {"tag": 2}),
+                ("Metric.Count", {"count": 3}),  # No tags
+                ("Metric.Mixed", 1, {"tag": 1}),
+                ("Metric.Mixed", {"count": 2}, {"tag": 2}),
+            ]
+        )
+
+    _test()
+    app = application_instance()
+    core_app = app._agent.application(app.name)
+    core_app.harvest()

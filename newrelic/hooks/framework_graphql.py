f2252acd126eba4819f326dadb81ab0d59ca2d3e
Danny Guinther <dguinther@neuralmagic.com>
2023-06-12 13:47:43 -0400

    Short circuit after transaction check

--- newrelic/hooks/framework_graphql.py
+++ newrelic/hooks/framework_graphql.py
@@ -320,10 +320,10 @@ def wrap_resolver(wrapped, instance, args, kwargs):
     transaction = current_transaction()
     if transaction is None:
         return wrapped(*args, **kwargs)
+    return wrapped(*args, **kwargs)
 
     name = callable_name(wrapped)
     transaction.set_transaction_name(name, "GraphQL", priority=13)
-    return wrapped(*args, **kwargs)
 
     with FunctionTrace(name, source=wrapped):
         with ErrorTrace(ignore=ignore_graphql_duplicate_exception):
@@ -377,6 +377,7 @@ def wrap_resolve_field(wrapped, instance, args, kwargs):
     transaction = current_transaction()
     if transaction is None:
         return wrapped(*args, **kwargs)
+    return wrapped(*args, **kwargs)
 
     if graphql_version() < (3, 0):
         bind_resolve_field = bind_resolve_field_v2
@@ -392,7 +393,6 @@ def wrap_resolve_field(wrapped, instance, args, kwargs):
     field_def = parent_type.fields.get(field_name)
     field_return_type = str(field_def.type) if field_def else "<unknown>"
 
-    return wrapped(*args, **kwargs)
     with GraphQLResolverTrace(field_name) as trace:
         with ErrorTrace(ignore=ignore_graphql_duplicate_exception):
             trace._add_agent_attribute("graphql.field.parentType", parent_type.name)

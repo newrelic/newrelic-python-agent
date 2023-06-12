99bd706a88be8f8a7f91a946f7efd41bc5ad3e38
Danny Guinther <dguinther@neuralmagic.com>
2023-06-12 13:25:19 -0400

    Disable wrap_resolve_field, enable wrap_execute_operation
    wrap_resolve_field DID contribute to the regression

--- newrelic/hooks/framework_graphql.py
+++ newrelic/hooks/framework_graphql.py
@@ -464,13 +464,13 @@ def instrument_graphql_execute(module):
     if hasattr(module, "ExecutionContext"):
         # wrap_function_wrapper(module, "ExecutionContext.__init__", wrap_executor_context_init)
 
-        if hasattr(module.ExecutionContext, "resolve_field"):
-            wrap_function_wrapper(module, "ExecutionContext.resolve_field", wrap_resolve_field)
-        elif hasattr(module.ExecutionContext, "execute_field"):
-            wrap_function_wrapper(module, "ExecutionContext.execute_field", wrap_resolve_field)
+        # if hasattr(module.ExecutionContext, "resolve_field"):
+            # wrap_function_wrapper(module, "ExecutionContext.resolve_field", wrap_resolve_field)
+        # elif hasattr(module.ExecutionContext, "execute_field"):
+            # wrap_function_wrapper(module, "ExecutionContext.execute_field", wrap_resolve_field)
 
-        # if hasattr(module.ExecutionContext, "execute_operation"):
-            # wrap_function_wrapper(module, "ExecutionContext.execute_operation", wrap_execute_operation)
+        if hasattr(module.ExecutionContext, "execute_operation"):
+            wrap_function_wrapper(module, "ExecutionContext.execute_operation", wrap_execute_operation)
 
     # if hasattr(module, "resolve_field"):
         # wrap_function_wrapper(module, "resolve_field", wrap_resolve_field)

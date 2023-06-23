81f3a9993a0dedd4858c707b64e419b463945d99
Hannah Stepanek <hstepanek@newrelic.com>
2023-06-23 12:35:58 -0700

    Set event type to inferenceData

--- newrelic/hooks/mlmodel_sklearn.py
+++ newrelic/hooks/mlmodel_sklearn.py
@@ -203,7 +203,7 @@ def create_label_event(transaction, _class, inference_id, instance, return_val):
                 # Don't include the raw value when inference_event_value is disabled.
                 if settings and settings.machine_learning.inference_events_value.enabled:
                     event["label_value"] = str(value)
-                transaction.record_custom_event("ML Model Label Event", event)
+                transaction.record_custom_event("inferenceData", event)
 
 
 def _get_label_names(user_defined_label_names, prediction_array):
@@ -298,7 +298,7 @@ def create_feature_event(transaction, _class, inference_id, instance, args, kwar
             # Don't include the raw value when inference_event_value is disabled.
             if settings and settings.machine_learning and settings.machine_learning.inference_events_value.enabled:
                 event["feature_value"] = str(value)
-            transaction.record_custom_event("ML Model Feature Event", event)
+            transaction.record_custom_event("inferenceData", event)
 
 
 def _nr_instrument_model(module, model_class):

fbe40eaf4eb9da57fd6cb881328087fedc0dc2d9
Tim Pansino <timpansino@gmail.com>
2023-07-26 14:22:53 -0700

    Add async generator wrapper

--- newrelic/common/async_wrapper.py
+++ newrelic/common/async_wrapper.py
@@ -18,18 +18,23 @@ from newrelic.common.coroutine import (
     is_coroutine_callable,
     is_asyncio_coroutine,
     is_generator_function,
+    is_async_generator_function,
 )
+
+try:
+    import asyncio
+except ImportError:
+    asyncio = None
 
 
 def evaluate_wrapper(wrapper_string, wrapped, trace):
     values = {'wrapper': None, 'wrapped': wrapped,
-            'trace': trace, 'functools': functools}
+            'trace': trace, 'functools': functools, "asyncio": asyncio}
     exec(wrapper_string, values)
     return values['wrapper']
 
 
 def coroutine_wrapper(wrapped, trace):
-
     WRAPPER = textwrap.dedent("""
     @functools.wraps(wrapped)
     async def wrapper(*args, **kwargs):
@@ -45,8 +50,6 @@ def coroutine_wrapper(wrapped, trace):
 
 def awaitable_generator_wrapper(wrapped, trace):
     WRAPPER = textwrap.dedent("""
-    import asyncio
-
     @functools.wraps(wrapped)
     @asyncio.coroutine
     def wrapper(*args, **kwargs):
@@ -81,9 +84,49 @@ def generator_wrapper(wrapped, trace):
     return wrapper
 
 
+def async_generator_wrapper(wrapped, trace):
+    WRAPPER = textwrap.dedent("""
+    @functools.wraps(wrapped)
+    async def wrapper(*args, **kwargs):
+        g = wrapped(*args, **kwargs)
+        value = None
+        with trace:
+            while True:
+                try:
+                    g.asend(value).send(None)
+                except StopAsyncIteration as e:
+                    # The underlying async generator has finished, return propagates a new StopAsyncIteration
+                    return
+                except StopIteration as e:
+                    # The call to async_generator_asend.send() should raise a StopIteration containing the yielded value
+                    yielded = e.value
+
+                try:
+                    value = yield yielded
+                except BaseException as e:
+                    # An exception was thrown with .athrow(), propagate to the original async generator.
+                    # Return value logic must be identical to .asend()
+                    try:
+                        g.athrow(type(e), e).send(None)
+                    except StopAsyncIteration as e:
+                        # The underlying async generator has finished, return propagates a new StopAsyncIteration
+                        return
+                    except StopIteration as e:
+                        # The call to async_generator_athrow.send() should raise a StopIteration containing a yielded value
+                        value = yield e.value
+    """)
+
+    try:
+        return evaluate_wrapper(WRAPPER, wrapped, trace)
+    except:
+        return wrapped
+
+
 def async_wrapper(wrapped):
     if is_coroutine_callable(wrapped):
         return coroutine_wrapper
+    elif is_async_generator_function(wrapped):
+        return async_generator_wrapper
     elif is_generator_function(wrapped):
         if is_asyncio_coroutine(wrapped):
             return awaitable_generator_wrapper

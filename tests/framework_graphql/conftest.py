9f6b640236f6dab22a6b5c48a12bcafefd749324
Hannah Stepanek <hstepanek@newrelic.com>
2023-08-02 11:07:29 -0700

    Fixup: merge

--- tests/framework_graphql/conftest.py
+++ tests/framework_graphql/conftest.py
@@ -14,7 +14,6 @@
 
 import pytest
 from testing_support.fixtures import (  # noqa: F401; pylint: disable=W0611
-    code_coverage_fixture,
     collector_agent_registration_fixture,
     collector_available_fixture,
 )

a1e773207b0cd18589487e39ae4ff1023adebd46
Tim Pansino <timpansino@gmail.com>
2023-10-30 13:03:28 -0700

    Drop all openai refs

--- newrelic/config.py
+++ newrelic/config.py
@@ -2038,26 +2038,6 @@ def _process_trace_cache_import_hooks():
 
 def _process_module_builtin_defaults():
     _process_module_definition(
-        "openai.api_resources.chat_completion",
-        "newrelic.hooks.mlmodel_openai",
-        "instrument_openai_api_resources_chat_completion",
-    )
-    _process_module_definition(
-        "openai.util",
-        "newrelic.hooks.mlmodel_openai",
-        "instrument_openai_util",
-    )
-    # _process_module_definition(
-    #     "openai.api_resources.chat_completion",
-    #     "newrelic.hooks.mlmodel_openai",
-    #     "instrument_openai_api_resources_chat_completion",
-    # )
-    # _process_module_definition(
-    #     "openai.util",
-    #     "newrelic.hooks.mlmodel_openai",
-    #     "instrument_openai_util",
-    # )
-    _process_module_definition(
         "asyncio.base_events",
         "newrelic.hooks.coroutines_asyncio",
         "instrument_asyncio_base_events",

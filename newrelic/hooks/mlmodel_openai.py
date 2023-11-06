e4076571e11470f4ff5de7dbcbc90efaf8d562d5
Hannah Stepanek <hstepanek@newrelic.com>
2023-11-06 15:00:12 -0800

    Add openai feedback support (#942)
    * Add get_ai_message_ids & message id capturing
    
    * Add tests
    
    * Remove generator
    
    * Add tests for conversation id unset
    
    * Add error code to mocked responses
    
    * Remove bedrock tests
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Uma Annamalai <uannamalai@newrelic.com>

--- newrelic/hooks/mlmodel_openai.py
+++ newrelic/hooks/mlmodel_openai.py
@@ -216,7 +216,7 @@ def wrap_chat_completion_create(wrapped, instance, args, kwargs):
     if choices:
         message_list.extend([choices[0].message])
 
-    create_chat_completion_message_event(
+    message_ids = create_chat_completion_message_event(
         transaction,
         settings.app_name,
         message_list,
@@ -228,6 +228,11 @@ def wrap_chat_completion_create(wrapped, instance, args, kwargs):
         request_id,
         conversation_id,
     )
+
+    # Cache message ids on transaction for retrieval after open ai call completion.
+    if not hasattr(transaction, "_nr_message_ids"):
+        transaction._nr_message_ids = {}
+    transaction._nr_message_ids[response_id] = message_ids
 
     return response
 
@@ -260,9 +265,12 @@ def create_chat_completion_message_event(
     request_id,
     conversation_id,
 ):
+    message_ids = []
     for index, message in enumerate(message_list):
+        message_id = "%s-%s" % (response_id, index)
+        message_ids.append(message_id)
         chat_completion_message_dict = {
-            "id": "%s-%s" % (response_id, index),
+            "id": message_id,
             "appName": app_name,
             "conversation_id": conversation_id,
             "request_id": request_id,
@@ -277,6 +285,7 @@ def create_chat_completion_message_event(
             "vendor": "openAI",
         }
         transaction.record_ml_event("LlmChatCompletionMessage", chat_completion_message_dict)
+    return (conversation_id, request_id, message_ids)
 
 
 async def wrap_embedding_acreate(wrapped, instance, args, kwargs):
@@ -456,7 +465,7 @@ async def wrap_chat_completion_acreate(wrapped, instance, args, kwargs):
     if choices:
         message_list.extend([choices[0].message])
 
-    create_chat_completion_message_event(
+    message_ids = create_chat_completion_message_event(
         transaction,
         settings.app_name,
         message_list,
@@ -468,6 +477,11 @@ async def wrap_chat_completion_acreate(wrapped, instance, args, kwargs):
         request_id,
         conversation_id,
     )
+
+    # Cache message ids on transaction for retrieval after open ai call completion.
+    if not hasattr(transaction, "_nr_message_ids"):
+        transaction._nr_message_ids = {}
+    transaction._nr_message_ids[response_id] = message_ids
 
     return response
 

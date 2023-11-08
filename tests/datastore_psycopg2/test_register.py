46c47d5382536061f643d78d0cd76f5ec7148b98
Hannah Stepanek <hstepanek@newrelic.com>
2023-11-07 17:18:25 -0800

    Add new register_type test

--- tests/datastore_psycopg2/test_register.py
+++ tests/datastore_psycopg2/test_register.py
@@ -13,59 +13,87 @@
 # limitations under the License.
 
 import os
+
 import psycopg2
 import psycopg2.extras
-
-from testing_support.validators.validate_transaction_errors import validate_transaction_errors
-from testing_support.validators.validate_transaction_metrics import validate_transaction_metrics
+from testing_support.validators.validate_transaction_errors import (
+    validate_transaction_errors,
+)
+from testing_support.validators.validate_transaction_metrics import (
+    validate_transaction_metrics,
+)
 from utils import DB_SETTINGS
 
 from newrelic.api.background_task import background_task
 
 
-@validate_transaction_metrics('test_register:test_register_json',
-        background_task=True)
+@validate_transaction_metrics("test_register:test_register_json", background_task=True)
 @validate_transaction_errors(errors=[])
 @background_task()
 def test_register_json():
     with psycopg2.connect(
-            database=DB_SETTINGS['name'], user=DB_SETTINGS['user'],
-            password=DB_SETTINGS['password'], host=DB_SETTINGS['host'],
-            port=DB_SETTINGS['port']) as connection:
+        database=DB_SETTINGS["name"],
+        user=DB_SETTINGS["user"],
+        password=DB_SETTINGS["password"],
+        host=DB_SETTINGS["host"],
+        port=DB_SETTINGS["port"],
+    ) as connection:
 
         cursor = connection.cursor()
 
         psycopg2.extras.register_json(connection, loads=lambda x: x)
         psycopg2.extras.register_json(cursor, loads=lambda x: x)
 
-@validate_transaction_metrics('test_register:test_register_range',
-        background_task=True)
+
+@validate_transaction_metrics("test_register:test_register_range", background_task=True)
 @validate_transaction_errors(errors=[])
 @background_task()
 def test_register_range():
     with psycopg2.connect(
-            database=DB_SETTINGS['name'], user=DB_SETTINGS['user'],
-            password=DB_SETTINGS['password'], host=DB_SETTINGS['host'],
-            port=DB_SETTINGS['port']) as connection:
+        database=DB_SETTINGS["name"],
+        user=DB_SETTINGS["user"],
+        password=DB_SETTINGS["password"],
+        host=DB_SETTINGS["host"],
+        port=DB_SETTINGS["port"],
+    ) as connection:
 
         type_name = "floatrange_" + str(os.getpid())
 
-        create_sql = ('CREATE TYPE %s AS RANGE (' % type_name +
-                      'subtype = float8,'
-                      'subtype_diff = float8mi)')
+        create_sql = "CREATE TYPE %s AS RANGE (" % type_name + "subtype = float8," "subtype_diff = float8mi)"
 
         cursor = connection.cursor()
 
         cursor.execute("DROP TYPE if exists %s" % type_name)
         cursor.execute(create_sql)
 
-        psycopg2.extras.register_range(type_name,
-                psycopg2.extras.NumericRange, connection)
+        psycopg2.extras.register_range(type_name, psycopg2.extras.NumericRange, connection)
 
         cursor.execute("DROP TYPE if exists %s" % type_name)
         cursor.execute(create_sql)
 
-        psycopg2.extras.register_range(type_name,
-                psycopg2.extras.NumericRange, cursor)
+        psycopg2.extras.register_range(type_name, psycopg2.extras.NumericRange, cursor)
 
         cursor.execute("DROP TYPE if exists %s" % type_name)
+
+
+@validate_transaction_metrics("test_register:test_register_type", background_task=True)
+@validate_transaction_errors(errors=[])
+@background_task()
+def test_register_type():
+    with psycopg2.connect(
+        database=DB_SETTINGS["name"],
+        user=DB_SETTINGS["user"],
+        password=DB_SETTINGS["password"],
+        host=DB_SETTINGS["host"],
+        port=DB_SETTINGS["port"],
+    ) as connection:
+
+        cursor = connection.cursor()
+
+        numeric_type = psycopg2.extensions.new_type(
+            (1700,), "DEC2FLOAT", lambda v, _: float(v) if v is not None else None
+        )
+        psycopg2.extensions.register_type(numeric_type, cursor)
+
+        numeric_array_type = psycopg2.extensions.new_array_type((1231,), "DEC2FLOATARRAY", numeric_type)
+        psycopg2.extensions.register_type(numeric_array_type, cursor)

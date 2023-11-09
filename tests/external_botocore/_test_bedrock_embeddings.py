0882ba4fc2ab25a456fd45dc741f1c6cc349a228
Timothy Pansino <11214426+TimPansino@users.noreply.github.com>
2023-11-09 13:45:31 -0800

    Bedrock Error Tracing (#966)
    * Cache Package Version Lookups (#946)
    
    * Cache _get_package_version
    
    * Add Python 2.7 support to get_package_version caching
    
    * [Mega-Linter] Apply linters fixes
    
    * Bump tests
    
    ---------
    
    Co-authored-by: SlavaSkvortsov <29122694+SlavaSkvortsov@users.noreply.github.com>
    Co-authored-by: TimPansino <TimPansino@users.noreply.github.com>
    
    * Fix Redis Generator Methods (#947)
    
    * Fix scan_iter for redis
    
    * Replace generator methods
    
    * Update instance info instrumentation
    
    * Remove mistake from uninstrumented methods
    
    * Add skip condition to asyncio generator tests
    
    * Add skip condition to asyncio generator tests
    
    ---------
    
    Co-authored-by: Lalleh Rafeei <lrafeei@newrelic.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    
    * Automatic RPM System Updates (#948)
    
    * Checkout old action
    
    * Adding RPM action
    
    * Add dry run
    
    * Incorporating action into workflow
    
    * Wire secret into custom action
    
    * Enable action
    
    * Correct action name
    
    * Fix syntax
    
    * Fix quoting issues
    
    * Drop pre-verification. Does not work on python
    
    * Fix merge artifact
    
    * Drop python 3.7 tests for Hypercorn (#954)
    
    * Fix pyenv installation for devcontainer (#936)
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    
    * Remove duplicate kafka import hook (#956)
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    
    * Initial bedrock error tracing commit
    
    * Handle 0.32.0.post1 version in tests (#963)
    
    * Add status code to mock bedrock server
    
    * Updating error response recording logic
    
    * Work on bedrock errror tracing
    
    * Chat completion error tracing
    
    * Adding embedding error tracing
    
    * Delete comment
    
    * Update moto
    
    ---------
    
    Co-authored-by: SlavaSkvortsov <29122694+SlavaSkvortsov@users.noreply.github.com>
    Co-authored-by: TimPansino <TimPansino@users.noreply.github.com>
    Co-authored-by: Lalleh Rafeei <lrafeei@newrelic.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Lalleh Rafeei <84813886+lrafeei@users.noreply.github.com>
    Co-authored-by: Hannah Stepanek <hstepanek@newrelic.com>

--- tests/external_botocore/_test_bedrock_embeddings.py
+++ tests/external_botocore/_test_bedrock_embeddings.py
@@ -18,7 +18,7 @@ embedding_expected_events = {
                 "duration": None,  # Response time varies each test run
                 "response.model": "amazon.titan-embed-text-v1",
                 "request.model": "amazon.titan-embed-text-v1",
-                "request_id": "75f1d3fe-6cde-4cf5-bdaf-7101f746ccfe",
+                "request_id": "11233989-07e8-4ecb-9ba6-79601ba6d8cc",
                 "response.usage.total_tokens": 6,
                 "response.usage.prompt_tokens": 6,
                 "vendor": "bedrock",
@@ -40,7 +40,7 @@ embedding_expected_events = {
                 "duration": None,  # Response time varies each test run
                 "response.model": "amazon.titan-embed-g1-text-02",
                 "request.model": "amazon.titan-embed-g1-text-02",
-                "request_id": "f7e78265-6b7c-4b3a-b750-0c1d00347258",
+                "request_id": "b10ac895-eae3-4f07-b926-10b2866c55ed",
                 "response.usage.total_tokens": 6,
                 "response.usage.prompt_tokens": 6,
                 "vendor": "bedrock",
@@ -48,4 +48,27 @@ embedding_expected_events = {
             },
         ),
     ]
+}
+
+embedding_expected_client_errors = {
+    "amazon.titan-embed-text-v1": {
+        "request_id": "aece6ad7-e2ff-443b-a953-ba7d385fd0cc",
+        "api_key_last_four_digits": "-KEY",
+        "request.model": "amazon.titan-embed-text-v1",
+        "vendor": "Bedrock",
+        "ingest_source": "Python",
+        "http.statusCode": 403,
+        "error.message": "The security token included in the request is invalid.",
+        "error.code": "UnrecognizedClientException",
+    },
+    "amazon.titan-embed-g1-text-02": {
+        "request_id": "73328313-506e-4da8-af0f-51017fa6ca3f",
+        "api_key_last_four_digits": "-KEY",
+        "request.model": "amazon.titan-embed-g1-text-02",
+        "vendor": "Bedrock",
+        "ingest_source": "Python",
+        "http.statusCode": 403,
+        "error.message": "The security token included in the request is invalid.",
+        "error.code": "UnrecognizedClientException",
+    },
 }

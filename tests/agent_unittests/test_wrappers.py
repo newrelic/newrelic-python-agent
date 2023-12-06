f12688ff89a09f0e8c8473484dae48ce293a52a7
Tim Pansino <timpansino@gmail.com>
2023-12-06 11:46:26 -0800

    Add descriptions to assertions

--- tests/agent_unittests/test_wrappers.py
+++ tests/agent_unittests/test_wrappers.py
@@ -26,17 +26,20 @@ def test_function_wrapper_attributes():
         return True
 
     exercise._nr_attr = 1
-    assert exercise._nr_attr == 1
+    assert exercise._nr_attr == 1, "_nr_ attributes should be stored on wrapper object and retrievable."
+
     exercise._self_attr = 2
-    assert exercise._self_attr == 2
-    assert exercise._nr_attr == 2
+    assert exercise._self_attr == 2, "_self_ attributes should be stored on wrapper object and retrievable."
+
+    assert exercise._nr_attr == 2, "_nr_ attributes share a namespace with _self_ attributes and should be overwritten."
+
     exercise._other_attr = 3
-    assert exercise._other_attr == 3
+    assert exercise._other_attr == 3, "All other attributes should be stored on wrapped object and retrievable."
 
     vars_ = vars(exercise)
-    assert "_nr_attr" not in vars_
-    assert "_self_attr" not in vars_
-    assert "_other_attr" in vars_
+    assert "_nr_attr" not in vars_, "_nr_ attributes should NOT appear in __dict__."
+    assert "_self_attr" not in vars_, "_self_ attributes should NOT appear in __dict__."
+    assert "_other_attr" in vars_, "Other types of attributes SHOULD appear in __dict__."
 
     assert exercise()
 

fd741e90bbe68fd702aba642206a036c07ca1337
Lalleh Rafeei <lrafeei@newrelic.com>
2023-12-20 13:27:28 -0800

    Add asimilarity_search support

--- tests/mlmodel_langchain/test_vectorstore.py
+++ tests/mlmodel_langchain/test_vectorstore.py
@@ -75,25 +75,39 @@ _test_vectorstore_modules_instrumented_ignored_classes = set(
 )
 
 
+# Test to check if all classes containing "similarity_search"
+# method are instrumented.  Prints out anything that is not
+# for the case of added vectorstores
 def test_vectorstore_modules_instrumented():
     from langchain_community import vectorstores
 
     vector_store_classes = tuple(vectorstores.__all__)
-    uninstrumented_classes = []
+    uninstrumented_sync_classes = []
+    uninstrumented_async_classes = []
     for class_name in vector_store_classes:
         class_ = getattr(vectorstores, class_name)
         if (
             not hasattr(class_, "similarity_search")
             or class_name in _test_vectorstore_modules_instrumented_ignored_classes
         ):
+            # If "similarity_search" is found, "asimilarity_search" will
+            # also be found, so separate logic is not necessary to check this.
             continue
 
         if not hasattr(getattr(class_, "similarity_search"), "__wrapped__"):
-            uninstrumented_classes.append(class_name)
+            uninstrumented_sync_classes.append(class_name)
+        if not hasattr(getattr(class_, "asimilarity_search"), "__wrapped__"):
+            uninstrumented_async_classes.append(class_name)
 
-    assert not uninstrumented_classes, "Uninstrumented classes found: %s" % str(uninstrumented_classes)
+    assert not uninstrumented_sync_classes, "Uninstrumented sync classes found: %s" % str(uninstrumented_sync_classes)
+    assert not uninstrumented_async_classes, "Uninstrumented async classes found: %s" % str(
+        uninstrumented_async_classes
+    )
 
 
+# Sync tests
+
+# Checks metrics for FAISS "similarity_search" instrumentation
 @reset_core_stats_engine()
 @validate_custom_events(vectorstore_recorded_events)
 # Two OpenAI LlmEmbedded, two LangChain LlmVectorSearch
@@ -106,27 +120,76 @@ def test_vectorstore_modules_instrumented():
     background_task=True,
 )
 @background_task()
-def test_pdf_pagesplitter_vectorstore_in_txn(set_trace_info, embeding_openai_client):
+def test_pdf_pagesplitter_vectorstore_in_txn(set_trace_info, embedding_openai_client):
     set_trace_info()
 
     script_dir = os.path.dirname(__file__)
     loader = PyPDFLoader(os.path.join(script_dir, "hello.pdf"))
     docs = loader.load()
 
-    faiss_index = FAISS.from_documents(docs, embeding_openai_client)
+    faiss_index = FAISS.from_documents(docs, embedding_openai_client)
     docs = faiss_index.similarity_search("Complete this sentence: Hello", k=1)
     assert "Hello world" in docs[0].page_content
 
 
 @reset_core_stats_engine()
 @validate_custom_event_count(count=0)
-def test_pdf_pagesplitter_vectorstore_outside_txn(set_trace_info, embeding_openai_client):
+def test_pdf_pagesplitter_vectorstore_outside_txn(set_trace_info, embedding_openai_client):
     set_trace_info()
 
     script_dir = os.path.dirname(__file__)
     loader = PyPDFLoader(os.path.join(script_dir, "hello.pdf"))
     docs = loader.load()
 
-    faiss_index = FAISS.from_documents(docs, embeding_openai_client)
+    faiss_index = FAISS.from_documents(docs, embedding_openai_client)
     docs = faiss_index.similarity_search("Complete this sentence: Hello", k=1)
+    assert "Hello world" in docs[0].page_content
+
+
+# Async tests
+
+
+@reset_core_stats_engine()
+@validate_custom_events(vectorstore_recorded_events)
+# Two OpenAI LlmEmbedded, two LangChain LlmVectorSearch
+@validate_custom_event_count(count=4)
+@validate_transaction_metrics(
+    name="test_vectorstore:test_async_pdf_pagesplitter_vectorstore_in_txn",
+    custom_metrics=[
+        ("Python/ML/Langchain/%s" % LANGCHAIN_VERSION, 1),
+    ],
+    background_task=True,
+)
+@background_task()
+def test_async_pdf_pagesplitter_vectorstore_in_txn(loop, set_trace_info, embedding_openai_client):
+    async def _test():
+        set_trace_info()
+
+        script_dir = os.path.dirname(__file__)
+        loader = PyPDFLoader(os.path.join(script_dir, "hello.pdf"))
+        docs = loader.load()
+
+        faiss_index = await FAISS.afrom_documents(docs, embedding_openai_client)
+        docs = await faiss_index.asimilarity_search("Complete this sentence: Hello", k=1)
+        return docs
+
+    docs = loop.run_until_complete(_test())
+    assert "Hello world" in docs[0].page_content
+
+
+@reset_core_stats_engine()
+@validate_custom_event_count(count=0)
+def test_async_pdf_pagesplitter_vectorstore_outside_txn(loop, set_trace_info, embedding_openai_client):
+    async def _test():
+        set_trace_info()
+
+        script_dir = os.path.dirname(__file__)
+        loader = PyPDFLoader(os.path.join(script_dir, "hello.pdf"))
+        docs = loader.load()
+
+        faiss_index = await FAISS.afrom_documents(docs, embedding_openai_client)
+        docs = await faiss_index.asimilarity_search("Complete this sentence: Hello", k=1)
+        return docs
+
+    docs = loop.run_until_complete(_test())
     assert "Hello world" in docs[0].page_content

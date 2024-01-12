d114ebe0d1a1697c15be50ee2a1d3a21a028f16e
Timothy Pansino <11214426+TimPansino@users.noreply.github.com>
2024-01-11 16:11:18 -0800

    Update web_transaction.py

--- newrelic/api/web_transaction.py
+++ newrelic/api/web_transaction.py
@@ -161,13 +161,6 @@ def _encode_nonce(nonce):
         return ' nonce="%s"' % ensure_str(nonce)  # Extra space intentional
 
 
-def _encode_nonce(nonce):
-    if not nonce:
-        return ""
-    else:
-        return ' nonce="%s"' % ensure_str(nonce)  # Extra space intentional
-
-
 class WebTransaction(Transaction):
     unicode_error_reported = False
     QUEUE_TIME_HEADERS = ("x-request-start", "x-queue-start")

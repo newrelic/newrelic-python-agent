410f3afb4e5b412963f1d8b5eaaeffc00072b0d1
Lalleh Rafeei <lrafeei@newrelic.com>
2024-01-30 12:29:31 -0800

    Update botocore tests. #1051

--- tests/external_botocore/test_botocore_dynamodb.py
+++ tests/external_botocore/test_botocore_dynamodb.py
@@ -12,11 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
 import uuid
 
 import botocore.session
-import moto
+from moto import mock_aws
 from testing_support.fixtures import dt_enabled
 from testing_support.validators.validate_span_events import validate_span_events
 from testing_support.validators.validate_transaction_metrics import (
@@ -30,13 +29,6 @@ from newrelic.api.background_task import background_task
 from newrelic.common.package_version_utils import get_package_version_tuple
 
 MOTO_VERSION = get_package_version_tuple("moto")
-
-# patch earlier versions of moto to support py37
-if sys.version_info >= (3, 7) and MOTO_VERSION <= (1, 3, 1):
-    import re
-
-    moto.packages.responses.responses.re._pattern_type = re.Pattern
-
 
 AWS_ACCESS_KEY_ID = "AAAAAAAAAAAACCESSKEY"
 AWS_SECRET_ACCESS_KEY = "AAAAAASECRETKEY"  # nosec (This is fine for testing purposes)
@@ -81,7 +73,7 @@ _dynamodb_rollup_metrics = [
     background_task=True,
 )
 @background_task()
-@moto.mock_dynamodb
+@mock_aws
 def test_dynamodb():
     session = botocore.session.get_session()
     client = session.create_client(
--- tests/external_botocore/test_botocore_s3.py
+++ tests/external_botocore/test_botocore_s3.py
@@ -12,12 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
 import uuid
 
 import botocore
 import botocore.session
-import moto
+from moto import mock_aws
 from testing_support.fixtures import dt_enabled
 from testing_support.validators.validate_span_events import validate_span_events
 from testing_support.validators.validate_transaction_metrics import (
@@ -30,12 +29,6 @@ from newrelic.common.package_version_utils import get_package_version_tuple
 MOTO_VERSION = MOTO_VERSION = get_package_version_tuple("moto")
 BOTOCORE_VERSION = get_package_version_tuple("botocore")
 
-
-# patch earlier versions of moto to support py37
-if sys.version_info >= (3, 7) and MOTO_VERSION <= (1, 3, 1):
-    import re
-
-    moto.packages.responses.responses.re._pattern_type = re.Pattern
 
 AWS_ACCESS_KEY_ID = "AAAAAAAAAAAACCESSKEY"
 AWS_SECRET_ACCESS_KEY = "AAAAAASECRETKEY"  # nosec
@@ -84,7 +77,7 @@ _s3_rollup_metrics = [
     background_task=True,
 )
 @background_task()
-@moto.mock_s3
+@mock_aws
 def test_s3():
     session = botocore.session.get_session()
     client = session.create_client(

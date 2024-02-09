179e1cc620b7efb4b7582ea53781f28d3c0f246e
Uma Annamalai <uannamalai@newrelic.com>
2024-02-09 15:40:41 -0800

    Fixup tests.

--- tests/mlmodel_openai/test_chat_completion_v1.py
+++ tests/mlmodel_openai/test_chat_completion_v1.py
@@ -140,7 +140,7 @@ chat_completion_recorded_events = [
 @validate_transaction_metrics(
     name="test_chat_completion_v1:test_openai_chat_completion_sync_in_txn_with_convo_id",
     custom_metrics=[
-        ("Python/ML/OpenAI/%s" % openai.__version__, 1),
+        ("Supportability/Python/ML/OpenAI/%s" % openai.__version__, 1),
     ],
     background_task=True,
 )
@@ -286,7 +286,7 @@ def test_openai_chat_completion_sync_outside_txn(sync_openai_client):
 @validate_transaction_metrics(
     name="test_chat_completion_v1:test_openai_chat_completion_sync_custom_events_insights_disabled",
     custom_metrics=[
-        ("Python/ML/OpenAI/%s" % openai.__version__, 1),
+        ("Supportability/Python/ML/OpenAI/%s" % openai.__version__, 1),
     ],
     background_task=True,
 )
@@ -330,7 +330,7 @@ def test_openai_chat_completion_async_conversation_id_unset(loop, set_trace_info
 @validate_transaction_metrics(
     name="test_chat_completion_v1:test_openai_chat_completion_async_conversation_id_set",
     custom_metrics=[
-        ("Python/ML/OpenAI/%s" % openai.__version__, 1),
+        ("Supportability/Python/ML/OpenAI/%s" % openai.__version__, 1),
     ],
     background_task=True,
 )
@@ -363,7 +363,7 @@ def test_openai_chat_completion_async_outside_transaction(loop, async_openai_cli
 @validate_transaction_metrics(
     name="test_chat_completion_v1:test_openai_chat_completion_async_disabled_custom_event_settings",
     custom_metrics=[
-        ("Python/ML/OpenAI/%s" % openai.__version__, 1),
+        ("Supportability/Python/ML/OpenAI/%s" % openai.__version__, 1),
     ],
     background_task=True,
 )

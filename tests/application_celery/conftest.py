a28172c25061ccf72e81c1a9a904ad3ae48ca210
Hannah Stepanek <hstepanek@newrelic.com>
2024-04-15 09:45:55 -0700

    Remove unused pytest

--- tests/application_celery/conftest.py
+++ tests/application_celery/conftest.py
@@ -11,8 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-import pytest
 from testing_support.fixtures import (  # noqa: F401; pylint: disable=W0611
     collector_agent_registration_fixture,
     collector_available_fixture,

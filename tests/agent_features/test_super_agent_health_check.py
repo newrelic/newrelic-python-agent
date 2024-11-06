1b5d4697c61fa6f12e9724117f12e086aea10d93
Uma Annamalai <uannamalai@newrelic.com>
2024-11-06 12:13:48 -0800

    Checkpoint.

--- tests/agent_features/test_super_agent_health_check.py
+++ tests/agent_features/test_super_agent_health_check.py
@@ -12,13 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import os
+import time
 import pytest
 import threading
 
-from testing_support.fixtures import override_application_settings
+from testing_support.fixtures import reset_core_stats_engine
+from testing_support.fixtures import override_generic_settings
+from newrelic.core.config import finalize_application_settings
+from agent_unittests.test_agent_protocol import HttpClientRecorder
 from newrelic.core.super_agent_health import is_valid_file_delivery_location, super_agent_health_instance
 from newrelic.config import initialize
+from newrelic.core.agent_protocol import AgentProtocol
 from newrelic.core.application import Application
+from newrelic.core.config import global_settings
+from newrelic.network.exceptions import DiscardDataForRequest
 
 _disable_agent_settings = {
     "monitor_mode": False,
@@ -124,7 +131,43 @@ def test_health_check_running_threads(monkeypatch, tmp_path):
     running_threads = threading.enumerate()
 
     assert len(running_threads) == 2
-    assert running_threads[1].name == "NR-Control-Health-Main-Thread"
+    assert running_threads[1].name == "APM-Control-Health-Main-Thread"
+
+
+def test_proxy_error_status(monkeypatch, tmp_path):
+    # Setup expected env vars to run super agent health check
+    monkeypatch.setenv("NEW_RELIC_SUPERAGENT_FLEET_ID", "1234")
+    file_path = tmp_path.as_uri()
+    monkeypatch.setenv("NEW_RELIC_SUPERAGENT_HEALTH_DELIVERY_LOCATION", file_path)
+
+    initialize()
+
+    # Mock a 407 error to generate a proxy error health status
+    HttpClientRecorder.STATUS_CODE = 407
+    settings = finalize_application_settings(
+        {
+            "license_key": "123LICENSEKEY",
+        }
+    )
+    protocol = AgentProtocol(settings, client_cls=HttpClientRecorder)
+
+    with pytest.raises(DiscardDataForRequest):
+        protocol.send("analytic_event_data")
+
+    # Give time for the scheduler to kick in and write to the health file
+    time.sleep(5)
+
+    # Grab the file we just wrote to and read its contents
+    health_files = os.listdir(tmp_path)
+    path_to_written_file = f"{tmp_path}/{health_files[0]}"
+    with open(path_to_written_file, 'r') as f:
+        contents = f.readlines()
+
+    # Assert on contents of health file
+    assert len(contents) == 5
+    assert contents[0] == "healthy: False\n"
+    assert contents[1] == "status: HTTP Proxy configuration error; response code 407\n"
+    assert contents[4] == "last_error: NR-APM-007\n"
 
 
 def test_multiple_activations_running_threads(monkeypatch, tmp_path):
@@ -142,38 +185,7 @@ def test_multiple_activations_running_threads(monkeypatch, tmp_path):
 
     running_threads = threading.enumerate()
 
-    #
     assert len(running_threads) == 6
-    assert running_threads[1].name == "NR-Control-Health-Main-Thread"
-    assert running_threads[2].name == "NR-Control-Health-Session-Thread"
-    assert running_threads[4].name == "NR-Control-Health-Session-Thread"
-
-
-
-def test_missing_license_key(monkeypatch, tmp_path):
-    # Setup expected env vars to run super agent health check
-    monkeypatch.setenv("NEW_RELIC_SUPERAGENT_FLEET_ID", "1234")
-    file_path = tmp_path.as_uri()
-    monkeypatch.setenv("NEW_RELIC_SUPERAGENT_HEALTH_DELIVERY_LOCATION", file_path)
-    #monkeypatch.setenv("NEW_RELIC_LICENSE_KEY", "")
-
-    #initialize()
-    # Write to health YAML file
-    super_agent_instance = super_agent_health_instance()
-    super_agent_instance.write_to_health_file()
-
-    # Grab the file we just wrote to and read its contents
-    health_files = os.listdir(tmp_path)
-    path_to_written_file = f"{tmp_path}/{health_files[0]}"
-    with open(path_to_written_file, 'r') as f:
-        contents = f.readlines()
-
-    # Assert on contents of health file
-    assert len(contents) == 5
-    assert contents[0] == "healthy: False\n"
-    assert contents[1] == "status: Invalid license key (HTTP status code 401)\n"
-    assert contents[2] == "start_time_unix_nano: 1234567890\n"
-    assert contents[3].startswith("status_time_unix_nano:") is True
-    assert contents[4] == "last_error: NR-APM-001\n"
-
-
+    assert running_threads[1].name == "APM-Control-Health-Main-Thread"
+    assert running_threads[2].name == "APM-Control-Health-Session-Thread"
+    assert running_threads[4].name == "APM-Control-Health-Session-Thread"

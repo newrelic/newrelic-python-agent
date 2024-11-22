d6347c90137e49d2bf5c430bb182a9868cc0f6e7
Hannah Stepanek <hstepanek@newrelic.com>
2024-11-22 15:24:50 -0800

    Attach arn to api_params & request_dict
    These are regenerated for each request as opposed to the events obj that persists across
    multiple requests. Unfortunately, we have to pass the arn from api_params to the
    request_dict because:
    1. api_params has validation key checks that exclude unknown params upon generating the
       request_dict
    2. the arn is not available after api_params are emitted because emit drops the param
       that contains the arn
    
    So...we capture the arn before it is lost when the api_params are emitted and pop it off
    before generating the request dict and put it onto the output request dict to be popped
    off later by the make request method and added to the trace to link the entity.

--- newrelic/hooks/external_botocore.py
+++ newrelic/hooks/external_botocore.py
@@ -30,7 +30,6 @@ from newrelic.api.time_trace import current_trace, get_trace_linking_metadata
 from newrelic.api.transaction import current_transaction
 from newrelic.common.async_wrapper import async_wrapper as get_async_wrapper
 from newrelic.common.object_wrapper import (
-    FunctionWrapper,
     ObjectProxy,
     function_wrapper,
     wrap_function_wrapper,
@@ -891,24 +890,39 @@ def sqs_message_trace(
     return _nr_sqs_message_trace_wrapper_
 
 
-def wrap_lambda_invoke(wrapped):
-    def _nr_wrap_lambda_invoke(wrapped, instance, args, kwargs):
-        transaction = current_transaction()
-        if not transaction:
-            return wrapped(*args, **kwargs)
-
-        bound_args = bind_args(wrapped, args, kwargs)
-        arn = bound_args["kwargs"].get("FunctionName")
-        if arn and hasattr(instance, "meta") and hasattr(instance.meta, "events"):
-            instance.meta.events._nr_lambda_arn = arn
-
+def wrap_emit_api_params(wrapped, instance, args, kwargs):
+    transaction = current_transaction()
+    if not transaction:
         return wrapped(*args, **kwargs)
 
-    return FunctionWrapper(wrapped, _nr_wrap_lambda_invoke)
+    bound_args = bind_args(wrapped, args, kwargs)
+
+    api_params = wrapped(*args, **kwargs)
+
+    arn = bound_args.get("api_params").get("FunctionName")
+    if arn and hasattr(arn, "startswith") and arn.startswith("arn:"):
+        api_params["_nr_arn"] = arn
+
+    return api_params
+
+
+def wrap_convert_to_request_dict(wrapped, instance, args, kwargs):
+    transaction = current_transaction()
+    if not transaction:
+        return wrapped(*args, **kwargs)
+
+    bound_args = bind_args(wrapped, args, kwargs)
+    arn = bound_args.get("api_params").pop("_nr_arn", None)
+
+    request_dict = wrapped(*args, **kwargs)
+
+    if arn:
+        request_dict["_nr_arn"] = arn
+
+    return request_dict
 
 
 CUSTOM_TRACE_POINTS = {
-    ("lambda", "invoke"): wrap_lambda_invoke,
     ("sns", "publish"): message_trace("SNS", "Produce", "Topic", extract(("TopicArn", "TargetArn"), "PhoneNumber")),
     ("dynamodb", "put_item"): datastore_trace("DynamoDB", extract("TableName"), "put_item"),
     ("dynamodb", "get_item"): datastore_trace("DynamoDB", extract("TableName"), "get_item"),
@@ -970,7 +984,8 @@ def _nr_endpoint_make_request_(wrapped, instance, args, kwargs):
     with ExternalTrace(library="botocore", url=url, method=method, source=wrapped) as trace:
         try:
             trace._add_agent_attribute("aws.operation", operation_model.name)
-            lambda_arn = getattr(instance._event_emitter, "_nr_lambda_arn", None)
+            bound_args = bind_args(wrapped, args, kwargs)
+            lambda_arn = bound_args.get("request_dict").pop("_nr_arn", None)
             if lambda_arn:
                 trace._add_agent_attribute("cloud.platform", "aws_lambda")
                 trace._add_agent_attribute("cloud.resource_id", lambda_arn)
@@ -991,5 +1006,9 @@ def instrument_botocore_endpoint(module):
 
 
 def instrument_botocore_client(module):
-    wrap_function_wrapper(module, "ClientCreator._create_api_method", _nr_clientcreator__create_api_method_)
-    wrap_function_wrapper(module, "ClientCreator._create_methods", _nr_clientcreator__create_methods)
+    if hasattr(module, "ClientCreator"):
+        wrap_function_wrapper(module, "ClientCreator._create_api_method", _nr_clientcreator__create_api_method_)
+        wrap_function_wrapper(module, "ClientCreator._create_methods", _nr_clientcreator__create_methods)
+    if hasattr(module, "BaseClient"):
+        wrap_function_wrapper(module, "BaseClient._convert_to_request_dict", wrap_convert_to_request_dict)
+        wrap_function_wrapper(module, "BaseClient._emit_api_params", wrap_emit_api_params)

92a4012f81d1b1104d82ccfd9bde3c2e5b8d5a34
fagnerzulin <fagner.zulin@grupoboticario.com.br>
2025-02-20 11:33:25 -0300

    Adds async Elasticsearch support

---
+++ tests/datastore_elasticsearch/test_async_elasticsearch.py
@@ -0,0 +1,238 @@
+# Copyright 2010 New Relic, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import pytest
+import elasticsearch._async.client as client
+from testing_support.fixtures import override_application_settings
+from testing_support.fixture.event_loop import event_loop as loop  # noqa: F401
+from testing_support.util import instance_hostname
+from testing_support.validators.validate_transaction_errors import validate_transaction_errors
+from testing_support.validators.validate_transaction_metrics import validate_transaction_metrics
+
+from newrelic.api.background_task import background_task
+
+from conftest import ES_VERSION, ES_SETTINGS
+
+
+# Settings
+
+_enable_instance_settings = {"datastore_tracer.instance_reporting.enabled": True}
+_disable_instance_settings = {"datastore_tracer.instance_reporting.enabled": False}
+
+# Metrics
+
+_base_scoped_metrics = [
+    ("Datastore/statement/Elasticsearch/_all/cluster.health", 1),
+    ("Datastore/statement/Elasticsearch/_all/search", 2),
+    ("Datastore/statement/Elasticsearch/address/index", 2),
+    ("Datastore/statement/Elasticsearch/address/search", 1),
+    ("Datastore/statement/Elasticsearch/contacts/index", 3),
+    ("Datastore/statement/Elasticsearch/contacts/indices.refresh", 1),
+    ("Datastore/statement/Elasticsearch/contacts/search", 2),
+    ("Datastore/statement/Elasticsearch/other/search", 2),
+]
+
+_base_rollup_metrics = [
+    ("Datastore/operation/Elasticsearch/cluster.health", 1),
+    ("Datastore/operation/Elasticsearch/index", 5),
+    ("Datastore/operation/Elasticsearch/indices.refresh", 1),
+    ("Datastore/operation/Elasticsearch/search", 7),
+    ("Datastore/statement/Elasticsearch/_all/cluster.health", 1),
+    ("Datastore/statement/Elasticsearch/_all/search", 2),
+    ("Datastore/statement/Elasticsearch/address/index", 2),
+    ("Datastore/statement/Elasticsearch/address/search", 1),
+    ("Datastore/statement/Elasticsearch/contacts/index", 3),
+    ("Datastore/statement/Elasticsearch/contacts/indices.refresh", 1),
+    ("Datastore/statement/Elasticsearch/contacts/search", 2),
+    ("Datastore/statement/Elasticsearch/other/search", 2),
+]
+
+# Version support
+
+
+def is_importable(module_path):
+    try:
+        __import__(module_path)
+        return True
+    except ImportError:
+        return False
+
+
+_all_count = 17
+
+if is_importable("elasticsearch._async.client.cat") or is_importable("elasticsearch._async.client.cat"):
+    _base_scoped_metrics.append(("Datastore/operation/Elasticsearch/cat.health", 1))
+    _base_rollup_metrics.append(("Datastore/operation/Elasticsearch/cat.health", 1))
+    _all_count += 1
+else:
+    _base_scoped_metrics.append(("Datastore/operation/Elasticsearch/cat.health", None))
+    _base_rollup_metrics.append(("Datastore/operation/Elasticsearch/cat.health", None))
+
+if is_importable("elasticsearch._async.client.nodes") or is_importable("elasticsearch._async.client.nodes"):
+    _base_scoped_metrics.append(("Datastore/operation/Elasticsearch/nodes.info", 1))
+    _base_rollup_metrics.append(("Datastore/operation/Elasticsearch/nodes.info", 1))
+    _all_count += 1
+else:
+    _base_scoped_metrics.append(("Datastore/operation/Elasticsearch/nodes.info", None))
+    _base_rollup_metrics.append(("Datastore/operation/Elasticsearch/nodes.info", None))
+
+if hasattr(client, "SnapshotClient") and hasattr(client.SnapshotClient, "status"):
+    _base_scoped_metrics.append(("Datastore/operation/Elasticsearch/snapshot.status", 1))
+    _base_rollup_metrics.append(("Datastore/operation/Elasticsearch/snapshot.status", 1))
+    _all_count += 1
+else:
+    _base_scoped_metrics.append(("Datastore/operation/Elasticsearch/snapshot.status", None))
+    _base_rollup_metrics.append(("Datastore/operation/Elasticsearch/snapshot.status", None))
+
+if hasattr(client.IndicesClient, "status"):
+    _base_scoped_metrics.append(("Datastore/statement/Elasticsearch/_all/indices.status", 1))
+    _base_rollup_metrics.extend(
+        [
+            ("Datastore/operation/Elasticsearch/indices.status", 1),
+            ("Datastore/statement/Elasticsearch/_all/indices.status", 1),
+        ]
+    )
+    _all_count += 1
+else:
+    _base_scoped_metrics.append(("Datastore/operation/Elasticsearch/indices.status", None))
+    _base_rollup_metrics.extend(
+        [
+            ("Datastore/operation/Elasticsearch/indices.status", None),
+            ("Datastore/statement/Elasticsearch/_all/indices.status", None),
+        ]
+    )
+
+_base_rollup_metrics.extend(
+    [
+        ("Datastore/all", _all_count),
+        ("Datastore/allOther", _all_count),
+        ("Datastore/Elasticsearch/all", _all_count),
+        ("Datastore/Elasticsearch/allOther", _all_count),
+    ]
+)
+
+# Instance info
+
+_disable_scoped_metrics = list(_base_scoped_metrics)
+_disable_rollup_metrics = list(_base_rollup_metrics)
+
+_enable_scoped_metrics = list(_base_scoped_metrics)
+_enable_rollup_metrics = list(_base_rollup_metrics)
+
+_host = instance_hostname(ES_SETTINGS["host"])
+_port = ES_SETTINGS["port"]
+
+_instance_metric_name = f"Datastore/instance/Elasticsearch/{_host}/{_port}"
+
+_enable_rollup_metrics.append((_instance_metric_name, _all_count))
+
+_disable_rollup_metrics.append((_instance_metric_name, None))
+
+# Query
+
+
+async def _exercise_es_v7(es):
+    await es.index(
+        index="contacts", doc_type="person", body={"name": "Joe Tester", "age": 25, "title": "QA Engineer"}, id=1
+    )
+    await es.index(
+        index="contacts", doc_type="person", body={"name": "Jessica Coder", "age": 32, "title": "Programmer"}, id=2
+    )
+    await es.index(
+        index="contacts", doc_type="person", body={"name": "Freddy Tester", "age": 29, "title": "Assistant"}, id=3
+    )
+    await es.indices.refresh("contacts")
+    await es.index(
+        index="address", doc_type="employee", body={"name": "Sherlock", "address": "221B Baker Street, London"}, id=1
+    )
+    await es.index(
+        index="address",
+        doc_type="employee",
+        body={"name": "Bilbo", "address": "Bag End, Bagshot row, Hobbiton, Shire"},
+        id=2,
+    )
+    await es.search(index="contacts", q="name:Joe")
+    await es.search(index="contacts", q="name:jessica")
+    await es.search(index="address", q="name:Sherlock")
+    await es.search(index=["contacts", "address"], q="name:Bilbo")
+    await es.search(index="contacts,address", q="name:Bilbo")
+    await es.search(index="*", q="name:Bilbo")
+    await es.search(q="name:Bilbo")
+    await es.cluster.health()
+
+    if hasattr(es, "cat"):
+        await es.cat.health()
+    if hasattr(es, "nodes"):
+        await es.nodes.info()
+    if hasattr(es, "snapshot") and hasattr(es.snapshot, "status"):
+        await es.snapshot.status()
+    if hasattr(es.indices, "status"):
+        await es.indices.status()
+
+
+async def _exercise_es_v8(es):
+    await es.index(index="contacts", body={"name": "Joe Tester", "age": 25, "title": "QA Engineer"}, id=1)
+    await es.index(index="contacts", body={"name": "Jessica Coder", "age": 32, "title": "Programmer"}, id=2)
+    await es.index(index="contacts", body={"name": "Freddy Tester", "age": 29, "title": "Assistant"}, id=3)
+    await es.indices.refresh(index="contacts")
+    await es.index(index="address", body={"name": "Sherlock", "address": "221B Baker Street, London"}, id=1)
+    await es.index(index="address", body={"name": "Bilbo", "address": "Bag End, Bagshot row, Hobbiton, Shire"}, id=2)
+    await es.search(index="contacts", q="name:Joe")
+    await es.search(index="contacts", q="name:jessica")
+    await es.search(index="address", q="name:Sherlock")
+    await es.search(index=["contacts", "address"], q="name:Bilbo")
+    await es.search(index="contacts,address", q="name:Bilbo")
+    await es.search(index="*", q="name:Bilbo")
+    await es.search(q="name:Bilbo")
+    await es.cluster.health()
+
+    if hasattr(es, "cat"):
+        await es.cat.health()
+    if hasattr(es, "nodes"):
+        await es.nodes.info()
+    if hasattr(es, "snapshot") and hasattr(es.snapshot, "status"):
+        await es.snapshot.status()
+    if hasattr(es.indices, "status"):
+        await es.indices.status()
+
+
+_exercise_es = _exercise_es_v7 if ES_VERSION < (8, 0, 0) else _exercise_es_v8
+
+
+# Test
+
+
+@validate_transaction_errors(errors=[])
+@validate_transaction_metrics(
+    "test_async_elasticsearch:test_async_elasticsearch_operation_disabled",
+    scoped_metrics=_disable_scoped_metrics,
+    rollup_metrics=_disable_rollup_metrics,
+    background_task=True,
+)
+@override_application_settings(_disable_instance_settings)
+@background_task()
+def test_async_elasticsearch_operation_disabled(async_client, loop):
+    loop.run_until_complete(_exercise_es(async_client))
+
+
+@validate_transaction_errors(errors=[])
+@validate_transaction_metrics(
+    "test_async_elasticsearch:test_async_elasticsearch_operation_enabled",
+    scoped_metrics=_enable_scoped_metrics,
+    rollup_metrics=_enable_rollup_metrics,
+    background_task=True,
+)
+@override_application_settings(_enable_instance_settings)
+@background_task()
+def test_async_elasticsearch_operation_enabled(async_client, loop):
+    loop.run_until_complete(_exercise_es(async_client))
---
+++ tests/datastore_elasticsearch/test_async_trace_node.py
@@ -0,0 +1,92 @@
+# Copyright 2010 New Relic, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from testing_support.fixtures import override_application_settings, validate_tt_parenting
+from testing_support.fixture.event_loop import event_loop as loop  # noqa: F401
+from testing_support.util import instance_hostname
+from testing_support.validators.validate_tt_collector_json import validate_tt_collector_json
+
+from newrelic.api.background_task import background_task
+
+from conftest import ES_SETTINGS, ES_VERSION
+
+# Settings
+
+_enable_instance_settings = {
+    "datastore_tracer.instance_reporting.enabled": True,
+    "datastore_tracer.database_name_reporting.enabled": True,
+}
+_disable_instance_settings = {
+    "datastore_tracer.instance_reporting.enabled": False,
+    "datastore_tracer.database_name_reporting.enabled": False,
+}
+_instance_only_settings = {
+    "datastore_tracer.instance_reporting.enabled": True,
+    "datastore_tracer.database_name_reporting.enabled": False,
+}
+
+# Expected parameters
+
+_enabled_required = {"host": instance_hostname(ES_SETTINGS["host"]), "port_path_or_id": str(ES_SETTINGS["port"])}
+_enabled_forgone = {"db.instance": "VALUE NOT USED"}
+
+_disabled_required = {}
+_disabled_forgone = {"host": "VALUE NOT USED", "port_path_or_id": "VALUE NOT USED", "db.instance": "VALUE NOT USED"}
+
+_instance_only_required = {"host": instance_hostname(ES_SETTINGS["host"]), "port_path_or_id": str(ES_SETTINGS["port"])}
+_instance_only_forgone = {"db.instance": "VALUE NOT USED"}
+
+_tt_parenting = ("TransactionNode", [("DatastoreNode", [])])
+
+
+# Query
+
+
+async def _exercise_es_v7(es):
+    await es.index(
+        index="contacts", doc_type="person", body={"name": "Joe Tester", "age": 25, "title": "QA Master"}, id=1
+    )
+
+
+async def _exercise_es_v8(es):
+    await es.index(index="contacts", body={"name": "Joe Tester", "age": 25, "title": "QA Master"}, id=1)
+
+
+_exercise_es = _exercise_es_v7 if ES_VERSION < (8, 0, 0) else _exercise_es_v8
+
+# Tests
+
+
+@override_application_settings(_enable_instance_settings)
+@validate_tt_collector_json(datastore_params=_enabled_required, datastore_forgone_params=_enabled_forgone)
+@validate_tt_parenting(_tt_parenting)
+@background_task()
+def test_async_trace_node_datastore_params_enable_instance(async_client, loop):
+    loop.run_until_complete(_exercise_es(async_client))
+
+
+@override_application_settings(_disable_instance_settings)
+@validate_tt_collector_json(datastore_params=_disabled_required, datastore_forgone_params=_disabled_forgone)
+@validate_tt_parenting(_tt_parenting)
+@background_task()
+def test_async_trace_node_datastore_params_disable_instance(async_client, loop):
+    loop.run_until_complete(_exercise_es(async_client))
+
+
+@override_application_settings(_instance_only_settings)
+@validate_tt_collector_json(datastore_params=_instance_only_required, datastore_forgone_params=_instance_only_forgone)
+@validate_tt_parenting(_tt_parenting)
+@background_task()
+def test_async_trace_node_datastore_params_instance_only(async_client, loop):
+    loop.run_until_complete(_exercise_es(async_client))

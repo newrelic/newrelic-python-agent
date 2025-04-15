f0dc5ae9b8b2d89279385383798362b1e2b75054
Lalleh Rafeei <lrafeei@newrelic.com>
2025-04-14 17:35:34 -0700

    debug

--- tests/framework_azure/test_application.py
+++ tests/framework_azure/test_application.py
@@ -48,7 +48,7 @@ import requests
 # @web_transaction(name="test_application:test_ping", group="AzureFunction")
 def test_ping():
     # response = requests.get("http://127.0.0.1:8080/basic?user=Reli")
-    assert requests.get("http://127.0.0.1:7071").status_code == 200
+    # assert requests.get("http://127.0.0.1:7071").status_code == 200
     response = requests.get("http://127.0.0.1:7071/basic?user=Reli")
     assert response.status_code == 200
     assert response.text == "Hello, Reli!"

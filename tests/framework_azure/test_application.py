7eb7392cef8f2f9fc6876ad6ec056c81569c3f41
Lalleh Rafeei <lrafeei@newrelic.com>
2025-04-21 14:25:21 -0700

    Change port

--- tests/framework_azure/test_application.py
+++ tests/framework_azure/test_application.py
@@ -49,7 +49,7 @@ import requests
 def test_ping():
     # response = requests.get("http://127.0.0.1:8080/basic?user=Reli")
     # assert requests.get("http://127.0.0.1:7071").status_code == 200
-    response = requests.get("http://127.0.0.1:7071/basic?user=Reli")
+    response = requests.get("http://127.0.0.1:8080/basic?user=Reli")
     assert response.status_code == 200
     assert response.text == "Hello, Reli!"
     assert response.headers["Content-Type"] == "text/plain; charset=utf-8"

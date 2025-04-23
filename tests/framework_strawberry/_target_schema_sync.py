a3a3d408b90018d549b08532fd4208f3f08e42a1
Tim Pansino <timpansino@gmail.com>
2025-04-22 17:36:55 -0700

    Fix UP007 non-pep604-annotation-union

--- tests/framework_strawberry/_target_schema_sync.py
+++ tests/framework_strawberry/_target_schema_sync.py
@@ -14,7 +14,7 @@
 
 from __future__ import annotations
 
-from typing import List, Optional, Union
+from typing import List, Union
 
 import strawberry
 
@@ -141,7 +141,7 @@ class Query:
     search: list[Item] = field(resolver=resolve_search)
     echo: str = field(resolver=resolve_echo)
     storage: Storage = field(resolver=resolve_storage)
-    error: Optional[str] = field(resolver=resolve_error)
+    error: str | None = field(resolver=resolve_error)
     error_non_null: str = field(resolver=resolve_error)
 
 

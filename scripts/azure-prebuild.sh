b2805f5cecd182d59176e0d06ff97c535f93d369
Lalleh Rafeei <lrafeei@newrelic.com>
2025-04-24 15:23:30 -0700

    Add license header

--- scripts/azure-prebuild.sh
+++ scripts/azure-prebuild.sh
@@ -1,11 +1,23 @@
+# Copyright 2010 New Relic, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 #!/bin/sh
 
-###########################################
-# This script is used to prebuild the New
-# Relic Python agent for Azure Web Apps.
-# It is executed in the Azure Web App 
-# environment before the app is started.
-###########################################
+#######################################################################
+# This script is used to prebuild the New Relic Python agent for Azure Web Apps.
+# It is executed in the Azure Web App environment before the app is started.
+#######################################################################
 
 # Retrieve files to use in startup script:
 curl -L https://raw.githubusercontent.com/newrelic/newrelic-agent-init-container/refs/heads/main/src/python/newrelic_k8s_operator.py > newrelic_k8s_operator.py

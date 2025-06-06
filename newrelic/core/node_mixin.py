c3c3ef84ca69ab61c34155c9fbf5bf16457c0b33
Hannah Stepanek <hstepanek@newrelic.com>
2025-06-06 10:33:53 -0700

    Reorder inheritance on span nodes
    Core tracing will be adding a bunch of logic to the span_event method which is called
    via super on inheriting classes before those inheriting classes add additional
    attributes. In order for the core tracing logic to work correctly, this inheritance
    call order has to be reversed; the inheriting classes must first add the attributes
    and THEN call super.

--- newrelic/core/node_mixin.py
+++ newrelic/core/node_mixin.py
@@ -52,11 +52,11 @@ class GenericNodeMixin:
     def span_event(self, settings, base_attrs=None, parent_guid=None, attr_class=dict):
         i_attrs = (base_attrs and base_attrs.copy()) or attr_class()
         i_attrs["type"] = "Span"
-        i_attrs["name"] = self.name
+        i_attrs["name"] = i_attrs.get("name") or self.name
         i_attrs["guid"] = self.guid
         i_attrs["timestamp"] = int(self.start_time * 1000)
         i_attrs["duration"] = self.duration
-        i_attrs["category"] = "generic"
+        i_attrs["category"] = i_attrs.get("category") or "generic"
 
         if parent_guid:
             i_attrs["parentId"] = parent_guid
@@ -108,24 +108,23 @@ class DatastoreNodeMixin(GenericNodeMixin):
         self._db_instance = db_instance_attr
         return db_instance_attr
 
-    def span_event(self, *args, **kwargs):
-        self.agent_attributes["db.instance"] = self.db_instance
-        attrs = super().span_event(*args, **kwargs)
-        i_attrs = attrs[0]
-        a_attrs = attrs[2]
+    def span_event(self, settings, base_attrs=None, parent_guid=None, attr_class=dict, *args, **kwargs):
+        a_attrs = self.agent_attributes
+        a_attrs["db.instance"] = self.db_instance
+        i_attrs = base_attrs and base_attrs.copy() or attr_class()
 
         i_attrs["category"] = "datastore"
         i_attrs["span.kind"] = "client"
 
         if self.product:
-            i_attrs["component"] = a_attrs["db.system"] = attribute.process_user_attribute("db.system", self.product)[1]
+            i_attrs["component"] = a_attrs["db.system"] = self.product
         if self.operation:
-            a_attrs["db.operation"] = attribute.process_user_attribute("db.operation", self.operation)[1]
+            a_attrs["db.operation"] = self.operation
         if self.target:
-            a_attrs["db.collection"] = attribute.process_user_attribute("db.collection", self.target)[1]
+            a_attrs["db.collection"] = self.target
 
         if self.instance_hostname:
-            peer_hostname = attribute.process_user_attribute("peer.hostname", self.instance_hostname)[1]
+            peer_hostname = self.instance_hostname
         else:
             peer_hostname = "Unknown"
 
@@ -133,7 +132,7 @@ class DatastoreNodeMixin(GenericNodeMixin):
 
         peer_address = f"{peer_hostname}:{self.port_path_or_id or 'Unknown'}"
 
-        a_attrs["peer.address"] = attribute.process_user_attribute("peer.address", peer_address)[1]
+        a_attrs["peer.address"] = peer_address
 
         # Attempt to treat port_path_or_id as an integer, fallback to not including it
         try:
@@ -141,4 +140,4 @@ class DatastoreNodeMixin(GenericNodeMixin):
         except Exception:
             pass
 
-        return attrs
+        return super().span_event(settings, base_attrs=i_attrs, parent_guid=parent_guid, attr_class=attr_class, *args, **kwargs)

669f51e6fd66e3fdcde025dc7be9d910cf7b74b4
Hannah Stepanek <hstepanek@newrelic.com>
2025-06-06 10:33:53 -0700

    Add support 4 partial granularity tracing

--- tests/agent_unittests/test_distributed_tracing_settings.py
+++ tests/agent_unittests/test_distributed_tracing_settings.py
@@ -14,6 +14,8 @@
 
 import pytest
 
+from newrelic.core.config import finalize_application_settings
+
 INI_FILE_EMPTY = b"""
 [newrelic]
 """
@@ -30,3 +32,35 @@ distributed_tracing.exclude_newrelic_header = true
 def test_distributed_trace_setings(ini, env, expected_format, global_settings):
     settings = global_settings()
     assert settings.distributed_tracing.exclude_newrelic_header == expected_format
+
+
+@pytest.mark.parametrize(
+    "ini,env",
+    (
+        (
+            INI_FILE_EMPTY,
+            {
+                "NEW_RELIC_ENABLED": "true",
+                "NEW_RELIC_DISTRIBUTED_TRACING_SAMPLER_REMOTE_PARENT_SAMPLED": "default",
+                "NEW_RELIC_DISTRIBUTED_TRACING_SAMPLER_REMOTE_PARENT_NOT_SAMPLED": "default",
+                "NEW_RELIC_DISTRIBUTED_TRACING_SAMPLER_FULL_GRANULARITY_REMOTE_PARENT_SAMPLED": "always_on",
+                "NEW_RELIC_DISTRIBUTED_TRACING_SAMPLER_FULL_GRANULARITY_REMOTE_PARENT_NOT_SAMPLED": "always_off",
+            },
+        ),
+        (
+            INI_FILE_EMPTY,
+            {
+                "NEW_RELIC_ENABLED": "true",
+                "NEW_RELIC_DISTRIBUTED_TRACING_SAMPLER_REMOTE_PARENT_SAMPLED": "always_on",
+                "NEW_RELIC_DISTRIBUTED_TRACING_SAMPLER_REMOTE_PARENT_NOT_SAMPLED": "always_off",
+            },
+        ),
+    ),
+)
+def test_full_granularity_precedence(ini, env, global_settings):
+    settings = global_settings()
+
+    app_settings = finalize_application_settings(settings=settings)
+
+    assert app_settings.distributed_tracing.sampler.full_granularity.remote_parent_sampled == "always_on"
+    assert app_settings.distributed_tracing.sampler.full_granularity.remote_parent_not_sampled == "always_off"

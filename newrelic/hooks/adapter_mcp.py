9abea90166bf3d0aad0bb0c72e9635acb587b28f
Uma Annamalai <uannamalai@newrelic.com>
2025-06-09 14:08:31 -0700

    Add instrumentation for MCP resources and prompts. (#1400)
    * Add instrumentation for MCP resources and prompts.
    
    * [MegaLinter] Apply linters fixes
    
    * Address review comments.
    
    * Remove if conditional.

--- newrelic/hooks/adapter_mcp.py
+++ newrelic/hooks/adapter_mcp.py
@@ -12,11 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import logging
+
 from newrelic.api.function_trace import FunctionTrace
 from newrelic.api.transaction import current_transaction
 from newrelic.common.object_names import callable_name
 from newrelic.common.object_wrapper import wrap_function_wrapper
 from newrelic.common.signature import bind_args
+
+_logger = logging.getLogger(__name__)
 
 
 async def wrap_call_tool(wrapped, instance, args, kwargs):
@@ -24,12 +28,48 @@ async def wrap_call_tool(wrapped, instance, args, kwargs):
     if not transaction:
         return await wrapped(*args, **kwargs)
 
+    func_name = callable_name(wrapped)
     bound_args = bind_args(wrapped, args, kwargs)
     tool_name = bound_args.get("name") or "tool"
-    func_name = callable_name(wrapped)
     function_trace_name = f"{func_name}/{tool_name}"
 
     with FunctionTrace(name=function_trace_name, group="Llm/tool/MCP", source=wrapped):
+        return await wrapped(*args, **kwargs)
+
+
+async def wrap_read_resource(wrapped, instance, args, kwargs):
+    transaction = current_transaction()
+    if not transaction:
+        return await wrapped(*args, **kwargs)
+
+    func_name = callable_name(wrapped)
+    bound_args = bind_args(wrapped, args, kwargs)
+    # Set a default value in case we can't parse out the URI scheme successfully
+    resource_scheme = "resource"
+
+    try:
+        resource_uri = bound_args.get("uri")
+        resource_scheme = getattr(resource_uri, "scheme", "resource")
+    except Exception:
+        _logger.warning("Unable to parse resource URI scheme for MCP read_resource call")
+
+    function_trace_name = f"{func_name}/{resource_scheme}"
+
+    with FunctionTrace(name=function_trace_name, group="Llm/resource/MCP", source=wrapped):
+        return await wrapped(*args, **kwargs)
+
+
+async def wrap_get_prompt(wrapped, instance, args, kwargs):
+    transaction = current_transaction()
+    if not transaction:
+        return await wrapped(*args, **kwargs)
+
+    func_name = callable_name(wrapped)
+    bound_args = bind_args(wrapped, args, kwargs)
+    prompt_name = bound_args.get("name") or "prompt"
+    function_trace_name = f"{func_name}/{prompt_name}"
+
+    with FunctionTrace(name=function_trace_name, group="Llm/prompt/MCP", source=wrapped):
         return await wrapped(*args, **kwargs)
 
 
@@ -37,3 +77,7 @@ def instrument_mcp_client_session(module):
     if hasattr(module, "ClientSession"):
         if hasattr(module.ClientSession, "call_tool"):
             wrap_function_wrapper(module, "ClientSession.call_tool", wrap_call_tool)
+        if hasattr(module.ClientSession, "read_resource"):
+            wrap_function_wrapper(module, "ClientSession.read_resource", wrap_read_resource)
+        if hasattr(module.ClientSession, "get_prompt"):
+            wrap_function_wrapper(module, "ClientSession.get_prompt", wrap_get_prompt)

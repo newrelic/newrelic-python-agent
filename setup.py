cc8de3a82374be7c5cf36115bc39c657562d33b5
Lalleh Rafeei <84813886+lrafeei@users.noreply.github.com>
2025-06-18 09:33:43 -0700

    Update setup.py
    Co-authored-by: Timothy Pansino <11214426+TimPansino@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -160,7 +160,7 @@ kwargs = dict(
         "git_describe_command": "git describe --dirty --tags --long --match *.*.*",
         "write_to": "newrelic/version.txt",
     },
-    setup_requires=["setuptools_scm>=3.2,<8.1"],
+    setup_requires=["setuptools_scm>=3.2,<9"],
     description="New Relic Python Agent",
     long_description=open(readme_file).read(),
     long_description_content_type="text/markdown",

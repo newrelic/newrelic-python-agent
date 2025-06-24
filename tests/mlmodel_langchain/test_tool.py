921a750582643db53d89977dbb49ee29ebf5d27f
Uma Annamalai <uannamalai@newrelic.com>
2025-06-23 18:29:45 -0700

    Revert "Add autogen agent instrumentation."
    This reverts commit a26766a9404b297e1bfb9e60717d9697216eb649.

--- tests/mlmodel_langchain/test_tool.py
+++ tests/mlmodel_langchain/test_tool.py
@@ -27,7 +27,6 @@ from testing_support.ml_testing_utils import (
     disabled_ai_monitoring_settings,
     events_with_context_attrs,
     set_trace_info,
-    tool_events_sans_content,
 )
 from testing_support.validators.validate_custom_event import validate_custom_event_count
 from testing_support.validators.validate_custom_events import validate_custom_events
@@ -58,6 +57,15 @@ def multi_arg_tool():
         return first_num + second_num
 
     return _multi_arg_tool
+
+
+def events_sans_content(event):
+    new_event = copy.deepcopy(event)
+    for _event in new_event:
+        del _event[1]["input"]
+        if "output" in _event[1]:
+            del _event[1]["output"]
+    return new_event
 
 
 single_arg_tool_recorded_events = [
@@ -100,7 +108,7 @@ def test_langchain_single_arg_tool(set_trace_info, single_arg_tool):
 
 @reset_core_stats_engine()
 @disabled_ai_monitoring_record_content_settings
-@validate_custom_events(tool_events_sans_content(single_arg_tool_recorded_events))
+@validate_custom_events(events_sans_content(single_arg_tool_recorded_events))
 @validate_custom_event_count(count=1)
 @validate_transaction_metrics(
     name="test_tool:test_langchain_single_arg_tool_no_content",
@@ -136,7 +144,7 @@ def test_langchain_single_arg_tool_async(set_trace_info, single_arg_tool, loop):
 
 @reset_core_stats_engine()
 @disabled_ai_monitoring_record_content_settings
-@validate_custom_events(tool_events_sans_content(single_arg_tool_recorded_events))
+@validate_custom_events(events_sans_content(single_arg_tool_recorded_events))
 @validate_custom_event_count(count=1)
 @validate_transaction_metrics(
     name="test_tool:test_langchain_single_arg_tool_async_no_content",
@@ -267,7 +275,7 @@ def test_langchain_error_in_run(set_trace_info, multi_arg_tool):
 @validate_error_trace_attributes(
     callable_name(pydantic_core._pydantic_core.ValidationError), exact_attrs={"agent": {}, "intrinsic": {}, "user": {}}
 )
-@validate_custom_events(tool_events_sans_content(multi_arg_error_recorded_events))
+@validate_custom_events(events_sans_content(multi_arg_error_recorded_events))
 @validate_custom_event_count(count=1)
 @validate_transaction_metrics(
     name="test_tool:test_langchain_error_in_run_no_content",
@@ -319,7 +327,7 @@ def test_langchain_error_in_run_async(set_trace_info, multi_arg_tool, loop):
 @validate_error_trace_attributes(
     callable_name(pydantic_core._pydantic_core.ValidationError), exact_attrs={"agent": {}, "intrinsic": {}, "user": {}}
 )
-@validate_custom_events(tool_events_sans_content(multi_arg_error_recorded_events))
+@validate_custom_events(events_sans_content(multi_arg_error_recorded_events))
 @validate_custom_event_count(count=1)
 @validate_transaction_metrics(
     name="test_tool:test_langchain_error_in_run_async_no_content",

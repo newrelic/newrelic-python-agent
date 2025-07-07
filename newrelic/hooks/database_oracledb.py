0a335001edf4adba80a95685221a70fe2dee49a3
Tim Pansino <timpansino@gmail.com>
2025-07-07 12:56:56 -0400

    Fix __enter__ methods

--- newrelic/hooks/database_oracledb.py
+++ newrelic/hooks/database_oracledb.py
@@ -14,8 +14,49 @@
 
 from newrelic.api.database_trace import register_database_client
 from newrelic.common.object_wrapper import wrap_object
-from newrelic.hooks.database_dbapi2 import ConnectionFactory
+from newrelic.hooks.database_dbapi2 import ConnectionFactory as DBAPI2ConnectionFactory
+from newrelic.hooks.database_dbapi2 import ConnectionWrapper as DBAPI2ConnectionWrapper
+from newrelic.hooks.database_dbapi2 import CursorWrapper as DBAPI2CursorWrapper
 from newrelic.hooks.database_dbapi2_async import AsyncConnectionFactory
+from newrelic.hooks.database_dbapi2_async import AsyncConnectionFactory as DBAPI2AsyncConnectionFactory
+from newrelic.hooks.database_dbapi2_async import AsyncConnectionWrapper as DBAPI2AsyncConnectionWrapper
+from newrelic.hooks.database_dbapi2_async import AsyncCursorWrapper as DBAPI2AsyncCursorWrapper
+
+
+class CursorWrapper(DBAPI2CursorWrapper):
+    def __enter__(self):
+        self.__wrapped__.__enter__()
+        return self
+
+
+class ConnectionWrapper(DBAPI2ConnectionWrapper):
+    __cursor_wrapper__ = CursorWrapper
+
+    def __enter__(self):
+        self.__wrapped__.__enter__()
+        return self
+
+
+class ConnectionFactory(DBAPI2ConnectionFactory):
+    __connection_wrapper__ = ConnectionWrapper
+
+
+class AsyncCursorWrapper(DBAPI2AsyncCursorWrapper):
+    async def __aenter__(self):
+        await self.__wrapped__.__aenter__()
+        return self
+
+
+class AsyncConnectionWrapper(DBAPI2AsyncConnectionWrapper):
+    __cursor_wrapper__ = AsyncCursorWrapper
+
+    async def __aenter__(self):
+        await self.__wrapped__.__aenter__()
+        return self
+
+
+class AsyncConnectionFactory(DBAPI2AsyncConnectionFactory):
+    __connection_wrapper__ = AsyncConnectionWrapper
 
 
 def instance_info(args, kwargs):

c02609c760bf879c454027b9b45d4a4a0174b32e
Hannah Stepanek <hstepanek@newrelic.com>
2025-07-14 17:13:28 -0700

    Add destination_name attr to all MessageTransactions

--- newrelic/hooks/messagebroker_kombu.py
+++ newrelic/hooks/messagebroker_kombu.py
@@ -194,7 +194,6 @@ def wrap_consumer_recieve_callback(wrapped, instance, args, kwargs):
                 source=wrapped,
             )
             created_transaction.__enter__()
-            created_transaction.destination_name = destination_name
 
             # Obtain consumer client_id to send up as agent attribute
             if hasattr(message, "channel") and hasattr(message.channel, "channel_id"):
@@ -242,6 +241,7 @@ def wrap_serialize(wrapped, instance, args, kwargs):
         return wrapped(*args, **kwargs)
 
     exchange = "Unknown"
+    breakpoint()
     if isinstance(transaction, MessageTransaction):
         exchange = transaction.destination_name
     else:

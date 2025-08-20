1985145b2d0119877d971d31a9c8395384cd664f
Lalleh Rafeei <lrafeei@newrelic.com>
2025-08-20 14:39:21 -0700

    Add sychronous schema tests

---
+++ tests/framework_graphenedjango/test_application.py
@@ -0,0 +1,143 @@
+# Copyright 2010 New Relic, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+
+from newrelic.common.package_version_utils import get_package_version
+
+# from testing_support.fixtures import dt_enabled
+# from testing_support.validators.validate_span_events import validate_span_events
+from testing_support.validators.validate_transaction_metrics import validate_transaction_metrics
+
+
+GRAPHENE_DJANGO_VERSION = get_package_version("graphene-django")
+
+
+def _graphql_base_rollup_metrics(framework, version, background_task=True):
+    graphql_version = get_package_version("graphql-core")
+
+    metrics = [(f"Python/Framework/GraphQL/{graphql_version}", 1), ("GraphQL/all", 1), (f"GraphQL/{framework}/all", 1)]
+    if background_task:
+        metrics.extend([("GraphQL/allOther", 1), (f"GraphQL/{framework}/allOther", 1)])
+    else:
+        metrics.extend([("GraphQL/allWeb", 1), (f"GraphQL/{framework}/allWeb", 1)])
+
+    if framework != "GraphQL":
+        metrics.append((f"Python/Framework/{framework}/{version}", 1))
+
+    return metrics
+
+
+_test_queries = [
+    ("{ hello }", "/hello", {"data": {"hello": "Hello!"}}),  # Basic query
+    (
+        "{ error }",
+        "/error",
+        {
+            "errors": [{"message": "Runtime Error!", "locations": [{"line": 1, "column": 3}], "path": ["error"]}],
+            "data": {"error": None},
+        },
+    ),  # Extract deepest path on field error
+    ('{ echo(echo: "test") }', "/echo", {"data": {"echo": "test"}}),  # Fields with arguments
+    (
+        "{ library(index: 0) { branch, book { isbn branch } } }",
+        "/library",
+        {
+            "data": {
+                "library": {
+                    "branch": "riverside",
+                    "book": [
+                        {"isbn": "a-fake-isbn", "branch": "riverside"},
+                        {"isbn": "a-third-fake-isbn", "branch": "riverside"},
+                    ],
+                }
+            }
+        },
+    ),  # Complex Example, 1 level
+    (
+        "{ library(index: 0) { book { author { first_name }} } }",
+        "/library.book.author.first_name",
+        {"data": {"library": {"book": [{"author": {"first_name": "New"}}, {"author": {"first_name": "Leslie"}}]}}},
+    ),  # Complex Example, 2 levels
+    (
+        "{ library(index: 0) { book { name } } }",
+        "/library.book.name",
+        {"data": {"library": {"book": [{"name": "Python Agent: The Book"}, {"name": "[Redacted]"}]}}},
+    ),  # Filtering
+    ('{ TestEcho: echo(echo: "test") }', "/echo", {"data": {"TestEcho": "test"}}),  # Aliases
+    ('{ search(contains: "A") { __typename ... on Book { name } } }', "/search<Book>.name", {'data': {'search': [{'__typename': 'Book', 'name': 'Python Agent: The Book'}, {'__typename': 'Book', 'name': "Ollies for O11y: A Sk8er's Guide to Observability"}]}}),  # InlineFragment
+    ('{ hello echo(echo: "test") }', "", {"data": {"hello": "Hello!", "echo": "test"}}),  # Multiple root selections. (need to decide on final behavior)
+    # FragmentSpread
+    (
+        "{ library(index: 0) { book { ...MyFragment } } } fragment MyFragment on Book { name }",  # Fragment filtering
+        "/library.book.name",
+        {"data": {"library": {"book": [{"name": "Python Agent: The Book"}, {"name": "[Redacted]"}]}}},
+    ),
+    (
+        "{ library(index: 0) { book { ...MyFragment } } } fragment MyFragment on Book { author { first_name } }",
+        "/library.book.author.first_name",
+        {"data": {"library": {"book": [{"author": {"first_name": "New"}}, {"author": {"first_name": "Leslie"}}]}}},
+    ),
+    (
+        "{ library(index: 0) { book { ...MyFragment } magazine { ...MagFragment } } } fragment MyFragment on Book { author { first_name } } fragment MagFragment on Magazine { name }",
+        "/library",
+        {'data': {'library': {'book': [{'author': {'first_name': 'New'}}, {'author': {'first_name': 'Leslie'}}], 'magazine': [{'name': 'Reli Updates Weekly'}, {'name': 'Node Weekly'}]}}},
+    ),
+]
+
+# TODO: Add background task parameterization?
+@pytest.mark.parametrize("query,expected_path,result", _test_queries)
+def test_application_query(app, query, expected_path, result):
+    field_metrics = [
+        (f"GraphQL/operation/GrapheneDjango/query/<anonymous>{'/' if expected_path=='' else expected_path}", 1),
+    ]
+    transaction_name = f"query/<anonymous>{expected_path}"
+
+    @validate_transaction_metrics(
+        transaction_name,
+        "GraphQL",
+        scoped_metrics=field_metrics,
+        rollup_metrics=field_metrics + _graphql_base_rollup_metrics("GrapheneDjango", GRAPHENE_DJANGO_VERSION, False),
+    )
+    def _test():
+        request_body = {"query": query}
+        response = app.post_json("/graphql", request_body)
+        assert response.json == result
+
+    _test()
+
+
+def test_application_mutate(app):
+    _test_mutation_scoped_metrics = [
+        ("GraphQL/resolve/GrapheneDjango/storage_add", 1),
+        ("GraphQL/operation/GrapheneDjango/mutation/<anonymous>/storage_add.string", 1),
+    ]
+
+    @validate_transaction_metrics(
+        "mutation/<anonymous>/storage_add.string",
+        "GraphQL",
+        scoped_metrics=_test_mutation_scoped_metrics,
+        rollup_metrics=_test_mutation_scoped_metrics + _graphql_base_rollup_metrics("GrapheneDjango", GRAPHENE_DJANGO_VERSION, False),
+    )
+    # @conditional_decorator(background_task(), is_bg)
+    def _test():
+        query = 'mutation { storage_add(string: "abc") { string } }'
+        request_body = {"query": query}
+        response = app.post_json("/graphql", request_body)
+        assert response.json == {'data': {'storage_add': {'string': 'abc'}}}
+
+    _test()
+    
+
+# TODO: Create async tests (with async target schema)
---
+++ tests/framework_graphenedjango/urls.py
@@ -0,0 +1,59 @@
+# Copyright 2010 New Relic, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from django.urls import path
+from graphene_django.views import GraphQLView
+# from _target_application import schema
+from _target_schema_sync import target_schema as schema
+
+urlpatterns = [
+    path("graphql", GraphQLView.as_view(graphiql=False, schema=schema), name="graphql"),
+]
+
+# try:
+#     from django.conf.urls.defaults import url
+# except ImportError:
+#     try:
+#         from django.conf.urls import url
+#     except ImportError:
+#         from django.urls import re_path as url
+
+# import views
+
+# urlpatterns = [
+#     url(r"^$", views.index, name="index"),
+#     url(r"^exception$", views.exception, name="exception"),
+#     url(r"^middleware_410$", views.middleware_410, name="middleware_410"),
+#     url(r"^permission_denied$", views.permission_denied, name="permission_denied"),
+#     url(r"^cbv$", views.MyView.as_view()),
+#     url(r"^deferred_cbv$", views.deferred_cbv),
+#     url(r"^html_insertion$", views.html_insertion, name="html_insertion"),
+#     url(r"^html_insertion_content_length$", views.html_insertion_content_length, name="html_insertion_content_length"),
+#     url(r"^html_insertion_manual$", views.html_insertion_manual, name="html_insertion_manual"),
+#     url(
+#         r"^html_insertion_unnamed_attachment_header$",
+#         views.html_insertion_unnamed_attachment_header,
+#         name="html_insertion_unnamed_attachment_header",
+#     ),
+#     url(
+#         r"^html_insertion_named_attachment_header$",
+#         views.html_insertion_named_attachment_header,
+#         name="html_insertion_named_attachment_header",
+#     ),
+#     url(r"^inclusion_tag$", views.inclusion_tag, name="inclusion_tag"),
+#     url(r"^template_tags$", views.template_tags, name="template_tags"),
+#     url(r"^render_exception_function", views.render_exception_function, name="render_exception_function"),
+#     url(r"^render_exception_class", views.RenderExceptionClass.as_view(), name="render_exception_class"),
+#     url(r"^gzip_html_insertion", views.gzip_html_insertion, name="gzip_html_insertion"),
+# ]

6510cc14d59ca2a021f4273fad96e8ab9e93389d
Hannah Stepanek <hstepanek@newrelic.com>
2025-09-24 14:41:34 -0700

    Put back nr.guid if synthetics resource id

--- newrelic/core/transaction_node.py
+++ newrelic/core/transaction_node.py
@@ -463,6 +463,8 @@ class TransactionNode(_TransactionNode):
         if self.errors:
             intrinsics["error"] = True
 
+        if self.synthetics_resource_id:
+            intrinsics["nr.guid"] = self.guid
         if self.parent_tx:
             intrinsics["parentId"] = self.parent_tx
 

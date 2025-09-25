baecae80c203a991dfc0b0a84c2007685a230732
Timothy Pansino <11214426+TimPansino@users.noreply.github.com>
2025-09-25 12:09:48 -0700

    Update Megalinter (#1510)
    * Update megalinter workflow
    
    * Update megalinter version
    
    * Fix RUF059: unused-unpacked-variable
    
    * Fix PLW1641: eq-without-hash
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- tests/agent_features/test_serverless_mode.py
+++ tests/agent_features/test_serverless_mode.py
@@ -56,7 +56,7 @@ def test_serverless_payload(capsys, serverless_application):
 
     _test()
 
-    out, err = capsys.readouterr()
+    out, _err = capsys.readouterr()
 
     # Validate that something is printed to stdout
     assert out

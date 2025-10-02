7134a6895e22358a2868872ddb9c04e59a7af709
Lalleh Rafeei <84813886+lrafeei@users.noreply.github.com>
2025-10-01 17:15:21 -0700

    Deprecate record_deploy command (#1500)
    * Deprecate record_deploy command
    
    * Add alternative to deprecation message
    
    * import warnings
    
    * [MegaLinter] Apply linters fixes
    
    * Update newrelic/admin/record_deploy.py
    
    Co-authored-by: Hannah Stepanek <hstepanek@newrelic.com>
    
    ---------
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Hannah Stepanek <hstepanek@newrelic.com>
    Co-authored-by: hmstepanek <30059933+hmstepanek@users.noreply.github.com>

--- newrelic/admin/record_deploy.py
+++ newrelic/admin/record_deploy.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 import getpass
+import warnings
 
 from newrelic.admin import command, usage
 from newrelic.common import agent_http, encoding_utils
@@ -108,6 +109,15 @@ def record_deploy(
 )
 def record_deploy_cmd(args):
     import sys
+
+    # Deprecation Warning
+    warnings.warn(
+        (
+            "The record_deploy command is deprecated and will be removed in the next major release. It is being removed in favor of other methods of creating a change tracking marker. See the documentation for replacement alternatives: https://docs.newrelic.com/docs/change-tracking/change-tracking-introduction/"
+        ),
+        DeprecationWarning,
+        stacklevel=2,
+    )
 
     if len(args) < 2:
         usage("record-deploy")

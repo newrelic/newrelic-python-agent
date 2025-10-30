b4a657860d90d874a77fc05851c3fd3627b53678
Uma Annamalai <uannamalai@newrelic.com>
2025-10-30 14:15:52 -0700

    Cleanup.
    Co-authored-by: Tim Pansino <tpansino@newrelic.com>

--- newrelic/api/time_trace.py
+++ newrelic/api/time_trace.py
@@ -361,6 +361,7 @@ class TimeTrace:
 
     def notice_error(self, error=None, attributes=None, expected=None, ignore=None, status_code=None):
         attributes = attributes if attributes is not None else {}
+
         # If no exception details provided, use current exception.
 
         # Pull from sys.exc_info if no exception is passed

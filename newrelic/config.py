fc27b9446991d34aa7c71e086c7d85fe71c6ca6c
Tim Pansino <timpansino@gmail.com>
2025-11-26 10:27:53 -0800

    Restore aiohttp hooks

--- newrelic/config.py
+++ newrelic/config.py
@@ -3107,6 +3107,12 @@ def _process_module_builtin_defaults():
     _process_module_definition("aiohttp.wsgi", "newrelic.hooks.framework_aiohttp", "instrument_aiohttp_wsgi")
     _process_module_definition("aiohttp.web", "newrelic.hooks.framework_aiohttp", "instrument_aiohttp_web")
     _process_module_definition(
+        "aiohttp.web_reqrep", "newrelic.hooks.framework_aiohttp", "instrument_aiohttp_web_response"
+    )
+    _process_module_definition(
+        "aiohttp.web_response", "newrelic.hooks.framework_aiohttp", "instrument_aiohttp_web_response"
+    )
+    _process_module_definition(
         "aiohttp.web_urldispatcher", "newrelic.hooks.framework_aiohttp", "instrument_aiohttp_web_urldispatcher"
     )
     _process_module_definition("aiohttp.client", "newrelic.hooks.framework_aiohttp", "instrument_aiohttp_client")

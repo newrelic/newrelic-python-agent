4d31395d6a30d3142468602b167eb8c0d9d17637
Tim Pansino <timpansino@gmail.com>
2025-11-26 10:48:52 -0800

    Remove unused imports

--- tests/external_httplib/test_httplib.py
+++ tests/external_httplib/test_httplib.py
@@ -12,9 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import base64
 import http.client as httplib
-import json
 
 import pytest
 from testing_support.external_fixtures import cache_outgoing_headers

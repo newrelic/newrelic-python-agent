4a189225a9b9a2a3e36f4d91fc092b02d579cd8d
Hannah Stepanek <hstepanek@newrelic.com>
2026-01-20 17:03:34 -0800

    Fix lint issues
      Error: tests/cross_agent/test_distributed_tracing_trace_context.py:123:38: C418 Unnecessary dict comprehension passed to `dict()` (remove the outer call to `dict()`)
      Error: tests/cross_agent/test_distributed_tracing_trace_context.py:124:37: C418 Unnecessary dict comprehension passed to `dict()` (remove the outer call to `dict()`)
      Error: tests/cross_agent/test_harvest_sampling_rates.py:64:27: FLY002 Consider `f"{setting}.{key}"` instead of string join
      Error: tests/cross_agent/test_harvest_sampling_rates.py:160:13: B007 Loop control variable `n` not used within loop body
      Error: tests/cross_agent/test_harvest_sampling_rates.py:163:13: B007 Loop control variable `n` not used within loop body
      Error: tests/cross_agent/test_harvest_sampling_rates.py:172:13: B007 Loop control variable `n` not used within loop body
      Error: tests/cross_agent/test_harvest_sampling_rates.py:181:13: B007 Loop control variable `n` not used within loop body
      Error: tests/cross_agent/test_harvest_sampling_rates.py:190:13: B007 Loop control variable `n` not used within loop body
      Error: tests/cross_agent/test_sampler_configuration.py:85:27: FLY002 Consider `f"{setting}.{key}"` instead of string join
      Error: tests/hybridagent_pika/test_pika_blocking_connection_consume_generator.py:69:13: B007 Loop control variable `method_frame` not used within loop body
      Error: tests/hybridagent_pika/test_pika_blocking_connection_consume_generator.py:88:13: B007 Loop control variable `method_frame` not used within loop body
      Error: tests/hybridagent_pika/test_pika_blocking_connection_consume_generator.py:222:9: RUF059 Unpacked variable `method` is never used
      Error: tests/hybridagent_pika/test_pika_blocking_connection_consume_generator.py:260:17: B007 Loop control variable `method_frame` not used within loop body
      Error: tests/hybridagent_pika/test_pika_blocking_connection_consume_generator.py:286:13: RUF059 Unpacked variable `method_frame` is never used
      Error: tests/hybridagent_pika/test_pika_blocking_connection_consume_generator.py:323:9: RUF059 Unpacked variable `method` is never used

--- tests/cross_agent/test_sampler_configuration.py
+++ tests/cross_agent/test_sampler_configuration.py
@@ -82,7 +82,7 @@ def parse_to_config_paths(settings, setting, config):
                 new_setting = ".".join([setting, f"_{key}"])
                 v = list(value.keys())[0] if isinstance(value, dict) else value
                 settings[new_setting] = v
-            new_setting = ".".join([setting, key])
+            new_setting = f"{setting}.{key}"
             parse_to_config_paths(settings, new_setting, value)
     else:
         new_setting = replace_section(setting)

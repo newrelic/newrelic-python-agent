e80e1a77d24494ec1bebff4be5c99359a3286189
Hannah Stepanek <hstepanek@newrelic.com>
2026-07-08 10:37:01 -0700

    Remove unnecessary fixture

--- tests/framework_wagtail/conftest.py
+++ tests/framework_wagtail/conftest.py
@@ -11,23 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-import pytest
-
-from newrelic.api.application import application_instance
-from newrelic.core.agent import agent_instance
-
-
-# Even though `django_collector_agent_registration_fixture()` also
-# has the application deletion during the breakdown of the fixture,
-# and `django_collector_agent_registration_fixture()` is scoped to
-# "function", not all modules are using this.  Some are using
-# `collector_agent_registration_fixture()` scoped to "module".
-# Therefore, for those instances, we need to make sure that the
-# application is removed from the agent.
-@pytest.fixture(scope="module", autouse=True)
-def remove_application_from_agent():
-    yield
-    application = application_instance()
-    if application and application.name and (application.name in agent_instance()._applications):
-        del agent_instance()._applications[application.name]

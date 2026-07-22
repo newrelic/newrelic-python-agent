6c5a1d2b3ebb3f97d74cb903c66624d129bdedd1
Lalleh Rafeei <lrafeei@gmail.com>
2026-07-22 12:30:18 -0700

    typing_extensions vs typing

--- tests/mlmodel_gemini/langchain_integration/conftest.py
+++ tests/mlmodel_gemini/langchain_integration/conftest.py
@@ -13,7 +13,13 @@
 # limitations under the License.
 
 import os
-from typing import Annotated, TypedDict
+import sys
+from typing import Annotated
+
+if sys.version_info[:2] < (3, 12):
+    from typing_extensions import TypeDict
+else:
+    from typing import TypedDict
 
 import pytest
 from langchain.chat_models import init_chat_model

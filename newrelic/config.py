6eba36b1dd4f002772f79a3bd685b10c9e060739
Hannah Stepanek <hstepanek@newrelic.com>
2026-07-23 09:50:46 -0700

    Fix Wagtail transaction names (#1705)
    * Fix wagtail transaction names
    
    * Fixup
    
    * Fixup wagtail
    
    * Use migration in wagtail tests
    
    * Fixup
    
    * Fixup lint issues
    
    * Apply ruff suggestions
    
    Co-authored-by: Hannah Stepanek <hmstepanek@gmail.com>
    
    * Fixup: license headers
    
    * [MegaLinter] Apply linters fixes
    
    * Remove unnecessary fixture
    
    * Cache Page import in global and move to django hook
    
    * Fixup: lint
    
    * Move fixtures back to conftest
    
    * [MegaLinter] Apply linters fixes
    
    ---------
    
    Co-authored-by: hmstepanek <30059933+hmstepanek@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Timothy Pansino <11214426+TimPansino@users.noreply.github.com>

--- newrelic/config.py
+++ newrelic/config.py
@@ -3002,6 +3002,10 @@ def _process_module_builtin_defaults():
     _process_module_definition("flask_restplus.api", "newrelic.hooks.component_flask_rest", "instrument_flask_rest")
     _process_module_definition("flask_restx.api", "newrelic.hooks.component_flask_rest", "instrument_flask_rest")
 
+    _process_module_definition(
+        "wagtail.models.pages", "newrelic.hooks.framework_django", "instrument_wagtail_models_pages"
+    )
+
     _process_module_definition("graphql_server", "newrelic.hooks.component_graphqlserver", "instrument_graphqlserver")
 
     _process_module_definition(

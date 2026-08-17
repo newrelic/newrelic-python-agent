c4a1a347878857d51cf99fcdada295f80f75a37e
Lalleh Rafeei <lrafeei@gmail.com>
2026-08-17 12:30:37 -0700

    Add clarifying comment

--- tests/testing_support/db_settings.py
+++ tests/testing_support/db_settings.py
@@ -259,6 +259,7 @@ def cosmos_settings():
     settings = [
         {
             "url": f"https://{host}:8081",
+            # NOTE: This is not a secret key
             "credential": "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
         }
     ]

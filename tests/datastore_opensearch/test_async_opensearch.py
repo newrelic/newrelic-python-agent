972c34f6429e25dc8c34beb731a3ace5333c21b4
Tim Pansino <timpansino@gmail.com>
2026-09-21 11:58:59 -0700

    Add opensearch test suite ported from elasticsearch

---
+++ tests/datastore_opensearch/test_async_opensearch.py
@@ -0,0 +1,166 @@
+# Copyright 2010 New Relic, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from conftest import OPENSEARCH_SETTINGS
+from testing_support.fixture.event_loop import event_loop as loop
+from testing_support.fixtures import override_application_settings
+from testing_support.util import instance_hostname
+from testing_support.validators.validate_transaction_errors import validate_transaction_errors
+from testing_support.validators.validate_transaction_metrics import validate_transaction_metrics
+
+from newrelic.api.background_task import background_task
+from newrelic.api.transaction import current_transaction
+
+# Settings
+
+_enable_instance_settings = {"datastore_tracer.instance_reporting.enabled": True}
+_disable_instance_settings = {"datastore_tracer.instance_reporting.enabled": False}
+
+# Metrics
+
+_base_scoped_metrics = [
+    ("Datastore/operation/OpenSearch/cat.health", 1),
+    ("Datastore/operation/OpenSearch/nodes.info", 1),
+    ("Datastore/operation/OpenSearch/snapshot.status", 1),
+    ("Datastore/statement/OpenSearch/_all/cluster.health", 1),
+    ("Datastore/statement/OpenSearch/_all/search", 2),
+    ("Datastore/statement/OpenSearch/address/index", 2),
+    ("Datastore/statement/OpenSearch/address/search", 1),
+    ("Datastore/statement/OpenSearch/contacts/index", 3),
+    ("Datastore/statement/OpenSearch/contacts/indices.refresh", 1),
+    ("Datastore/statement/OpenSearch/contacts/search", 2),
+    ("Datastore/statement/OpenSearch/other/search", 2),
+]
+
+_all_count = 17
+_base_rollup_metrics = [
+    ("Datastore/all", _all_count),
+    ("Datastore/allOther", _all_count),
+    ("Datastore/OpenSearch/all", _all_count),
+    ("Datastore/OpenSearch/allOther", _all_count),
+    ("Datastore/operation/OpenSearch/cat.health", 1),
+    ("Datastore/operation/OpenSearch/cluster.health", 1),
+    ("Datastore/operation/OpenSearch/index", 5),
+    ("Datastore/operation/OpenSearch/indices.refresh", 1),
+    ("Datastore/operation/OpenSearch/nodes.info", 1),
+    ("Datastore/operation/OpenSearch/search", 7),
+    ("Datastore/operation/OpenSearch/snapshot.status", 1),
+    ("Datastore/statement/OpenSearch/_all/cluster.health", 1),
+    ("Datastore/statement/OpenSearch/_all/search", 2),
+    ("Datastore/statement/OpenSearch/address/index", 2),
+    ("Datastore/statement/OpenSearch/address/search", 1),
+    ("Datastore/statement/OpenSearch/contacts/index", 3),
+    ("Datastore/statement/OpenSearch/contacts/indices.refresh", 1),
+    ("Datastore/statement/OpenSearch/contacts/search", 2),
+    ("Datastore/statement/OpenSearch/other/search", 2),
+]
+
+# Instance info
+
+_disable_scoped_metrics = list(_base_scoped_metrics)
+_disable_rollup_metrics = list(_base_rollup_metrics)
+
+_enable_scoped_metrics = list(_base_scoped_metrics)
+_enable_rollup_metrics = list(_base_rollup_metrics)
+
+_host = instance_hostname(OPENSEARCH_SETTINGS["host"])
+_port = OPENSEARCH_SETTINGS["port"]
+
+_instance_metric_name = f"Datastore/instance/OpenSearch/{_host}/{_port}"
+
+_enable_rollup_metrics.append((_instance_metric_name, _all_count))
+
+_disable_rollup_metrics.append((_instance_metric_name, None))
+
+# Query
+
+
+async def _exercise_opensearch(client):
+    await client.index(index="contacts", body={"name": "Joe Tester", "age": 25, "title": "QA Engineer"}, id=1)
+    await client.index(index="contacts", body={"name": "Jessica Coder", "age": 32, "title": "Programmer"}, id=2)
+    await client.index(index="contacts", body={"name": "Freddy Tester", "age": 29, "title": "Assistant"}, id=3)
+    await client.indices.refresh(index="contacts")
+    await client.index(index="address", body={"name": "Sherlock", "address": "221B Baker Street, London"}, id=1)
+    await client.index(
+        index="address", body={"name": "Bilbo", "address": "Bag End, Bagshot row, Hobbiton, Shire"}, id=2
+    )
+    await client.search(index="contacts", q="name:Joe")
+    await client.search(index="contacts", q="name:jessica")
+    await client.search(index="address", q="name:Sherlock")
+    await client.search(index=["contacts", "address"], q="name:Bilbo")
+    await client.search(index="contacts,address", q="name:Bilbo")
+    await client.search(index="*", q="name:Bilbo")
+    await client.search(q="name:Bilbo")
+    await client.cluster.health()
+
+    if hasattr(client, "cat"):
+        await client.cat.health()
+    if hasattr(client, "nodes"):
+        await client.nodes.info()
+    if hasattr(client, "snapshot") and hasattr(client.snapshot, "status"):
+        await client.snapshot.status()
+    if hasattr(client.indices, "status"):
+        await client.indices.status()
+
+
+# Test
+
+
+@validate_transaction_errors(errors=[])
+@validate_transaction_metrics(
+    "test_async_opensearch:test_async_opensearch_operation_disabled",
+    scoped_metrics=_disable_scoped_metrics,
+    rollup_metrics=_disable_rollup_metrics,
+    background_task=True,
+)
+@override_application_settings(_disable_instance_settings)
+@background_task()
+def test_async_opensearch_operation_disabled(async_client, loop):
+    loop.run_until_complete(_exercise_opensearch(async_client))
+
+
+@validate_transaction_errors(errors=[])
+@validate_transaction_metrics(
+    "test_async_opensearch:test_async_opensearch_operation_enabled",
+    scoped_metrics=_enable_scoped_metrics,
+    rollup_metrics=_enable_rollup_metrics,
+    background_task=True,
+)
+@override_application_settings(_enable_instance_settings)
+@background_task()
+def test_async_opensearch_operation_enabled(async_client, loop):
+    loop.run_until_complete(_exercise_opensearch(async_client))
+
+
+@validate_transaction_errors(errors=[])
+@validate_transaction_metrics(
+    "test_async_opensearch:test_async_opensearch_operation_enabled_empty_transaction_settings",
+    scoped_metrics=_enable_scoped_metrics,
+    rollup_metrics=_enable_rollup_metrics,
+    background_task=True,
+)
+@override_application_settings(_enable_instance_settings)
+@background_task()
+def test_async_opensearch_operation_enabled_empty_transaction_settings(async_client, loop):
+    transaction = current_transaction()
+    settings = transaction._settings
+    transaction._settings = None
+
+    loop.run_until_complete(_exercise_opensearch(async_client))
+
+    transaction._settings = settings
+
+
+def test_async_opensearch_no_transaction(async_client, loop):
+    loop.run_until_complete(_exercise_opensearch(async_client))
---
+++ tests/datastore_opensearch/test_multiple_dbs.py
@@ -0,0 +1,117 @@
+# Copyright 2010 New Relic, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+from conftest import OPENSEARCH_MULTIPLE_SETTINGS
+from opensearchpy import OpenSearch
+from testing_support.fixtures import override_application_settings
+from testing_support.util import instance_hostname
+from testing_support.validators.validate_transaction_metrics import validate_transaction_metrics
+
+from newrelic.api.background_task import background_task
+
+# Settings
+
+_enable_instance_settings = {"datastore_tracer.instance_reporting.enabled": True}
+_disable_instance_settings = {"datastore_tracer.instance_reporting.enabled": False}
+
+# Metrics
+
+_base_scoped_metrics = (("Datastore/statement/OpenSearch/contacts/index", 2),)
+
+_base_rollup_metrics = (
+    ("Datastore/all", 2),
+    ("Datastore/allOther", 2),
+    ("Datastore/OpenSearch/all", 2),
+    ("Datastore/OpenSearch/allOther", 2),
+    ("Datastore/operation/OpenSearch/index", 2),
+    ("Datastore/statement/OpenSearch/contacts/index", 2),
+)
+
+_disable_scoped_metrics = list(_base_scoped_metrics)
+_disable_rollup_metrics = list(_base_rollup_metrics)
+
+_enable_scoped_metrics = list(_base_scoped_metrics)
+_enable_rollup_metrics = list(_base_rollup_metrics)
+
+if len(OPENSEARCH_MULTIPLE_SETTINGS) > 1:
+    opensearch_1 = OPENSEARCH_MULTIPLE_SETTINGS[0]
+    opensearch_2 = OPENSEARCH_MULTIPLE_SETTINGS[1]
+
+    host_1 = instance_hostname(opensearch_1["host"])
+    port_1 = opensearch_1["port"]
+
+    host_2 = instance_hostname(opensearch_2["host"])
+    port_2 = opensearch_2["port"]
+
+    instance_metric_name_1 = f"Datastore/instance/OpenSearch/{host_1}/{port_1}"
+    instance_metric_name_2 = f"Datastore/instance/OpenSearch/{host_2}/{port_2}"
+
+    _enable_rollup_metrics.extend([(instance_metric_name_1, 1), (instance_metric_name_2, 1)])
+
+    _disable_rollup_metrics.extend([(instance_metric_name_1, None), (instance_metric_name_2, None)])
+
+# Query
+
+
+def _exercise_opensearch(client):
+    client.index(index="contacts", body={"name": "Joe Tester", "age": 25, "title": "QA Engineer"}, id=1)
+
+
+# Test
+
+
+@pytest.fixture(scope="session")
+def clients(loop):
+    clients = []
+    for db in OPENSEARCH_MULTIPLE_SETTINGS:
+        opensearch_url = f"http://{db['host']}:{db['port']}"
+        clients.append(OpenSearch(opensearch_url))
+
+    yield clients
+
+    for client in clients:
+        client.close()
+
+
+@pytest.mark.skipif(
+    len(OPENSEARCH_MULTIPLE_SETTINGS) < 2, reason="Test environment not configured with multiple databases."
+)
+@override_application_settings(_enable_instance_settings)
+@validate_transaction_metrics(
+    "test_multiple_dbs:test_multiple_dbs_enabled",
+    scoped_metrics=_enable_scoped_metrics,
+    rollup_metrics=_enable_rollup_metrics,
+    background_task=True,
+)
+@background_task()
+def test_multiple_dbs_enabled(clients):
+    for client in clients:
+        _exercise_opensearch(client)
+
+
+@pytest.mark.skipif(
+    len(OPENSEARCH_MULTIPLE_SETTINGS) < 2, reason="Test environment not configured with multiple databases."
+)
+@override_application_settings(_disable_instance_settings)
+@validate_transaction_metrics(
+    "test_multiple_dbs:test_multiple_dbs_disabled",
+    scoped_metrics=_disable_scoped_metrics,
+    rollup_metrics=_disable_rollup_metrics,
+    background_task=True,
+)
+@background_task()
+def test_multiple_dbs_disabled(clients):
+    for client in clients:
+        _exercise_opensearch(client)
